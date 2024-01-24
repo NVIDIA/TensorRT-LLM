8bb181ca58a9423549e987a150362825e15ac8eb
mulin.lyh <mulin.lyh@taobao.com>
2024-01-24 14:17:33 +0800

    # This is a combination of 3 commits. # This is the 1st commit message:
    add download models form www.modelscope.cn
    
    # This is the commit message #2:
    
    debug
    
    # This is the commit message #3:
    
    debug

---
+++ examples/qwen/prepare_model.py
@@ -0,0 +1,60 @@
+import argparse
+import os
+
+from tensorrt_llm.logger import logger
+
+def add_model_argument(parser: argparse.ArgumentParser):
+    """Add parameters for download upload model from www.modelscope.cn
+
+    Args:
+        parser (argparse.ArgumentParser): The command argument parser.
+    """
+    parser.add_argument('--model', 
+                        type=str, 
+                        required=True,
+                        help='Specify model id of www.modelscope.cn, or model directory')
+    parser.add_argument('--revision', 
+                        type=str, 
+                        default=None,
+                        help='Specify the model version')   
+    parser.add_argument('--upload',
+                        action='store_true',
+                        default=False,
+                        help='Upload the converted model to www.modelscope.cn')
+    
+ 
+def prepare_model_files(model_id: str,
+                       revision: str = None,
+                       cache_dir: str = None)->str:
+    """Download model from www.modelscope.cn
+
+    Args:
+        model_id (str): The model if
+        revision (str, optional): The model revision. Defaults to None.
+        cache_dir (str, optional): The model cache dir. Defaults to None.
+
+    Returns:
+        str: The downloaded model path.
+    """
+    try:
+        from modelscope import snapshot_download
+        if not os.path.exists(model_id):
+            model_path = snapshot_download(model_id=model_id,
+                                           cache_dir=cache_dir,
+                                           revision=revision)
+            return model_path
+    except ImportError as e:
+        logger.warn(
+            "Use model from www.modelscope.cn need pip install modelscope"
+        )
+        raise e
+
+def upload_model(path: str,
+                 model_id: str):
+    """Upload build output to www.modelscope.cn
+
+    Args:
+        path (str): The path of the build output.
+        model_id (str): The model id in modelscope.
+    """
+    pass
