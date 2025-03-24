675a8389b09dbc8a8bd7cab6b4e7f08eb76de311
Anand Joseph <anajoseph@nvidia.com>
2025-03-24 12:13:11 +0530

    Remove soundfile from requirements
    Signed-off-by: Anand Joseph <anajoseph@nvidia.com>

--- examples/canary/utils.py
+++ examples/canary/utils.py
@@ -20,7 +20,6 @@ from typing import Union, Iterable, Tuple, TextIO, List, Dict
 import numpy as np
 import torch
 import numpy
-import soundfile as sf
 import click
 import kaldialign, logging
 import torch.nn.functional as F
