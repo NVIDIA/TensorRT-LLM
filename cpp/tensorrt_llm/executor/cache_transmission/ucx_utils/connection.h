fa59fa935067fb09470b1fff071edc7bed08f9b6
roeya <165803633+RoeyAzran1992@users.noreply.github.com>
2025-03-25 03:39:05 -0700

    initial ucx support
    Signed-off-by: roeya <165803633+RoeyAzran1992@users.noreply.github.com>

--- cpp/tensorrt_llm/executor/cache_transmission/ucx_utils/connection.h
+++ cpp/tensorrt_llm/executor/cache_transmission/ucx_utils/connection.h
@@ -23,6 +23,91 @@
 namespace tensorrt_llm::executor::kv_cache
 {
 
+class UcxConnectionManager;
+
+class UcxConnection : public Connection
+{
+public:
+    UcxConnection() = default;
+    explicit UcxConnection(
+        uint64_t connectionId, std::shared_ptr<ucxx::Endpoint> endpoint, std::shared_ptr<UcxConnectionManager> manager);
+    // UcxConnection(UcxConnection&& other);
+    void send(DataContext const& ctx, void const* data, size_t size) const override;
+    void recv(DataContext const& ctx, void* data, size_t size) const override;
+
+private:
+    void initializeEndpointTag(int maxTryTimes = 10);
+    /*
+        static constexpr ucxx::Tag kID_TAG{1};
+        static constexpr ucxx::Tag kDATA_TAG{2};
+
+        // Set of bit map used to represent UCXComm flags
+        ucxx::Tag mInfoTag{1};
+
+        static constexpr ucxx::TagMask mEndpointMask{(((uint64_t) 1 << (32 + 1)) - 1) << 32};
+        static constexpr ucxx::TagMask mRequestIdMask{(((uint64_t) 1 << (16 + 1)) - 1) << 16};
+        static constexpr ucxx::TagMask mFlagMask{(((uint64_t) 1 << (16 + 1)) - 1)};
+    */
+
+    // a send tag is defined as:
+    // | local port (16 bits) | remote port (16 bits) | truncated request id (16 bits) | UCXComm flags (16 bits) |
+    // a recv tag is defined as:
+    // | remote port (16 bits) | local port (16 bits) | truncated request id (16 bits) | UCXComm flags (16 bits) |
+    ucxx::Tag mSendTag{0};
+    ucxx::Tag mRecvTag{0};
+
+    mutable std::mutex mMtx;
+    mutable std::condition_variable mCv;
+    uint64_t mConnectionId;
+    std::shared_ptr<ucxx::Endpoint> mEndpoint;
+    std::shared_ptr<UcxConnectionManager> mManager;
+};
+
+// class UcxServer
+// {
+// public:
+//     UcxServer(uint16_t listenerPort = 0);
+//     [[nodiscard]] ConnectionManager const& getConnectionManager() const;
+
+// private:
+//     struct NetINfoT
+//     {
+//         std::string interface;
+//         std::string ipv4;
+//         std::string ipv6;
+//         int idx = -1;
+//     };
+
+//     static void listenerCallback(ucp_conn_request_h conn_request, void* arg);
+//     void startListener(uint16_t listenerPort);
+
+//     UcxConnectionManager& mManager;
+//     std::unordered_map<std::string, NetINfoT> mNetInfoMap;
+//     std::shared_ptr<ucxx::Context> mContext;
+//     std::shared_ptr<ucxx::Worker> mWorker;
+//     std::shared_ptr<ucxx::Listener> mListener;
+// };
+
+// class UcxClient
+// {
+// public:
+//     UcxClient();
+//     void connect(std::string const& ip, std::uint16_t port);
+
+//     [[nodiscard]] ConnectionManager const& getConnectionManager() const
+//     {
+//         return mManager;
+//     }
+
+// private:
+//     void initSelfIps();
+
+//     UcxConnectionManager mManager;
+//     std::shared_ptr<ucxx::Context> mContext;
+//     std::shared_ptr<ucxx::Worker> mWorker;
+//     std::unordered_set<std::string> mSelfIps;
+// };
+
 #if defined(__clang__)
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
