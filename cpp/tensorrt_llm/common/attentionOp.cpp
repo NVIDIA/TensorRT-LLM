e9df23f81549e6aeb460c15c87f703f2137baca3
Perkz Zheng <67892460+PerkzZheng@users.noreply.github.com>
2025-03-25 18:03:29 +0800

    fix: [MLA] fix the bug with fp8 MLA kernels on Blackwell. (#3008)
    * update cubins
    * update error message
    
    ---------
    
    Signed-off-by: Perkz Zheng <67892460+PerkzZheng@users.noreply.github.com>

--- cpp/tensorrt_llm/common/attentionOp.cpp
+++ cpp/tensorrt_llm/common/attentionOp.cpp
@@ -2191,8 +2191,8 @@ int AttentionOp::initialize() noexcept
     if (mFP8ContextFMHA)
     {
         TLLM_CHECK_WITH_INFO(mEnableContextFMHA, "FP8 FMHA cannot be enabled because Context FMHA is not supported.");
-        TLLM_CHECK_WITH_INFO(mSM == 89 || mSM == 90 || mSM == 100,
-            "FP8 FMHA cannot be enabled except on Ada or Hopper or Blackwell Arch.");
+        TLLM_CHECK_WITH_INFO(
+            mSM == 89 || mSM == 90 || mSM == 100, "FP8 FMHA can only be enabled on sm_89, sm_90 or sm_100.");
     }
 
     // Check requirements for FP4 output.
