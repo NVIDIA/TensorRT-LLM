ff35af77ea60ab53dfbf5b6f113e67055d75f9ed
WeiHaocheng <20514172+WeiHaocheng@users.noreply.github.com>
2025-04-01 18:31:52 +0800

    feat: refactor scaffolding worker and support openai api worker (#3166)
    Signed-off-by: Fred Wei <20514172+WeiHaocheng@users.noreply.github.com>
    Signed-off-by: fredw <20514172+WeiHaocheng@users.noreply.github.com>

--- tests/unittest/scaffolding/test_scaffolding.py
+++ tests/unittest/scaffolding/test_scaffolding.py
@@ -1,71 +1,30 @@
-from pathlib import Path
-
 # isort: off
-from utils.llm_data import llm_models_root
 # isort: on
 
-import pytest
+from scaffolding.test_worker import create_trtllm_worker
 
-from tensorrt_llm._torch.pyexecutor.config import PyTorchConfig
 from tensorrt_llm.scaffolding.controller import (MajorityVoteController,
                                                  NativeGenerationController)
 from tensorrt_llm.scaffolding.scaffolding_llm import ScaffoldingLlm
-from tensorrt_llm.scaffolding.worker import ProposerWorker, SamplingParams
-
-
-@pytest.fixture(scope="module")
-def deepseek_distill_7b_path() -> Path:
-    model_dir = llm_models_root() / "DeepSeek-R1/DeepSeek-R1-Distill-Qwen-7B"
-    return model_dir
-
-
-@pytest.fixture(scope="module")
-def sampling_params():
-    return SamplingParams(max_tokens=2048)
-
-
-@pytest.fixture(scope="module")
-def pytorch_config():
-    return PyTorchConfig(
-        mixed_decoder=True,
-        enable_overlap_scheduler=True,
-    )
-
-
-@pytest.fixture(scope="module")
-def default_prompt():
-    prompt = "Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?\r\n\r\n"
-    return prompt
-
-
-def create_proposer_worker(deepseek_distill_7b_path, sampling_params,
-                           pytorch_config):
-    return ProposerWorker(
-        deepseek_distill_7b_path,
-        pytorch_backend_config=pytorch_config,
-        sampling_params=sampling_params,
-    )
 
 
 def create_scaffolding_llm_with_native_generation_controller(
-        deepseek_distill_7b_path, sampling_params, pytorch_config):
-    proposer_worker = create_proposer_worker(deepseek_distill_7b_path,
-                                             sampling_params, pytorch_config)
+        deepseek_distill_7b_path):
+    trtllm_worker = create_trtllm_worker(deepseek_distill_7b_path)
     prototype_generation_controller = NativeGenerationController()
     return ScaffoldingLlm(
         prototype_generation_controller,
-        {NativeGenerationController.WorkerTag.GENERATION: proposer_worker},
+        {NativeGenerationController.WorkerTag.GENERATION: trtllm_worker},
     )
 
 
 def create_scaffolding_llm_with_majority_vote_controller(
-        deepseek_distill_7b_path, sampling_params, pytorch_config, samples_num):
-    proposer_worker = create_proposer_worker(deepseek_distill_7b_path,
-                                             sampling_params, pytorch_config)
+        deepseek_distill_7b_path, samples_num):
+    trtllm_worker = create_trtllm_worker(deepseek_distill_7b_path)
 
     workers = {}
     prototype_generation_controller = NativeGenerationController()
-    workers[NativeGenerationController.WorkerTag.GENERATION] = proposer_worker
+    workers[NativeGenerationController.WorkerTag.GENERATION] = trtllm_worker
 
     prototype_majority_vote_controller = MajorityVoteController(
         prototype_generation_controller,
@@ -80,20 +39,18 @@ def create_scaffolding_llm_with_majority_vote_controller(
     return llm
 
 
-def test_unbatched_scaffolding_sync(default_prompt, deepseek_distill_7b_path,
-                                    sampling_params, pytorch_config):
+def test_unbatched_scaffolding_sync(default_prompt, deepseek_distill_7b_path):
     scaffolding_llm = create_scaffolding_llm_with_native_generation_controller(
-        deepseek_distill_7b_path, sampling_params, pytorch_config)
+        deepseek_distill_7b_path)
     result = scaffolding_llm.generate(default_prompt)
     assert isinstance(result.output.output_str, str) and len(
         result.output.output_str) > 0, "Output should be a non-empty string"
     scaffolding_llm.shutdown(shutdown_wokers=True)
 
 
-def test_batched_scaffolding_sync(default_prompt, deepseek_distill_7b_path,
-                                  sampling_params, pytorch_config):
+def test_batched_scaffolding_sync(default_prompt, deepseek_distill_7b_path):
     scaffolding_llm = create_scaffolding_llm_with_native_generation_controller(
-        deepseek_distill_7b_path, sampling_params, pytorch_config)
+        deepseek_distill_7b_path)
     batch_size = 3
     prompts = [default_prompt] * batch_size
     results = scaffolding_llm.generate(prompts)
@@ -104,12 +61,11 @@ def test_batched_scaffolding_sync(default_prompt, deepseek_distill_7b_path,
     scaffolding_llm.shutdown(shutdown_wokers=True)
 
 
-def test_async_scaffolding_generation(default_prompt, deepseek_distill_7b_path,
-                                      sampling_params, pytorch_config):
+def test_async_scaffolding_generation(default_prompt, deepseek_distill_7b_path):
 
     async def run_async_test():
         scaffolding_llm = create_scaffolding_llm_with_native_generation_controller(
-            deepseek_distill_7b_path, sampling_params, pytorch_config)
+            deepseek_distill_7b_path)
         future = scaffolding_llm.generate_async(default_prompt)
         result = await future.aresult()
         assert isinstance(result.output.output_str, str) and len(
@@ -120,13 +76,9 @@ def test_async_scaffolding_generation(default_prompt, deepseek_distill_7b_path,
     asyncio.run(run_async_test())
 
 
-def test_majority_vote(default_prompt, deepseek_distill_7b_path,
-                       sampling_params, pytorch_config):
+def test_majority_vote(default_prompt, deepseek_distill_7b_path):
     scaffolding_llm = create_scaffolding_llm_with_majority_vote_controller(
-        deepseek_distill_7b_path,
-        sampling_params,
-        pytorch_config,
-        samples_num=3)
+        deepseek_distill_7b_path, samples_num=3)
     result = scaffolding_llm.generate(default_prompt)
     assert isinstance(result.output.output_str, str) and len(
         result.output.output_str) > 0, "Output should be a non-empty string"
