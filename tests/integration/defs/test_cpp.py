676af9b3caff3c87a81fef11c846787bbdeacd17
Dom Brown <3886319+DomBrown@users.noreply.github.com>
2025-04-01 18:12:39 +0000

    Fix build
    Signed-off-by: Dom Brown <3886319+DomBrown@users.noreply.github.com>

--- tests/integration/defs/test_cpp.py
+++ tests/integration/defs/test_cpp.py
@@ -16,6 +16,7 @@ build_script_dir = _pl.Path(
 assert build_script_dir.is_dir()
 _sys.path.append(str(build_script_dir))
 
+from build_wheel import main as build_trt_llm
 from defs.conftest import llm_models_root
 
 
@@ -155,11 +156,11 @@ def build_google_tests(request, build_dir):
 
     print(f"Using CUDA arch: {cuda_arch}")
 
-    # build_trt_llm(cuda_architectures=cuda_arch,
-    #               job_count=12,
-    #               use_ccache=True,
-    #               clean=True,
-    #               trt_root="/usr/local/tensorrt")
+    build_trt_llm(cuda_architectures=cuda_arch,
+                  job_count=12,
+                  use_ccache=True,
+                  clean=True,
+                  trt_root="/usr/local/tensorrt")
 
     make_google_tests = [
         "cmake",
