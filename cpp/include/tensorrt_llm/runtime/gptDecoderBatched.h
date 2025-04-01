d7386d14a8ce7e923182f9a65594e4f85aa6cee3
Robin Kobus <19427718+Funatiq@users.noreply.github.com>
2025-04-01 12:47:31 +0200

    refactor: Simplify disableLookahead and improve numDecodingEngineTokens handling (#3103)
    * refactor: Simplifiy disableLookahead method
    
    Signed-off-by: Robin Kobus <19427718+Funatiq@users.noreply.github.com>
    
    * Update DecoderBuffers comments
    
    Signed-off-by: Robin Kobus <19427718+Funatiq@users.noreply.github.com>
    
    * refactor: Move numDecodingEngineTokens to DecoderState
    
    This commit introduces new methods in the DecoderState class to manage the number of tokens for each request in a batch. The following changes were made:
    - Added `getNumDecodingEngineTokens()` to retrieve the number of tokens for all requests.
    - Added `getNumDecodingEngineTokens(SizeType32 batchIdx)` to get the token count for a specific request.
    - Added `setNumDecodingEngineTokens(SizeType32 batchIdx, SizeType32 numTokens)` to set the token count for a specific request.
    - Updated the setup method to initialize the token count vector based on the maximum batch size.
    - Refactored the `CreateNewDecoderRequests` class to utilize the new token management methods, improving clarity and maintainability.
    
    Signed-off-by: Robin Kobus <19427718+Funatiq@users.noreply.github.com>
    
    * refactor: Improve shape variables in DecoderState
    
    Signed-off-by: Robin Kobus <19427718+Funatiq@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Robin Kobus <19427718+Funatiq@users.noreply.github.com>

--- cpp/include/tensorrt_llm/runtime/gptDecoderBatched.h
+++ cpp/include/tensorrt_llm/runtime/gptDecoderBatched.h
@@ -54,8 +54,7 @@ public:
         SizeType32 maxTokensPerStep, nvinfer1::DataType dtype, ModelConfig const& modelConfig,
         WorldConfig const& worldConfig) override;
 
-    void disableLookahead(
-        SizeType32 maxBatchSize, RequestVector const& genRequests, TensorPtr const& batchSlots) override;
+    void disableLookahead(RequestVector const& genRequests, TensorPtr const& batchSlots) override;
 
     DecoderFinishedEventPtr forwardAsync(decoder_batch::Output& output, decoder_batch::Input const& input) override;
     void forward(decoder_batch::Output& output, decoder_batch::Input const& input) override;
@@ -65,7 +64,12 @@ public:
     [[nodiscard]] CudaEvent finalize(decoder::DecoderState const& decoderState, SizeType32 batchSlot,
         SamplingConfig const& samplingConfig, bool streaming) const override;
 
-    decoder::DecoderState& getDecoderState() const
+    decoder::DecoderState& getDecoderState()
+    {
+        return *mDecoderState;
+    }
+
+    decoder::DecoderState const& getDecoderState() const
     {
         return *mDecoderState;
     }
