3cf706635023189f98438beb3c6d9e85cbc705ab
Enwei Zhu <21126786+syuoni@users.noreply.github.com>
2025-04-02 17:29:57 +0800

    test: Accuracy test improvement (Part 3.2): Move Qwen tests (NvBug 5135332) (#3219)
    * remove test_llm_models_multi_gpu.py
    
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>
    
    * fix
    
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>
    
    * qwen 2.5
    
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>
    
    * fix
    
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>
    
    * upgrade
    
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api.py
+++ tests/integration/defs/accuracy/test_llm_api.py
@@ -48,3 +48,132 @@ class TestMixtral8x7B(LlmapiAccuracyTestHarness):
             task.evaluate(llm)
             task = MMLU(self.MODEL_NAME)
             task.evaluate(llm)
+
+
+class TestQwen2_7BInstruct(LlmapiAccuracyTestHarness):
+    MODEL_NAME = "Qwen/Qwen2-7B-Instruct"
+    MODEL_PATH = f"{llm_models_root()}/Qwen2-7B-Instruct"
+    EXTRA_EVALUATOR_KWARGS = dict(
+        apply_chat_template=True,
+        system_prompt=
+        "You are a helpful assistant, please summarize the article entered by the user with one or two sentences."
+    )
+
+    def test_auto_dtype(self):
+        with LLM(self.MODEL_PATH) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+
+    def test_weight_only(self):
+        quant_config = QuantConfig(QuantAlgo.W8A16)
+        with LLM(self.MODEL_PATH, quant_config=quant_config) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+
+    @skip_pre_ada
+    def test_fp8(self):
+        quant_config = QuantConfig(QuantAlgo.FP8)
+        with LLM(self.MODEL_PATH, quant_config=quant_config) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+
+    @pytest.mark.skip_less_device(2)
+    def test_tp2(self):
+        with LLM(self.MODEL_PATH, tensor_parallel_size=2) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+
+
+class TestQwen2_5_0_5BInstruct(LlmapiAccuracyTestHarness):
+    MODEL_NAME = "Qwen/Qwen2.5-0.5B-Instruct"
+    MODEL_PATH = f"{llm_models_root()}/Qwen2.5-0.5B-Instruct"
+    EXTRA_EVALUATOR_KWARGS = dict(
+        apply_chat_template=True,
+        system_prompt=
+        "You are a helpful assistant, please summarize the article entered by the user with one or two sentences."
+    )
+
+    def test_auto_dtype(self):
+        with LLM(self.MODEL_PATH) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+            task = MMLU(self.MODEL_NAME)
+            task.evaluate(llm)
+
+    @skip_pre_ada
+    def test_fp8(self):
+        quant_config = QuantConfig(QuantAlgo.FP8)
+        with LLM(self.MODEL_PATH, quant_config=quant_config) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+            task = MMLU(self.MODEL_NAME)
+            task.evaluate(llm)
+
+
+class TestQwen2_5_1_5BInstruct(LlmapiAccuracyTestHarness):
+    MODEL_NAME = "Qwen/Qwen2.5-1.5B-Instruct"
+    MODEL_PATH = f"{llm_models_root()}/Qwen2.5-1.5B-Instruct"
+    EXTRA_EVALUATOR_KWARGS = dict(
+        apply_chat_template=True,
+        system_prompt=
+        "You are a helpful assistant, please summarize the article entered by the user with one or two sentences."
+    )
+
+    def test_auto_dtype(self):
+        with LLM(self.MODEL_PATH) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+            task = MMLU(self.MODEL_NAME)
+            task.evaluate(llm)
+
+    def test_weight_only(self):
+        quant_config = QuantConfig(QuantAlgo.W8A16)
+        with LLM(self.MODEL_PATH, quant_config=quant_config) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+
+    @skip_pre_ada
+    def test_fp8(self):
+        quant_config = QuantConfig(QuantAlgo.FP8)
+        with LLM(self.MODEL_PATH, quant_config=quant_config) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+            task = MMLU(self.MODEL_NAME)
+            task.evaluate(llm)
+
+
+class TestQwen2_5_7BInstruct(LlmapiAccuracyTestHarness):
+    MODEL_NAME = "Qwen/Qwen2.5-7B-Instruct"
+    MODEL_PATH = f"{llm_models_root()}/Qwen2.5-7B-Instruct"
+    EXTRA_EVALUATOR_KWARGS = dict(
+        apply_chat_template=True,
+        system_prompt=
+        "You are a helpful assistant, please summarize the article entered by the user with one or two sentences."
+    )
+
+    def test_auto_dtype(self):
+        with LLM(self.MODEL_PATH) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+            task = MMLU(self.MODEL_NAME)
+            task.evaluate(llm)
+
+    @skip_pre_ada
+    def test_fp8(self):
+        quant_config = QuantConfig(QuantAlgo.FP8)
+        with LLM(self.MODEL_PATH, quant_config=quant_config) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=self.EXTRA_EVALUATOR_KWARGS)
+            task = MMLU(self.MODEL_NAME)
+            task.evaluate(llm)
