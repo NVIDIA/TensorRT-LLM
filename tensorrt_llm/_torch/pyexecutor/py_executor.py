1ee6c93564fafdf6360f800c1f679a4e12b454aa
Patrice Castonguay <55748270+pcastonguay@users.noreply.github.com>
2025-04-02 15:56:22 -0400

    feat: Add option to run disaggregated serving without ctx servers, to benchmark gen only
    Signed-off-by: Patrice Castonguay <55748270+pcastonguay@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/py_executor.py
+++ tensorrt_llm/_torch/pyexecutor/py_executor.py
@@ -1410,6 +1410,12 @@ class PyExecutor:
     @nvtx_range("_recv_disagg_gen_cache")
     def _recv_disagg_gen_cache(self, new_gen_reqs):
 
+        # For gen-only benchmarking, mark new gen request as transmission complete right away
+        if os.getenv("TRTLLM_DISAGG_BENCHMARK_GEN_ONLY") == "1":
+            for req in new_gen_reqs:
+                req.state = LlmRequestState.DISAGG_GENERATION_TRANS_COMPLETE
+            return
+
         if os.getenv("TRTLLM_DISABLE_KV_CACHE_TRANSFER_OVERLAP") == "1":
             for req in new_gen_reqs:
                 self.kv_cache_transceiver.request_and_receive_sync(req)
