0846822ab91410c9da41e47b99fbf5d4b7c0ec97
Jin Li <59594262+liji-nv@users.noreply.github.com>
2025-04-03 05:22:41 +0000

    feat: Introduce UB allocator for pytorch flow
    * Instead of allocating UserBuffers at beginning of runtime, UB buffers
      are now managed with global allocator. The allocator will dynamically
    assign free UB buffer or allocate new buffer for torch tensor. It makes
    userbuffers easier to use.
    
    * In common usecase, the Userbuffers will be allocated correctly during
      warm up stage. There is no dynamic allocation during inference.
    
    * UB fusion pattern is rewroten using the new UB Allocator. It contains
      following passes:
    
    1. Fuse Quant with allreduce, replace with UB impl, and insert a
       copy_to_userbuffers. Currently the normal allreduce still does not
       support FP8 quant. So this need to be done in UB pass
    2. Convert all supported allreduce with UB and insert copy_to_userbuffers.
    3. Fuse op before ar with the copy_to_userbuffers. So the op directly
       writes to the userbuffer
    4. Remove userbuffers finalize if the output is connect to another UB
       allreduce.
    
    Signed-off-by: Jin Li <59594262+liji-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/custom_ops/torch_custom_ops.py
+++ tensorrt_llm/_torch/custom_ops/torch_custom_ops.py
@@ -5,3 +5,9 @@ import torch
 @torch.library.custom_op("trtllm::bmm_out", mutates_args=("out", ))
 def bmm_out(a: torch.Tensor, b: torch.Tensor, out: torch.Tensor) -> None:
     torch.bmm(a, b, out=out)
+
+
+# Used to WAR an issue that torch.add not allowed to use out in target pattern
+@torch.library.custom_op("trtllm::add_out", mutates_args=("out", ))
+def add_out(a: torch.Tensor, b: torch.Tensor, out: torch.Tensor) -> None:
+    torch.add(a, b, out=out)
