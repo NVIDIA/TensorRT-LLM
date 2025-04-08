16fe4330ca421114c552ca0c09fcdee14f78888f
Maximiliano Levi <maxilevi77@gmail.com>
2025-04-08 09:02:30 +0100

    fix: comment
    Signed-off-by: Maximiliano Levi <maxilevi77@gmail.com>

--- tensorrt_llm/layers/embedding.py
+++ tensorrt_llm/layers/embedding.py
@@ -177,7 +177,7 @@ class PromptTuningEmbedding(Embedding):
 
         # tasks: [batch_size, seq_len]
         # prompt_tokens: [batch_size, seq_len]
-        # if speculative decoding is enabled tasks is [batch_size, seq_len + max_draft_len],
+        # if speculative decoding is enabled the shape of prompt_tokens is [batch_size, seq_len + max_draft_len],
         # so we need to expand tasks to [batch_size, seq_len + max_draft_len]
         tasks = expand(tasks, shape(prompt_tokens))
         prompt_tokens = prompt_tokens + tasks
