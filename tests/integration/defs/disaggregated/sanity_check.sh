f146d77617911d6d44e4814760269f3d2e991a1a
Patrice Castonguay <55748270+pcastonguay@users.noreply.github.com>
2025-04-09 12:03:54 -0700

    fix: Fixing issue with first gen token being returned twice with streaming
    Signed-off-by: Patrice Castonguay <55748270+pcastonguay@users.noreply.github.com>

--- tests/integration/defs/disaggregated/sanity_check.sh
+++ tests/integration/defs/disaggregated/sanity_check.sh
@@ -111,6 +111,7 @@ fi
 
 if [[ "${TEST_DESC}" != "gen_only" ]]; then
   expected_strings=("The capital of Germany is Berlin" "Asyncio is a Python library")
+  no_expected_strings=("Berlin Berlin")
   if [[ "${TEST_DESC}" =~ "deepseek_v3_lite" ]]; then
     expected_strings=("Berlin" "Asyncio is a powerful tool")
   fi
@@ -119,4 +120,10 @@ if [[ "${TEST_DESC}" != "gen_only" ]]; then
       grep "${expected_string}" output.json
       grep "${expected_string}" output_streaming.json
   done
+
+  for not_expected_string in "${not_expected_strings[@]}"; do
+      grep -v "${not_expected_string}" output.json
+      grep -v "${not_expected_string}" output_streaming.json
+  done
+
 fi
