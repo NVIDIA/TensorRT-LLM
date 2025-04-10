772a7a1e7ecfca4b4b488eb160758ef13a13df31
Enwei Zhu <21126786+syuoni@users.noreply.github.com>
2025-04-10 07:32:34 +0000

    add gsm8k
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>

---
+++ tensorrt_llm/evaluate/lm_eval.py
@@ -0,0 +1,146 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from abc import abstractmethod
+from typing import Iterable, List, Optional, Tuple, Union
+
+import click
+import lm_eval
+from tqdm import tqdm
+
+from .._torch import LLM as PyTorchLLM
+from ..llmapi import LLM, RequestOutput
+from ..logger import logger
+from ..sampling_params import SamplingParams
+from .interface import Evaluator
+
+
+class LmEvalWrapper(lm_eval.api.model.TemplateLM):
+
+    def __init__(self, llm: Union[LLM, PyTorchLLM]):
+        super().__init__()
+        self.llm = llm
+
+    @property
+    def eot_token_id(self) -> int:
+        return self.llm.tokenizer.eos_token_id
+
+    def tok_encode(self, string: str, **kwargs) -> List[int]:
+        return self.llm.tokenizer.encode(string, **kwargs)
+
+    def _loglikelihood_tokens(self, requests,
+                              **kwargs) -> List[Tuple[float, bool]]:
+        raise NotImplementedError()
+
+    def loglikelihood_rolling(self,
+                              requests,
+                              disable_tqdm: bool = False) -> List[float]:
+        raise NotImplementedError()
+
+    def generate_until(self, requests, disable_tqdm: bool = False) -> List[str]:
+        outputs = []
+        for request in tqdm(requests,
+                            desc="Submitting requests",
+                            disable=disable_tqdm):
+            prompt, gen_kwargs = request.args
+            sampling_params = SamplingParams(
+                max_tokens=gen_kwargs.get("max_gen_toks", 256),
+                top_p=gen_kwargs.get("top_p", None),
+                temperature=gen_kwargs.get("temperature", None),
+                stop=gen_kwargs.get("until", None),
+                include_stop_str_in_output=False)
+            output = self.llm.generate_async(prompt,
+                                             sampling_params=sampling_params)
+            outputs.append(output)
+
+        for output in tqdm(outputs,
+                           desc="Fetching responses",
+                           disable=disable_tqdm):
+            output.result()
+
+        return [output.outputs[0].text for output in outputs]
+
+
+class LmEvalEvaluator(Evaluator):
+
+    def __init__(self,
+                 task_name: str,
+                 dataset_path: str = None,
+                 num_samples: int = None,
+                 random_seed: int = 0,
+                 apply_chat_template: bool = False,
+                 system_prompt: Optional[str] = None):
+        super().__init__(apply_chat_template=apply_chat_template,
+                         system_prompt=system_prompt)
+        self.task_dict = lm_eval.tasks.get_task_dict(task_name)
+
+    def generate_samples(self) -> Iterable[tuple]:
+        raise NotImplementedError()
+
+    def compute_score(self, outputs: List[RequestOutput], references: List[str],
+                      *auxiliaries) -> float:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_score(self, results: dict):
+        raise NotImplementedError()
+
+    def evaluate(self,
+                 llm: Union[LLM, PyTorchLLM],
+                 sampling_params: Optional[SamplingParams] = None) -> float:
+        results = lm_eval.evaluate(
+            lm=LmEvalWrapper(llm),
+            task_dict=self.task_dict,
+        )
+        logger.info(f"Lm eval results:\n{lm_eval.utils.make_table(results)}")
+        return self.get_score(results)
+
+
+class GSM8K(LmEvalEvaluator):
+
+    def __init__(self,
+                 dataset_path: str = None,
+                 num_samples: int = None,
+                 random_seed: int = 0,
+                 apply_chat_template: bool = False,
+                 system_prompt: Optional[str] = None):
+        super().__init__(task_name="gsm8k",
+                         dataset_path=dataset_path,
+                         num_samples=num_samples,
+                         random_seed=random_seed,
+                         apply_chat_template=apply_chat_template,
+                         system_prompt=system_prompt)
+
+    def get_score(self, results: dict):
+        return results["results"]["gsm8k"]["exact_match,strict-match"]
+
+    @click.command("gsm8k")
+    @click.option("--dataset_path", type=str, default=None)
+    @click.option("--num_samples", type=int, default=None)
+    @click.option("--random_seed", type=int, default=0)
+    @click.option("--check_accuracy", is_flag=True, default=False)
+    @click.option("--accuracy_threshold", type=float, default=15)
+    @click.pass_context
+    @staticmethod
+    def command(ctx, dataset_path: str, num_samples: int, random_seed: int,
+                check_accuracy: bool, accuracy_threshold: float) -> None:
+        llm: Union[LLM, PyTorchLLM] = ctx.obj
+        evaluator = GSM8K(dataset_path,
+                          num_samples=num_samples,
+                          random_seed=random_seed)
+        accuracy = evaluator.evaluate(llm)
+        llm.shutdown()
+
+        if check_accuracy:
+            assert accuracy >= accuracy_threshold, f"Expected accuracy >= {accuracy_threshold}, but got {accuracy}"
