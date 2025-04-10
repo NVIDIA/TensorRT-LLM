9bcfddc82a301938ac7df219064e3b0fcfe7c2e6
junq <22017000+QiJune@users.noreply.github.com>
2025-04-10 17:14:03 +0800

    fix ci
    Signed-off-by: junq <22017000+QiJune@users.noreply.github.com>

--- tensorrt_llm/_torch/modules/gated_mlp.py
+++ tensorrt_llm/_torch/modules/gated_mlp.py
@@ -5,6 +5,8 @@ import torch
 import torch.nn.functional as F
 from torch import nn
 
+from tensorrt_llm.mapping import Mapping
+
 from ..custom_ops import IS_FLASHINFER_AVAIABLE
 from ..distributed import AllReduceParams
 from ..model_config import ModelConfig
