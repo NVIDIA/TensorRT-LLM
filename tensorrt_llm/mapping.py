e6a8a4415aa58ae61d58b5846351cb56a0688e16
junq <22017000+QiJune@users.noreply.github.com>
2025-04-10 14:45:03 +0800

    polish
    Signed-off-by: junq <22017000+QiJune@users.noreply.github.com>

--- tensorrt_llm/mapping.py
+++ tensorrt_llm/mapping.py
@@ -262,9 +262,10 @@ class Mapping(object):
 
     @rank.setter
     def rank(self, rank: int):
-        if not isinstance(rank, int) or rank < 0:
-            # TODO(qijun): support attention_dp_size
-            if not self.enable_attention_dp and rank >= self.world_size:
+        # TODO(qijun): skip check for enable_attention_dp temporarily, will support attention_dp_size
+        if not self.enable_attention_dp:
+            if not isinstance(rank,
+                              int) or rank < 0 and rank >= self.world_size:
                 raise ValueError(
                     f"Rank should be an integer between 0 and {self.world_size-1}, but got {rank}."
                 )
