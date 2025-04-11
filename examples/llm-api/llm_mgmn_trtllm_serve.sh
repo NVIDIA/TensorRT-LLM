9563fc084d88b9fc80a7b8c5284680a349630b01
chunweiy <chunweiy@nvidia.com>
2025-04-11 02:52:02 +0000

    make LLM-API slurm examples executable
    Signed-off-by: chunweiy <chunweiy@nvidia.com>

--- examples/llm-api/llm_mgmn_trtllm_serve.sh
+++ examples/llm-api/llm_mgmn_trtllm_serve.sh
@@ -8,7 +8,7 @@
 #SBATCH -e logs/trtllm-serve.err
 #SBATCH -J trtllm-serve
 
-### Run trtllm-serve on Slurm
+### Run trtllm-serve with pytorch backend on Slurm
 
 # NOTE, this feature is experimental and may not work on all systems.
 # The trtllm-llmapi-launch is a script that launches the LLM-API code on
@@ -20,22 +20,60 @@
 
 # This docker image should have tensorrt_llm installed, or you need to install
 # it in the task.
-container_image=<docker_image>
-mount_dir=<mount_dir>
-mount_dest=<mount_dest>
-workdir=<workdir>
 
-# Just launch trtllm-serve job with trtllm-llmapi-launch command.
+# The following variables are expected to be set in the environment:
+# You can set them via --export in the srun/sbatch command.
+#   CONTAINER_IMAGE: the docker image to use, you'd better install tensorrt_llm in it, or install it in the task.
+#   MOUNT_DIR: the directory to mount in the container
+#   MOUNT_DEST: the destination directory in the container
+#   WORKDIR: the working directory in the container
+#   SOURCE_ROOT: the path to the TensorRT-LLM source
+#   PROLOGUE: the prologue to run before the script
+#   LOCAL_MODEL: the local model directory to use, NOTE: downloading from HF is
+#      not supported in Slurm mode, you need to download the model and put it in
+#      the LOCAL_MODEL directory.
+
+export prepare_dataset="$SOURCE_ROOT/benchmarks/cpp/prepare_dataset.py"
+export data_path="$WORKDIR/token-norm-dist.txt"
+
+echo "Preparing dataset..."
 srun -l \
-    --container-image=${container_image} \
-    --container-mounts=${mount_dir}:${mount_dest} \
-    --container-workdir=${workdir} \
-    --export=ALL,PYTHONPATH=${TEKIT_ROOT} \
+    -N 1 \
+    -n 1 \
+    --container-image=${CONTAINER_IMAGE} \
+    --container-name="prepare-name" \
+    --container-mounts=${MOUNT_DIR}:${MOUNT_DEST} \
+    --container-workdir=${WORKDIR} \
+    --export=ALL \
     --mpi=pmix \
     bash -c "
+        $PROLOGUE
+        python3 $prepare_dataset \
+            --tokenizer=$LOCAL_MODEL \
+            --stdout token-norm-dist \
+            --num-requests=100 \
+            --input-mean=128 \
+            --output-mean=128 \
+            --input-stdev=0 \
+            --output-stdev=0 > $data_path
+    "
+
+echo "Starting trtllm-serve..."
+# Just launch trtllm-serve job with trtllm-llmapi-launch command.
+srun -l \
+    --container-image=${CONTAINER_IMAGE} \
+    --container-mounts=${MOUNT_DIR}:${MOUNT_DEST} \
+    --container-workdir=${WORKDIR} \
+    --export=ALL,PYTHONPATH=${SOURCE_ROOT} \
+    --mpi=pmix \
+    bash -c "
+        set -ex
+        $PROLOGUE
+        export PATH=$PATH:~/.local/bin
+
         trtllm-llmapi-launch \
-         trtllm-serve \
-            --model <model_name> \
-            --model_path <model_path> \
-            <remaining_options>
+         trtllm-serve $LOCAL_MODEL \
+            --tp_size 16 \
+            --backend pytorch \
+            ${ADDITIONAL_OPTIONS}
     "
