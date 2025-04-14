fix: fusedmoe output and use None to disable final scaling

tensorrt_llm/_torch/modules/fused_moe.py
@@ -612,7 +612,7 @@ class FusedMoE(nn.Module):
             assert self.routing_method.top_k == 1, "Current walkaround only supports top-1 routing"
             x = x * token_final_scales.to(x.dtype)
             # TODO: remove this once we have correct fusedmoe kernel ready
-            token_final_scales = torch.ones_like(token_final_scales)
+            token_final_scales = None
 
         x_sf = None
         if self.has_any_quant:
@@ -673,11 +673,16 @@ class FusedMoE(nn.Module):
         if min_latency_mode:
             assert not self.reduce_results
             return final_hidden_states
+        else:
+            # Custom op requires all inputs are in the same type.
+            # Only in min_latency_mode, the output is a list of tensors.
+            # Otherwise, the output should be unpacked as a single tensor.
+            final_hidden_states = final_hidden_states[0]
 
         if self.reduce_results and self.parallel_size > 1:
             return self.all_reduce(final_hidden_states)
         else:
-            return final_hidden_states[0]
+            return final_hidden_states
 
     def forward(
         self,