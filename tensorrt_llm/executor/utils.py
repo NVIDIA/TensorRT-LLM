525179190078b722dac952ccd1c2e25ae540b4ac
Superjomn <328693+Superjomn@users.noreply.github.com>
2025-04-17 05:15:32 +0000

    fix hmac in remote mpi session
    Signed-off-by: Superjomn <328693+Superjomn@users.noreply.github.com>

--- tensorrt_llm/executor/utils.py
+++ tensorrt_llm/executor/utils.py
@@ -41,8 +41,12 @@ def create_mpi_comm_session(
         print_colored_debug(
             f"Using RemoteMpiPoolSessionClient to bind to external MPI processes at {get_spawn_proxy_process_ipc_addr_env()}\n",
             "yellow")
+        hmac_key = os.getenv("TLLM_SPAWN_PROXY_PROCESS_IPC_HMAC_KEY")
+        # Convert the hex string to bytes
+        if hmac_key is not None:
+            hmac_key = bytes.fromhex(hmac_key)
         return RemoteMpiCommSessionClient(
-            get_spawn_proxy_process_ipc_addr_env())
+            addr=get_spawn_proxy_process_ipc_addr_env(), hmac_key=hmac_key)
     else:
         print_colored_debug(
             f"Using MpiCommSession to bind to external MPI processes\n",
