8f4594376ff413a7b826423f14fb6f9e525c8f26
Xiwen Yu <13230610+VALLIS-NERIA@users.noreply.github.com>
2025-04-19 01:08:24 +0800

    integrate sm120 fusedmoe
    Signed-off-by: Xiwen Yu <13230610+VALLIS-NERIA@users.noreply.github.com>

--- cpp/tensorrt_llm/cutlass_extensions/include/cutlass_extensions/gemm_configs.h
+++ cpp/tensorrt_llm/cutlass_extensions/include/cutlass_extensions/gemm_configs.h
@@ -140,6 +140,17 @@ enum class CutlassTileConfigSM100
     CtaShape256x256x128B,
 };
 
+enum class CutlassTileConfigSM120
+{
+    // Signals that we should run heuristics do choose a config
+    Undefined,
+
+    // Signals that we should run heuristics do choose a config
+    ChooseWithHeuristic,
+
+    CtaShape128x128x256B,
+};
+
 enum class MainloopScheduleType
 {
     AUTO, // Automatically selects between pingpong and cooperative schedules on Hopper. On older architectures, this
@@ -387,6 +398,7 @@ struct CutlassGemmConfig
     // config options for sm90
     CutlassTileConfigSM90 tile_config_sm90 = CutlassTileConfigSM90::ChooseWithHeuristic;
     CutlassTileConfigSM100 tile_config_sm100 = CutlassTileConfigSM100::ChooseWithHeuristic;
+    CutlassTileConfigSM120 tile_config_sm120 = CutlassTileConfigSM120::ChooseWithHeuristic;
     MainloopScheduleType mainloop_schedule = MainloopScheduleType::AUTO;
     EpilogueScheduleType epilogue_schedule = EpilogueScheduleType::AUTO;
     ClusterShape cluster_shape = ClusterShape::ClusterShape_1x1x1;
@@ -427,10 +439,21 @@ struct CutlassGemmConfig
     {
     }
 
+    CutlassGemmConfig(CutlassTileConfigSM120 tile_config_sm120, MainloopScheduleType mainloop_schedule,
+        EpilogueScheduleType epilogue_schedule, ClusterShape cluster_shape)
+        : tile_config_sm120(tile_config_sm120)
+        , mainloop_schedule(mainloop_schedule)
+        , epilogue_schedule(epilogue_schedule)
+        , cluster_shape(cluster_shape)
+        , sm_version(120)
+        , is_tma_warp_specialized(true)
+    {
+    }
+
     int getTileConfigAsInt() const
     {
         if (sm_version == 120)
-            return (int) tile_config_sm80;
+            return (int) tile_config_sm120;
         if (sm_version >= 100)
             return (int) tile_config_sm100;
         if (sm_version == 90)
