6d00ae69ca1c2fdc4c8e3e55646fb4815b780aed
yechank <161688079+yechank-nvidia@users.noreply.github.com>
2025-04-22 15:22:39 +0900

    fix bandit
    Signed-off-by: yechank <161688079+yechank-nvidia@users.noreply.github.com>

--- tensorrt_llm/inputs/utils.py
+++ tensorrt_llm/inputs/utils.py
@@ -337,7 +337,7 @@ async def async_load_video(
 def encode_base64_content_from_url(content_url: str) -> str:
     """Encode a content retrieved from a remote url to base64 format."""
 
-    with requests.get(content_url) as response:
+    with requests.get(content_url, timeout=10) as response:
         response.raise_for_status()
         result = base64.b64encode(response.content).decode('utf-8')
 
