ebadee9cfc974b7708953f718a6b11102f35dcc8
Superjomn <328693+Superjomn@users.noreply.github.com>
2025-04-22 05:33:26 +0000

    disable test
    Signed-off-by: Superjomn <328693+Superjomn@users.noreply.github.com>

--- tests/unittest/llmapi/test_mpi_session.py
+++ tests/unittest/llmapi/test_mpi_session.py
@@ -53,6 +53,7 @@ def run_client(server_addr, values_to_process):
         return f"Error in client: {str(e)}"
 
 
+@pytest.mark.skip(reason="https://nvbugspro.nvidia.com/bug/5179666")
 @pytest.mark.parametrize("task_type", ["submit", "submit_sync"])
 def test_remote_mpi_session(task_type: Literal["submit", "submit_sync"]):
     """Test RemoteMpiPoolSessionClient and RemoteMpiPoolSessionServer interaction"""
