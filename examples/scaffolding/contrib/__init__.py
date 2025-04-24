0d39927b4e793e943ef4aa1adee44eea8b685556
fredw (generated by with_the_same_user script) <20514172+WeiHaocheng@users.noreply.github.com>
2025-04-24 09:07:28 +0000

    feat(part 2): Enhance the integrated robustness of scaffolding with __init__.py #3305
    Signed-off-by: fredw (generated by with_the_same_user script) <20514172+WeiHaocheng@users.noreply.github.com>

--- examples/scaffolding/contrib/__init__.py
+++
@@ -1,8 +0,0 @@
-from tensorrt_llm.scaffolding import *  # noqa
-
-from .Dynasor.dynasor_controller import DynasorGenerationController
-
-__all__ = [
-    'NativeStreamGenerationController', 'StreamGenerationTask',
-    'stream_generation_handler', 'DynasorGenerationController'
-]
