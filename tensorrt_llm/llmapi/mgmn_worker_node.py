6e92c90d1886ca06287f939569d110eedfbba84f
bhsueh_NV <11360707+byshiue@users.noreply.github.com>
2025-04-24 22:25:41 +0800

    fix: Fix/fused moe 0.19 (#3799)
    * fix bug of stream init
    
    Signed-off-by: bhsueh <11360707+byshiue@users.noreply.github.com>
    
    * fix bug
    
    Signed-off-by: bhsueh <11360707+byshiue@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: bhsueh <11360707+byshiue@users.noreply.github.com>

--- tensorrt_llm/llmapi/mgmn_worker_node.py
+++ tensorrt_llm/llmapi/mgmn_worker_node.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 import logging
 
+import torch
 from mpi4py.futures import MPICommExecutor
 from mpi4py.MPI import COMM_WORLD
 
+from tensorrt_llm._utils import global_mpi_rank, local_mpi_size
+
 # For multi-node MPI, the worker nodes should launch MPICommExecutor to accept tasks sent from rank0
+
+device_id = global_mpi_rank() % local_mpi_size()
+torch.cuda.set_device(device_id)
 with MPICommExecutor(COMM_WORLD) as executor:
     if executor is not None:
         raise RuntimeError(f"rank{COMM_WORLD.rank} should not have executor")
