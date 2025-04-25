c8bbbcc20b7b43851f4ff112ee53c5328e06bca0
Keddy Jin <jin.gq@aliyun.com>
2025-04-25 11:37:57 +0800

    add qwen3 dense model pytorch backend support, initial commit
    solve the results error issue
    
    add qwen3 moe model pytorch backend support
    
    reformat the code

--- tensorrt_llm/_torch/modules/attention.py
+++ tensorrt_llm/_torch/modules/attention.py
@@ -41,8 +41,10 @@ class Attention(nn.Module):
         config = config or ModelConfig()
         self.hidden_size = hidden_size
         self.num_heads = num_attention_heads
-        self.head_dim = getattr(config.pretrained_config, "head_dim",
-                                self.hidden_size // self.num_heads)
+        if config:
+            self.head_dim = getattr(config.pretrained_config, "head_dim", self.hidden_size // self.num_heads)
+        else: 
+            self.head_dim = self.hidden_size // self.num_heads
         self.num_key_value_heads = num_key_value_heads
         self.num_key_value_groups = self.num_heads // self.num_key_value_heads
         self.max_position_embeddings = max_position_embeddings
@@ -129,6 +131,8 @@ class Attention(nn.Module):
         self.rotary_emb = None
         self.apply_rotary_emb = (not self.enable_rope_fusion
                                  and pos_embd_params is not None)
+        if config.pretrained_config and config.pretrained_config.model_type == 'qwen3':
+            self.apply_rotary_emb = True
         if self.apply_rotary_emb:
             self.rotary_emb = RotaryEmbedding(
                 pos_embd_params.rope,
@@ -182,6 +186,16 @@ class Attention(nn.Module):
         if self.apply_rotary_emb and position_ids is not None:
             q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size],
                                 dim=-1)
+            if hasattr(self, 'q_norm') and hasattr(self, 'k_norm'):
+                # Add qk-norm
+                q_by_head = q.view(*q.shape[:-1], q.shape[-1] // self.head_dim,
+                                    self.head_dim)
+                q_by_head = self.q_norm(q_by_head)
+                q = q_by_head.view(q.shape)
+                k_by_head = k.view(*k.shape[:-1], k.shape[-1] // self.head_dim,
+                                    self.head_dim)
+                k_by_head = self.k_norm(k_by_head)
+                k = k_by_head.view(k.shape)                 
             q, k = self.rotary_emb(position_ids, [q, k])
 
         out_scale = None
