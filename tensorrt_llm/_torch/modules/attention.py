11edf1870b91e45ac5e2a36da9ee2f7bd69b178b
bhsueh <11360707+byshiue@users.noreply.github.com>
2025-05-01 06:01:29 -0700

    fix bug of attention
    Signed-off-by: bhsueh <11360707+byshiue@users.noreply.github.com>

--- tensorrt_llm/_torch/modules/attention.py
+++ tensorrt_llm/_torch/modules/attention.py
@@ -186,18 +186,28 @@ class Attention(nn.Module):
         if self.apply_rotary_emb and position_ids is not None:
             q, k, v = qkv.split([self.q_size, self.kv_size, self.kv_size],
                                 dim=-1)
-            # Add qk-norm
-            q_l2norm = lambda: self.q_norm(q.reshape(-1, self.head_dim)
-                                           ).reshape(-1, self.q_size)
-            k_l2norm = lambda: self.k_norm(k.reshape(-1, self.head_dim)
-                                           ).reshape(-1, self.kv_size)
-            q, k = maybe_execute_in_parallel(
-                q_l2norm,
-                k_l2norm,
-                self.ln_events[0],
-                self.ln_events[1],
-                self.aux_stream,
-            )
+            if hasattr(self, 'q_norm') and hasattr(self, 'k_norm'):
+                # Add qk-norm
+                if hasattr(self, 'ln_events'):
+                    q_l2norm = lambda: self.q_norm(q.reshape(-1, self.head_dim)
+                                                   ).reshape(-1, self.q_size)
+                    k_l2norm = lambda: self.k_norm(k.reshape(-1, self.head_dim)
+                                                   ).reshape(-1, self.kv_size)
+                    q, k = maybe_execute_in_parallel(
+                        q_l2norm,
+                        k_l2norm,
+                        self.ln_events[0],
+                        self.ln_events[1],
+                        self.aux_stream,
+                    )
+                else:
+                    q_by_head = q.reshape(-1, self.head_dim)
+                    q_by_head = self.q_norm(q_by_head)
+                    q = q_by_head.view(q.shape)
+                    k_by_head = k.reshape(-1, self.head_dim)
+                    k_by_head = self.k_norm(k_by_head)
+                    k = k_by_head.view(k.shape)
+
             q, k = self.rotary_emb(position_ids, [q, k])
         out_scale = None
 
