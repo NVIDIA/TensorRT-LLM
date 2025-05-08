5ecade01f964c41069fb4781ca8245a8d39c0e00
bhsueh <11360707+byshiue@users.noreply.github.com>
2025-05-07 20:34:46 -0700

    fix bug of attention dp on qwen3
    Signed-off-by: bhsueh <11360707+byshiue@users.noreply.github.com>

--- tensorrt_llm/_torch/models/modeling_qwen3_moe.py
+++ tensorrt_llm/_torch/models/modeling_qwen3_moe.py
@@ -74,7 +74,7 @@ class Qwen3MoE(nn.Module):
                 (0, 0, 0, max_num_token - hidden_states.shape[0]))
         router_logits = self.gate(hidden_states)
         final_hidden_states = self.experts(hidden_states, router_logits,
-                                           all_rank_num_tokens)
+                                           all_rank_num_tokens=all_rank_num_tokens)
 
         return final_hidden_states.view(orig_shape)
 
@@ -183,14 +183,22 @@ class Qwen3MoEModel(DecoderModel):
         self.padding_idx = config.pretrained_config.pad_token_id
         self.aux_stream = torch.cuda.Stream()
 
-        self.embed_tokens = Embedding(
-            config.pretrained_config.vocab_size,
-            config.pretrained_config.hidden_size,
-            dtype=config.pretrained_config.torch_dtype,
-            mapping=config.mapping,
-            tensor_parallel_mode=TensorParallelMode.COLUMN,
-            gather_output=True,
-        )
+        if model_config.mapping.enable_attention_dp:
+            # When attention_dp is enabled, we cannot do all_reduce since
+            # the problem size of different ranks are different. 
+            # So, we don't do parallelism here.
+            self.embed_tokens = nn.Embedding(config.pretrained_config.vocab_size,
+                                             config.pretrained_config.hidden_size,
+                                             dtype=config.pretrained_config.torch_dtype)
+        else:
+            self.embed_tokens = Embedding(
+                config.pretrained_config.vocab_size,
+                config.pretrained_config.hidden_size,
+                dtype=config.pretrained_config.torch_dtype,
+                mapping=config.mapping,
+                tensor_parallel_mode=TensorParallelMode.COLUMN,
+                gather_output=True,
+            )
         self.layers = nn.ModuleList([
             Qwen3MoEDecoderLayer(
                 model_config,
