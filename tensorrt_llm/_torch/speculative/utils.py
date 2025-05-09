5591b8d9151df5c1d0cdc50a76d94c499d0e2c52
Po-Han Huang <pohanh@nvidia.com>
2025-05-09 00:30:41 -0700

    feat: Improve perf of AllGather-Top1 after LMHead
    Currently, after we run LMHead, we first run AllGather to collect the
    logits for the entire vocab size, and then apply top1. This has two perf
    issues: (1) AllGather is slow (2) we are doing redundant top1 across tp
    ranks.
    
    Therefore, replace the AllGather-Top1 with Top1-AllReduce-Top1. The idea
    is to first get the per-rank top1, and then use AllReduce to collect the
    per-rank top1 argmax/max values, and then do argmax again.
    
    This change requires turning off the AllGather in LMHead as well as
    handling the additional AllReduce in Eagle3OneModelWorker.

--- tensorrt_llm/_torch/speculative/utils.py
+++ tensorrt_llm/_torch/speculative/utils.py
@@ -65,12 +65,12 @@ def get_num_spec_layers(spec_config):
         return 0
 
 
-def get_spec_worker(spec_config):
+def get_spec_worker(spec_config, mapping):
     if spec_config.spec_dec_mode.is_mtp():
         return MTPWorker(spec_config)
     elif spec_config.spec_dec_mode.is_mtp_eagle():
         return MTPEagleWorker(spec_config)
     elif spec_config.spec_dec_mode.is_eagle3_one_model():
-        return Eagle3OneModelWorker(spec_config)
+        return Eagle3OneModelWorker(spec_config, mapping)
     else:
         return None
