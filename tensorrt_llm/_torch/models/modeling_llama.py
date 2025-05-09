184f88053d43da80268e80d61fb4bcaffadffdae
Rohan Varma <rohanv@nvidia.com>
2025-05-09 15:10:05 -0700

    add bidirectional support and fix EarlyStopDecoder unsqueeze to be compatible with LogitsStorage
    Signed-off-by: Rohan Varma <rohanv@nvidia.com>

--- tensorrt_llm/_torch/models/modeling_llama.py
+++ tensorrt_llm/_torch/models/modeling_llama.py
@@ -200,8 +200,6 @@ class LlamaAttention(Attention):
             dtype=config.torch_dtype,
             config=model_config,
         )
-
-
 class Llama4MoE(nn.Module):
 
     def __init__(
@@ -481,6 +479,7 @@ class LlamaDecoderLayer(DecoderLayer):
         self,
         model_config: ModelConfig[LlamaConfig],
         layer_idx: int,
+        bidirectional: bool = False,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         super().__init__()
         config = model_config.pretrained_config
@@ -506,6 +505,10 @@ class LlamaDecoderLayer(DecoderLayer):
         self.post_attention_layernorm = RMSNorm(hidden_size=config.hidden_size,
                                                 eps=config.rms_norm_eps,
                                                 dtype=config.torch_dtype)
+        
+        self.attention_mask = PredefinedAttentionMask.CAUSAL
+        if bidirectional:
+            self.attention_mask = PredefinedAttentionMask.FULL
 
     def forward(
         self,
@@ -528,6 +531,7 @@ class LlamaDecoderLayer(DecoderLayer):
             position_ids=position_ids,
             hidden_states=hidden_states,
             attn_metadata=attn_metadata,
+            attention_mask=self.attention_mask,
             **kwargs,
         )
 
@@ -716,7 +720,7 @@ class Llama4Model(DecoderModel):
 
 class LlamaModel(DecoderModel):
 
-    def __init__(self, model_config: ModelConfig[LlamaConfig]):
+    def __init__(self, model_config: ModelConfig[LlamaConfig], bidirectional=False):
         super().__init__(model_config)
         config = self.model_config.pretrained_config
         self.padding_idx = config.pad_token_id
@@ -760,6 +764,7 @@ class LlamaModel(DecoderModel):
             LlamaDecoderLayer(
                 model_config,
                 layer_idx,
+                bidirectional=bidirectional
             ) for layer_idx in range(config.num_hidden_layers)
         ])
         self.norm = RMSNorm(hidden_size=config.hidden_size,
