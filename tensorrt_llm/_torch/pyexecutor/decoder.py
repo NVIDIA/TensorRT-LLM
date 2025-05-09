d530f77e5fb6b21c21d5ef41dc792b85ac2c6671
Robin Kobus <19427718+Funatiq@users.noreply.github.com>
2025-05-09 09:18:22 +0000

    fixup! chore: Partition context requests in MicroBatchScheduler
    Signed-off-by: Robin Kobus <19427718+Funatiq@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/decoder.py
+++ tensorrt_llm/_torch/pyexecutor/decoder.py
@@ -263,6 +263,7 @@ class TorchDecoder(Decoder):
 
         if hasattr(scheduled_requests, 'chunked_requests'):
             request_idx += len(scheduled_requests.chunked_requests)
+            token_idx += len(scheduled_requests.chunked_requests)
 
         for request in scheduled_requests.context_requests:
             if request.get_context_remaining_length() != 0:
