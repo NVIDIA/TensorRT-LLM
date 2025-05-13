b74762be9cef8a065dde4a78115d6b545ba1b981
Yiqing Yan <yiqingy@nvidia.com>
2025-05-13 03:29:39 +0000

    update images
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>

--- jenkins/controlCCache.groovy
+++ jenkins/controlCCache.groovy
@@ -1,7 +1,7 @@
 
 import java.lang.InterruptedException
 
-DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:pytorch-25.04-py3-x86_64-ubuntu24.04-trt10.10.0.31-skip-devel-202505111320-4049"
+DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:pytorch-25.04-py3-x86_64-ubuntu24.04-trt10.10.0.31-skip-devel-202505121727-4049"
 
 def createKubernetesPodConfig(image)
 {
