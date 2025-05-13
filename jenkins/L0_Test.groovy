fda8b0277a6225525b7aaa2851590276fb68dd12
Yiqing Yan <yiqingy@nvidia.com>
2025-05-13 14:59:12 +0800

    [Infra][TRTLLM-4374] Upgrade TRT 10.10.0 GA, CUDA 12.9 GA and DLFW 25.04 (#4049)
    * [TRTLLM-4374] Upgrade TRT 10.10.0 GA, CUDA 12.9 GA and DLFW 25.04
    
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>
    
    * fix review
    
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>
    
    * update images
    
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>
    
    * Update jenkins/L0_Test.groovy
    
    Co-authored-by: Yanchao Lu <yanchaol@nvidia.com>
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>
    
    * update image name
    
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>
    
    ---------
    
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>
    Co-authored-by: Yanchao Lu <yanchaol@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -35,11 +35,11 @@ linuxPkgName = ( env.targetArch == AARCH64_TRIPLE ? "tensorrt-llm-sbsa-release-s
 // available tags can be found in: https://urm.nvidia.com/artifactory/sw-tensorrt-docker/tensorrt-llm/
 // [base_image_name]-[arch]-[os](-[python_version])-[trt_version]-[torch_install_type]-[stage]-[date]-[mr_id]
 LLM_DOCKER_IMAGE = env.dockerImage
-LLM_ROCKYLINUX8_PY310_DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:cuda-12.8.1-devel-rocky8-x86_64-rocky8-py310-trt10.9.0.34-skip-tritondevel-202505110947-4191"
-LLM_ROCKYLINUX8_PY312_DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:cuda-12.8.1-devel-rocky8-x86_64-rocky8-py312-trt10.9.0.34-skip-tritondevel-202505110947-4191"
+LLM_ROCKYLINUX8_PY310_DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:cuda-12.9.0-devel-rocky8-x86_64-rocky8-py310-trt10.10.0.31-skip-tritondevel-202505121727-4049"
+LLM_ROCKYLINUX8_PY312_DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:cuda-12.9.0-devel-rocky8-x86_64-rocky8-py312-trt10.10.0.31-skip-tritondevel-202505121727-4049"
 
 // DLFW torch image
-DLFW_IMAGE = "nvcr.io/nvidia/pytorch:25.03-py3"
+DLFW_IMAGE = "nvcr.io/nvidia/pytorch:25.04-py3"
 
 //Ubuntu base image
 UBUNTU_22_04_IMAGE = "urm.nvidia.com/docker/ubuntu:22.04"
@@ -1580,9 +1580,10 @@ def launchTestJobs(pipeline, testFilter, dockerNode=null)
 
             def fullWheelPath = "${cpu_arch}/${wheelPath}${wheelName}"
 
-            sanityRunner("Sanity check") {
-                runPackageSanityCheck(pipeline, fullWheelPath, values[3], cpver)
-            }
+            // TODO: Re-enable the sanity check after updating GPU testers' driver version.
+            // sanityRunner("Sanity check") {
+            //     runPackageSanityCheck(pipeline, fullWheelPath, values[3], cpver)
+            // }
 
             def checkPipStage = false
             if (cpu_arch == X86_64_TRIPLE) {
