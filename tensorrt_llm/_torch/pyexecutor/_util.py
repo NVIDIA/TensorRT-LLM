0daf44462f4c18096e16538dc0b7b14399fae286
ixlmar <206748156+ixlmar@users.noreply.github.com>
2025-05-13 14:52:07 +0000

    fix: add missing () in branch condition
    Signed-off-by: ixlmar <206748156+ixlmar@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/_util.py
+++ tensorrt_llm/_torch/pyexecutor/_util.py
@@ -413,7 +413,7 @@ def create_py_executor_instance(dist,
             lora_config.trtllm_modules_to_hf_modules)
 
     num_micro_batches = 1
-    if mapping.has_pp:
+    if mapping.has_pp():
         num_micro_batches = mapping.pp_size + pytorch_backend_config.enable_overlap_scheduler
 
     resources["seq_slot_manager"] = SeqSlotManager(
