319ff97e17d94bc79b10aec674c5ed8fa307d011
moraxu <mguzek@nvidia.com>
2025-05-13 01:53:23 +0000

    Add fixes
    Signed-off-by: moraxu <mguzek@nvidia.com>

--- tests/integration/defs/accuracy/test_llm_api_pytorch.py
+++ tests/integration/defs/accuracy/test_llm_api_pytorch.py
@@ -295,36 +295,6 @@ class TestLlama4ScoutInstruct(LlmapiAccuracyTestHarness):
             task = GSM8K(self.MODEL_NAME)
             task.evaluate(llm)
 
-    @pytest.mark.skip_less_device(8)
-    @skip_pre_hopper
-    @parametrize_with_ids("cuda_graph", [False, True])
-    @parametrize_with_ids("fp8kv", [False, True])
-    @pytest.mark.parametrize("tp_size,pp_size,ep_size", [(8, 1, 1), (8, 1, 4),
-                                                         (8, 1, 8)],
-                             ids=["tp8", "tp8ep4", "tp8ep8"])
-    def test_fp8(self, cuda_graph, fp8kv, tp_size, pp_size, ep_size):
-        quant_config = QuantConfig(quant_algo=QuantAlgo.FP8)
-        if fp8kv:
-            quant_config.kv_cache_quant_algo = QuantAlgo.FP8
-            pytorch_config = PyTorchConfig(kv_cache_dtype="fp8")
-        else:
-            pytorch_config = PyTorchConfig()
-
-        with LLM(self.MODEL_PATH,
-                 tensor_parallel_size=tp_size,
-                 pipeline_parallel_size=pp_size,
-                 moe_expert_parallel_size=ep_size,
-                 use_cuda_graph=cuda_graph,
-                 quant_config=quant_config,
-                 pytorch_backend_config=pytorch_config) as llm:
-            assert llm.args.quant_config.quant_algo == QuantAlgo.FP8
-            if fp8kv:
-                assert llm.args.quant_config.kv_cache_quant_algo == QuantAlgo.FP8
-            task = MMLU(self.MODEL_NAME)
-            task.evaluate(llm)
-            task = GSM8K(self.MODEL_NAME)
-            task.evaluate(llm)
-
 
 class TestMistral7B(LlmapiAccuracyTestHarness):
     MODEL_NAME = "mistralai/Mistral-7B-v0.1"
@@ -974,8 +944,16 @@ class TestNemotronUltra(LlmapiAccuracyTestHarness):
 
     @pytest.mark.skip_less_device(8)
     @pytest.mark.skip_device_not_contain(["H100", "B200"])
-    def test_auto_dtype(self):
-        with LLM(self.MODEL_PATH) as llm:
+    @parametrize_with_ids("cuda_graph", [False, True])
+    @pytest.mark.parametrize("tp_size,pp_size,ep_size", [(8, 1, 1), (8, 1, 4),
+                                                         (8, 1, 8)],
+                             ids=["tp8", "tp8ep4", "tp8ep8"])
+    def test_auto_dtype(self, cuda_graph, tp_size, pp_size, ep_size):
+        with LLM(self.MODEL_PATH,
+                 tensor_parallel_size=tp_size,
+                 pipeline_parallel_size=pp_size,
+                 moe_expert_parallel_size=ep_size,
+                 use_cuda_graph=cuda_graph) as llm:
             task = CnnDailymail(self.MODEL_NAME)
             task.evaluate(llm)
             task = MMLU(self.MODEL_NAME)
@@ -988,15 +966,23 @@ class TestNemotronUltra(LlmapiAccuracyTestHarness):
 
     # @pytest.mark.skip_less_device(8)
     # @pytest.mark.skip_device_not_contain(["H100", "B200"])
-    # def test_fp8_tp8(self):
+    # @parametrize_with_ids("cuda_graph", [False, True])
+    # @pytest.mark.parametrize("tp_size,pp_size,ep_size", [(8, 1, 1), (8, 1, 4), (8, 1, 8)],
+    #                         ids=["tp8", "tp8ep4", "tp8ep8"])
+    # def test_fp8_tp8(self, cuda_graph, tp_size, pp_size, ep_size):
     #     model_path = f"{llm_models_root()}/nemotron-nas/Llama-3_1-Nemotron-Ultra-253B-v1-FP8"
-    #     with LLM(model_path, tensor_parallel_size=8) as llm:
+    #     with LLM(model_path,
+    #             tensor_parallel_size=tp_size,
+    #             pipeline_parallel_size=pp_size,
+    #             moe_expert_parallel_size=ep_size,
+    #             use_cuda_graph=cuda_graph) as llm:
     #         assert llm.args.quant_config.quant_algo == QuantAlgo.FP8
     #         assert llm.args.quant_config.kv_cache_quant_algo == QuantAlgo.FP8
     #         task = CnnDailymail(self.MODEL_NAME)
     #         task.evaluate(llm)
     #         task = MMLU(self.MODEL_NAME)
     #         task.evaluate(llm)
+
 
 class TestNemotronH(LlmapiAccuracyTestHarness):
     MODEL_NAME = "nvidia/Nemotron-H-8B-Base-8K"
@@ -1158,3 +1144,20 @@ class TestQwen3_32B(LlmapiAccuracyTestHarness):
             task.evaluate(llm)
             task = MMLU(self.MODEL_NAME)
             task.evaluate(llm)
+
+
+class TestPhi4MiniInstruct(LlmapiAccuracyTestHarness):
+    MODEL_NAME = "microsoft/Phi-4-mini-instruct"
+    MODEL_PATH = f"{llm_models_root()}/Phi-4-mini-instruct"
+
+    def test_auto_dtype(self):
+        with LLM(self.MODEL_PATH) as llm:
+            task = CnnDailymail(self.MODEL_NAME)
+            task.evaluate(llm)
+            task = MMLU(self.MODEL_NAME)
+            task.evaluate(llm)
+            task = GSM8K(self.MODEL_NAME)
+            task.evaluate(llm)
+            task = GPQADiamond(self.MODEL_NAME)
+            task.evaluate(llm,
+                          extra_evaluator_kwargs=dict(apply_chat_template=True))
