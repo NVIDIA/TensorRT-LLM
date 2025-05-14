8280c3d4f2135e88baa6ab8163a35a60183272f1
brb-nv <169953907+brb-nv@users.noreply.github.com>
2025-05-13 23:02:44 -0700

    feat: Support Gemma3-1b-it in Pytorch workflow (#3999)
    Signed-off-by: Balaram Buddharaju <169953907+brb-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/models/__init__.py
+++ tensorrt_llm/_torch/models/__init__.py
@@ -4,6 +4,7 @@ from .modeling_auto import AutoModelForCausalLM
 from .modeling_bert import BertForSequenceClassification
 from .modeling_clip import CLIPVisionModel
 from .modeling_deepseekv3 import DeepseekV3ForCausalLM
+from .modeling_gemma3 import Gemma3ForCausalLM
 from .modeling_llama import LlamaForCausalLM
 from .modeling_llava_next import LlavaNextModel
 from .modeling_mistral import MistralForCausalLM
@@ -27,6 +28,7 @@ __all__ = [
     "BertForSequenceClassification",
     "CLIPVisionModel",
     "DeepseekV3ForCausalLM",
+    "Gemma3ForCausalLM",
     "LlamaForCausalLM",
     "LlavaNextModel",
     "MistralForCausalLM",
