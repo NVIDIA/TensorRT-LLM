fe1dc3c5f6251fe5e35df1af263e7d24f8f075bc
Ruodi <200874449+ruodil@users.noreply.github.com>
2025-05-14 07:50:21 +0000

    add llama_v4_scout_instruct and llama_v4_maverick into perf test
    Signed-off-by: Ruodi <200874449+ruodil@users.noreply.github.com>

--- tests/integration/defs/perf/test_perf.py
+++ tests/integration/defs/perf/test_perf.py
@@ -58,8 +58,13 @@ MODEL_PATH_DICT = {
     "llama_v3.1_nemotron_nano_8b": "Llama-3.1-Nemotron-Nano-8B-v1",
     "llama_v3.3_nemotron_super_49b":
     "nemotron-nas/Llama-3_3-Nemotron-Super-49B-v1",
+<<<<<<< HEAD
     "llama_v3.1_nemotron_ultra_253b":
     "nemotron-nas/Llama-3_1-Nemotron-Ultra-253B-v1",
+=======
+    "llama_v4_scout_instruct_17b_16e": "Llama-4-Scout-17B-16E-Instruct",
+    "llama_v4_maverick_instruct_17b_128e": "Llama-4-Maverick-17B-128E-Instruct",
+>>>>>>> af69d0be9 (add llama_v4_scout_instruct and llama_v4_maverick into perf test)
     # "llama_30b": "llama-models/llama-30b-hf",
     "mixtral_8x7b_v0.1": "Mixtral-8x7B-v0.1",
     "mixtral_8x7b_v0.1_instruct": "Mixtral-8x7B-Instruct-v0.1",
