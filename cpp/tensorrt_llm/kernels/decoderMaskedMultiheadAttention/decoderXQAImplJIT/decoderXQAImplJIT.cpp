6620b73c9cc17cd64fc5c3b1318d40ffc89d6f93
Bo Li <22713281+bobboli@users.noreply.github.com>
2025-05-14 22:36:36 +0800

    Change log level from warning to debug.
    Signed-off-by: Bo Li <22713281+bobboli@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/decoderMaskedMultiheadAttention/decoderXQAImplJIT/decoderXQAImplJIT.cpp
+++ cpp/tensorrt_llm/kernels/decoderMaskedMultiheadAttention/decoderXQAImplJIT/decoderXQAImplJIT.cpp
@@ -99,7 +99,7 @@ bool DecoderXQAImplJIT::shouldUse(XQAParams const& umbrellaXQAParams, bool forCo
                 return true;
             }
         }
-        TLLM_LOG_WARNING("JIT XQA is not used: no supported configuration found for any beam_width");
+        TLLM_LOG_DEBUG("JIT XQA is not used: no supported configuration found for any beam_width");
         return false;
     }
     else
@@ -108,13 +108,13 @@ bool DecoderXQAImplJIT::shouldUse(XQAParams const& umbrellaXQAParams, bool forCo
         bool isConfigSupported = supportConfig(xqaParams, forConfigurePlugin);
         if (!isConfigSupported)
         {
-            TLLM_LOG_WARNING("JIT XQA is not used: unsupported configuration");
+            TLLM_LOG_DEBUG("JIT XQA is not used: unsupported configuration");
             return false;
         }
         bool hasPerfGain = mayHavePerfGain(xqaParams);
         if (!hasPerfGain)
         {
-            TLLM_LOG_WARNING("JIT XQA is not used: no performance gain");
+            TLLM_LOG_DEBUG("JIT XQA is not used: no performance gain");
             return false;
         }
         return true;
