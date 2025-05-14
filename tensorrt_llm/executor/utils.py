d77de9d008ecb4d9a9ba69935bac1400ae7ecc56
Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
2025-05-14 21:35:39 +0000

    Revert "fix: Use hmac authentication for pickle encryption (#3384)"
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>

--- tensorrt_llm/executor/utils.py
+++ tensorrt_llm/executor/utils.py
@@ -3,7 +3,7 @@ import concurrent.futures
 import os
 from concurrent.futures import ProcessPoolExecutor
 from queue import Empty, Queue
-from typing import Any, Callable, List, NamedTuple, Optional
+from typing import Any, Callable, List, NamedTuple
 
 from strenum import StrEnum
 
@@ -143,12 +143,12 @@ class IntraProcessQueue:
 
 
 class WorkerCommIpcAddrs(NamedTuple):
-    ''' IPC addresses (str) and HMAC keys (bytes) for communication with the worker processes. '''
-    request_queue_addr: tuple[str, Optional[bytes]]
-    request_error_queue_addr: tuple[str, Optional[bytes]]
-    result_queue_addr: tuple[str, Optional[bytes]]
-    stats_queue_addr: tuple[str, Optional[bytes]]
-    kv_cache_events_queue_addr: tuple[str, Optional[bytes]]
+    ''' IPC addresses for communication with the worker processes. '''
+    request_queue_addr: str
+    request_error_queue_addr: str
+    result_queue_addr: str
+    stats_queue_addr: str
+    kv_cache_events_queue_addr: str
 
 
 def is_llm_response(instance):
