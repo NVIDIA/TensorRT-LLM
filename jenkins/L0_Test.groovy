a6ba7676ecfc42ef0e29a64d271ca78df4e5e077
qqiao <qqiao@nvidia.com>
2025-05-14 18:35:46 -0700

    Try another timeout opt to kill test thread
    Signed-off-by: qqiao <qqiao@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1013,6 +1013,8 @@ def runLLMTestlistOnPlatformImpl(pipeline, platform, testList, config=VANILLA_CO
             extraInternalEnv,
             "pytest",
             "-v",
+            "-s",
+            "--timeout-method=thread",
             "--apply-test-list-correction",
             "--splitting-algorithm least_duration",
             "--timeout=${pytestTestTimeout}",
