6ca13ad091fc5d0aaee0b42fb2aaeeafccd761c5
Pengyun Lin <81065165+LinPoly@users.noreply.github.com>
2025-05-16 06:58:04 +0000

    Enhance error message
    Signed-off-by: Pengyun Lin <81065165+LinPoly@users.noreply.github.com>

--- tensorrt_llm/executor/worker.py
+++ tensorrt_llm/executor/worker.py
@@ -395,17 +395,27 @@ class ExecutorBindingsWorker(GenerationExecutor):
 
         assert request.id is not None
 
-        def _deduce_max_tokens(request, executor_config):
+        def _deduce_max_tokens(request: GenerationRequest,
+                               executor_config: tllm.ExecutorConfig) -> int:
             if request.sampling_params.max_tokens:
                 return request.sampling_params.max_tokens
             # deduce max_tokens when it's not set by user
             query_token_len = len(
                 request.query_token_ids) if request.query_token_ids else 0
-            cp_size = 1 if not hasattr(
-                executor_config, "mapping") else executor_config.mapping.cp_size
-            default_max_tokens = self._executor_config.max_seq_len - len(
-                prompt_token_ids) / cp_size - query_token_len
-            return int(default_max_tokens)
+            cp_size = 1 if (not hasattr(executor_config, "mapping")
+                            or executor_config.mapping.cp_size
+                            is None) else executor_config.mapping.cp_size
+            if not hasattr(executor_config, "max_seq_len"):
+                raise RuntimeError(
+                    "max_tokens for sampling is not set and cannot be deduced")
+            splited_prompt_len = int(len(prompt_token_ids) / cp_size)
+            default_max_tokens = executor_config.max_seq_len - splited_prompt_len - query_token_len
+            if default_max_tokens < 0:
+                raise ValueError(
+                    f"Deduced max_tokens {default_max_tokens} is less than 0, because"
+                    f"prompt length {splited_prompt_len} plus query length {query_token_len} "
+                    f"is larger than max_seq_len {executor_config.max_seq_len}")
+            return default_max_tokens
 
         try:
             executor_request = tllm.Request(
