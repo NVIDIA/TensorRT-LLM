ad8bbdb053b3408016db4b79ae8f3a9a3ed29ffb
Tracin <10434017+Tracin@users.noreply.github.com>
2025-05-15 22:40:40 -0700

    Fix chat template kwargs bug.
    Signed-off-by: Tracin <10434017+Tracin@users.noreply.github.com>

--- tensorrt_llm/serve/openai_server.py
+++ tensorrt_llm/serve/openai_server.py
@@ -194,7 +194,8 @@ class OpenAIServer:
                 tools=tool_dicts,
                 documents=request.documents,
                 chat_template=request.chat_template,
-                chat_template_kwargs=request.chat_template_kwargs or {})
+                chat_template_kwargs=request.chat_template_kwargs or {},
+            )
             prompt = prompt_inputs(prompt)
 
             mm_data = await mm_coroutines
