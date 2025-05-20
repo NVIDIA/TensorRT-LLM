19a5e0a0b28ba562a5ea4d463defcc2a7be645c9
Caron Zhang <caronzh03@gmail.com>
2025-05-19 17:21:50 -0700

    Fix logits error

--- examples/pytorch/quickstart.py
+++ examples/pytorch/quickstart.py
@@ -1,24 +1,29 @@
 from tensorrt_llm import SamplingParams
 from tensorrt_llm._torch import LLM
 import torch
+from sentence_transformers import SentenceTransformer
 
 
 def main():
     prompts = [
-        "Hello, my name is",
-        "The president of the United States is",
         "The capital of France is",
-        "The future of AI is",
     ]
-    sampling_params = SamplingParams(max_tokens=32, return_context_logits=True)
+    sbert_model_path = "./my_custom_model"
+    sbert = SentenceTransformer(sbert_model_path)
+    sbert_embeddings = sbert.encode(prompts)
+    print(sbert_embeddings)
+    print(f"shape: {sbert_embeddings.shape}")
 
+    print("=======")
+
+    sampling_params = SamplingParams(max_tokens=32, return_context_logits=True)
     llm = LLM(model='./converted-classification-model/')
     outputs = llm.generate(prompts, sampling_params)
 
     tllm_logits = []
     for i, output in enumerate(outputs):
         prompt = output.prompt
-        tllm_logit = output.context_logits.cpu()
+        tllm_logit = output.context_logits.cpu()[0, :]
         print(f"Prompt: {prompt!r}, Context logits: {tllm_logit}")
         tllm_logits += [tllm_logit]
     # Stack the output
