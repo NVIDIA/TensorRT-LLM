1cffa997929fd38debf236e782b7a58e3c0285c7
Dom Brown <3886319+DomBrown@users.noreply.github.com>
2025-05-21 21:26:21 +0100

    test: Split test_simple into mpi_utils and cache transceiver tests for DGX (#4451)
    Signed-off-by: Dom Brown <3886319+DomBrown@users.noreply.github.com>

--- tests/integration/defs/cpp/test_multi_gpu.py
+++ tests/integration/defs/cpp/test_multi_gpu.py
@@ -44,10 +44,11 @@ def get_multi_gpu_env(kv_cache_type=KVCacheType.NONE, llama_multi_gpu=False):
     return env
 
 
-def run_simple_multi_gpu_tests(build_dir: _pl.Path, timeout=1500):
+def run_mpi_utils_tests(build_dir, timeout=300):
+
     tests_dir = build_dir / "tests"
-    cpp_env = {**_os.environ}
-    # Utils tests
+    mgpu_env = get_multi_gpu_env()
+
     mpi_utils_test = [
         "mpirun",
         "-n",
@@ -55,56 +56,31 @@ def run_simple_multi_gpu_tests(build_dir: _pl.Path, timeout=1500):
         "--allow-run-as-root",
         "mpiUtilsTest",
     ]
-    _cpp.run_command(mpi_utils_test, cwd=tests_dir, env=cpp_env, timeout=300)
+    _cpp.run_command(mpi_utils_test,
+                     cwd=tests_dir,
+                     env=mgpu_env,
+                     timeout=timeout)
 
-    # Cache transceiver MPI tests
-    new_env = get_multi_gpu_env(kv_cache_type=KVCacheType.MPI)
+
+def run_cache_transceiver_tests(build_dir: _pl.Path,
+                                nprocs=2,
+                                kv_cache_type=KVCacheType.MPI,
+                                timeout=600):
+
+    tests_dir = build_dir / "tests"
+    mgpu_env = get_multi_gpu_env(kv_cache_type=kv_cache_type)
 
     cache_trans_test = [
         "mpirun",
         "-n",
-        "2",
+        f"{nprocs}",
         "--allow-run-as-root",
         "batch_manager/cacheTransceiverTest",
     ]
-    _cpp.run_command(cache_trans_test, cwd=tests_dir, env=new_env, timeout=300)
-
-    cache_trans_test_8_proc = [
-        "mpirun",
-        "-n",
-        "8",
-        "--allow-run-as-root",
-        "batch_manager/cacheTransceiverTest",
-    ]
-    _cpp.run_command(cache_trans_test_8_proc,
+    _cpp.run_command(cache_trans_test,
                      cwd=tests_dir,
-                     env=new_env,
-                     timeout=600)
-
-    # Cache transceiver tests with UCX
-    new_env = get_multi_gpu_env(kv_cache_type=KVCacheType.UCX)
-
-    cache_trans_test = [
-        "mpirun",
-        "-n",
-        "2",
-        "--allow-run-as-root",
-        "batch_manager/cacheTransceiverTest",
-    ]
-    _cpp.run_command(cache_trans_test, cwd=tests_dir, env=new_env, timeout=300)
-
-    # Cache transceiver tests
-    cache_trans_test_8_proc = [
-        "mpirun",
-        "-n",
-        "8",
-        "--allow-run-as-root",
-        "batch_manager/cacheTransceiverTest",
-    ]
-    _cpp.run_command(cache_trans_test_8_proc,
-                     cwd=tests_dir,
-                     env=new_env,
-                     timeout=600)
+                     env=mgpu_env,
+                     timeout=timeout)
 
 
 def run_llama_executor_leader_tests(build_dir: _pl.Path, timeout=1500):
@@ -447,12 +423,25 @@ def multi_gpu_model(request, prepare_model_multi_gpu):
 
 @pytest.mark.parametrize("build_google_tests", ["80", "86", "89", "90"],
                          indirect=True)
-def test_simple(build_google_tests, build_dir):
+def test_mpi_utils(build_google_tests, build_dir):
+
+    if platform.system() != "Windows":
+        run_mpi_utils_tests(build_dir, timeout=300)
+
+
+@pytest.mark.parametrize("build_google_tests", ["80", "86", "89", "90"],
+                         indirect=True)
+@pytest.mark.parametrize("kvcache_type", [KVCacheType.MPI, KVCacheType.UCX],
+                         ids=["mpi_kvcache", "ucx_kvcache"])
+@pytest.mark.parametrize("nprocs", [2, 8], ids=["2proc", "8proc"])
+def test_cache_transceiver(build_google_tests, nprocs, kvcache_type, build_dir):
 
     if platform.system() != "Windows":
 
-        run_simple_multi_gpu_tests(build_dir=build_dir,
-                                   timeout=_cpp.default_test_timeout)
+        run_cache_transceiver_tests(build_dir=build_dir,
+                                    nprocs=nprocs,
+                                    kv_cache_type=kvcache_type,
+                                    timeout=600)
 
 
 @pytest.mark.parametrize("build_google_tests", ["80", "86", "89", "90"],
