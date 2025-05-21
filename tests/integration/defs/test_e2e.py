4ea29b307275f26f75afd608cedf37952d73714f
Yuan Tong <13075180+tongyuantongyu@users.noreply.github.com>
2025-05-21 11:01:14 +0800

    fix: cleanup process tree for disaggregated test (#4116)
    Signed-off-by: Yuan Tong <13075180+tongyuantongyu@users.noreply.github.com>

--- tests/integration/defs/test_e2e.py
+++ tests/integration/defs/test_e2e.py
@@ -599,7 +599,7 @@ def test_trtllm_bench_pytorch_backend_sanity(llm_root, llm_venv,
                                      dir="./",
                                      delete=True,
                                      delete_on_close=True) as running_log:
-        check_call(benchmark_cmd, shell=True, running_log=running_log)
+        check_call(benchmark_cmd, shell=True, stdout=running_log)
         if model_id in mapping and not use_extra_config:
             # extra config defines max kv cache tokens number to be 40000 which makes the checking
             # the checking process not unified.
@@ -631,7 +631,7 @@ def test_trtllm_bench_mgmn(llm_root, llm_venv):
                                      dir="./",
                                      delete=True,
                                      delete_on_close=True) as running_log:
-        check_call(benchmark_cmd, shell=True, running_log=running_log)
+        check_call(benchmark_cmd, shell=True, stdout=running_log)
         _check_mem_usage(running_log, [30, 0, 0, 0])
 
 
@@ -783,7 +783,7 @@ def test_trtllm_bench_iteration_log(llm_root, llm_venv, model_name,
                     dir="./",
                     delete=True,
                     delete_on_close=True) as running_log:
-                check_call(benchmark_cmd, shell=True, running_log=running_log)
+                check_call(benchmark_cmd, shell=True, stdout=running_log)
                 _check_mem_usage(running_log, [19.4, 0, 0, 0])
         else:
             check_call(benchmark_cmd, shell=True)
@@ -1309,7 +1309,7 @@ def test_ptp_quickstart(llm_root, llm_venv):
                                      delete=True,
                                      delete_on_close=True) as running_log:
         venv_check_call(llm_venv, [str(example_root / "quickstart.py")],
-                        running_log=running_log)
+                        stdout=running_log)
         _check_mem_usage(running_log, [4.60, 0, 0, 0])
 
 
@@ -1372,7 +1372,7 @@ def test_ptp_quickstart_advanced(llm_root, llm_venv, model_name, model_path):
             ]
             if "Qwen3" in model_name:
                 cmds.append(f"--kv_cache_fraction=0.6")
-            llm_venv.run_cmd(cmds, running_log=running_log)
+            llm_venv.run_cmd(cmds, stdout=running_log)
             if model_name in mapping:
                 _check_mem_usage(running_log, [mapping[model_name], 0, 0, 0])
 
@@ -1400,7 +1400,7 @@ def test_ptq_quickstart_advanced_mtp(llm_root, llm_venv, model_name,
                 "--model_dir",
                 f"{llm_models_root()}/{model_path}",
             ],
-            running_log=running_log)
+            stdout=running_log)
         _check_mem_usage(running_log, [54.50, 0, 0, 0])
 
 
@@ -1457,7 +1457,7 @@ def test_ptp_quickstart_advanced_eagle3(llm_root, llm_venv, model_name,
             "--disable_kv_cache_reuse",
             "--disable_overlap_scheduler",
         ],
-                         running_log=running_log)
+                         stdout=running_log)
         _check_mem_usage(running_log, [25.2, 0, 0, 0])
 
 
@@ -1491,7 +1491,7 @@ def test_ptp_quickstart_advanced_deepseek_r1_8gpus(llm_root, llm_venv,
             "--max_seq_len=3000",
             "--disable_kv_cache_reuse",
         ],
-                         running_log=running_log)
+                         stdout=running_log)
         _check_mem_usage(running_log, [106.3, 0, 0, 0], 8)
 
 
@@ -1530,7 +1530,7 @@ def test_relaxed_acceptance_quickstart_advanced_deepseek_r1_8gpus(
             "--relaxed_topk=10",
             "--relaxed_delta=0.5",
         ],
-                         running_log=running_log)
+                         stdout=running_log)
         _check_mem_usage(running_log, [85.6, 0, 0, 0], 8)
     # TODO: relaxed acceptance is incompatible with attention dp
     # "--enable_attention_dp"
@@ -1580,7 +1580,7 @@ def test_ptp_quickstart_advanced_8gpus(llm_root, llm_venv, model_name,
             f"{llm_models_root()}/{model_path}",
             "--tp_size=8",
         ],
-                         running_log=running_log)
+                         stdout=running_log)
         if model_name in mapping:
             _check_mem_usage(running_log, [mapping[model_name], 0, 0, 0], 8)
 
@@ -1623,7 +1623,7 @@ def test_ptp_quickstart_advanced_mixed_precision(llm_root, llm_venv):
             "--model_dir",
             f"{llm_models_root()}/{model_path}",
         ],
-                         running_log=running_log)
+                         stdout=running_log)
         _check_mem_usage(running_log, [12.0, 0, 0, 0])
 
 
@@ -1814,7 +1814,7 @@ def test_ptp_quickstart_multimodal(llm_root, llm_venv, model_name, model_path,
             "--media",
             *functionality_inputs[modality]["media"],
         ],
-                         running_log=running_log)
+                         stdout=running_log)
 
         if model_name in mapping:
             peak, fraction = mapping[model_name]
