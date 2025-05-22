83e02f9a905a56edc7de6b7df419f23b27f6c60b
xinhe-nv <200704525+xinhe-nv@users.noreply.github.com>
2025-05-22 15:39:31 +0800

    fix test issues
    Signed-off-by: xinhe-nv <200704525+xinhe-nv@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api_pytorch.py
+++ tests/integration/defs/accuracy/test_llm_api_pytorch.py
@@ -890,6 +890,7 @@ class TestNemotronNas(LlmapiAccuracyTestHarness):
             task.evaluate(llm)
 
 
+@pytest.mark.skip_less_device_memory(80000)
 class TestNemotronSuper(LlmapiAccuracyTestHarness):
     MODEL_NAME = "nvidia/Llama-3_3-Nemotron-Super-49B-v1"
     MODEL_PATH = f"{llm_models_root()}/nemotron-nas/Llama-3_3-Nemotron-Super-49B-v1"
