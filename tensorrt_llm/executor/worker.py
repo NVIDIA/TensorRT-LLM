c7e4d1c8b998ee19bcd6912a07018def6296255e
coldwaterq <coldwaterq@users.noreply.github.com>
2025-05-22 22:06:29 -0700

    fix: [nvbugs/5066257] serialization improvments (#3869)
    * added a restricted pcikler and depickler in a sepparate serialization function.
    
    Signed-off-by: coldwaterq@users.noreply.github.com <coldwaterq@users.noreply.github.com>
    
    * updated IPC to remove approved classes, removed the serialization function because it didn't work for all objects that made debugging harder, added tests.
    
    Signed-off-by: coldwaterq@users.noreply.github.com <coldwaterq@users.noreply.github.com>
    
    * removed LLM arg and moved class registration to a serialization module function. Also added missing classes to approved list.
    
    Signed-off-by: coldwaterq <coldwaterq@users.noreply.github.com>
    
    * cleaned up a couple files to reduce conflicts with main.
    
    Signed-off-by: coldwaterq <coldwaterq@users.noreply.github.com>
    
    * fix unit tests
    
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    
    * reorder BASE_ZMQ_CLASSES list alphabetically
    
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    
    * fix tests and move LogitsProcessor registration to base class
    
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    
    * revert changes to import log of tensorrt_llm._torch.models
    
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    
    * added comments to explain why BASE_ZMQ_CLASSES has to be passed into spawned child processes
    
    Signed-off-by: coldwaterq <coldwaterq@users.noreply.github.com>
    
    * fix tests and move LogitsProcessor registration to base class
    
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    
    * additional comments for multiprocess approved list sync
    
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    
    * add dataclass from tests
    
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: coldwaterq@users.noreply.github.com <coldwaterq@users.noreply.github.com>
    Signed-off-by: coldwaterq <coldwaterq@users.noreply.github.com>
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    Co-authored-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
    Signed-off-by: darraghdog <darragh.hanley@gmail.com>

--- tensorrt_llm/executor/worker.py
+++ tensorrt_llm/executor/worker.py
@@ -12,6 +12,7 @@ from typing import Callable, Dict, List, Optional, Tuple, Union
 
 import torch
 
+import tensorrt_llm.executor.serialization as serialization
 from tensorrt_llm.logger import logger
 
 from .._utils import (KVCacheEventSerializer, global_mpi_rank, mpi_comm,
@@ -584,7 +585,11 @@ def worker_main(
     is_llm_executor: Optional[
         bool] = True,  # whether it's the main executor instance
     lora_config: Optional[LoraConfig] = None,
+    BASE_ZMQ_CLASSES: Dict = serialization.BASE_ZMQ_CLASSES,
 ) -> None:
+    # The base classes for ZMQ serialization. Passed through from the parent process to ensure
+    # that children processes include any classes added at runtime (such as those from `register_approved_ipc_class`).
+    serialization.BASE_ZMQ_CLASSES = BASE_ZMQ_CLASSES
     mpi_comm().barrier()
     print_colored_debug(f"Worker {mpi_rank()} entering worker_main...\n",
                         "green")
@@ -679,12 +684,11 @@ def worker_main(
         assert isinstance(proxy_result_queue, tuple)
         for i in range(postproc_worker_config.num_postprocess_workers):
             fut = postproc_worker_pool.submit(
-                postproc_worker_main,
-                result_queues[i].address,
+                postproc_worker_main, result_queues[i].address,
                 proxy_result_queue,
                 postproc_worker_config.postprocess_tokenizer_dir,
                 PostprocWorker.default_record_creator,
-            )
+                serialization.BASE_ZMQ_CLASSES)
             postprocess_worker_futures.append(fut)
 
     # Error handling in the Worker/MPI process
