4af884a668b9cb41913c5ce4cda7e229d58ebded
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2025-05-26 03:39:09 +0000

    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
    add build_config to llm_args for trt backend

--- tensorrt_llm/commands/serve.py
+++ tensorrt_llm/commands/serve.py
@@ -41,7 +41,10 @@ def get_llm_args(model: str,
         gpus_per_node = device_count()
         if gpus_per_node == 0:
             raise ValueError("No GPU devices found on the node")
-
+    build_config = BuildConfig(max_batch_size=max_batch_size,
+                               max_num_tokens=max_num_tokens,
+                               max_beam_width=max_beam_width,
+                               max_seq_len=max_seq_len)
     kv_cache_config = KvCacheConfig(
         free_gpu_memory_fraction=free_gpu_memory_fraction)
 
@@ -64,6 +67,7 @@ def get_llm_args(model: str,
         "moe_expert_parallel_size": moe_expert_parallel_size,
         "gpus_per_node": gpus_per_node,
         "trust_remote_code": trust_remote_code,
+        "build_config": build_config,
         "max_batch_size": max_batch_size,
         "max_num_tokens": max_num_tokens,
         "max_beam_width": max_beam_width,
