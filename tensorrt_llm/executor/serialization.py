0dfc68bacde742d7615d37664d61dc6a94aa20bb
Tailing Yuan <yuantailing@gmail.com>
2025-05-27 07:50:29 +0000

    fix serialization
    Signed-off-by: Tailing Yuan <yuantailing@gmail.com>

--- tensorrt_llm/executor/serialization.py
+++ tensorrt_llm/executor/serialization.py
@@ -48,6 +48,7 @@ BASE_ZMQ_CLASSES = {
     "tensorrt_llm._torch.models.modeling_siglip": ["SiglipVisionModel"],
     "tensorrt_llm._torch.models.modeling_vila": ["VilaModel"],
     ### ending import of torch models classes
+    "tensorrt_llm._torch.pyexecutor.config": ["PyTorchConfig", "LoadFormat"],
     "tensorrt_llm._torch.pyexecutor.llm_request":
     ["LogitsStorage", "PyResult", "LlmResult", "LlmResponse", "LogProbStorage"],
     "tensorrt_llm.auto_parallel.config": ["AutoParallelConfig", "CostModel"],
