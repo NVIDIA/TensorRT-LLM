0618bfceee5ba5a2a5f3cd8e62e1399d3ef6d0b2
Izzy Putterman <iputterman@nvidia.com>
2025-05-29 08:12:25 -0700

    formatting
    Signed-off-by: Izzy Putterman <iputterman@nvidia.com>

--- tensorrt_llm/llmapi/__init__.py
+++ tensorrt_llm/llmapi/__init__.py
@@ -3,12 +3,14 @@ from ..executor import CompletionOutput, RequestError
 from ..sampling_params import GuidedDecodingParams, SamplingParams
 from .build_cache import BuildCacheConfig
 from .llm import LLM, RequestOutput
-from .llm_args import (
-    BatchingType, CacheTransceiverConfig, CalibConfig, CapacitySchedulerPolicy,
-    ContextChunkingPolicy, DraftTargetDecodingConfig, DynamicBatchConfig,
-    EagleDecodingConfig, ExtendedRuntimePerfKnobConfig, KvCacheConfig, LlmArgs,
-    LookaheadDecodingConfig, MedusaDecodingConfig, MTPDecodingConfig,
-    NGramDecodingConfig, SchedulerConfig, TorchLlmArgs, TrtLlmArgs)
+from .llm_args import (BatchingType, CacheTransceiverConfig, CalibConfig,
+                       CapacitySchedulerPolicy, ContextChunkingPolicy,
+                       DraftTargetDecodingConfig, DynamicBatchConfig,
+                       EagleDecodingConfig, ExtendedRuntimePerfKnobConfig,
+                       KvCacheConfig, LlmArgs, LookaheadDecodingConfig,
+                       MedusaDecodingConfig, MTPDecodingConfig,
+                       NGramDecodingConfig, SchedulerConfig, TorchLlmArgs,
+                       TrtLlmArgs)
 from .llm_utils import (BuildConfig, KvCacheRetentionConfig, QuantAlgo,
                         QuantConfig)
 from .mpi_session import MpiCommSession
