d7b82ea07481d7f2ee5b019076dede2d871113f5
Yan Chunwei <328693+Superjomn@users.noreply.github.com>
2025-05-29 10:32:35 +0800

    chore: rename ExecutorBindingsWorker/Proxy (#4716)
    Signed-off-by: Superjomn <328693+Superjomn@users.noreply.github.com>
    Signed-off-by: darraghdog <darragh.hanley@gmail.com>

--- tensorrt_llm/executor/serialization.py
+++ tensorrt_llm/executor/serialization.py
@@ -77,7 +77,7 @@ BASE_ZMQ_CLASSES = {
         "Logprob", "LogProbsResult", "ResponseWrapper"
     ],
     "tensorrt_llm.executor.utils": ["ErrorResponse", "WorkerCommIpcAddrs"],
-    "tensorrt_llm.executor.worker": ["ExecutorBindingsWorker", "worker_main"],
+    "tensorrt_llm.executor.worker": ["GenerationExecutorWorker", "worker_main"],
     "tensorrt_llm.llmapi.llm_args": [
         "_ModelFormatKind", "_ParallelConfig", "CalibConfig",
         "CapacitySchedulerPolicy", "KvCacheConfig", "LookaheadDecodingConfig",
