3323f4de151a95f009bae33e809d1af2e7a774d3
Jin Li <59594262+liji-nv@users.noreply.github.com>
2025-05-30 04:13:59 -0700

    [fix] Do not sreuse dummy request KVCache
    Signed-off-by: Jin Li <59594262+liji-nv@users.noreply.github.com>

--- cpp/tensorrt_llm/batch_manager/kvCacheManager.cpp
+++ cpp/tensorrt_llm/batch_manager/kvCacheManager.cpp
@@ -1436,7 +1436,9 @@ void BlockManager::releaseBlocks(GenerationRequest& sequence, OptionalRef<LlmReq
     // the max attention window).
     // - The sequence did not switch to cyclic kv-cache during generation phase.
     //  A sequence is cyclic if its *minimum window size* is crossed, even if other window sizes were not reached.
-    bool const storeBlocksForReuse = sequence.getBeamWidth() == 1 && llmRequest.has_value() && !sequence.isCyclic();
+    // - The sequence is not a dummy request.
+    bool const storeBlocksForReuse = sequence.getBeamWidth() == 1 && llmRequest.has_value() && !sequence.isCyclic()
+        && !llmRequest->isDummyRequest();
     for (auto& [_, manager] : mWindowBlockManagers)
     {
         if (storeBlocksForReuse)
@@ -1988,7 +1990,7 @@ void KVCacheManager::storeContextBlocks(LlmRequest const& llmRequest)
 {
     auto const requestId = llmRequest.mRequestId;
     auto& sequence = getSequence(requestId);
-    if (mEnableBlockReuse && !sequence.isCyclic())
+    if (mEnableBlockReuse && !sequence.isCyclic() && !llmRequest.isDummyRequest())
     {
         mBlockManager.storeContextBlocks(sequence, llmRequest);
     }
