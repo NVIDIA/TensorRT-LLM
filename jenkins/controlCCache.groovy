0fe77fc3741f65db9b1d88d3b1546eeb6710d089
ShiXiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
2025-06-02 10:59:54 +0000

    update images
    Signed-off-by: ShiXiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>

--- jenkins/controlCCache.groovy
+++ jenkins/controlCCache.groovy
@@ -1,7 +1,7 @@
 
 import java.lang.InterruptedException
 
-DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:pytorch-25.04-py3-x86_64-ubuntu24.04-trt10.10.0.31-skip-tritondevel-202506011326-9420"
+DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:pytorch-25.04-py3-x86_64-ubuntu24.04-trt10.10.0.31-skip-tritondevel-202506021004-9420"
 
 def createKubernetesPodConfig(image)
 {
