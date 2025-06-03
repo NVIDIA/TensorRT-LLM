77585698c890eb7129b81fc02cb3a5fcca4578b9
Amit Zuker <203509407+amitz-nv@users.noreply.github.com>
2025-06-03 14:54:57 +0000

    Simplified code, improved tests to validate gen logits data, added disabled overlap scheduler test case
    Signed-off-by: Amit Zuker <203509407+amitz-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/handle_generation_logits.py
+++ tensorrt_llm/_torch/pyexecutor/handle_generation_logits.py
@@ -18,21 +18,13 @@ class HandleGenerationLogits:
             beam_width = llm_req.get_beam_width_by_iter()
             seq_slot = llm_req.seq_slot
 
-            # logits_view shape: [beamWidth, 1, vocabSize] - that 1 in the middle is added from unsqueeze in sample_async
+            # logits_view shape: [beamWidth, vocabSize]
             logits_view = logits[logits_index:logits_index + beam_width]
 
-            # if beam_width > 1:
-            #    decoder_buffer_logits[seq_slot] = logits_view
-            #    decoder_buffer_logits[seq_slot].unsqueeze(0)
-            # else:
-            #    print(f"{seq_slot=}")
-            #    decoder_buffer_logits[seq_slot] = logits_view[:logits_view.shape[0], :1, :logits_view.shape[1]]
-
-            decoder_buffer_logits[seq_slot] = logits_view
-
             if beam_width > 1:
-                # TODO: Why is this necessary?
-                decoder_buffer_logits.unsqueeze(0)
+                decoder_buffer_logits[seq_slot] = logits_view.unsqueeze(0)
+            else:
+                decoder_buffer_logits[seq_slot] = logits_view.unsqueeze(1)
 
             if llm_req.py_return_generation_logits:
                 llm_req.py_result.append_generation_logits(logits_view)
