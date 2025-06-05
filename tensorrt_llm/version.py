21541f589632b04bde8fb68abe032853bca66fa2
Marek Wawrzos <mwawrzos@nvidia.com>
2025-06-05 08:52:17 -0700

    update verions

--- tensorrt_llm/version.py
+++ tensorrt_llm/version.py
@@ -12,4 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "0.18.2+98c1ecd9"
+__version__ = "0.18.2+2d8bfc2f"
