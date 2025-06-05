f612a9dbd7353b47c30dc6d722a53291e8afec84
Hui Gao <huig@nvidia.com>
2025-06-05 10:24:48 +0000

    Add back change missed during rebase
    Signed-off-by: Hui Gao <huig@nvidia.com>

--- tensorrt_llm/llmapi/llm_args.py
+++ tensorrt_llm/llmapi/llm_args.py
@@ -958,12 +958,12 @@ class BaseLlmArgs(BaseModel):
         exclude=True,  # exclude from serialization
         alias="_mpi_session")
 
-    allreduce_strategy: Optional[str] = Field(
-        default="AUTO",
-        description="The optional AllReduce strategy to use.",
-        json_schema_extra={"type": "Optional[str]"},
-        exclude=True,  # exclude from serialization
-    )
+    allreduce_strategy: Optional[
+        Literal["AUTO", "NCCL", "UB", "MINLATENCY", "ONESHOT", "TWOSHOT",
+                "LOWPRECISION", "MNNVL"]] = Field(
+                    default="AUTO",
+                    description="The optional AllReduce strategy to use.",
+                )
 
     @print_traceback_on_error
     def model_post_init(self, __context: Any):
