646d7889e9461c27cab660f00c51fd1a06cab0ab
qqiao <qqiao@nvidia.com>
2025-06-05 02:21:12 -0700

    Fix comments and update image tag
    Signed-off-by: qqiao <qqiao@nvidia.com>

--- docker/common/install_tensorrt.sh
+++ docker/common/install_tensorrt.sh
@@ -9,8 +9,8 @@ CUDA_VER="12.9" # 12.9.0
 # Keep the installation for cuDNN if users want to install PyTorch with source codes.
 # PyTorch 2.x can compile with cuDNN v9.
 CUDNN_VER="9.10.1.4-1"
-# NCCL version 2.26.5-1+cuda12.9 used in the NGC PyTorch 25.05 image but not existing in public.
-# There is a perf regression, we have to downgrade NCCL version to 2.25.1-1+cuda12.8 instead.
+# NCCL version 2.26.x used in the NGC PyTorch 25.05 image but has a performance regression issue.
+# Use NCCL version 2.25.1 instead.
 NCCL_VER="2.25.1-1+cuda12.8"
 # cuBLAS version 12.9.0.13 used in the NGC PyTorch 25.05 image but not existing in public.
 # Use cuBLAS version 12.9.0.13 instead.
