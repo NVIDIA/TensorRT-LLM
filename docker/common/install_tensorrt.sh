f9082a71681ec7ad16f2b59d07cd1c5c5d2f0653
Yiqing Yan <yiqingy@nvidia.com>
2025-06-05 14:03:39 +0800

    Downgrade NCCL version from 2.26.5 to 2.25.1 (#4931)
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>

--- docker/common/install_tensorrt.sh
+++ docker/common/install_tensorrt.sh
@@ -9,9 +9,9 @@ CUDA_VER="12.9" # 12.9.0
 # Keep the installation for cuDNN if users want to install PyTorch with source codes.
 # PyTorch 2.x can compile with cuDNN v9.
 CUDNN_VER="9.9.0.52-1"
-# NCCL version 2.26.3 used in the NGC PyTorch 25.04 image but not existing in public.
-# Use NCCL version 2.26.5 instead.
-NCCL_VER="2.26.5-1+cuda12.9"
+# NCCL version 2.26.x used in the NGC PyTorch 25.04 image but has a performance regression issue.
+# Use NCCL version 2.25.1 instead.
+NCCL_VER="2.25.1-1+cuda12.8"
 # cuBLAS version 12.9.0.2 used in the NGC PyTorch 25.04 image but not existing in public.
 # Use cuBLAS version 12.9.0.13 instead.
 CUBLAS_VER="12.9.0.13-1"
