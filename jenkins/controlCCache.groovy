1e369658f12695ba14f284f7d2557c9bfda0f168
dongxuy04 <78518666+dongxuy04@users.noreply.github.com>
2025-06-08 10:25:18 +0800

    feat: large-scale EP(part 6: Online EP load balancer integration for GB200 nvfp4) (#4818)
    Signed-off-by: Dongxu Yang <78518666+dongxuy04@users.noreply.github.com>
    Signed-off-by: ShiXiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>
    Co-authored-by: ShiXiaowei02 <39303645+Shixiaowei02@users.noreply.github.com>

--- jenkins/controlCCache.groovy
+++ jenkins/controlCCache.groovy
@@ -1,7 +1,7 @@
 
 import java.lang.InterruptedException
 
-DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:pytorch-25.04-py3-x86_64-ubuntu24.04-trt10.10.0.31-skip-tritondevel-202505211401-4539"
+DOCKER_IMAGE = "urm.nvidia.com/sw-tensorrt-docker/tensorrt-llm:pytorch-25.04-py3-x86_64-ubuntu24.04-trt10.10.0.31-skip-tritondevel-202506021004-9420"
 
 def createKubernetesPodConfig(image)
 {
