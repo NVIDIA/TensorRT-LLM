d7810e09a42658f151db21507d3e387b7e1c93c0
Mingyang Jiang <13463932+jmydurant@users.noreply.github.com>
2025-06-11 17:04:40 +0800

    fix: fix bug when attn softmax stats max is not multiplied by bmm scale
    Signed-off-by: Mingyang Jiang <13463932+jmydurant@users.noreply.github.com>

--- cpp/tests/unit_tests/kernels/mlaChunkedPrefillTest.cu
+++ cpp/tests/unit_tests/kernels/mlaChunkedPrefillTest.cu
@@ -123,6 +123,7 @@ void selfAttentionRef(T* output, T* const Q, T* const KV, int batch_size, int nu
         int global_q_offset = cu_seq_q_len[b] * num_heads * head_size;
         int global_kv_offset = cu_seq_kv_len[b] * 2 * num_heads * head_size;
         int global_softmax_offset = cu_seq_q_len[b] * num_heads * 2;
+        float bmm1_scale = 1.F / std::sqrt(static_cast<float>(head_size));
         if (curr_q_len == 0 || curr_kv_len == 0)
         {
             continue; // skip empty sequences
@@ -177,7 +178,9 @@ void selfAttentionRef(T* output, T* const Q, T* const KV, int batch_size, int nu
                 float sum = 0;
                 for (int s_kv = 0; s_kv < curr_kv_len; s_kv++)
                 {
-                    P[s_q * curr_kv_len + s_kv] = std::exp(P[s_q * curr_kv_len + s_kv]);
+                    // P[s_q * curr_kv_len + s_kv] = std::exp(P[s_q * curr_kv_len + s_kv] * bmm1_scale);
+                    // hack for real mla kernel
+                    P[s_q * curr_kv_len + s_kv] = std::exp(P[s_q * curr_kv_len + s_kv] * 0.072168784);
                     sum += P[s_q * curr_kv_len + s_kv];
                 }
                 for (int s_kv = 0; s_kv < curr_kv_len; s_kv++)
@@ -914,8 +917,8 @@ protected:
     }
 };
 
-// using MLATypes = ::testing::Types<half, __nv_bfloat16, float>;
-using MLATypes = ::testing::Types<half>;
+using MLATypes = ::testing::Types<half, __nv_bfloat16, float>;
+
 TYPED_TEST_SUITE(MlaChunkedPrefillTest, MLATypes);
 
 TYPED_TEST(MlaChunkedPrefillTest, MlaChunkedPrefillDefault)
