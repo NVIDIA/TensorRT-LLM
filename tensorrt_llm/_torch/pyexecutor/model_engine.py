60db2d6a6192e0dd0ac0f859043f4c1ac99546bf
Zheng Duan <200704041+zhengd-nv@users.noreply.github.com>
2025-06-11 01:21:36 +0000

    handling no files to load
    Signed-off-by: Zheng Duan <200704041+zhengd-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/model_engine.py
+++ tensorrt_llm/_torch/pyexecutor/model_engine.py
@@ -155,6 +155,8 @@ def prefetch_files(file_names: List[str]):
     # Find out the files to prefetch for the current rank.
     # Each rank loads files with indices local_rank, local_rank + local_mpi_size, local_rank + 2*local_mpi_size, etc.
     local_file_names = file_names[local_mpi_rank()::local_mpi_size()]
+    if len(local_file_names) == 0:
+        return
 
     max_processes = min(multiprocessing.cpu_count() * 2, 16,
                         len(local_file_names))
