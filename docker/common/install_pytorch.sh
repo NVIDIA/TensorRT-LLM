f170ad5962bdb2d971f8efc5ba439b2da8d1e283
qqiao <qqiao@nvidia.com>
2025-06-11 19:42:44 -0700

    Update doc and waive P1 bug related cases
    Signed-off-by: qqiao <qqiao@nvidia.com>

--- docker/common/install_pytorch.sh
+++ docker/common/install_pytorch.sh
@@ -4,8 +4,8 @@ set -ex
 
 # Use latest stable version from https://pypi.org/project/torch/#history
 # and closest to the version specified in
-# https://docs.nvidia.com/deeplearning/frameworks/pytorch-release-notes/rel-25-04.html#rel-25-04
-TORCH_VERSION="2.7.0"
+# https://docs.nvidia.com/deeplearning/frameworks/pytorch-release-notes/rel-25-05.html#rel-25-05
+TORCH_VERSION="2.7.1"
 SYSTEM_ID=$(grep -oP '(?<=^ID=).+' /etc/os-release | tr -d '"')
 
 prepare_environment() {
