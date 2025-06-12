e372a8ef293f8afb65394d6c1045da0d3997dfc5
Enwei Zhu <21126786+syuoni@users.noreply.github.com>
2025-06-12 11:05:28 +0000

    add json_mode_eval
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>

--- tensorrt_llm/evaluate/mmlu.py
+++ tensorrt_llm/evaluate/mmlu.py
@@ -190,7 +190,7 @@ class MMLU(Evaluator):
                                                  include_answer=False)
                 prompt = train_prompt + prompt_end
                 label = test_df.iloc[i, test_df.shape[1] - 1]
-                yield prompt, label, subject
+                yield prompt, None, label, subject
 
     def compute_score(self, outputs: List[RequestOutput], references: List[str],
                       subjects: List[str]) -> float:
