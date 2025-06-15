87766ca5a85d7d1c8117499ce13a165f569346c7
Shahar Mor <17088876+shaharmor98@users.noreply.github.com>
2025-06-15 05:06:19 +0000

    fix
    Signed-off-by: Shahar Mor <17088876+shaharmor98@users.noreply.github.com>

--- lora_profile_tp1_scenario3.sh
+++ lora_profile_tp1_scenario3.sh
@@ -118,7 +118,6 @@ if [ ! -f "${EG_DIR}/data/token-norm-dist-with-lora.json" ]; then
         --num-requests ${NUM_REQUESTS} \
         --input-mean ${ISL} --input-stdev 0 \
         --output-mean ${OSL} --output-stdev 0 \
-        --dataset-meta-data lora_task_id=0
     echo "Dataset prepared"
 else
     echo "Dataset already exists, skipping"
