68a9786056d60d914e36479753bcd10b1c347600
Yanchao Lu <yanchaol@nvidia.com>
2025-06-17 20:39:50 +0800

    Revert temp changes
    Signed-off-by: Yanchao Lu <yanchaol@nvidia.com>

--- jenkins/L0_MergeRequest.groovy
+++ jenkins/L0_MergeRequest.groovy
@@ -990,12 +990,10 @@ def launchStages(pipeline, reuseBuild, testFilter, enableFailFast, globalVars)
                     }
                 }
                 stage(testStageName) {
-                    /*
                     if (SBSA_TEST_CHOICE == STAGE_CHOICE_SKIP) {
                         echo "SBSA test job is skipped due to Jenkins configuration"
                         return
                     }
-                    */
                     try {
                         def parameters = getCommonParameters()
 
