39beaf35adb4cd7be1d01b7699413f7fae7373b3
bhsueh <11360707+byshiue@users.noreply.github.com>
2025-06-18 09:44:59 +0000

    fix issue of pre-commit
    Signed-off-by: bhsueh <11360707+byshiue@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api_pytorch.py
+++ tests/integration/defs/accuracy/test_llm_api_pytorch.py
@@ -1360,12 +1360,16 @@ class TestQwen3_30B_A3B(LlmapiAccuracyTestHarness):
     @skip_pre_blackwell
     @pytest.mark.parametrize(
         "tp_size,pp_size,ep_size,attention_dp,cuda_graph,overlap_scheduler,moe_backend",
-        [(1, 1, 1, True, True, True, "CUTLASS"),
-         (1, 1, 1, False, True, True, "TRTLLM"),
-         (4, 1, 4, False, True, True, "CUTLASS"),
-         (4, 1, 4, False, True, True, "TRTLLM"),
-         ],
-        ids=["latency_moe_cutlass", "latency_moe_trtllm", "4gpu_latency_moe_trtllm", "4gpu_latency_moe_cutlass"],
+        [
+            (1, 1, 1, True, True, True, "CUTLASS"),
+            (1, 1, 1, False, True, True, "TRTLLM"),
+            (4, 1, 4, False, True, True, "CUTLASS"),
+            (4, 1, 4, False, True, True, "TRTLLM"),
+        ],
+        ids=[
+            "latency_moe_cutlass", "latency_moe_trtllm",
+            "4gpu_latency_moe_trtllm", "4gpu_latency_moe_cutlass"
+        ],
     )
     def test_nvfp4(
         self,
