0c02df25b989ec105979819e986b070783792a8a
Daniel Campora <961215+dcampora@users.noreply.github.com>
2025-06-18 11:01:00 +0000

    Added missing file.
    Signed-off-by: Daniel Campora <961215+dcampora@users.noreply.github.com>

--- cpp/include/tensorrt_llm/batch_manager/llmRequest.h
+++ cpp/include/tensorrt_llm/batch_manager/llmRequest.h
@@ -1769,7 +1769,7 @@ public:
     {
         if (finishReason == executor::FinishReason::kTIMED_OUT)
         {
-            printf("Request %ld finished by timeout after %f sec\n", mRequestId,
+            TLLM_LOG_DEBUG("Request %ld finished by timeout after %f sec", mRequestId,
                 std::chrono::duration<float>(std::chrono::steady_clock::now() - mStartTime).count());
         }
         if (finishReason == executor::FinishReason::kCANCELLED)
