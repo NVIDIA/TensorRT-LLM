01300c8b1d7cd74a1d52e4f8aa1c06134db63298
Tracin <10434017+Tracin@users.noreply.github.com>
2025-06-19 03:21:21 -0700

    Support MXFP4 moe.
    Signed-off-by: Tracin <10434017+Tracin@users.noreply.github.com>

--- tensorrt_llm/_torch/modules/fused_moe/interface.py
+++ tensorrt_llm/_torch/modules/fused_moe/interface.py
@@ -124,6 +124,12 @@ class MoE(nn.Module):
         )
 
     @property
+    def has_w4a8_mxfp4_fp8(self):
+        assert self._weights_created
+        return self.quant_config is not None and self.quant_config.layer_quant_mode.has_w4a8_mxfp4_fp8(
+        )
+
+    @property
     def enable_alltoall(self):
         """ enable_alltoall (bool): whether to enable alltoall instead of allgather/reducescatter
         """
