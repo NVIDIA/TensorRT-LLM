3ec8d8f01b53b1c85444aaff31c1ee87854a6a16
Omer Ullman Argov <118735753+omera-nv@users.noreply.github.com>
2025-06-20 23:26:36 +0300

    increase batch size
    Signed-off-by: Omer Ullman Argov <118735753+omera-nv@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_cli_flow.py
+++ tests/integration/defs/accuracy/test_cli_flow.py
@@ -419,7 +419,7 @@ class TestVicuna7B(CliFlowAccuracyTestHarness):
     @parametrize_with_ids("cuda_graph", [False, True])
     def test_medusa(self, cuda_graph, mocker):
         mocker.patch.object(self.__class__, "EXAMPLE_FOLDER", "medusa")
-        mocker.patch.object(CnnDailymail, "MAX_BATCH_SIZE", 8)
+        #mocker.patch.object(CnnDailymail, "MAX_BATCH_SIZE", 8)
 
         extra_summarize_args = [
             "--medusa_choices=[[0], [0, 0], [1], [0, 1], [2], [0, 0, 0], [1, 0], [0, 2], [3], [0, 3], [4], [0, 4], [2, 0], [0, 5], [0, 0, 1], [5], [0, 6], [6], [0, 7], [0, 1, 0], [1, 1], [7], [0, 8], [0, 0, 2], [3, 0], [0, 9], [8], [9], [1, 0, 0], [0, 2, 0], [1, 2], [0, 0, 3], [4, 0], [2, 1], [0, 0, 4], [0, 0, 5], [0, 0, 0, 0], [0, 1, 1], [0, 0, 6], [0, 3, 0], [5, 0], [1, 3], [0, 0, 7], [0, 0, 8], [0, 0, 9], [6, 0], [0, 4, 0], [1, 4], [7, 0], [0, 1, 2], [2, 0, 0], [3, 1], [2, 2], [8, 0], [0, 5, 0], [1, 5], [1, 0, 1], [0, 2, 1], [9, 0], [0, 6, 0], [0, 0, 0, 1], [1, 6], [0, 7, 0]]"
