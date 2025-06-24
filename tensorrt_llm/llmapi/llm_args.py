53a7adf28f1e1de5a224e85f71acd18101d57195
QI JUN <22017000+QiJune@users.noreply.github.com>
2025-06-24 11:40:28 +0800

    split _build_model method for TorchLlm and TrtLlm
    Signed-off-by: QI JUN <22017000+QiJune@users.noreply.github.com>

--- tensorrt_llm/llmapi/llm_args.py
+++ tensorrt_llm/llmapi/llm_args.py
@@ -954,12 +954,6 @@ class BaseLlmArgs(BaseModel):
         default=None,
         description="The parser to separate reasoning content from output.")
 
-    garbage_collection_gen0_threshold: int = Field(
-        default=20000,
-        description=
-        "Threshold for Python garbage collection of generation 0 objects."
-        "Lower values trigger more frequent garbage collection.")
-
     # TODO[Superjomn]: To deprecate this config.
     decoding_config: Optional[object] = Field(
         default=None,
@@ -1622,14 +1616,11 @@ class TorchCompileConfig(BaseModel):
 
 class TorchLlmArgs(BaseLlmArgs):
 
-    # Just a dummy BuildConfig to allow code reuse with the TrtLlmArgs
-    build_config: Optional[object] = Field(
-        default=None,
-        description="Build config.",
-        exclude_from_json=True,
-        json_schema_extra={"type": f"Optional[{get_type_repr(BuildConfig)}]"})
-
-    # PyTorch backend specific configurations
+    garbage_collection_gen0_threshold: int = Field(
+        default=20000,
+        description=
+        "Threshold for Python garbage collection of generation 0 objects."
+        "Lower values trigger more frequent garbage collection.")
 
     use_cuda_graph: bool = Field(
         default=False,
