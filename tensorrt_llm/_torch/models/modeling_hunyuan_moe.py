f81cd35555cf5928f837e0fa391e61d1fc8ca15e
jesonxiang <jesonxiang@tencent.com>
2025-06-27 19:32:10 +0800

    rebase with main branch.

--- tensorrt_llm/_torch/models/modeling_hunyuan_moe.py
+++ tensorrt_llm/_torch/models/modeling_hunyuan_moe.py
@@ -12,10 +12,10 @@ from ..attention_backend import AttentionMetadata
 from ..attention_backend.interface import (PositionalEmbeddingParams,
                                            PredefinedAttentionMask, RopeParams)
 from ..model_config import ModelConfig
-from ..modules.attention import Attention, QkNormType
+from ..modules.attention import Attention
 from ..modules.decoder_layer import DecoderLayer
 from ..modules.embedding import Embedding
-from ..modules.fused_moe import RenormalizeMoeRoutingMethod, FusedMoE
+from ..modules.fused_moe import RenormalizeMoeRoutingMethod, CutlassFusedMoE, VanillaMoE, create_moe
 from ..modules.gated_mlp import GatedMLP
 from ..modules.linear import Linear, TensorParallelMode
 from ..modules.multi_stream_utils import maybe_execute_in_parallel
@@ -53,7 +53,7 @@ class HunyuanMoE(nn.Module):
 
         reduce_results = True
 
-        self.experts = FusedMoE(
+        self.experts = create_moe(
             num_experts=self.num_experts,
             routing_method=RenormalizeMoeRoutingMethod(top_k=self.top_k),
             hidden_size=self.hidden_dim,
@@ -112,6 +112,7 @@ class HunYuanAttention(Attention):
             rope=RopeParams.from_config(config),
             is_neox=True,
         ) if self.use_rope else None
+        self.use_qk_norm = use_qk_norm
 
         super().__init__(
             hidden_size=config.hidden_size,
@@ -120,8 +121,6 @@ class HunYuanAttention(Attention):
             max_position_embeddings=config.max_position_embeddings,
             bias=config.attention_bias,
             pos_embd_params=pos_embd_params,
-            qk_norm_type=QkNormType.post_rope
-            if use_qk_norm else QkNormType.none,
             layer_idx=layer_idx,
             dtype=config.torch_dtype,
             config=model_config,
@@ -136,6 +135,17 @@ class HunYuanAttention(Attention):
                                      dtype=config.torch_dtype)
         self.aux_stream = aux_stream
         self.ln_events = [torch.cuda.Event(), torch.cuda.Event()]
+
+    def apply_rope(self, q: torch.Tensor, k: Optional[torch.Tensor],
+                   v: Optional[torch.Tensor], position_ids: torch.Tensor):
+        q, k, v = self.split_qkv(q, k, v)
+        if position_ids is not None:
+            q, k, v = super().apply_rope(q, k, v, position_ids)
+        # Llama4 applies QK norm after RoPE.
+        if self.use_qk_norm:
+            q, k = self.apply_qk_norm(q, k)
+
+        return q, k, v
 
     def apply_qk_norm(self, q, k):
 
@@ -373,7 +383,7 @@ class HunYuanMoEV1ForCausalLM(DecoderModelForCausalLM[HunYuanModel,
                 else:
                     name = name.replace('gate', 'gate.wg')
                     module_weights = filter_weights(name, weights)
-                    if isinstance(module, FusedMoE):
+                    if isinstance(module, CutlassFusedMoE) or isinstance(module, VanillaMoE):
                         # model.layers.{idx}.mlp.experts
                         updated_module_weights = {}
                         for weight_name, weight_value in module_weights.items():
