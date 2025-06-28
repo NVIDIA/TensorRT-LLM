619709fc33bd5dc268f19d6a741fe7ed51c0f8f5
Lucas Liebenwein <11156568+lucaslie@users.noreply.github.com>
2025-06-28 22:52:14 +0300

    [AutoDeploy] merge feat/ad-2025-06-13 (#5556)
    Signed-off-by: Lucas Liebenwein <11156568+lucaslie@users.noreply.github.com>

--- tensorrt_llm/bench/dataclasses/configuration.py
+++ tensorrt_llm/bench/dataclasses/configuration.py
@@ -9,12 +9,13 @@ from pydantic import (BaseModel, Field, PositiveFloat, field_validator,
                       model_validator)
 
 import tensorrt_llm.bindings.executor as trtllm
+from tensorrt_llm._torch.auto_deploy import LlmArgs as _AutoDeployLlmArgs
 from tensorrt_llm._torch.pyexecutor.config import PyTorchConfig
 from tensorrt_llm.llmapi import (BatchingType, CapacitySchedulerPolicy,
                                  ContextChunkingPolicy, DynamicBatchConfig,
                                  ExtendedRuntimePerfKnobConfig, KvCacheConfig,
                                  SchedulerConfig)
-from tensorrt_llm.llmapi.llm_args import TorchCompileConfig, _AutoDeployLlmArgs
+from tensorrt_llm.llmapi.llm_args import TorchCompileConfig
 from tensorrt_llm.llmapi.llm_utils import update_llm_args_with_extra_options
 from tensorrt_llm.models.modeling_utils import SpeculativeDecodingMode
 
@@ -112,7 +113,7 @@ class PerformanceOptions:
 
     def get_autodeploy_perf_config(self) -> _AutoDeployLlmArgs:
         ad_config = _AutoDeployLlmArgs(**self.pytorch_config)
-        ad_config.attn_backend = "FlashInfer"
+        ad_config.attn_backend = "flashinfer"
         ad_config.torch_compile_config = TorchCompileConfig()
         ad_config.skip_loading_weights = True
         return ad_config
