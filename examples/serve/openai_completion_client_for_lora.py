7776bb8e3a70a75d50d1ea2f4768ce2e4f64dbb8
Talor Abramovich <talora@nvidia.com>
2025-06-29 07:40:50 +0000

    extract lora path in trtllm-serve test
    Signed-off-by: Talor Abramovich <talora@nvidia.com>

--- examples/serve/openai_completion_client_for_lora.py
+++ examples/serve/openai_completion_client_for_lora.py
@@ -10,20 +10,19 @@ client = OpenAI(
     api_key="tensorrt_llm",
 )
 
+lora_path = Path(
+    os.environ.get("LLM_MODELS_ROOT")) / "llama-models" / "luotuo-lora-7b-0.1"
+assert lora_path.exists(), f"Lora path {lora_path} does not exist"
+
 response = client.completions.create(
     model="llama-7b-hf",
     prompt="美国的首都在哪里? \n答案:",
     max_tokens=20,
     extra_body={
         "lora_request": {
-            "lora_name":
-            "luotuo-lora-7b-0.1",
-            "lora_int_id":
-            0,
-            "lora_path":
-            str(
-                Path(os.environ.get("LLM_MODELS_ROOT")) / "llm-models" /
-                "llama-models" / "luotuo-lora-7b-0.1")
+            "lora_name": "luotuo-lora-7b-0.1",
+            "lora_int_id": 0,
+            "lora_path": str(lora_path)
         }
     },
 )
