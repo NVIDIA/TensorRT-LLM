2a7642bdafb725f48cd817ffefd032483df86359
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2025-07-01 13:20:49 +0000

    cache_transceiver assert
    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/py_executor.py
+++ tensorrt_llm/_torch/pyexecutor/py_executor.py
@@ -1296,14 +1296,6 @@ class PyExecutor:
             # generation request. In IFB, we only get context request from request queue
             # In IFB, we only get context request from request queue
 
-            has_disagg_request = any(
-                req_item.request.request_type == RequestType.
-                REQUEST_TYPE_CONTEXT_ONLY or req_item.request.request_type ==
-                RequestType.REQUEST_TYPE_GENERATION_ONLY
-                for req_item in new_requests_cur_rank)
-            if has_disagg_request:
-                assert self.kv_cache_transceiver is not None, "kv_cache_transceiver is disabled, please set 'cache_transceiver_config: backend` in config file for disaggregated serving"
-
             if self.kv_cache_transceiver:
                 for req_item in new_requests_cur_rank:
                     if req_item.request.request_type == RequestType.REQUEST_TYPE_CONTEXT_ONLY:
