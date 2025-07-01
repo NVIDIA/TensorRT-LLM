bd917246a8e3379cc10a1f3b589ed72af4d1c1b8
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2025-07-01 01:27:19 -0700

    Add comment
    Signed-off-by: Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api_pytorch.py
+++ tests/integration/defs/accuracy/test_llm_api_pytorch.py
@@ -61,6 +61,10 @@ class TestLlama3_1_8B(LlmapiAccuracyTestHarness):
                              ids=["stream_interval_4", "stream_interval_64"])
     def test_nvfp4_streaming(self, stream_interval):
         model_path = f"{llm_models_root()}/nvfp4-quantized/Meta-Llama-3.1-8B"
+
+        # When stream_interval < 32, hf incremental detokenization is used.
+        # When stream_interval >= 32, trtllm implemented incremental detokenization is used.
+        # The behavior is due to perf considerations, while both paths need to be tested.
         with LLM(model_path, stream_interval=stream_interval) as llm:
             assert llm.args.quant_config.quant_algo == QuantAlgo.NVFP4
             assert llm.args.quant_config.kv_cache_quant_algo == QuantAlgo.FP8
