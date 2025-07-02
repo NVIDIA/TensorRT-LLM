40bb0f6e3770a281339c6427b02d6ae67ad11c85
tinyinl <tinyinl@nvidia.com>
2025-07-02 14:00:07 -0700

    clean unuse variable
    Signed-off-by: tinyinl <tinyinl@nvidia.com>

--- cpp/tensorrt_llm/common/attentionOp.cpp
+++ cpp/tensorrt_llm/common/attentionOp.cpp
@@ -2563,7 +2563,6 @@ int AttentionOp::initialize() noexcept
         fmhaParams.attnLogitSoftcappingScale = mAttnLogitSoftcappingScale;
         fmhaParams.hasAlibi = isALiBi();
         fmhaParams.scaleAlibi = isAliBiWithScale();
-        fmhaParams.dataTypeOut = data_type;
 
         // Load kernels from the pre-compiled cubins.
         mFmhaDispatcher.reset(new FmhaDispatcher(fmhaParams));
