c5377a86d11ee95fc937f7d88f0aecd0ba92fa77
Venky Ganesh <23023424+venkywonka@users.noreply.github.com>
2025-07-08 15:03:44 -0700

    touch some modules
    Signed-off-by: Venky Ganesh <23023424+venkywonka@users.noreply.github.com>

--- tensorrt_llm/_torch/models/modeling_deepseekv3.py
+++ tensorrt_llm/_torch/models/modeling_deepseekv3.py
@@ -24,7 +24,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 # --------------------------------------------------
-
+# touch
 import math
 import os
 import warnings
