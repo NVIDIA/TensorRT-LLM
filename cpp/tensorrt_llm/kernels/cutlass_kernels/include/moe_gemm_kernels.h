29f0edc32d043a2895d4e58d790feedb043c9709
zkyue <51477259+zkyue@users.noreply.github.com>
2025-07-09 18:25:52 +0800

    fix use_wfp4afp4 define error
    Signed-off-by: zkyue <51477259+zkyue@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/cutlass_kernels/include/moe_gemm_kernels.h
+++ cpp/tensorrt_llm/kernels/cutlass_kernels/include/moe_gemm_kernels.h
@@ -285,7 +285,7 @@ public:
 
 #if defined(ENABLE_FP4)
     static constexpr bool use_fp4 = std::is_same_v<T, __nv_fp4_e2m1>;
-    static constexpr bool use_wfp4afp4 = std::is_same_v<T, __nv_fp8_e4m3> && std::is_same_v<WeightType, __nv_fp4_e2m1>;
+    static constexpr bool use_wfp4afp4 = std::is_same_v<T, __nv_fp4_e2m1> && std::is_same_v<WeightType, __nv_fp4_e2m1>;
 #else
     static constexpr bool use_fp4 = false;
     static constexpr bool use_wfp4afp4 = false;
