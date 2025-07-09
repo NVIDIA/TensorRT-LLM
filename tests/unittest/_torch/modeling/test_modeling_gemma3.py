3e626ff9134adfe069552ed8844c232fdb04e239
brb-nv <169953907+brb-nv@users.noreply.github.com>
2025-07-09 14:18:04 -0700

    feat: Custom masking utils for Gemma3 VLM (#5853)
    Signed-off-by: Balaram Buddharaju <169953907+brb-nv@users.noreply.github.com>
    Signed-off-by: Yuxin <yuxinz@nvidia.com>

--- tests/unittest/_torch/modeling/test_modeling_gemma3.py
+++ tests/unittest/_torch/modeling/test_modeling_gemma3.py
@@ -9,6 +9,7 @@ from transformers import Gemma3ForCausalLM as HFGemma3ForCausalLM
 from transformers.cache_utils import HybridCache
 
 import tensorrt_llm
+from tensorrt_llm._torch.attention_backend import FlashInferAttentionMetadata
 from tensorrt_llm._torch.attention_backend.utils import get_attention_backend
 from tensorrt_llm._torch.metadata import KVCacheParams
 from tensorrt_llm._torch.model_config import ModelConfig
@@ -132,8 +133,8 @@ class TestGemma3(unittest.TestCase):
 
     def test_gemma3_sanity(self):
 
-        config_dict = deepcopy(
-            GEMMA3_1B_CONFIG)  # Using 1B config for sanity test.
+        # Using 1B config for sanity test.
+        config_dict = deepcopy(GEMMA3_1B_CONFIG)
         gemma3_config = Gemma3Config.from_dict(config_dict)
 
         dtype = gemma3_config.torch_dtype
@@ -303,16 +304,26 @@ class TestGemma3(unittest.TestCase):
         position_ids = [torch.arange(0, input_ids.size(-1), dtype=torch.int32)]
         position_ids = torch.cat(position_ids).unsqueeze(0).cuda()
 
+        # This helps us better test the custom masking utils for Gemma3 VLM as well
+        # as SWA plumbing for FlashInfer. All tokens being text tokens should yield
+        # same results as using global or local attention for appropriate layers.
+        if backend == "FLASHINFER":
+            image_token_mask = torch.tensor(
+                [False, False, False, False, False, False, False, False],
+                device=device)
+        else:
+            image_token_mask = None
+
         with torch.inference_mode():
             attn_metadata.prepare()
             logits = gemma3.forward(input_ids=input_ids,
                                     position_ids=position_ids,
-                                    attn_metadata=attn_metadata)
+                                    attn_metadata=attn_metadata,
+                                    image_token_mask=image_token_mask)
             ref = hf_gemma3.forward(input_ids=input_ids.unsqueeze(0),
                                     position_ids=position_ids,
                                     past_key_values=hf_cache,
                                     use_cache=True)
-
             torch.testing.assert_close(logits,
                                        ref.logits[:, -1].float(),
                                        atol=0.05,
@@ -358,3 +369,142 @@ class TestGemma3(unittest.TestCase):
                                        rtol=0.05)
 
         kv_cache_manager.shutdown()
+
+    def test_gemma3_flashinfer_mask(self):
+        config_dict = deepcopy(GEMMA3_1B_CONFIG)
+        gemma3_config = Gemma3Config.from_dict(config_dict)
+
+        dtype = gemma3_config.torch_dtype
+        device = torch.device('cuda')
+
+        model_config = ModelConfig(pretrained_config=gemma3_config,
+                                   attn_backend="FLASHINFER")
+        gemma3 = Gemma3ForCausalLM(model_config).to(device)
+
+        input_ids = torch.tensor([100, 200, 300, 400, 500, 600, 700, 800],
+                                 dtype=torch.int,
+                                 device=device)
+
+        context_sequence_lengths = [3, 2, 1]
+        sequence_lengths = context_sequence_lengths + [1, 1]
+        past_seen_tokens = [0, 0, 0, 2, 1]
+        request_ids = list(range(len(sequence_lengths)))
+        token_nums = (torch.tensor(past_seen_tokens) +
+                      torch.tensor(sequence_lengths)).tolist()
+        prompt_lens = token_nums[:3] + past_seen_tokens[3:]
+
+        num_blocks = 100
+        tokens_per_block = 128
+        max_seq_len = num_blocks * tokens_per_block
+        batch_size = len(context_sequence_lengths) + 2
+        kv_cache_manager = self.get_kv_cache_manager(
+            dtype=dtype,
+            config=gemma3_config,
+            tokens_per_block=tokens_per_block,
+            max_seq_len=max_seq_len,
+            batch_size=batch_size,
+            num_blocks=num_blocks)
+        kv_cache_manager.add_dummy_requests(request_ids, token_nums)
+
+        metadata_cls = get_attention_backend(model_config.attn_backend).Metadata
+        assert metadata_cls == FlashInferAttentionMetadata
+        attn_metadata = metadata_cls(
+            seq_lens=torch.tensor(sequence_lengths, dtype=torch.int),
+            num_contexts=len(context_sequence_lengths),
+            kv_cache_params=KVCacheParams(
+                use_cache=True,
+                num_cached_tokens_per_seq=past_seen_tokens,
+            ),
+            kv_cache_manager=kv_cache_manager,
+            request_ids=request_ids,
+            prompt_lens=prompt_lens,
+            max_num_requests=len(context_sequence_lengths) + 2,
+            max_num_tokens=8192,
+        )
+        attn_metadata.prepare()
+
+        # Test for case where all tokens are text tokens. `test_gemma3_global_mask` and `test_gemma3_local_mask`
+        # test for individual requests where there's a mix of text and image tokens.
+        image_token_mask = torch.tensor(
+            [False, False, False, False, False, False, False, False],
+            device=device)
+        causal_mask = gemma3.get_flashinfer_attention_mask(
+            image_token_mask=image_token_mask, attn_metadata=attn_metadata)
+        # Causal mask for context request 1.
+        ctx_request_1_mask = torch.tensor(
+            [[True, False, False], [True, True, False], [True, True, True]],
+            device=device)
+        # Causal mask for context request 2.
+        ctx_request_2_mask = torch.tensor([[True, False], [True, True]],
+                                          device=device)
+        # Causal mask for context request 3.
+        ctx_request_3_mask = torch.tensor([[True]], device=device)
+
+        expected_causal_mask = torch.cat([
+            ctx_request_1_mask.flatten(),
+            ctx_request_2_mask.flatten(),
+            ctx_request_3_mask.flatten(),
+        ],
+                                         dim=0)
+        torch.testing.assert_close(causal_mask, expected_causal_mask)
+
+    def test_gemma3_global_context_mask(self) -> None:
+        config_dict = deepcopy(GEMMA3_1B_CONFIG)
+        gemma3_config = Gemma3Config.from_dict(config_dict)
+        device = torch.device('cuda')
+        model_config = ModelConfig(pretrained_config=gemma3_config,
+                                   attn_backend="FLASHINFER")
+        gemma3 = Gemma3ForCausalLM(model_config).to(device)
+
+        image_token_mask = torch.tensor(
+            [False, False, True, True, True, True, False, False], device=device)
+        attention_mask = gemma3.get_context_mask(
+            image_token_mask=image_token_mask, effective_sliding_window=None)
+
+        # Text tokens attend to each other in causal fashion. Image tokens attend in causal fashion
+        # as well as to all other image tokens.
+        expected_attention_mask = torch.tensor(
+            [[True, False, False, False, False, False, False, False],
+             [True, True, False, False, False, False, False, False],
+             [True, True, True, True, True, True, False, False],
+             [True, True, True, True, True, True, False, False],
+             [True, True, True, True, True, True, False, False],
+             [True, True, True, True, True, True, False, False],
+             [True, True, True, True, True, True, True, False],
+             [True, True, True, True, True, True, True, True]],
+            device=device)
+        torch.testing.assert_close(attention_mask, expected_attention_mask)
+
+        # Even for a sliding window larger than sequence length, the attention mask is the same as the global mask.
+        attention_mask = gemma3.get_context_mask(
+            image_token_mask=image_token_mask, effective_sliding_window=10)
+        torch.testing.assert_close(attention_mask, expected_attention_mask)
+
+        # Even for sliding window same as sequence length, the attention mask is the same as the global mask.
+        attention_mask = gemma3.get_context_mask(
+            image_token_mask=image_token_mask, effective_sliding_window=8)
+        torch.testing.assert_close(attention_mask, expected_attention_mask)
+
+    def test_gemma3_local_context_mask(self) -> None:
+        config_dict = deepcopy(GEMMA3_1B_CONFIG)
+        gemma3_config = Gemma3Config.from_dict(config_dict)
+        device = torch.device('cuda')
+        model_config = ModelConfig(pretrained_config=gemma3_config,
+                                   attn_backend="FLASHINFER")
+        gemma3 = Gemma3ForCausalLM(model_config).to(device)
+
+        image_token_mask = torch.tensor(
+            [False, False, True, True, True, True, False, False], device=device)
+        attention_mask = gemma3.get_context_mask(
+            image_token_mask=image_token_mask, effective_sliding_window=2)
+        expected_attention_mask = torch.tensor(
+            [[True, False, False, False, False, False, False, False],
+             [True, True, False, False, False, False, False, False],
+             [False, True, True, True, True, True, False, False],
+             [False, False, True, True, True, True, False, False],
+             [False, False, True, True, True, True, False, False],
+             [False, False, True, True, True, True, False, False],
+             [False, False, False, False, False, True, True, False],
+             [False, False, False, False, False, False, True, True]],
+            device=device)
+        torch.testing.assert_close(attention_mask, expected_attention_mask)
