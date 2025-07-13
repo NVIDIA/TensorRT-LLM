2e1c55e3ceee0824fc3b3a9069a41ee1c7a0bc92
Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>
2025-07-13 10:25:57 -0700

    test 007 node
    Signed-off-by: Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1,4 +1,4 @@
-@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@main']) _
+@Library(['bloom-jenkins-shared-lib@dev-yuanjingx-add_back_007_node_in_b200_slurm_job', 'trtllm-jenkins-shared-lib@main']) _
 
 import java.lang.InterruptedException
 import groovy.transform.Field
