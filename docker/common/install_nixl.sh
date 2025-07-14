ee00517edcc6898e12993d17e3015a09ba06ce65
Bo Deng <deemod@nvidia.com>
2025-07-14 03:33:36 +0000

    Update install_nixl.sh
    Signed-off-by: Bo Deng <deemod@nvidia.com>

--- docker/common/install_nixl.sh
+++ docker/common/install_nixl.sh
@@ -24,12 +24,15 @@ if [[ -z "$cuda_path" ]]; then
     exit 1
 fi
 
+ln -sf $cuda_path $CUDA_PATH/lib64/libcuda.so.1
+
 meson setup builddir \
     -Ducx_path=$UCX_INSTALL_PATH \
     -Dcudapath_lib="$CUDA_PATH/lib64" \
     -Dcudapath_inc="$CUDA_PATH/include" \
     -Dgds_path="$GDS_PATH" \
-    -Dinstall_headers=true
+    -Dinstall_headers=true \
+    -Dstatic_plugins=UCX
 
 cd builddir && ninja install
 cd ../..
