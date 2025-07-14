3c69487bc5bd6d750f8355afa6c3a8d444cb7064
Daniel Afrimi <danielafrimi8@gmail.com>
2025-07-14 13:59:26 +0000

    rebased
    Signed-off-by: Daniel Afrimi <danielafrimi8@gmail.com>

--- tensorrt_llm/bench/benchmark/utils/general.py
+++ tensorrt_llm/bench/benchmark/utils/general.py
@@ -22,7 +22,7 @@ _KV_CACHE_MAP = {
     QuantAlgo.NVFP4.value: "fp8",
 }
 
-ALL_SUPPORTED_BACKENDS = ["pytorch", "_autodeploy", "cpp", "tensorrt"]
+ALL_SUPPORTED_BACKENDS = ["pytorch", "_autodeploy", "tensorrt"]
 
 
 def get_settings_from_engine(
