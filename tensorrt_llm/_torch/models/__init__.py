40caf523ddbbd32cd73a3ddacb39d89d2e76778f
yechank <161688079+yechank-nvidia@users.noreply.github.com>
2025-07-14 23:14:56 +0900

    fix: update model
    Signed-off-by: yechank <161688079+yechank-nvidia@users.noreply.github.com>

--- tensorrt_llm/_torch/models/__init__.py
+++ tensorrt_llm/_torch/models/__init__.py
@@ -4,7 +4,6 @@ from .modeling_auto import AutoModelForCausalLM
 from .modeling_bert import BertForSequenceClassification
 from .modeling_clip import CLIPVisionModel
 from .modeling_deepseekv3 import DeepseekV3ForCausalLM
-from .modeling_exaone4 import Exaone4ForCausalLM
 from .modeling_gemma3 import Gemma3ForCausalLM
 from .modeling_gemma3vl import Gemma3Model
 from .modeling_hyperclovax import HCXVisionForCausalLM
@@ -31,7 +30,6 @@ __all__ = [
     "BertForSequenceClassification",
     "CLIPVisionModel",
     "DeepseekV3ForCausalLM",
-    "Exaone4ForCausalLM",
     "Gemma3ForCausalLM",
     "HCXVisionForCausalLM",
     "Gemma3Model",
