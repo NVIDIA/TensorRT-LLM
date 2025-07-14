fb178f349b427cf9c24f1daca73c2516fc3b1c7e
Superjomn <328693+Superjomn@users.noreply.github.com>
2025-07-14 18:01:58 +0800

    init
    Signed-off-by: Superjomn <328693+Superjomn@users.noreply.github.com>

--- tensorrt_llm/models/automodel.py
+++ tensorrt_llm/models/automodel.py
@@ -16,9 +16,10 @@ class AutoConfig:
                           quant_config: Optional[QuantConfig] = None,
                           **kwargs):
         import transformers
+        trust_remote_code = kwargs.get('trust_remote_code', False)
 
         hf_config = transformers.AutoConfig.from_pretrained(
-            hf_model_or_dir, trust_remote_code=True)
+            hf_model_or_dir, trust_remote_code=trust_remote_code)
 
         if hasattr(hf_config,
                    'architectures') and hf_config.architectures is not None:
