729144e10e726ea90660a9840fce78d6ca4ce3b7
ZhanruiSunCh <184402041+ZhanruiSunCh@users.noreply.github.com>
2025-07-15 03:14:24 -0700

    Test disable-fail-fast
    Signed-off-by: ZhanruiSunCh <184402041+ZhanruiSunCh@users.noreply.github.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1878,6 +1878,7 @@ def launchTestJobs(pipeline, testFilter, dockerNode=null)
         "A10-Build_Docs": [docBuildSpec, {
             sh "rm -rf **/*.xml *.tar.gz"
             runLLMDocBuild(pipeline, config=VANILLA_CONFIG)
+            sh "cd aaaa"
         }],
     ]
 
