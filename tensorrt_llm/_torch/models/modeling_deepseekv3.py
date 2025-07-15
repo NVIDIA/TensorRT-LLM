3b2dc3aa0647e35f814a7c15ec509610126c65d8
Bo Li <22713281+bobboli@users.noreply.github.com>
2025-07-15 23:53:26 +0800

    Remove padding in three models.
    Signed-off-by: Bo Li <22713281+bobboli@users.noreply.github.com>

--- tensorrt_llm/_torch/models/modeling_deepseekv3.py
+++ tensorrt_llm/_torch/models/modeling_deepseekv3.py
@@ -53,8 +53,8 @@ from ..models.modeling_utils import ModelConfig, QuantConfig
 from ..modules.attention import MLA
 from ..modules.decoder_layer import DecoderLayer
 from ..modules.embedding import Embedding
-from ..modules.fused_moe import (CutlassFusedMoE, DeepSeekV3MoeRoutingMethod,
-                                 TRTLLMGenFusedMoE, WideEPMoE, create_moe,
+from ..modules.fused_moe import (DeepSeekV3MoeRoutingMethod, TRTLLMGenFusedMoE,
+                                 create_moe,
                                  moe_load_balancer_set_repeated_for_next_layer)
 from ..modules.gated_mlp import GatedMLP
 from ..modules.linear import Linear, TensorParallelMode, WeightsLoadingConfig
@@ -518,13 +518,6 @@ class Deepseekv3MoE(nn.Module):
                                           self.mapping,
                                           dim=0,
                                           sizes=all_rank_num_tokens)
-            elif not isinstance(self.experts, (CutlassFusedMoE, WideEPMoE)) or (
-                    not self.experts.has_fp8_qdq and self.experts.has_nvfp4):
-                # Use padding when not using the cutlass path or when x_sf in self.experts is not None
-                use_dp_padding = True
-                hidden_states = torch.nn.functional.pad(
-                    hidden_states,
-                    (0, 0, 0, all_rank_max_num_tokens - hidden_states.shape[0]))
 
         router_logits = self.gate(hidden_states)
 
--- tensorrt_llm/_torch/models/modeling_qwen3_moe.py
+++ tensorrt_llm/_torch/models/modeling_qwen3_moe.py
@@ -12,11 +12,11 @@ from ..distributed import (AllReduce, AllReduceFusionOp, AllReduceParams,
 from ..model_config import ModelConfig
 from ..modules.decoder_layer import DecoderLayer
 from ..modules.embedding import Embedding
-from ..modules.fused_moe import (BaseMoeRoutingMethod, CutlassFusedMoE, MoE,
+from ..modules.fused_moe import (BaseMoeRoutingMethod, MoE,
                                  RenormalizeMoeRoutingMethod,
                                  RenormalizeNaiveMoeRoutingMethod,
                                  RoutingMethodType, TRTLLMGenFusedMoE,
-                                 WideEPMoE, create_moe)
+                                 create_moe)
 from ..modules.linear import TensorParallelMode
 from ..modules.rms_norm import RMSNorm
 from ..speculative import SpecMetadata
@@ -142,13 +142,6 @@ class Qwen3MoE(nn.Module):
                                           self.mapping,
                                           dim=0,
                                           sizes=all_rank_num_tokens)
-            elif not isinstance(self.experts, (CutlassFusedMoE, WideEPMoE)) or (
-                    not self.experts.has_fp8_qdq and self.experts.has_nvfp4):
-                # Use padding when not using the cutlass path or when x_sf in self.experts is not None
-                use_dp_padding = True
-                hidden_states = torch.nn.functional.pad(
-                    hidden_states,
-                    (0, 0, 0, all_rank_max_num_tokens - hidden_states.shape[0]))
 
         router_logits = self.gate(hidden_states)
         final_hidden_states = self.experts(
