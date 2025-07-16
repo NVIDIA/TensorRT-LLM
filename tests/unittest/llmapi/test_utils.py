2c491e18ca82d4a37f4fae19e62e10dac3f42984
Superjomn <328693+Superjomn@users.noreply.github.com>
2025-07-16 08:52:09 +0800

    remove DocTagger
    We can use status label directly.
    
    Signed-off-by: Superjomn <328693+Superjomn@users.noreply.github.com>

--- tests/unittest/llmapi/test_utils.py
+++
@@ -1,8 +0,0 @@
-from tensorrt_llm.llmapi import LlmArgs
-from tensorrt_llm.llmapi.utils import DocTagger, generate_api_docs_as_docstring
-
-
-def test_doc_tagger():
-    doc_tagger = DocTagger()
-    doc_tagger()
-    print(generate_api_docs_as_docstring(LlmArgs))
