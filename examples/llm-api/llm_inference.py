3780ccbbd9ec69470717e4088a05583db8c79643
nv-guomingz <137257613+nv-guomingz@users.noreply.github.com>
2025-07-17 03:33:04 +0000

    doc: sync llm api example
    Signed-off-by: nv-guomingz <137257613+nv-guomingz@users.noreply.github.com>

--- examples/llm-api/llm_inference.py
+++ examples/llm-api/llm_inference.py
@@ -1,8 +1,7 @@
-### Generate text
-import tempfile
-
-from tensorrt_llm import SamplingParams
-from tensorrt_llm._tensorrt_engine import LLM
+### :title Generate text
+### :order 0
+### :section Basics
+from tensorrt_llm import LLM, SamplingParams
 
 
 def main():
@@ -11,13 +10,9 @@ def main():
     # or TensorRT Model Optimizer's quantized checkpoints like nvidia/Llama-3.1-8B-Instruct-FP8 on HF.
     llm = LLM(model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 
-    # You can save the engine to disk and load it back later, the LLM class can accept either a HF model or a TRT-LLM engine.
-    llm.save(tempfile.mkdtemp())
-
     # Sample prompts.
     prompts = [
         "Hello, my name is",
-        "The president of the United States is",
         "The capital of France is",
         "The future of AI is",
     ]
@@ -32,7 +27,6 @@ def main():
 
     # Got output like
     # Prompt: 'Hello, my name is', Generated text: '\n\nJane Smith. I am a student pursuing my degree in Computer Science at [university]. I enjoy learning new things, especially technology and programming'
-    # Prompt: 'The president of the United States is', Generated text: 'likely to nominate a new Supreme Court justice to fill the seat vacated by the death of Antonin Scalia. The Senate should vote to confirm the'
     # Prompt: 'The capital of France is', Generated text: 'Paris.'
     # Prompt: 'The future of AI is', Generated text: 'an exciting time for us. We are constantly researching, developing, and improving our platform to create the most advanced and efficient model available. We are'
 
