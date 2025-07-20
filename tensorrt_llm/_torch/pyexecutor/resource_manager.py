beaf2d724c4a2e306a035f3a45e52ff13399f92b
amitz-nv <203509407+amitz-nv@users.noreply.github.com>
2025-07-20 08:00:14 +0300

    [TRTLLM-5826][feat] Support pytorch LoRA adapter eviction (#5616)
    Signed-off-by: Amit Zuker <203509407+amitz-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/resource_manager.py
+++ tensorrt_llm/_torch/pyexecutor/resource_manager.py
@@ -1218,7 +1218,7 @@ class PeftCacheManager(BaseResourceManager):
         pass
 
     def free_resources(self, request: LlmRequest):
-        pass
+        self.impl.mark_request_done(request)
 
     def shutdown(self):
         pass
