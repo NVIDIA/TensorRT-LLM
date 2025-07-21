fix: Draft model calculation in KVCacheManager and KvCacheCreator - Introduced a method to calculate KV size per token and updated memory estimation to ensure proper handling of max_gpu_total_bytes. - Changed KVCacheManager to still use `max_tokens` in non-VSWA case as its`calculate_max_num_blocks` doesn't consider the draft model tokens calculation.
Signed-off-by: qixiang-99 <203170375+qixiang-99@users.noreply.github.com>

tensorrt_llm/_torch/pyexecutor/resource_manager.py
@@ -477,12 +477,7 @@ class KVCacheManager(BaseResourceManager):
         free_mem, total_mem = torch.cuda.mem_get_info()
 
         assert free_mem_fraction < 1.0, f"Invalid freeMemFraction, freeMemFraction {free_mem_fraction} must be smaller than 1.0"
-        primary_pool_memory_bytes = int(free_mem * free_mem_fraction)
-        # Respect max_gpu_total_bytes if provided
-        if kv_cache_config.max_gpu_total_bytes > 0:
-            primary_pool_memory_bytes = min(primary_pool_memory_bytes,
-                                            kv_cache_config.max_gpu_total_bytes)
-        max_tokens = primary_pool_memory_bytes / cache_size_bytes_per_token
+        max_tokens = free_mem_fraction * free_mem / cache_size_bytes_per_token
 
         # If user specified a number of tokens
         if kv_cache_config.max_tokens is not None: