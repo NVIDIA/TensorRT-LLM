7bfe654cf111abe5befac8f8b6bc14dc7693e459
Hao Lu <14827759+hlu1@users.noreply.github.com>
2025-07-21 22:35:21 +0000

    Fix rebase
    Signed-off-by: Hao Lu <14827759+hlu1@users.noreply.github.com>

--- tensorrt_llm/_torch/model_config.py
+++ tensorrt_llm/_torch/model_config.py
@@ -193,9 +193,9 @@ class ModelConfig(Generic[TConfig]):
         json_quant_configs = quant_config_dict['quantization']
 
         quant_config.quant_algo = json_quant_configs.get('quant_algo', None)
-            # fp8_pb_wo from modelopt is the same as FP8_BLOCK_SCALES
-            if quant_config.quant_algo == "fp8_pb_wo":
-                quant_config.quant_algo = 'FP8_BLOCK_SCALES'
+        # fp8_pb_wo from modelopt is the same as FP8_BLOCK_SCALES
+        if quant_config.quant_algo == "fp8_pb_wo":
+            quant_config.quant_algo = 'FP8_BLOCK_SCALES'
         quant_config.kv_cache_quant_algo = json_quant_configs.get(
             'kv_cache_quant_algo', None)
         quant_config.group_size = json_quant_configs.get('group_size', None)
