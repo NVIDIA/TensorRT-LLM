8327d61a5edab4bd7a3270bb361d9846cbd6a4d3
jthomson04 <jwillthomson19@gmail.com>
2025-07-22 09:56:58 -0700

    Initial cpp binding stuff
    Signed-off-by: jthomson04 <jwillthomson19@gmail.com>

---
+++ cpp/include/tensorrt_llm/batch_manager/kvCacheConnector.h
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2022-2024, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "tensorrt_llm/batch_manager/llmRequest.h"
+#include "tensorrt_llm/runtime/common.h"
+
+#include <cstdint>
+#include <vector>
+
+using SizeType32 = tensorrt_llm::runtime::SizeType32;
+using RequestIdType = tensorrt_llm::batch_manager::LlmRequest::RequestIdType;
+
+using namespace tensorrt_llm::batch_manager;
+
+namespace tensorrt_llm::batch_manager::kv_connector
+{
+enum KvCacheConnectorRole : std::int8_t
+{
+    Scheduler,
+    Worker
+};
+
+class KvCacheConnector
+{
+public:
+    explicit KvCacheConnector(KvCacheConnectorRole role);
+    virtual ~KvCacheConnector() = default;
+
+    [[nodiscard]] KvCacheConnectorRole role() const;
+
+    //
+    // WORKER SIDE METHODS
+    //
+
+    // TODO(jothomson): Need arguments here.
+    virtual void registerKvCaches();
+
+    // TODO(jothomson): Need arguments here.
+    virtual void startLoadKv() = 0;
+
+    virtual void waitForLayerLoad(SizeType32 layer_idx) = 0;
+
+    // TODO(jothomson): Need arguments here.
+    virtual void saveKvLayer(SizeType32 layer_idx) = 0;
+
+    virtual void waitForSave() = 0;
+
+    virtual std::tuple<std::vector<RequestIdType>, std::vector<RequestIdType>> getFinished(
+        std::vector<RequestIdType> const& finishedReqIds);
+
+    //
+    // SCHEDULER SIDE METHODS
+    //
+
+    virtual std::tuple<SizeType32, bool> getNumNewMatchedTokens(LlmRequest const& request, SizeType32 numComputedTokens)
+        = 0;
+
+    // TODO(jothomson): Need arguments here. Also, is this even needed?
+    virtual void updateStateAfterAlloc();
+
+    virtual bool requestFinished(LlmRequest const& request);
+
+private:
+    KvCacheConnectorRole mRole;
+};
+} // namespace tensorrt_llm::batch_manager::kv_connector
