4bd4df48687e8f7b35ed97907a8b4c0f97ee6b4d
jthomson04 <jwillthomson19@gmail.com>
2025-07-22 16:43:17 -0700

    Basic connector tests
    Signed-off-by: jthomson04 <jwillthomson19@gmail.com>

---
+++ tests/unittest/bindings/test_connector_bindings.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from tensorrt_llm._torch.pyexecutor.connector import KvCacheConnector
+from tensorrt_llm.bindings.internal.batch_manager import (KvCacheConnectorRole,
+                                                          LlmRequest)
+
+
+class BasicConnector(KvCacheConnector):
+
+    def __init__(self, role: KvCacheConnectorRole):
+        super().__init__(role)
+
+    def build_connector_metadata(self) -> Optional[object]:
+        return {"test": "test"}
+
+    def start_load_kv(self):
+        pass
+
+    def wait_for_layer_load(self, layer_idx: int):
+        pass
+
+    def save_kv_layer(self, layer_idx: int):
+        pass
+
+    def wait_for_save(self):
+        pass
+
+    def get_num_new_matched_tokens(
+            self, request: LlmRequest,
+            num_computed_tokens: int) -> tuple[int, bool]:
+        return 16, True
+
+
+def test_basic_init():
+    connector = BasicConnector(KvCacheConnectorRole.Scheduler)
+
+    assert connector.role == KvCacheConnectorRole.Scheduler
+
+    assert connector.build_connector_metadata() == {"test": "test"}
+
+    # Try calling some of the other virtual methods.
+    connector.save_kv_layer(0)
+    connector.wait_for_save()
+
+    connector_worker = BasicConnector(KvCacheConnectorRole.Worker)
+
+    assert connector_worker.role == KvCacheConnectorRole.Worker
