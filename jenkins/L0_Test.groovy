d0ad4ca3072b6f5385e26d02ce592be8c8ef6ec8
Yiqing Yan <yiqingy@nvidia.com>
2025-07-24 03:14:53 +0000

    Add fmha test in A10
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1958,6 +1958,7 @@ def launchTestJobs(pipeline, testFilter, dockerNode=null)
         "A10-PyTorch-Post-Merge-1": ["a10", "l0_a10", 1, 1],
         "A10-TensorRT-Post-Merge-1": ["a10", "l0_a10", 1, 2],
         "A10-TensorRT-Post-Merge-2": ["a10", "l0_a10", 2, 2],
+        "A10-Fmha-Post-Merge-1": ["a10", "l0_a10", 1, 1],
         "A30-TensorRT-Post-Merge-1": ["a30", "l0_a30", 1, 6],
         "A30-TensorRT-Post-Merge-2": ["a30", "l0_a30", 2, 6],
         "A30-TensorRT-Post-Merge-3": ["a30", "l0_a30", 3, 6],
