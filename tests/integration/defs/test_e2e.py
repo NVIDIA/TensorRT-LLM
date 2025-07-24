560ca3ac8ee7d61590ba8da76971c6d3fed9cb88
Daniel Campora <961215+dcampora@users.noreply.github.com>
2025-07-24 12:13:06 +0000

    Adapt e2e test format.
    Signed-off-by: Daniel Campora <961215+dcampora@users.noreply.github.com>

--- tests/integration/defs/test_e2e.py
+++ tests/integration/defs/test_e2e.py
@@ -2208,7 +2208,10 @@ def test_ptp_quickstart_multimodal(llm_root, llm_venv, model_name, model_path,
             "image": [
                 ["dramatic", "moody", "ocean", "stormy", "sky", "waves"],
                 ["large", "dome", "yosemite", "landmark", "rock", "road"],
-                ["highway", "traffic", "vehicles", "lanes", "congestion", "road"],
+                [
+                    "highway", "traffic", "vehicles", "lanes", "congestion",
+                    "road"
+                ],
             ],
             "video": [
                 ["woman", "neon", "night", "jacket", "wet"],
