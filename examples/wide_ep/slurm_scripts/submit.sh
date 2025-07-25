79aba32067ef2eca75e57541faf13a8623b11531
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2025-07-25 01:24:16 -0700

    Update
    Signed-off-by: Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>

--- examples/wide_ep/slurm_scripts/submit.sh
+++ examples/wide_ep/slurm_scripts/submit.sh
@@ -6,23 +6,14 @@ echo "Make sure that SLURM parameters are correctly set in \`disaggr_torch.slurm
 mtp_size=0
 ntasks_per_node=4 # 4 GPUs per GB200 node
 
-# dep8
-for b in 1 64 1024; do
-    concurrency=$((b * 8))
-    ctx_num=$(((concurrency + 5499)/5500))
-    total_node_num=$((ctx_num + 2))
-    ntasks=$((total_node_num * ntasks_per_node))
-    sbatch --nodes=${total_node_num} --ntasks=${ntasks} --ntasks-per-node=${ntasks_per_node} --segment=${total_node_num} disaggr_torch.slurm ${ctx_num} 4 4 4480 true 1 8 1024 1024 true "0.8" 0 "$mtp_size" "$concurrency"
-done
-
 # dep16 eplb0, 256, 288
 for b in 1 64 1024; do
     concurrency=$((b * 16))
     ctx_num=$(((concurrency + 5499)/5500))
     total_node_num=$((ctx_num + 4))
     ntasks=$((total_node_num * ntasks_per_node))
-    sbatch --nodes=${total_node_num} --ntasks=${ntasks} --ntasks-per-node=${ntasks_per_node} --segment=${total_node_num} disaggr_torch.slurm ${ctx_num} 4 4 4480 true 1 16 1024 1024 true "0.7" 0 "$mtp_size" "$concurrency"
-    sbatch --nodes=${total_node_num} --ntasks=${ntasks} --ntasks-per-node=${ntasks_per_node} --segment=${total_node_num} disaggr_torch.slurm ${ctx_num} 4 4 4480 true 1 16 1024 1024 true "0.7" 256 "$mtp_size" "$concurrency"
+    # sbatch --nodes=${total_node_num} --ntasks=${ntasks} --ntasks-per-node=${ntasks_per_node} --segment=${total_node_num} disaggr_torch.slurm ${ctx_num} 4 4 4480 true 1 16 1024 1024 true "0.7" 0 "$mtp_size" "$concurrency"
+    # sbatch --nodes=${total_node_num} --ntasks=${ntasks} --ntasks-per-node=${ntasks_per_node} --segment=${total_node_num} disaggr_torch.slurm ${ctx_num} 4 4 4480 true 1 16 1024 1024 true "0.7" 256 "$mtp_size" "$concurrency"
     sbatch --nodes=${total_node_num} --ntasks=${ntasks} --ntasks-per-node=${ntasks_per_node} --segment=${total_node_num} disaggr_torch.slurm ${ctx_num} 4 4 4480 true 1 16 1024 1024 true "0.7" 288 "$mtp_size" "$concurrency"
 done
 
