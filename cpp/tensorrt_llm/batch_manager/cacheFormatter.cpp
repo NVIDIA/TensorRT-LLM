33fcf7507d5987ac00f53042d13f3041768bcdde
Patrice Castonguay <55748270+pcastonguay@users.noreply.github.com>
2025-07-25 08:30:49 -0700

    Fix issue with nb_kv_heads_per_layer
    Signed-off-by: Patrice Castonguay <55748270+pcastonguay@users.noreply.github.com>

--- cpp/tensorrt_llm/batch_manager/cacheFormatter.cpp
+++ cpp/tensorrt_llm/batch_manager/cacheFormatter.cpp
@@ -816,17 +816,7 @@ void CacheFormatter::unformat(TransferSession& session)
         TLLM_LOG_WARNING("CacheFormatter::inquireSupport: only support same number of layers");
         TLLM_LOG_WARNING("self: %d dest %d", selfConfig.getModelConfig().mNbKvHeadsPerLayer.size(),
             destConfig.getModelConfig().mNbKvHeadsPerLayer.size());
-
-        auto selfTotalLayers = selfConfig.getModelConfig().mNbKvHeadsPerLayer.size()
-            * selfConfig.getParallelConfig().mPipelineParallelism;
-        auto destTotalLayers = destConfig.getModelConfig().mNbKvHeadsPerLayer.size()
-            * destConfig.getParallelConfig().mPipelineParallelism;
-        if (selfTotalLayers != destTotalLayers)
-        {
-            TLLM_LOG_WARNING("CacheFormatter::inquireSupport: incompatible total layer counts: self=%d, dest=%d",
-                static_cast<int>(selfTotalLayers), static_cast<int>(destTotalLayers));
-            return false;
-        }
+        return false;
     }
     int selfNumLayers = selfConfig.getModelConfig().mNbKvHeadsPerLayer.size();
     int selfPPSize = selfConfig.getParallelConfig().mPipelineParallelism;
