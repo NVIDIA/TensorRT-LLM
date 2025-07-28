386434b07aa14fdcd32bf7afe62364baf160f9b3
Eran Geva <19514940+MrGeva@users.noreply.github.com>
2025-07-28 07:10:10 -0700

    refactored compile_limit
    Signed-off-by: Eran Geva <19514940+MrGeva@users.noreply.github.com>
    
    removed changes made to TorchCompileCompiler
    
    Signed-off-by: Eran Geva <19514940+MrGeva@users.noreply.github.com>
    
    set cache_size_limit in TorchCompileCompiler
    
    Signed-off-by: Eran Geva <19514940+MrGeva@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/compile/backends/torch_compile.py
+++ tensorrt_llm/_torch/auto_deploy/compile/backends/torch_compile.py
@@ -3,11 +3,19 @@
 import torch
 import torch.nn as nn
 
+from tensorrt_llm._torch.auto_deploy.utils.logger import ad_logger
+
 from ..compiler import BackendCompiler, BackendRegistry
 
 
 @BackendRegistry.register("torch-compile")
 class TorchCompileCompiler(BackendCompiler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Global torch config, set the torch compile cache to fix up to llama 405B
+        torch._dynamo.config.cache_size_limit = 20
+        ad_logger.info(f"Setting cache size limit to {torch._dynamo.config.cache_size_limit}")
+
     def compile(self) -> nn.Module:
         """Compile the model using torch.compile."""
         return torch.compile(self.gm, dynamic=True)
--- tensorrt_llm/_torch/auto_deploy/compile/backends/torch_opt.py
+++ tensorrt_llm/_torch/auto_deploy/compile/backends/torch_opt.py
@@ -2,6 +2,8 @@
 
 import torch
 
+from tensorrt_llm._torch.auto_deploy.utils.logger import ad_logger
+
 from ..compiler import BackendRegistry
 from .torch_cudagraph import CapturedGraph, TorchCudagraphCompiler
 
@@ -9,6 +11,17 @@ from .torch_cudagraph import CapturedGraph, TorchCudagraphCompiler
 @BackendRegistry.register("torch-opt")
 class TorchOptCompiler(TorchCudagraphCompiler):
     """Compiler that uses both torch.compile and CUDA graphs."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        torch._dynamo.config.recompile_limit = max(
+            len(self.cuda_graph_batch_sizes), torch._dynamo.config.recompile_limit
+        )
+        ad_logger.info(f"Setting recompile limit to {torch._dynamo.config.recompile_limit}")
+
+        # Global torch config, set the torch compile cache to fix up to llama 405B
+        torch._dynamo.config.cache_size_limit = 20
+        ad_logger.info(f"Setting cache size limit to {torch._dynamo.config.cache_size_limit}")
 
     def _init_captured_graph(self, gm, in_spec, out_spec) -> CapturedGraph:
         gm = torch.compile(gm, dynamic=True)
