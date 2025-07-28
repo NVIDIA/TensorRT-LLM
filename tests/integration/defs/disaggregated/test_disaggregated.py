d2900d73c60f4954bec58ed1043610b106fce037
raayandhar <rdhar@nvidia.com>
2025-07-28 01:43:50 +0000

    fix a test and small changes for disagg+pp
    Signed-off-by: raayandhar <rdhar@nvidia.com>

--- tests/integration/defs/disaggregated/test_disaggregated.py
+++ tests/integration/defs/disaggregated/test_disaggregated.py
@@ -65,8 +65,8 @@ def get_test_config(test_desc, example_dir, test_root):
         (4, f"{test_configs_root}/disagg_config_ctxtp2_genpp2.yaml"),
         "ctxpp2_gentp2":
         (4, f"{test_configs_root}/disagg_config_ctxpp2_gentp2.yaml"),
-        "ctxtp2pp2_gentp2tp2":
-        (8, f"{test_configs_root}/disagg_config_ctxpp2tp2_genpp2tp2.yaml"),
+        "ctxtp2pp2_gentp2pp2":
+        (8, f"{test_configs_root}/disagg_config_ctxtp2pp2_gentp2pp2.yaml"),
         "ctxpp4_genpp4":
         (8, f"{test_configs_root}/disagg_config_ctxpp4_genpp4.yaml"),
         "deepseek_v3_lite_fp8_mpi":
@@ -573,26 +573,6 @@ def test_disaggregated_ctxpp2_genpp2(disaggregated_test_root, llm_venv,
 @pytest.mark.skip_less_device(4)
 @pytest.mark.parametrize("llama_model_root", ['TinyLlama-1.1B-Chat-v1.0'],
                          indirect=True)
-def test_disaggregated_ctxpp2tp2_genpp2tp2(disaggregated_test_root, llm_venv,
-                                           disaggregated_example_root,
-                                           llama_model_root):
-    src_dst_dict = {
-        llama_model_root:
-        f"{llm_venv.get_working_directory()}/TinyLlama/TinyLlama-1.1B-Chat-v1.0",
-    }
-    for src, dst in src_dst_dict.items():
-        if not os.path.islink(dst):
-            os.makedirs(os.path.dirname(dst), exist_ok=True)
-            os.symlink(src, dst, target_is_directory=True)
-    run_disaggregated_test(disaggregated_example_root,
-                           "ctxpp2tp2_genpp2tp2",
-                           env=llm_venv._new_env,
-                           cwd=llm_venv.get_working_directory())
-
-
-@pytest.mark.skip_less_device(4)
-@pytest.mark.parametrize("llama_model_root", ['TinyLlama-1.1B-Chat-v1.0'],
-                         indirect=True)
 def test_disaggregated_ctxtp2_genpp2(disaggregated_test_root, llm_venv,
                                      disaggregated_example_root,
                                      llama_model_root):
@@ -626,6 +606,46 @@ def test_disaggregated_ctxpp2_gentp2(disaggregated_test_root, llm_venv,
             os.symlink(src, dst, target_is_directory=True)
     run_disaggregated_test(disaggregated_example_root,
                            "ctxpp2_gentp2",
+                           env=llm_venv._new_env,
+                           cwd=llm_venv.get_working_directory())
+
+
+@pytest.mark.skip_less_device(4)
+@pytest.mark.parametrize("llama_model_root", ['TinyLlama-1.1B-Chat-v1.0'],
+                         indirect=True)
+def test_disaggregated_ctxtp2pp2_gentp2pp2(disaggregated_test_root, llm_venv,
+                                           disaggregated_example_root,
+                                           llama_model_root):
+    src_dst_dict = {
+        llama_model_root:
+        f"{llm_venv.get_working_directory()}/TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    }
+    for src, dst in src_dst_dict.items():
+        if not os.path.islink(dst):
+            os.makedirs(os.path.dirname(dst), exist_ok=True)
+            os.symlink(src, dst, target_is_directory=True)
+    run_disaggregated_test(disaggregated_example_root,
+                           "ctxtp2pp2_gentp2pp2",
+                           env=llm_venv._new_env,
+                           cwd=llm_venv.get_working_directory())
+
+
+@pytest.mark.skip_less_device(4)
+@pytest.mark.parametrize("llama_model_root", ['TinyLlama-1.1B-Chat-v1.0'],
+                         indirect=True)
+def test_disaggregated_ctxpp4_genpp4(disaggregated_test_root, llm_venv,
+                                     disaggregated_example_root,
+                                     llama_model_root):
+    src_dst_dict = {
+        llama_model_root:
+        f"{llm_venv.get_working_directory()}/TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+    }
+    for src, dst in src_dst_dict.items():
+        if not os.path.islink(dst):
+            os.makedirs(os.path.dirname(dst), exist_ok=True)
+            os.symlink(src, dst, target_is_directory=True)
+    run_disaggregated_test(disaggregated_example_root,
+                           "ctxpp4_genpp4",
                            env=llm_venv._new_env,
                            cwd=llm_venv.get_working_directory())
 
