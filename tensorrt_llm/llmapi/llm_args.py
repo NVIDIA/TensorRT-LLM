004eaf98946a9a9c3fcc7a0a745e0bfa157e9811
Amit Zuker <203509407+amitz-nv@users.noreply.github.com>
2025-07-29 13:20:22 +0000

    Fix validate_peft_cache_config
    Signed-off-by: Amit Zuker <203509407+amitz-nv@users.noreply.github.com>

--- tensorrt_llm/llmapi/llm_args.py
+++ tensorrt_llm/llmapi/llm_args.py
@@ -1628,7 +1628,7 @@ class BaseLlmArgs(StrictBaseModel):
 
     @model_validator(mode="after")
     def validate_peft_cache_config(self):
-        if self.backend == "pytorch" and self.peft_cache_config.lora_prefetch_dir is not None:
+        if self.backend == "pytorch" and self.peft_cache_config is not None and self.peft_cache_config.lora_prefetch_dir is not None:
             logger.warning(
                 "LoRA prefetch is not supported with pytorch backend")
         return self
