943e059620bec5bbac4966d40759f3135441664d
Yiqing Yan <yiqingy@nvidia.com>
2025-07-30 07:46:39 +0000

    test only modify pytorch files
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>

--- jenkins/L0_MergeRequest.groovy
+++ jenkins/L0_MergeRequest.groovy
@@ -777,6 +777,7 @@ def getOnlyOneGroupChanged(pipeline, testFilter, globalVars) {
             "examples/pytorch/",
             "examples/scaffolding/",
             "docs/",
+            "jenkins/",
         ],
         "Triton": [
             "tests/integration/defs/triton_server/",
