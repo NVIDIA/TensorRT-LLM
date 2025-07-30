da4963e5951d0611baafbaa0a7db294cddb71e57
Yiqing Yan <yiqingy@nvidia.com>
2025-07-30 03:16:16 +0000

    test for modify triton and other files
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>

--- jenkins/L0_MergeRequest.groovy
+++ jenkins/L0_MergeRequest.groovy
@@ -796,7 +796,6 @@ def getOnlyOneGroupChanged(pipeline, testFilter, globalVars) {
         "Triton": [
             "tests/integration/defs/triton_server/",
             "triton_backend/",
-            "jenkins/",
         ],
     ]
 
