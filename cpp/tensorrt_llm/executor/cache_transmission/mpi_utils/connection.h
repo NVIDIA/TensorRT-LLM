d2f20b048fea27c97598f1b6fc51881b8a5b4084
Shunkang <182541032+Shunkangz@users.noreply.github.co>
2025-07-31 07:45:13 +0000

    Add timeout into interface
    Signed-off-by: Shunkang <182541032+Shunkangz@users.noreply.github.co>

--- cpp/tensorrt_llm/executor/cache_transmission/mpi_utils/connection.h
+++ cpp/tensorrt_llm/executor/cache_transmission/mpi_utils/connection.h
@@ -30,8 +30,10 @@ class MpiConnection : public Connection
 {
 public:
     MpiConnection(mpi::MpiComm const* comm, int rank);
-    void send(DataContext const& ctx, void const* data, size_t size) const override;
-    void recv(DataContext const& ctx, void* data, size_t size) const override;
+    void send(DataContext const& ctx, void const* data, size_t size,
+        std::chrono::milliseconds timeout = std::chrono::milliseconds{0}) const override;
+    void recv(DataContext const& ctx, void* data, size_t size,
+        std::chrono::milliseconds timeout = std::chrono::milliseconds{0}) const override;
 
 private:
     mpi::MpiComm const* mComm{};
