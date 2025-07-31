f62d4495d0f0bf9fd70e99da2f65acbbdbcd72d8
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2025-07-31 09:41:31 -0700

    Polish
    Signed-off-by: Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>

--- examples/disaggregated/slurm/submit.sh
+++ examples/disaggregated/slurm/submit.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
+partition=<partition>
+account=<account>
+job_name=<job_name>
+container_image=<container_image>
+mounts=<mounts>  # e.g. /mnt/data:/mnt/data
+workdir=<workdir>  # Path to disaggr_torch.slurm
+model_dir=<model_dir>  # Path to the model checkpoint
+
+ntasks_per_node=4 # 4 GPUs per GB200 node
 total_node_num=8
 ntasks=$((total_node_num * ntasks_per_node))
-ntasks_per_node=4 # 4 GPUs per GB200 node
 
-ctx_num=1
 concurrency=8
 isl=1024
 osl=1024
 multi_round=10
 streaming=true
-container_image=<container_image>
-mounts=<mounts>  # e.g. /mnt/data:/mnt/data
-workdir=<workdir>  # Path to disaggr_torch.slurm
-model_dir=<model_dir>  # Path to the model checkpoint
+
+args=(
+    1 4 4 4480 true          # Context servers arguments
+    1 8 1024 1024 true "0.8" # Generation servers arguments
+    0 0                      # Other arguments
+    $concurrency             # Benchmarking arguments
+    $isl
+    $osl
+    $multi_round
+    $streaming
+    $container_image         # User specific arguments
+    $mounts
+    $workdir
+    $model_dir
+)
 
 # This command starts a job with 8 nodes, 32 GPUs in total.
 # The server will include 4 context workers with DEP4, and 1 generation worker with DEP8.
@@ -26,16 +44,4 @@ sbatch --nodes=${total_node_num} \
     --job-name=${job_name} \
     --gres=gpu:${ntasks_per_node} \
     --segment=${total_node_num} \
-    ${workdir}/disaggr_torch.slurm \
-        ${ctx_num} 4 4 4480 true \  # Context servers arguments
-        1 8 1024 1024 true "0.8" \  # Generation servers arguments
-        0 0 \  # Other arguments
-        $concurrency \  # Benchmarking arguments
-        $isl \
-        $osl \
-        $multi_round \
-        $streaming \
-        $container_image \  # User specific arguments
-        $mounts \
-        $workdir \
-        $model_dir
+    ${workdir}/disaggr_torch.slurm "${args[@]}"
