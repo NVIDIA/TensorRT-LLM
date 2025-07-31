07679bf4b2e4de0a4733af60595be8568faf0aa4
Jhao-Ting Chen <jhaotingc@nvidia.com>
2025-07-31 14:54:23 -0700

    enable multiblock in torch attentionOp.cpp
    Signed-off-by: Jhao-Ting Chen <jhaotingc@nvidia.com>

--- cpp/tensorrt_llm/kernels/decoderMaskedMultiheadAttention/decoderXQAImplJIT/decoderXQAImplJIT.cpp
+++ cpp/tensorrt_llm/kernels/decoderMaskedMultiheadAttention/decoderXQAImplJIT/decoderXQAImplJIT.cpp
@@ -441,13 +441,15 @@ void DecoderXQAImplJIT::runImpl(XQAParams const& xqaParams, KVCacheBuffer const&
         uint32_t multi_block = 1;
         if (xqaParams.multi_block_mode)
         {
-            multi_block = computeMultiBlockCount(xqaParams, xqaParams.batch_size, multiprocessor_count);
-        }
-        // A WAR to enable Hopper XQA multi-token multi_block mode
-        if (isSpecDec && isGMMAKernel)
-        {
-            multi_block = computeMultiBlockCountSpecDecGMMA(
-                xqaParams, xqaParams.batch_size, multiprocessor_count, specDecBlocks);
+            if (isSpecDec && isGMMAKernel)
+            {
+                multi_block = computeMultiBlockCountSpecDecGMMA(
+                    xqaParams, xqaParams.batch_size, multiprocessor_count, specDecBlocks);
+            }
+            else
+            {
+                multi_block = computeMultiBlockCount(xqaParams, xqaParams.batch_size, multiprocessor_count);
+            }
         }
         uint32_t const nbKVHeads = xqaParams.num_kv_heads;
         auto const gridDim = (isGMMAKernel ? dim3{specDecBlocks, multi_block, nbKVHeads * xqaParams.batch_size}
