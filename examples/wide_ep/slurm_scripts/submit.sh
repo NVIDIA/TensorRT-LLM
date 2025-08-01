75d18a83328ac0c9506b2f5568c7029abcecce2e
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2025-08-01 00:15:19 -0700

    Update
    Signed-off-by: Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>

--- examples/wide_ep/slurm_scripts/submit.sh
+++ examples/wide_ep/slurm_scripts/submit.sh
@@ -9,7 +9,7 @@ container_image=<container_image>
 mounts=<mounts>  # e.g. /mnt/data:/mnt/data
 workdir=<workdir>  # Path to disaggr_torch.slurm
 model_dir=<model_dir>  # Path to the model checkpoint
-repo_dir=<repo_dir>  # Path to the repo, if this is empty, the pre-installed TensorRT-LLM will be used
+repo_dir=<repo_dir>  # Path to the repo to install TensorRT-LLM, if this is empty, the pre-installed version will be used
 
 mtp_size=0
 ntasks_per_node=4 # 4 GPUs per GB200 node
