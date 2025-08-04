0ee4114cc7fee566785802d5a2ef1061a6f83239
Hao Lu <haolu@nvidia.com>
2025-08-04 14:27:20 -0700

    Update gpt_oss example
    Signed-off-by: Hao Lu <haolu@nvidia.com>
    Signed-off-by: Hao Lu <14827759+hlu1@users.noreply.github.com>

--- examples/models/core/gpt_oss/openai_chat_client_function_calling.py
+++ examples/models/core/gpt_oss/openai_chat_client_function_calling.py
@@ -8,7 +8,7 @@ system_prompt = """You are ChatGPT, a large language model trained by OpenAI.
 Knowledge cutoff: 2024-06
 Current date: 2025-06-28
 
-reasoning: high
+Reasoning: high
 
 # Valid channels: analysis, commentary, final. Channel must be included for every message.
 Calls to these tools must go to the commentary channel: 'functions'."""
