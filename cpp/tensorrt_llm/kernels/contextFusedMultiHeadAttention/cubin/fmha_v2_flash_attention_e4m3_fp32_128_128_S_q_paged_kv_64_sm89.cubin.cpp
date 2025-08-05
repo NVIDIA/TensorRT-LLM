c9eebcb4541d961ab390f0bd0a22e2c89f1bcc78
Haohang Huang <31998628+symphonylyh@users.noreply.github.com>
2025-08-05 00:47:41 -0700

    [TRTLLM-6674][feat] (Breaking Change) Hopper SWA non-cyclic kernels + KV reuse + Spec Dec (#6379)
    Signed-off-by: Haohang Huang <31998628+symphonylyh@users.noreply.github.com>
    Signed-off-by: symphonylyh <31998628+symphonylyh@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/contextFusedMultiHeadAttention/cubin/fmha_v2_flash_attention_e4m3_fp32_128_128_S_q_paged_kv_64_sm89.cubin.cpp
+++ cpp/tensorrt_llm/kernels/contextFusedMultiHeadAttention/cubin/fmha_v2_flash_attention_e4m3_fp32_128_128_S_q_paged_kv_64_sm89.cubin.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b797da09627dbf7661ccad3e8b7fd741330f008b3f8e033b7a3c7787a7233e1d
-size 2003768
+oid sha256:132d83639e34af1b431abdcb3f09542d0389030b85752e18a3ae221ead7d24a3
+size 1965880
