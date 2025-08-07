0419019a946b80e497981fd0e79fae5506c8d1c5
Robin Kobus <19427718+Funatiq@users.noreply.github.com>
2025-08-07 13:05:49 +0000

    feat: introduce flashinfer utility functions and refactor imports
    - Added `flashinfer_utils.py` to manage flashinfer availability and PDL environment variable.
    - Updated imports across multiple modules to source `IS_FLASHINFER_AVAILABLE` and `ENABLE_PDL` from the new utility file.
    - This refactor improves code organization and maintains clarity regarding flashinfer's integration.
    - This change fixes circular import issues in tensorrt_llm/_torch/attention_backend/__init__.py.
    
    Signed-off-by: Robin Kobus <19427718+Funatiq@users.noreply.github.com>

---
+++ tensorrt_llm/_torch/flashinfer_utils.py
@@ -0,0 +1,27 @@
+import os
+import platform
+import traceback
+
+from ..logger import logger
+
+IS_FLASHINFER_AVAILABLE = False
+
+
+def get_env_enable_pdl():
+    return os.environ.get("TRTLLM_ENABLE_PDL", "0") == "1"
+
+
+ENABLE_PDL = get_env_enable_pdl()
+if ENABLE_PDL:
+    logger.info("PDL is enabled")
+
+if platform.system() != "Windows":
+    try:
+        import flashinfer
+        logger.info(f"flashinfer is available: {flashinfer.__version__}")
+        IS_FLASHINFER_AVAILABLE = True
+    except ImportError:
+        traceback.print_exc()
+        print(
+            "flashinfer is not installed properly, please try pip install or building from source codes"
+        )
