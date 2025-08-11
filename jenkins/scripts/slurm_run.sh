b0c2c8c2e89dcbcb6dbaf34b8c52b680f3aa8cbe
Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>
2025-08-11 12:31:11 -0700

    Refactor L0 Test code
    consolidate test code for slurm jobs and regular jobs
    
    Signed-off-by: Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>

--- jenkins/scripts/slurm_run.sh
+++ jenkins/scripts/slurm_run.sh
@@ -2,17 +2,27 @@
 cd $resourcePathNode
 llmSrcNode=$resourcePathNode/TensorRT-LLM/src
 
-# generate .coveragerc in workspace
-coverageConfigFile="$jobWorkspace/.coveragerc"
-cat << EOF > "$coverageConfigFile"
-[run]
-branch = True
-data_file = $jobWorkspace/.coverage.$stageName
-[paths]
-source =
-    $llmSrcNode/tensorrt_llm/
-    ---wheel_path---/tensorrt_llm/
-EOF
+set_value_in_command() {
+    # Parameters
+    local key="$1"
+    local value="$2"
+    local command="$3"
+
+    # Transform the key
+    local placeholder="__PLACEHOLDER_${key}__"
+
+    # Check if placeholder exists
+    if [[ "$command" != *"$placeholder"* ]]; then
+        echo "Error: placeholder '$placeholder' not found in the command" >&2
+        return 1
+    fi
+
+    # Replace all occurrences
+    local result="${command//${placeholder}/${value}}"
+
+    # Return the result
+    echo "$result"
+}
 
 resultsPath=$jobWorkspace/results
 mkdir -p $resultsPath
@@ -27,54 +37,38 @@ if [ $SLURM_LOCALID -eq 0 ]; then
     cd $resourcePathNode &&  pip3 install --force-reinstall --no-deps TensorRT-LLM/tensorrt_llm-*.whl
     git config --global --add safe.directory "*"
     gpuUuids=$(nvidia-smi -q | grep "GPU UUID" | awk '{print $4}' | tr '\n' ',' || true)
-    echo "HOST_NODE_NAME = $HOST_NODE_NAME ; GPU_UUIDS = =$gpuUuids ; STAGE_NAME = $stageName"
+    echo "HOST_NODE_NAME = $hostname ; GPU_UUIDS = =$gpuUuids ; STAGE_NAME = $stageName"
     touch install_lock.lock
 else
     while [ ! -f install_lock.lock ]; do
         sleep 5
     done
 fi
-testList="$testList_$splitId"
-export CPP_TEST_TIMEOUT_OVERRIDDEN=$pytestTestTimeout
-export LLM_ROOT=$llmSrcNode
-export LLM_MODELS_ROOT=$MODEL_CACHE_DIR
+
 export UCX_TLS=^gdr_copy
 cd $llmSrcNode/tests/integration/defs
-testCmdLines=(
-    "$llmSrcNode/tensorrt_llm/llmapi/trtllm-llmapi-launch"
-    "pytest"
-    "-v"
-    "--timeout-method=thread"
-    "--timeout=$pytestTestTimeout"
-    "--test-list=$testListPathNode"
-    "--waives-file=$waivesListPathNode"
-    "--rootdir $llmSrcNode/tests/integration/defs"
-    "--test-prefix=$stageName"
-    "--splits $splits"
-    "--group $splitId"
-    "--output-dir=$jobWorkspace/"
-    "--csv=$resultsPath/report.csv"
-    "--junit-xml $resultsPath/results.xml"
-    "-o junit_logging=out-err"
-)
-if [ "$perfMode" = "true" ]; then
-    testCmdLines+=(
-        "--perf"
-        "--perf-log-formats csv"
-        "--perf-log-formats yaml"
-    )
+if [ $nodeCount -gt 1 ]; then
+    pytestCommand=$(set_value_in_command "pytest" "$llmSrcNode/tensorrt_llm/llmapi/trtllm-llmapi-launch" "$pytestCommand")
 fi
+
+# get trtllm wheel path and add to pytest command
 trtllmWhlPath=$(pip3 show tensorrt_llm | grep Location | cut -d ' ' -f 2)
 trtllmWhlPath=$(echo "$trtllmWhlPath" | sed 's/[[:space:]]+/_/g')
 echo "TRTLLM WHEEL PATH: $trtllmWhlPath"
-sed -i "s|---wheel_path---|$trtllmWhlPath|g" "$coverageConfigFile"
-testCmdLines+=(
-    "--cov=$llmSrcNode/examples/"
-    "--cov=$llmSrcNode/tensorrt_llm/"
-    "--cov=$trtllmWhlPath/tensorrt_llm/"
-    "--cov-report="
-    "--cov-config=$coverageConfigFile"
-)
+pytestCommand=$(set_value_in_command "TRTLLM_WHL_PATH" "$trtllmWhlPath" "$pytestCommand")
+
+# generate .coveragerc in workspace and add file path to pytest command
+cat << EOF > $jobWorkspace/.coveragerc
+[run]
+branch = True
+data_file = $jobWorkspace/.coverage.$stageName
+[paths]
+source =
+    $llmSrcNode/tensorrt_llm/
+    $trtllmWhlPath/tensorrt_llm/
+EOF
+pytestCommand=$(set_value_in_command "coverageConfigFile" "$jobWorkspace/.coveragerc" "$pytestCommand")
+
 containerPipLLMLibPath=$(pip3 show tensorrt_llm | grep "Location" | awk -F ":" '{ gsub(/ /, "", $2); print $2"/tensorrt_llm/libs"}')
 containerPipLLMLibPath=$(echo "$containerPipLLMLibPath" | sed 's/[[:space:]]+/_/g')
 containerLDLibPath=$LD_LIBRARY_PATH
@@ -87,7 +81,24 @@ export LD_LIBRARY_PATH=$containerLDLibPath
 echo "Library Path:"
 echo "$LD_LIBRARY_PATH"
 env | sort
-fullCmd="${testCmdLines[*]}"
 echo "Running: $testCase"
-echo "Full Command: $fullCmd"
-eval $fullCmd
+echo "Full Command: $pytestCommand"
+eval $pytestCommand
+
+if [ "$perfMode" = "true" ]; then
+    if [[ "$stageName" == *PyTorch* ]]; then
+        basePerfFilename="base_perf_pytorch.csv"
+    else
+        basePerfFilename="base_perf.csv"
+    fi
+    basePerfPath="$llmSrcNode/tests/integration/defs/perf/$basePerfFilename"
+    echo "Check perf result"
+    python3 $llmSrcNode/tests/integration/defs/perf/sanity_perf_check.py \
+        $stageName/perf_script_test_results.csv \
+        $basePerfPath
+    echo "Check perf report"
+    python3 $llmSrcNode/tests/integration/defs/perf/create_perf_comparison_report.py \
+        --output_path $stageName/report.pdf \
+        --files $stageName/perf_script_test_results.csv \
+        $basePerfPath
+fi
