3a4d36c76e4f2f8e7c8ab5cb31b9045268150b47
Shreyas Misra <shreyasm@nvidia.com>
2025-08-12 11:27:49 -0700

    Simplify
    Signed-off-by: Shreyas Misra <shreyasm@nvidia.com>

--- tensorrt_llm/_torch/models/modeling_llama.py
+++ tensorrt_llm/_torch/models/modeling_llama.py
@@ -10,8 +10,10 @@ from transformers import (AutoProcessor, Llama4Config, Llama4VisionModel,
 from transformers.modeling_utils import load_sharded_checkpoint
 from transformers.models.llama4.modeling_llama4 import Llama4MultiModalProjector
 
+import flashinfer.comm as flashinfer_comm
+
 from tensorrt_llm._torch.distributed import (AllReduce, AllReduceFusionOp,
-                                             AllReduceParams, MoEAllReduce)
+                                             AllReduceParams, MoEAllReduce, FlashInferAllReduce, FlashInferAllReduceParams)
 from tensorrt_llm._torch.models.checkpoints.base_weight_mapper import \
     BaseWeightMapper
 from tensorrt_llm._utils import get_sm_version
@@ -686,6 +688,12 @@ class LlamaDecoderLayer(DecoderLayer):
                                       or self.mapping.tp_size == 1
                                       or self.enable_attention_dp)
 
+        self.flash_infer_all_reduce = FlashInferAllReduce(
+                mapping=self.mapping,
+                hidden_dim=8192,
+                strategy=flashinfer_comm.AllReduceStrategyType.TWOSHOT,
+                dtype=config.torch_dtype)
+
     def forward(
         self,
         position_ids: torch.IntTensor,
@@ -706,9 +714,16 @@ class LlamaDecoderLayer(DecoderLayer):
             attn_metadata=attn_metadata,
             attention_mask=self.attention_mask,
             all_reduce_params=AllReduceParams(
-                enable_allreduce=not self.disable_attn_allreduce),
+                enable_allreduce=False),
             **kwargs,
         )
+        hidden_states = self.flash_infer_all_reduce(
+            hidden_states,
+            all_reduce_params=FlashInferAllReduceParams(
+                strategy=flashinfer_comm.AllReduceStrategyType.TWOSHOT,
+                fusion_op=flashinfer_comm.AllReduceFusionOp.NONE,
+                config_mode=flashinfer_comm.AllReduceStrategyConfig.USE_MEMCPY,
+            ))
         # Fully Connected
         if self.PRE_MLP_FUSION:
             if self.is_nvfp4 or self.is_fp8_quant:
@@ -746,7 +761,7 @@ class LlamaDecoderLayer(DecoderLayer):
         hidden_states = self.mlp(
             hidden_states,
             final_all_reduce_params=AllReduceParams(
-                enable_allreduce=not self.disable_mlp_allreduce),
+                enable_allreduce=False),
             **kwargs,
         )
 
