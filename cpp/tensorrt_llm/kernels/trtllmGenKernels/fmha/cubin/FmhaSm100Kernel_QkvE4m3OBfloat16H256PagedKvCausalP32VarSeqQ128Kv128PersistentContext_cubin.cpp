b38e9e5238852f9df82610726678976facca3b5c
Perkz Zheng <67892460+PerkzZheng@users.noreply.github.com>
2025-08-13 01:38:06 +0000

    add missing cubins
    Signed-off-by: Perkz Zheng <67892460+PerkzZheng@users.noreply.github.com>

---
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin/FmhaSm100Kernel_QkvE4m3OBfloat16H256PagedKvCausalP32VarSeqQ128Kv128PersistentContext_cubin.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ae601b1d2907ed15d34353cb7df65ac07bf03d4b7991d5c5c4c368b4ca25552
+size 773815
---
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin/FmhaSm100Kernel_QkvE4m3OBfloat16H64PagedKvDenseP64VarSeqQ128Kv128StaticContext_cubin.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:efd21c6f478775af366fca10e101949f566e29b15dedc6467126f3b05d81dfee
+size 836259
