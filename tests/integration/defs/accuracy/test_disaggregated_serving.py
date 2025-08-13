4d0075de13245d86dc7dd28cad293c698e82d81c
Wangshanshan <30051912+dominicshanshan@users.noreply.github.com>
2025-08-13 03:54:53 -0700

    Run pre-commit.
    Signed-off-by: Wangshanshan <30051912+dominicshanshan@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_disaggregated_serving.py
+++ tests/integration/defs/accuracy/test_disaggregated_serving.py
@@ -4,7 +4,6 @@
 # Please take a look at the existing test_llm_api_pytorch.py file for reference.
 import concurrent
 import contextlib
-import json
 import os
 import tempfile
 import time
@@ -20,7 +19,6 @@ import yaml
 from tensorrt_llm.executor.result import GenerationResultBase
 from tensorrt_llm.llmapi import CompletionOutput, RequestOutput, SamplingParams
 from tensorrt_llm.llmapi.llm_args import LlmArgs
-from tensorrt_llm.llmapi.tokenizer import load_hf_tokenizer
 
 from ..conftest import (get_device_count, llm_models_root, parametrize_with_ids,
                         skip_pre_hopper)
@@ -599,7 +597,6 @@ class TestDeepSeekV3Lite(LlmapiAccuracyTestHarness):
     MODEL_NAME = "deepseek-ai/DeepSeek-V3-Lite"
     MODEL_PATH = f"{llm_models_root()}/DeepSeek-V3-Lite/bf16"
 
-    
     def test_nixl_backend(self):
         ctx_server_config = {
             "disable_overlap_scheduler": True,
