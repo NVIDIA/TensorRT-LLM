6a0ae81a372d95b9f889ce9210557b532b1440a2
Pengyun Lin <81065165+LinPoly@users.noreply.github.com>
2025-08-13 07:10:40 +0000

    Add test
    Signed-off-by: Pengyun Lin <81065165+LinPoly@users.noreply.github.com>

--- tests/integration/defs/test_e2e.py
+++ tests/integration/defs/test_e2e.py
@@ -1441,6 +1441,13 @@ def test_trtllm_serve_lora_example(llm_root, llm_venv):
          str(test_root / "_test_trtllm_serve_lora.py")])
 
 
+def test_openai_harmony(llm_root, llm_venv):
+    test_root = unittest_path() / "llmapi" / "apps"
+    llm_venv.run_cmd(
+        ["-m", "pytest",
+         str(test_root / "_test_openai_harmony.py")])
+
+
 @pytest.mark.parametrize("backend", ["pytorch", "trt"])
 def test_openai_misc_example(llm_root, llm_venv, backend: str):
     test_root = unittest_path() / "llmapi" / "apps"
