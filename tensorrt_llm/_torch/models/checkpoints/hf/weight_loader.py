d7a45e709a6e116eba16a42937cfe592c2f58504
Lance Liao <108499334+lancelly@users.noreply.github.com>
2025-08-14 20:45:49 -0700

    fix potential hang caused by python multiprocessing
    Signed-off-by: Lance Liao <108499334+lancelly@users.noreply.github.com>

--- tensorrt_llm/_torch/models/checkpoints/hf/weight_loader.py
+++ tensorrt_llm/_torch/models/checkpoints/hf/weight_loader.py
@@ -1,6 +1,7 @@
 import glob
 import multiprocessing
 import os
+from concurrent.futures import ThreadPoolExecutor
 from typing import Any, List
 
 import psutil
@@ -120,7 +121,7 @@ class HfWeightLoader(BaseWeightLoader):
         if len(local_file_names) == 0:
             return
 
-        max_processes = min(multiprocessing.cpu_count() * 2, 16,
-                            len(local_file_names))
-        with multiprocessing.Pool(processes=max_processes) as pool:
-            pool.map(self._prefetch_one_file, local_file_names)
+        max_workers = min(multiprocessing.cpu_count() * 2, 16,
+                          len(local_file_names))
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            list(executor.map(self._prefetch_one_file, local_file_names))
