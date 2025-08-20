b511f9a36a8e637ad9ca632641397b495ee20afa
qqiao <qqiao@nvidia.com>
2025-08-20 01:45:15 -0700

    Use a debug branch of bloom
    Signed-off-by: qqiao <qqiao@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1,4 +1,4 @@
-@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@main']) _
+@Library(['bloom-jenkins-shared-lib@emma/debug_retry_cmd', 'trtllm-jenkins-shared-lib@main']) _
 
 import java.lang.InterruptedException
 import groovy.transform.Field
