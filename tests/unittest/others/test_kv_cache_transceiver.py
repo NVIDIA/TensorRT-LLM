fda143204088a2746df5196b5fe8a3590ee789a5
Bo Deng <deemod@nvidia.com>
2025-08-20 21:38:53 -0700

    refactor again
    Signed-off-by: Bo Deng <deemod@nvidia.com>

--- tests/unittest/others/test_kv_cache_transceiver.py
+++ tests/unittest/others/test_kv_cache_transceiver.py
@@ -42,30 +42,46 @@ def fill_kv_cache_buffer(kv_cache_manager):
         buffer.copy_(random_values)
 
 
+@pytest.fixture(scope="function")
+def ctx_gen_kv_cache_dtype(request):
+    if request.param == "ctx_fp8_gen_fp8":
+        return DataType.FP8, DataType.FP8
+    elif request.param == "ctx_fp16_gen_fp16":
+        return DataType.HALF, DataType.HALF
+    elif request.param == "ctx_bf16_gen_bf16":
+        return DataType.BF16, DataType.BF16
+    else:
+        raise ValueError(f"Invalid config: {request.param}")
+
+
 @pytest.mark.timeout(120)
-@pytest.mark.parametrize("kv_cache_dtype",
-                         [DataType.FP8, DataType.HALF, DataType.BF16],
-                         ids=["fp8", "fp16", "bf16"])
+@pytest.mark.parametrize(
+    "ctx_gen_kv_cache_dtype",
+    ["ctx_fp8_gen_fp8", "ctx_fp16_gen_fp16", "ctx_bf16_gen_bf16"],
+    ids=["ctx_fp8_gen_fp8", "ctx_fp16_gen_fp16", "ctx_bf16_gen_bf16"],
+    indirect=True)
 @pytest.mark.parametrize("attention_type",
                          [AttentionTypeCpp.DEFAULT, AttentionTypeCpp.MLA],
-                         ids=["default_attention", "mla_attention"])
-def test_kv_cache_transceiver_single_process(kv_cache_dtype, attention_type):
+                         ids=["mha", "mla"])
+def test_kv_cache_transceiver_single_process(ctx_gen_kv_cache_dtype,
+                                             attention_type):
     # Init kv_cache manager and cache transceiver
     mapping = Mapping(world_size=1, rank=0)
-    kv_cache_manager_0 = create_kv_cache_manager(mapping, kv_cache_dtype)
-    kv_cache_manager_1 = create_kv_cache_manager(mapping, kv_cache_dtype)
+    ctx_kv_cache_dtype, gen_kv_cache_dtype = ctx_gen_kv_cache_dtype
+    kv_cache_manager_ctx = create_kv_cache_manager(mapping, ctx_kv_cache_dtype)
+    kv_cache_manager_gen = create_kv_cache_manager(mapping, gen_kv_cache_dtype)
 
     cache_transceiver_config = trtllm.CacheTransceiverConfig(
         backend=trtllm.CacheTransceiverBackendType.DEFAULT,
         max_tokens_in_buffer=512)
 
-    kv_cache_transceiver_0 = create_kv_cache_transceiver(
-        mapping, kv_cache_manager_0, attention_type, cache_transceiver_config)
+    kv_cache_transceiver_ctx = create_kv_cache_transceiver(
+        mapping, kv_cache_manager_ctx, attention_type, cache_transceiver_config)
 
-    kv_cache_transceiver_1 = create_kv_cache_transceiver(
-        mapping, kv_cache_manager_1, attention_type, cache_transceiver_config)
+    kv_cache_transceiver_gen = create_kv_cache_transceiver(
+        mapping, kv_cache_manager_gen, attention_type, cache_transceiver_config)
 
-    fill_kv_cache_buffer(kv_cache_manager_0)
+    fill_kv_cache_buffer(kv_cache_manager_ctx)
 
     # init ctx request
     sampling_params = SamplingParams()
@@ -78,10 +94,11 @@ def test_kv_cache_transceiver_single_process(kv_cache_dtype, attention_type):
         is_streaming=False,
         llm_request_type=LlmRequestType.LLMREQUEST_TYPE_CONTEXT_ONLY)
 
-    kv_cache_manager_0.impl.add_sequence(ctx_request.py_request_id,
-                                         ctx_request.prompt_len, 1, ctx_request)
+    kv_cache_manager_ctx.impl.add_sequence(ctx_request.py_request_id,
+                                           ctx_request.prompt_len, 1,
+                                           ctx_request)
     # send ctx request
-    kv_cache_transceiver_0.respond_and_send_async(ctx_request)
+    kv_cache_transceiver_ctx.respond_and_send_async(ctx_request)
 
     # init gen request
     gen_request = LlmRequest(
@@ -94,16 +111,15 @@ def test_kv_cache_transceiver_single_process(kv_cache_dtype, attention_type):
         llm_request_type=LlmRequestType.LLMREQUEST_TYPE_GENERATION_ONLY,
         context_phase_params=ctx_request.context_phase_params)
 
-    kv_cache_manager_1.impl.add_sequence(gen_request.py_request_id,
-                                         gen_request.prompt_len, 1, gen_request)
+    kv_cache_manager_gen.impl.add_sequence(gen_request.py_request_id,
+                                           gen_request.prompt_len, 1,
+                                           gen_request)
     # send gen request
-    kv_cache_transceiver_1.request_and_receive_async(gen_request)
+    kv_cache_transceiver_gen.request_and_receive_async(gen_request)
 
-    if kv_cache_transceiver_0.check_context_transfer_status(1):
-        raise Exception("Context transfer status is not 0")
-    if kv_cache_transceiver_1.check_gen_transfer_status(1):
-        raise Exception("Gen transfer status is not 0")
+    kv_cache_transceiver_ctx.check_context_transfer_status(1)
+    kv_cache_transceiver_gen.check_gen_transfer_status(1)
 
     assert torch.equal(
-        kv_cache_manager_1.get_buffers(0),
-        kv_cache_manager_0.get_buffers(0)), "different kv-cache values"
+        kv_cache_manager_gen.get_buffers(0),
+        kv_cache_manager_ctx.get_buffers(0)), "different kv-cache values"
