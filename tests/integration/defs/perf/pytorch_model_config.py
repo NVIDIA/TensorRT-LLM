07c711eb1f759cbf3da02ef7e93bd050fdbf1c83
Wanli Jiang <35160485+Wanli-Jiang@users.noreply.github.com>
2025-08-22 10:00:04 +0800

    [TRTLLM-6825][fix] Update lora for phi4-mm (#6817)
    Signed-off-by: Wanli Jiang <35160485+Wanli-Jiang@users.noreply.github.com>

--- tests/integration/defs/perf/pytorch_model_config.py
+++ tests/integration/defs/perf/pytorch_model_config.py
@@ -191,15 +191,17 @@ def get_model_yaml_config(model_label: str,
         }
         if 'phi_4_multimodal_instruct' in model_label:
             lora_config['lora_config']['lora_target_modules'] = [
-                "attn_qkv", "attn_dense", "mlp_h_to_4h", "mlp_4h_to_h"
+                "attn_qkv", "attn_dense", "mlp_gate_up", "mlp_4h_to_h"
             ]
             lora_config['lora_config']['trtllm_modules_to_hf_modules'] = {
                 "attn_qkv": "qkv_proj",
                 "attn_dense": "o_proj",
-                "mlp_h_to_4h": "gate_up_proj",
+                "mlp_gate_up": "gate_up_proj",
                 "mlp_4h_to_h": "down_proj"
             }
             lora_config['lora_config']['max_lora_rank'] = 320
+            lora_config['lora_config'][
+                'swap_gate_up_proj_lora_b_weight'] = False
         base_config.update(lora_config)
 
     kv_cache_config = base_config.get('kv_cache_config', KvCacheConfig())
