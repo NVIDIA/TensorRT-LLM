6f574decf64c869d09560caeec29134a76424208
Yanchao Lu <yanchaol@nvidia.com>
2025-08-25 08:48:43 +0800

    Update jenkins/L0_Test.groovy
    Signed-off-by: Yanchao Lu <yanchaol@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1,4 +1,4 @@
-@Library(['bloom-jenkins-shared-lib@dev-yanchaol-empty-partition', 'trtllm-jenkins-shared-lib@main']) _
+@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@main']) _
 
 import java.lang.InterruptedException
 import groovy.transform.Field
