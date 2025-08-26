35031e46631ef403cc889a7f6f1cc48ec94154a5
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2025-08-26 05:12:44 -0700

    Fix
    Signed-off-by: Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>

--- examples/disaggregated/slurm/disagg_profiler/__init__.py
+++ examples/disaggregated/slurm/disagg_profiler/__init__.py
@@ -4,10 +4,8 @@ This package contains the job management and parameter sweeping functionality
 for the TRT-LLM disaggregated serving launcher.
 """
 
-from .job_manager import JobManager, calculate_nodes_needed, wait_for_server
+from .job_manager import JobManager, wait_for_server
 
 __all__ = [
-    'JobManager', 'calculate_nodes_needed', 'wait_for_server',
-    'ParameterSweeper', 'AutoSweeper', 'get_slurm_allocation',
-    'run_sweep_configuration'
+    'JobManager', 'wait_for_server',
 ]
