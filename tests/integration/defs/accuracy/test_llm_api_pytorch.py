4082ad627c2f42e0b4b29b10814a531e80c8cd14
Yaran Wu <28771492+aalanwyr@users.noreply.github.com>
2025-08-27 03:15:19 +0000

    fix issue
    Signed-off-by: Yaran Wu <28771492+aalanwyr@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api_pytorch.py
+++ tests/integration/defs/accuracy/test_llm_api_pytorch.py
@@ -2860,7 +2860,7 @@ class TestQwQ_32B(LlmapiAccuracyTestHarness):
     )
 
     def test_auto_dtype(self):
-        kv_cache_config = KvCacheConfig(free_gpu_memory_fraction=1)
+        kv_cache_config = KvCacheConfig(free_gpu_memory_fraction=0.9)
         with LLM(self.MODEL_PATH,
                  max_num_tokens=16384,
                  kv_cache_config=kv_cache_config) as llm:
@@ -2871,7 +2871,7 @@ class TestQwQ_32B(LlmapiAccuracyTestHarness):
 
     @pytest.mark.skip_less_device(2)
     def test_auto_dtype_tp2(self):
-        kv_cache_config = KvCacheConfig(free_gpu_memory_fraction=1)
+        kv_cache_config = KvCacheConfig(free_gpu_memory_fraction=0.9)
 
         with LLM(self.MODEL_PATH,
                  max_num_tokens=16384,
