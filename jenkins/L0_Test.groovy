d22a3171bdf8707cb4c80c1310b265dad9c9eaa0
Yuxian Qiu <142763828+yuxianq@users.noreply.github.com>
2025-08-27 02:25:15 +0000

    Fix CI error.
    Signed-off-by: Yuxian Qiu <142763828+yuxianq@users.noreply.github.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1893,7 +1893,7 @@ def launchTestJobs(pipeline, testFilter, dockerNode=null)
     fullSet += SBSASlurmTestConfigs.keySet()
 
     multiNodesSBSAConfigs = (1..7).collectEntries { i ->
-        ["GB200-8_GPUs-2_Nodes-PyTorch-Post-Merge-${i}", ["gb200-multi-node", "l0_gb200_multi_nodes", i, 7, 8, 2]]
+        ["GB200-8_GPUs-2_Nodes-PyTorch-Post-Merge-${i}".toString(), ["gb200-multi-node", "l0_gb200_multi_nodes", i, 7, 8, 2]]
     }
     fullSet += multiNodesSBSAConfigs.keySet()
 
