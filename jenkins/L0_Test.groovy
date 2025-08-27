f2b66a5c0701aecd404604e52d73234b68c9e297
Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>
2025-08-27 10:10:15 -0700

    switch to main branch for bsl

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1,4 +1,4 @@
-@Library(['bloom-jenkins-shared-lib@dev-yuanjingx-slurm_refactor', 'trtllm-jenkins-shared-lib@main']) _
+@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@main']) _
 
 import java.lang.InterruptedException
 import groovy.transform.Field
