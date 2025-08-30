e344635f8dc34fd98ab854a6262732d1bf70b6a5
greg-kwasniewski1 <213329731+greg-kwasniewski1@users.noreply.github.com>
2025-08-30 12:21:03 -0700

    Fixed default arguments for sharding
    Signed-off-by: greg-kwasniewski1 <213329731+greg-kwasniewski1@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/llm_args.py
+++ tensorrt_llm/_torch/auto_deploy/llm_args.py
@@ -173,7 +173,7 @@ class AutoDeployConfig(DynamicYamlMixInForSettings, BaseSettings):
     )
 
     sharding_dims: List[str] = Field(
-        default=["tp", "ep", "dp"],
+        default=["tp", "ep", "bmm"],
         description="The sharding methods to apply by the heuristic sharding stage.",
     )
 
