56896547b64b82eaca42355638c5dec592ff571a
Netanel Haber <58652339+netanel-haber@users.noreply.github.com>
2025-09-01 15:29:26 +0000

    move produce_stopwords to llm_request, alongside convert_wordlist
    Signed-off-by: Netanel Haber <58652339+netanel-haber@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/sampler_utils.py
+++ tensorrt_llm/_torch/pyexecutor/sampler_utils.py
@@ -13,11 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from itertools import pairwise
-
 from tensorrt_llm.bindings.executor import FinishReason
 
-from .llm_request import LlmRequest
+from .llm_request import LlmRequest, produce_stop_words
 
 BEAM_0 = 0
 SINGLE_BEAM_WIDTH = 1
@@ -27,15 +25,6 @@ def max_token_criteria_1_beam(request: LlmRequest, max_seq_len: int) -> bool:
     num_tokens = request.get_num_tokens(BEAM_0)
     return (num_tokens - request.py_orig_prompt_len
             >= request.py_max_new_tokens) or (num_tokens >= max_seq_len)
-
-
-def produce_stop_words(py_stop_words_list: list[list[int]]):
-    """inverts `.../llm_request::def convert_wordlist()`"""
-    stop_words_list, prefix_sum = py_stop_words_list
-    for start, end in pairwise((0, *prefix_sum)):  # first element: prepend 0
-        if end == -1:  # -1 is a sentinel value in convert_wordlist
-            break
-        yield stop_words_list[start:end]
 
 
 def stop_token_criteria(py_stop_words_list: list[list[int]] | None,
