01af9de89ac648fd20dc6118089437dbd1adba70
qqiao <qqiao@nvidia.com>
2025-09-01 20:19:39 -0700

    Test crun fix
    Signed-off-by: qqiao <qqiao@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1,4 +1,4 @@
-@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@main']) _
+@Library(['bloom-jenkins-shared-lib@dev-yanchaol-crun', 'trtllm-jenkins-shared-lib@main']) _
 
 import java.lang.InterruptedException
 import groovy.transform.Field
