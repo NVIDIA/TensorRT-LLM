9bfea2c039eaa6d15e1f2ac1eed88bf91ed5da1c
Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>
2025-09-02 09:08:07 -0700

    Update
    Signed-off-by: Kaiyu Xie <26294424+kaiyux@users.noreply.github.com>

--- tests/unittest/_torch/modules/test_fused_moe.py
+++ tests/unittest/_torch/modules/test_fused_moe.py
@@ -301,8 +301,7 @@ def test_fused_moe_alltoall(alltoall_method_type):
                          ids=lambda s: s.name)
 def test_fused_moe_alltoall_fp4(alltoall_method_type):
 
-    if alltoall_method_type == AlltoallMethodType.DeepEPLowLatency \
-            or alltoall_method_type == AlltoallMethodType.DeepEP:
+    if alltoall_method_type == AlltoallMethodType.DeepEPLowLatency:
         pytest.skip("Skipped due to https://nvbugs/5467531")
 
     world_size = 4
