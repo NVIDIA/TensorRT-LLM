bd7436c4b6a8db9d7b683fd3ab9531f9a9435f8b
junq <22017000+QiJune@users.noreply.github.com>
2025-09-02 15:41:45 -0700

    spawn parallel workers for testing
    Signed-off-by: junq <22017000+QiJune@users.noreply.github.com>

--- tests/integration/defs/test_unittests.py
+++ tests/integration/defs/test_unittests.py
@@ -153,7 +153,7 @@ def test_unittests_v2(llm_root, llm_venv, case: str, output_dir, request):
             output_dir,
             f'parallel-sub-results-unittests-{case_fn}.xml.intermediate')
         parallel_command = command + [
-            "-n", f"{num_workers}", f"--junitxml={parallel_output_xml}"
+            "-n", f"{num_workers}", "--dist-start-method", "spawn", f"--junitxml={parallel_output_xml}"
         ]
         passed = run_command(parallel_command)
 
