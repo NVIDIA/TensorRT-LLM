96f39144965d63a99d536c4811c4b68c540267f8
Zongfei Jing <20381269+zongfeijing@users.noreply.github.com>
2025-09-09 05:22:04 +0800

    [None][chore] Make low_precision_combine as a llm arg (#7598)
    Signed-off-by: Zongfei Jing <20381269+zongfeijing@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/config.py
+++ tensorrt_llm/_torch/pyexecutor/config.py
@@ -55,6 +55,7 @@ class PyTorchConfig:
     moe_backend: str = 'CUTLASS'
 
     moe_disable_finalize_fusion: bool = False
+    use_low_precision_moe_combine: bool = False
 
     enable_mixed_sampler: bool = False
     """
