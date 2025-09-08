e30e0c86932accf1d5689275ab4900daf18c16f9
Xiwen Yu <13230610+VALLIS-NERIA@users.noreply.github.com>
2025-09-08 21:02:35 +0800

    waive
    Signed-off-by: Xiwen Yu <13230610+VALLIS-NERIA@users.noreply.github.com>

--- tests/unittest/_torch/auto_deploy/unit/singlegpu/test_ad_build_small_single.py
+++ tests/unittest/_torch/auto_deploy/unit/singlegpu/test_ad_build_small_single.py
@@ -64,11 +64,12 @@ def _check_ad_config(experiment_config: ExperimentConfig, llm_args: LlmArgs):
             attn_backend="triton",
             compile_backend="torch-simple",
         ),
-        get_small_model_config(
-            "meta-llama/Llama-4-Scout-17B-16E-Instruct",
-            attn_backend="flashinfer",
-            compile_backend="torch-simple",
-        ),
+        # disabled due to https://nvbugspro.nvidia.com/bug/5505835
+        # get_small_model_config(
+        #     "meta-llama/Llama-4-Scout-17B-16E-Instruct",
+        #     attn_backend="flashinfer",
+        #     compile_backend="torch-simple",
+        # ),
         get_small_model_config(
             "deepseek-ai/DeepSeek-V3",
             attn_backend="triton",
