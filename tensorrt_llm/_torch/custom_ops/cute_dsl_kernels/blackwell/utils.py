4dd97f774038cd4b25c039ff2e297421bc92fc95
Mindy Li <11663212+limin2021@users.noreply.github.com>
2025-09-08 21:21:30 -0700

    update.
    Signed-off-by: Mindy Li <11663212+limin2021@users.noreply.github.com>

--- tensorrt_llm/_torch/custom_ops/cute_dsl_kernels/blackwell/utils.py
+++ tensorrt_llm/_torch/custom_ops/cute_dsl_kernels/blackwell/utils.py
@@ -1,14 +1,6 @@
 """
-Copyright (c) 2025 by FlashInfer team.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-  http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
 """
 
 import ctypes
@@ -19,7 +11,7 @@ from cutlass._mlir import ir
 from cutlass.cute.typing import AddressSpace, Numeric, Pointer, Type
 
 
-# WAR for CuTeDSL make_ptr implementation for flashinfer
+# WAR for CuTeDSL make_ptr implementation
 class _Pointer(Pointer):
     """Runtime representation of a pointer that can inter-operate with
     various data structures, including numpy arrays and device memory.
