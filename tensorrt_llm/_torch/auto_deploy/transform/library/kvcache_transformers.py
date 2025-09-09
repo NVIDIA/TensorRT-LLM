0b46c8cd87417fef3c97e033c65393ae07b72965
Lucas Liebenwein <11156568+lucaslie@users.noreply.github.com>
2025-09-08 22:23:26 -0700

    transformers mode refined
    Signed-off-by: Lucas Liebenwein <11156568+lucaslie@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/transform/library/kvcache_transformers.py
+++ tensorrt_llm/_torch/auto_deploy/transform/library/kvcache_transformers.py
@@ -9,11 +9,108 @@ import torch.fx as fx
 from torch.fx import GraphModule, Node
 from transformers.modeling_utils import ALL_ATTENTION_FUNCTIONS
 
-from ...custom_ops.attention_interface import AttentionDescriptor, AttentionRegistry
+from ...custom_ops.attention_interface import AttentionDescriptor
 from ...models.factory import ModelFactory
 from ...shim.interface import CachedSequenceInterface
-from ..interface import SharedConfig, TransformInfo, TransformRegistry
+from ..interface import BaseTransform, SharedConfig, TransformInfo, TransformRegistry
 from .kvcache import InsertCachedAttention
+
+
+def fake_profiler_mha(
+    module: torch.nn.Module,
+    query: torch.Tensor,
+    key: torch.Tensor,
+    value: torch.Tensor,
+    attention_mask: Optional[torch.Tensor],
+    profiling_metadata: Dict[str, Any],
+    **kwargs,
+):
+    """Fake attn to populated attention nodes of each layer."""
+    # logic adopted from
+    # https://github.com/huggingface/transformers/blob/main/src/transformers/integrations/sdpa_attention.py#L73
+    if "is_causal" in kwargs:
+        is_causal = kwargs["is_causal"]
+    else:
+        is_causal = (
+            query.shape[2] > 1 and attention_mask is None and getattr(module, "is_causal", True)
+        )
+
+    # construct kwargs for bsnd_grouped_sdpa
+    node_kwargs = {"attn_mask": attention_mask, "is_causal": is_causal}
+    kwargs_to_op = {
+        "dropout": "dropout_p",
+        "scaling": "scale",
+        "scale": "scale",
+        "s_aux": "sinks",
+        "sinks": "sinks",
+        "sliding_window": "sliding_window",
+        "logit_cap": "logit_cap",
+    }
+    for k_kwargs, k_op_kwargs in kwargs_to_op.items():
+        if k_kwargs in kwargs:
+            node_kwargs[k_op_kwargs] = kwargs[k_kwargs]
+
+    # store new fake attention node in graph module
+    graph: fx.Graph = profiling_metadata["gm"].graph
+    q_fake = graph.placeholder(name="q_fake")
+    q_fake.meta["val"] = torch.empty_like(query.transpose(1, 2), device="meta", dtype=query.dtype)
+    k_fake = graph.placeholder(name="k_fake")
+    k_fake.meta["val"] = torch.empty_like(key.transpose(1, 2), device="meta", dtype=key.dtype)
+    v_fake = graph.placeholder(name="v_fake")
+    v_fake.meta["val"] = torch.empty_like(value.transpose(1, 2), device="meta", dtype=value.dtype)
+
+    module._node_ref = graph.call_function(
+        torch.ops.auto_deploy.torch_attention_bsnd_grouped_sdpa,
+        args=(q_fake, k_fake, v_fake),
+        kwargs=node_kwargs,
+    )
+
+    # update num matches
+    profiling_metadata["num_matches"] += 1
+
+    # Return fake outputs
+    attn_output = torch.empty_like(query.transpose(1, 2).contiguous())
+    attn_weights = None
+
+    return attn_output, attn_weights
+
+
+@TransformRegistry.register("detect_hf_attn_layers")
+class DetectHFAttnLayers(BaseTransform):
+    """Detect the number of attn layers in the model and store a node-like reference for them.
+
+    This is achieved by running a single forward pass to profile the model.
+    """
+
+    def _apply(
+        self,
+        gm: GraphModule,
+        cm: CachedSequenceInterface,
+        factory: ModelFactory,
+        shared_config: SharedConfig,
+    ) -> Tuple[GraphModule, TransformInfo]:
+        model = gm.factory_model
+
+        # Register profiler attn operator
+        ALL_ATTENTION_FUNCTIONS.register("ad_profile_mha", fake_profiler_mha)
+        attn_implementation = model.config._attn_implementation
+        model.config._attn_implementation = "ad_profile_mha"
+
+        # update the graph module with the fake attn nodes during the profiling run
+        profiling_metadata = {"gm": gm, "num_matches": 0}
+        model.forward(*cm.args, profiling_metadata=profiling_metadata)
+
+        # switch back to original attn implementation
+        model.config._attn_implementation = attn_implementation
+
+        info = TransformInfo(
+            skipped=False,
+            num_matches=profiling_metadata["num_matches"],
+            is_clean=True,
+            has_valid_shapes=True,
+        )
+
+        return gm, info
 
 
 def get_cached_attn(
@@ -68,7 +165,7 @@ def get_cached_attn(
     return cached_attn
 
 
-def forward_cached2(gm: GraphModule, *cm_args):
+def forward_cached(gm: GraphModule, *cm_args):
     """Pre-process cached arguments for attention and then run regular factory forward."""
     # retrieve uncached args
     uncached_args = cm_args[: gm.num_uncached_args]
@@ -90,64 +187,10 @@ def forward_cached2(gm: GraphModule, *cm_args):
     return gm.factory_model.forward(*uncached_args, cached_attn_args_lookup=cached_attn_args_lookup)
 
 
-def fake_profiler_mha(
-    module: torch.nn.Module,
-    query: torch.Tensor,
-    key: torch.Tensor,
-    value: torch.Tensor,
-    attention_mask: Optional[torch.Tensor],
-    graph_module: GraphModule,
-    **kwargs,
-):
-    """Fake attn to populated attention nodes of each layer."""
-    # logic adopted from
-    # https://github.com/huggingface/transformers/blob/main/src/transformers/integrations/sdpa_attention.py#L73
-    if "is_causal" in kwargs:
-        is_causal = kwargs["is_causal"]
-    else:
-        is_causal = (
-            query.shape[2] > 1 and attention_mask is None and getattr(module, "is_causal", True)
-        )
-
-    # construct kwargs for bsnd_grouped_sdpa
-    node_kwargs = {"attn_mask": attention_mask, "is_causal": is_causal}
-    kwargs_to_op = {
-        "dropout": "dropout_p",
-        "scaling": "scale",
-        "scale": "scale",
-        "s_aux": "sinks",
-        "sinks": "sinks",
-        "sliding_window": "sliding_window",
-        "logit_cap": "logit_cap",
-    }
-    for k_kwargs, k_op_kwargs in kwargs_to_op.items():
-        if k_kwargs in kwargs:
-            node_kwargs[k_op_kwargs] = kwargs[k_kwargs]
-
-    # store new fake attention node in graph module
-    graph: fx.Graph = graph_module.graph
-    q_fake = graph.placeholder(name="q_fake")
-    q_fake.meta["val"] = torch.empty_like(query.transpose(1, 2), device="meta", dtype=query.dtype)
-    k_fake = graph.placeholder(name="k_fake")
-    k_fake.meta["val"] = torch.empty_like(key.transpose(1, 2), device="meta", dtype=key.dtype)
-    v_fake = graph.placeholder(name="v_fake")
-    v_fake.meta["val"] = torch.empty_like(value.transpose(1, 2), device="meta", dtype=value.dtype)
-
-    module._node_ref = graph.call_function(
-        torch.ops.auto_deploy.torch_attention_bsnd_grouped_sdpa,
-        args=(q_fake, k_fake, v_fake),
-        kwargs=node_kwargs,
-    )
-
-    # Return fake outputs
-    attn_output = torch.empty_like(query.transpose(1, 2).contiguous())
-    attn_weights = None
-
-    return attn_output, attn_weights
-
-
-@TransformRegistry.register("transformers_replace_cached_attn2")
-class HFReplaceCachedAttn2(InsertCachedAttention):
+# TODO: how running different kv cache transforms look like? This one below wouldn't work if we
+# had multiple types attention to replace...
+@TransformRegistry.register("transformers_replace_cached_attn")
+class HFReplaceCachedAttn(InsertCachedAttention):
     """Replace cached attention for the factory model, update inputs and outputs, and patch the gm forward."""
 
     def _process_get_metadata(
@@ -186,35 +229,15 @@ class HFReplaceCachedAttn2(InsertCachedAttention):
         factory: ModelFactory,
         shared_config: SharedConfig,
     ) -> Tuple[GraphModule, TransformInfo]:
-        attn_descriptor = AttentionRegistry.get(self.config.attn_backend)
-        model = gm.factory_model
-
-        # First, we run a profile to get the number of attns and per-rank kv cache shapes.
-        # We do this to avoid any assumptions about params naming or tp plan of the hf model.
-
-        # Register fake profiler attn
-        ALL_ATTENTION_FUNCTIONS.register("ad_cached_mha", fake_profiler_mha)
-        model.config._attn_implementation = "ad_cached_mha"
-
-        # update the graph module with the fake attn nodes from the profiling run
-        model.forward(*cm.args, graph_module=gm)
-
-        # Ends profiling, switch back to real attn operator
-        ALL_ATTENTION_FUNCTIONS.register("ad_cached_mha", get_cached_attn(attn_descriptor))
-
-        # check regular number of args
-        gm.num_uncached_args = len(cm.args)
-
-        # patch forward method to be ready for cached attn
-        gm.node_to_cache_buffer_indices: Dict[Node, List[int]] = {}
+        # patch gm so we can run the insert cached attn transform
+        gm.node_to_cache_buffer_names: Dict[Node, List[str]] = {}
         gm.node_to_constants: Dict[Node, List[Any]] = {}
-        gm.forward = types.MethodType(forward_cached2, gm)
 
-        # switch to cached attn inputs now
+        # switch to to cached inputs now as expected by insert cached attn transform
+        gm.num_uncached_args = len(cm.args)
         cm.info.switch_to_cached_attn_inputs()
 
-        # configure and store attn nodes, add caches to cm via super()._apply
-        gm.node_to_cache_buffer_names: Dict[Node, List[str]] = {}  # need this for super()._apply
+        # run actual insert cached attn transform
         gm, info = super()._apply(gm, cm, factory, shared_config)
 
         # we retrieve a key to index map for cm.args
@@ -226,8 +249,14 @@ class HFReplaceCachedAttn2(InsertCachedAttention):
         ]
 
         # store a map from node to cache_buffer indices
+        gm.node_to_cache_buffer_indices: Dict[Node, List[int]] = {}
         for node, c_b_names in gm.node_to_cache_buffer_names.items():
             gm.node_to_cache_buffer_indices[node] = [cm_keys_to_index[k] for k in c_b_names]
+
+        # switch to cached forward which requires the above computed index maps
+        ALL_ATTENTION_FUNCTIONS.register("ad_cached_mha", get_cached_attn(self.attn_descriptor))
+        gm.factory_model.config._attn_implementation = "ad_cached_mha"
+        gm.forward = types.MethodType(forward_cached, gm)
 
         # we assume graph is clean again by definition
         info_dict = info.model_dump()
