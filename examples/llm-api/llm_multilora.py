63c0714835284f320b6aeff36449c41143a82024
Amit Zuker <203509407+amitz-nv@users.noreply.github.com>
2025-09-15 15:31:28 +0000

    Change examples/llm-api/llm_multilora.py to be able to receive the LoRA adapters dir paths through cmd line, change test_llmapi_example_multilora to pass these LoRA adapters path as cmd line args to avoid downloading from HF
    Signed-off-by: Amit Zuker <203509407+amitz-nv@users.noreply.github.com>

--- examples/llm-api/llm_multilora.py
+++ examples/llm-api/llm_multilora.py
@@ -1,6 +1,7 @@
 ### :section Customization
 ### :title Generate text with multiple LoRA adapters
 ### :order 5
+import click
 from huggingface_hub import snapshot_download
 
 from tensorrt_llm import LLM
@@ -8,17 +9,37 @@ from tensorrt_llm.executor import LoRARequest
 from tensorrt_llm.lora_helper import LoraConfig
 
 
-def main():
+@click.command()
+@click.option("--chatbot_lora_dir",
+              type=str,
+              default=None,
+              help="Path to the chatbot LoRA directory")
+@click.option("--mental_health_lora_dir",
+              type=str,
+              default=None,
+              help="Path to the mental health LoRA directory")
+@click.option("--tarot_lora_dir",
+              type=str,
+              default=None,
+              help="Path to the tarot LoRA directory")
+def main(chatbot_lora_dir: str | None, mental_health_lora_dir: str | None,
+         tarot_lora_dir: str | None):
 
     # Download the LoRA adapters from huggingface hub.
-    lora_dir1 = snapshot_download(repo_id="snshrivas10/sft-tiny-chatbot")
-    lora_dir2 = snapshot_download(
-        repo_id="givyboy/TinyLlama-1.1B-Chat-v1.0-mental-health-conversational")
-    lora_dir3 = snapshot_download(repo_id="barissglc/tinyllama-tarot-v1")
+    if chatbot_lora_dir is None:
+        chatbot_lora_dir = snapshot_download(
+            repo_id="snshrivas10/sft-tiny-chatbot")
+    if mental_health_lora_dir is None:
+        mental_health_lora_dir = snapshot_download(
+            repo_id=
+            "givyboy/TinyLlama-1.1B-Chat-v1.0-mental-health-conversational")
+    if tarot_lora_dir is None:
+        tarot_lora_dir = snapshot_download(
+            repo_id="barissglc/tinyllama-tarot-v1")
 
     # Currently, we need to pass at least one lora_dir to LLM constructor via build_config.lora_config.
     # This is necessary because it requires some configuration in the lora_dir to build the engine with LoRA support.
-    lora_config = LoraConfig(lora_dir=[lora_dir1],
+    lora_config = LoraConfig(lora_dir=[chatbot_lora_dir],
                              max_lora_rank=64,
                              max_loras=3,
                              max_cpu_loras=3)
@@ -39,10 +60,11 @@ def main():
     for output in llm.generate(prompts,
                                lora_request=[
                                    None,
-                                   LoRARequest("chatbot", 1, lora_dir1), None,
-                                   LoRARequest("mental-health", 2, lora_dir2),
+                                   LoRARequest("chatbot", 1, chatbot_lora_dir),
                                    None,
-                                   LoRARequest("tarot", 3, lora_dir3)
+                                   LoRARequest("mental-health", 2,
+                                               mental_health_lora_dir), None,
+                                   LoRARequest("tarot", 3, tarot_lora_dir)
                                ]):
         prompt = output.prompt
         generated_text = output.outputs[0].text
