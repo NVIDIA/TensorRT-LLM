4b30a472e79bccf6c09548eacb48a1b969ad16d7
Mindy Li <11663212+limin2021@users.noreply.github.com>
2025-09-17 02:56:55 -0700

    fix
    Signed-off-by: Mindy Li <11663212+limin2021@users.noreply.github.com>

--- tensorrt_llm/_torch/cute_dsl_utils.py
+++ tensorrt_llm/_torch/cute_dsl_utils.py
@@ -7,6 +7,8 @@ IS_CUTLASS_DSL_AVAILABLE = False
 
 if platform.system() != "Windows":
     try:
+        import cutlass  # noqa
+        import cutlass.cute as cute  # noqa
         logger.info(f"cutlass dsl is available")
         IS_CUTLASS_DSL_AVAILABLE = True
     except ImportError:
