f3392f07bf7864b065c3ff85fb93a4b39aaa6652
Yukun He <23156053+hyukn@users.noreply.github.com>
2025-09-18 08:25:58 +0000

    [None][feat] Allreduce benchmark modification Modify allreduce benchmark to avoid invalid shapes, with some minor revising. Fix a minor issue in allreduceOp that strategy can not be overridden when ONESHOT or TWOSHOT is set.
    Signed-off-by: Yukun He <23156053+hyukn@users.noreply.github.com>

--- tests/microbenchmarks/all_reduce.py
+++ tests/microbenchmarks/all_reduce.py
@@ -14,9 +14,11 @@
 # limitations under the License.
 
 from argparse import ArgumentParser
+from itertools import product
 
 # isort: off
 import torch
+import pandas as pd
 # isort: on
 try:
     from cuda.bindings import runtime as cudart
@@ -27,15 +29,17 @@ import tensorrt_llm as tllm
 from tensorrt_llm import Mapping
 from tensorrt_llm._torch.distributed import AllReduce, AllReduceFusionOp
 from tensorrt_llm._torch.modules.rms_norm import RMSNorm
-from tensorrt_llm._utils import local_mpi_rank, local_mpi_size
+from tensorrt_llm._utils import local_mpi_rank, local_mpi_size, nvtx_range
 from tensorrt_llm.bindings.internal.runtime import delay_kernel
 from tensorrt_llm.functional import AllReduceParams, AllReduceStrategy
+from tensorrt_llm.plugin.plugin import CustomAllReduceHelper
 
 
 def allreduce_benchmark(dtype: str,
                         test_range: str = "1,10000000,10",
                         no_header: bool = False,
-                        enable_cudagraph: bool = False):
+                        enable_cudagraph: bool = False,
+                        output_csv: str = None):
     tllm.logger.set_level('error')
     world_size = tllm.mpi_world_size()
     rank = tllm.mpi_rank()
@@ -52,120 +56,145 @@ def allreduce_benchmark(dtype: str,
 
     torch_dtype = tllm._utils.str_dtype_to_torch(dtype)
     min_size, max_size, ratio = [int(i) for i in test_range.split(",")]
-    inner_loop = 1200
+    inner_loop = 200
     outer_loop = 10
 
-    size = min_size
-    hidden_size = size
-    bs = 1
-    if mapping.rank == 0 and not no_header:
-        print(
-            f"{'world_size':<15}, {'dtype':<10}, {'message size':<15}, {'strategy':<10}, {'fusion':<20}, {'version':<10}, {'duration (ms)':<10}"
-        )
+    hidden_size = min_size
+    num_tokens = 8
+    size = max(num_tokens * hidden_size, min_size)
+
+    # generate shape list
+    shape_list = []
+    # make it simple
     while size < max_size:
-        input = torch.ones((bs, hidden_size), dtype=torch_dtype, device="cuda")
-
-        for version in ["v1"]:
-            for fusion in [
-                    AllReduceFusionOp.RESIDUAL_RMS_NORM, AllReduceFusionOp.NONE
-            ]:
-                for strategy in [
-                        AllReduceStrategy.NCCL,
-                        AllReduceStrategy.ONESHOT,
-                        AllReduceStrategy.TWOSHOT,
-                ]:
-                    if size >= 25600000 and fusion != AllReduceFusionOp.NONE:
-                        continue
-                    allreduce = AllReduce(mapping=mapping, strategy=strategy)
-                    if fusion == AllReduceFusionOp.RESIDUAL_RMS_NORM:
-                        norm_weight = torch.randn((hidden_size, ),
-                                                  dtype=torch_dtype,
-                                                  device="cuda")
-                        norm = RMSNorm(hidden_size=hidden_size,
-                                       dtype=torch_dtype,
-                                       eps=1e-5).cuda()
-                        norm.weight.data.copy_(norm_weight)
-                        if version == "v1":
-                            params = {
-                                "all_reduce_params":
-                                AllReduceParams(fusion_op=fusion,
-                                                residual=input,
-                                                norm_weight=norm.weight,
-                                                eps=norm.variance_epsilon)
-                            }
-                        else:
-                            params = {
-                                "reduce_fusion_inputs": [input, norm.weight],
-                                "eps": norm.variance_epsilon,
-                                "fusion_op": fusion
-                            }
-                    else:
-                        if version == "v1":
-                            params = {
-                                "all_reduce_params":
-                                AllReduceParams(fusion_op=fusion)
-                            }
-                        else:
-                            continue
-
-                    def func(input):
-                        for _ in range(inner_loop):
-                            input = allreduce(input, **params)
-                            if fusion == AllReduceFusionOp.RESIDUAL_RMS_NORM:
-                                input = input[0]
-                        return input
-
-                    start = [
-                        torch.cuda.Event(enable_timing=True)
-                        for _ in range(outer_loop)
-                    ]
-                    stop = [
-                        torch.cuda.Event(enable_timing=True)
-                        for _ in range(outer_loop)
-                    ]
-                    graph = torch.cuda.CUDAGraph()
-
-                    stream = torch.cuda.Stream()
-                    with torch.cuda.stream(stream):
-                        if enable_cudagraph:
-                            for _ in range(2):
-                                func(input)
-                            with torch.cuda.graph(graph, stream=stream):
-                                output = func(input)
-                        tllm.mpi_barrier()
-                        delay_kernel(2000000, stream)
-                        torch.cuda.profiler.start()
-                        for i in range(outer_loop):
-                            start[i].record(stream)
-                            if enable_cudagraph:
-                                graph.replay()
-                            else:
-                                output = func(input)
-                            stop[i].record(stream)
-
-                    torch.cuda.synchronize()
-                    torch.cuda.profiler.stop()
-                    runtimes = [
-                        start[i].elapsed_time(stop[i])
-                        for i in range(outer_loop)
-                    ]
-                    median_ms = sorted(runtimes)[len(runtimes) // 2]
-
-                    if fusion == AllReduceFusionOp.NONE:
-                        allreduce_ref = (input * world_size)**inner_loop
-                        torch.testing.assert_close(output, allreduce_ref)
-
-                    if mapping.rank == 0:
-                        print(
-                            f"{mapping.world_size:<15}, {dtype:<10}, {size:<15}, {strategy.name:<10}, {fusion.name:<20}, {version:<10}, {median_ms:<10.2f}"
-                        )
-
+        shape_list.append((num_tokens, hidden_size))
         size *= ratio
         if hidden_size * ratio > 4096:
-            bs *= ratio
+            num_tokens *= ratio
         else:
             hidden_size *= ratio
-        assert size == bs * hidden_size
+
+    fusion_patterns = [
+        AllReduceFusionOp.NONE,
+        AllReduceFusionOp.RESIDUAL_RMS_NORM,
+    ]
+    strategies = [
+        AllReduceStrategy.NCCL,
+        AllReduceStrategy.ONESHOT,
+        AllReduceStrategy.TWOSHOT,
+    ]
+    versions = ["v1"]
+    df = pd.DataFrame()
+    for (num_tokens, hidden_size) in shape_list:
+        message_size = num_tokens * hidden_size * torch.finfo(
+            torch_dtype).bits // 8
+        if num_tokens < mapping.tp_size:
+            continue
+        if message_size > CustomAllReduceHelper.max_workspace_size_auto(
+                mapping.tp_size):
+            continue
+
+        input = torch.ones((num_tokens, hidden_size),
+                           dtype=torch_dtype,
+                           device="cuda")
+        norm_weight = torch.randn((hidden_size, ),
+                                  dtype=torch_dtype,
+                                  device="cuda")
+        norm = RMSNorm(hidden_size=hidden_size, dtype=torch_dtype,
+                       eps=1e-5).cuda()
+        norm.weight.data.copy_(norm_weight)
+        for version, fusion, strategy in product(versions, fusion_patterns,
+                                                 strategies):
+            allreduce = AllReduce(mapping=mapping, strategy=strategy)
+            allreduce_params = AllReduceParams(
+                fusion_op=fusion,
+                residual=input,
+                norm_weight=norm.weight,
+                eps=norm.variance_epsilon,
+            )
+
+            allreduce = AllReduce(mapping=mapping, strategy=strategy)
+
+            def func(input):
+                for _ in range(inner_loop):
+                    output = allreduce(input,
+                                       all_reduce_params=allreduce_params)
+                return output if fusion == AllReduceFusionOp.NONE else output[0]
+
+            start = [
+                torch.cuda.Event(enable_timing=True) for _ in range(outer_loop)
+            ]
+            stop = [
+                torch.cuda.Event(enable_timing=True) for _ in range(outer_loop)
+            ]
+            graph = torch.cuda.CUDAGraph()
+
+            stream = torch.cuda.Stream()
+            with torch.cuda.stream(stream), nvtx_range(
+                    f"allreudce: shape={num_tokens}x{hidden_size} fusion={fusion} strategy={strategy}"
+            ):
+                if enable_cudagraph:
+                    # CUDA graph warmup then capture
+                    for _ in range(2):
+                        func(input)
+                    with torch.cuda.graph(graph, stream=stream):
+                        output = func(input)
+                # warmup for no cuda graph
+                func(input)
+
+                tllm.mpi_barrier()
+                # add delay to avoid the effect of host time overhead
+                delay_kernel(20000, stream)
+
+                torch.cuda.synchronize()
+                torch.cuda.profiler.start()
+
+                # start profiling loop
+                for i in range(outer_loop):
+                    start[i].record(stream)
+                    if enable_cudagraph:
+                        graph.replay()
+                    else:
+                        output = func(input)
+                    stop[i].record(stream)
+
+            torch.cuda.synchronize()
+            torch.cuda.profiler.stop()
+            runtimes = [
+                start[i].elapsed_time(stop[i]) for i in range(outer_loop)
+            ]
+            median_ms = sorted(runtimes)[len(runtimes) // 2] / inner_loop
+
+            if fusion == AllReduceFusionOp.NONE:
+                allreduce_ref = (input * world_size)
+                torch.testing.assert_close(output,
+                                           allreduce_ref,
+                                           atol=1e-2,
+                                           rtol=1e-2,
+                                           msg="Allreduce result mismatch")
+
+            if mapping.rank == 0:
+                df = pd.concat([
+                    df,
+                    pd.DataFrame({
+                        "world_size": [mapping.world_size],
+                        "dtype": [dtype],
+                        "size": [message_size],
+                        "shape": [f"{num_tokens}x{hidden_size}"],
+                        "strategy": [strategy.name],
+                        "fusion": [fusion.name],
+                        "version": [version],
+                        "time (us)": [median_ms * 1000]
+                    })
+                ])
+
+    # print the dataframe
+    if mapping.rank == 0:
+        print(df)
+
+    # # save the dataframe to a csv file
+    if mapping.rank == 0:
+        df.to_csv(f"{output_csv}", index=False)
 
 
 if __name__ == "__main__":
@@ -178,7 +207,9 @@ if __name__ == "__main__":
         help="min_size,max_size,multiplicative_ratio")
     parser.add_argument("--no-header", action="store_true")
     parser.add_argument("--enable-cudagraph", action="store_true")
+    parser.add_argument("--output_csv", type=str, default=None)
+
     args = parser.parse_args()
 
     allreduce_benchmark(args.dtype, args.range, args.no_header,
-                        args.enable_cudagraph)
+                        args.enable_cudagraph, args.output_csv)
