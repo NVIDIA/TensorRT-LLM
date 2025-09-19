008396338708c96bc35a02875083f34c4ef6f587
yufeiwu <230315618+yufeiwu-nv@users.noreply.github.com>
2025-09-19 15:07:44 +0000

    [None][test] Update llm_models_root function to improve path handling and validation without docker(baremetal)
    Signed-off-by: yufeiwu <230315618+yufeiwu-nv@users.noreply.github.com>

--- tests/integration/defs/conftest.py
+++ tests/integration/defs/conftest.py
@@ -75,11 +75,20 @@ def wget(url, out):
 
 
 def llm_models_root() -> str:
-    """return LLM_MODELS_ROOT path if it is set in env, assert when it's set but not a valid path"""
-    DEFAULT_LLM_MODEL_ROOT = os.path.join("/scratch.trt_llm_data", "llm-models")
-    LLM_MODELS_ROOT = os.environ.get("LLM_MODELS_ROOT", DEFAULT_LLM_MODEL_ROOT)
+    """Return LLM_MODELS_ROOT path if it is set in env, assert when it's set but not a valid path."""
 
-    return LLM_MODELS_ROOT
+    root = Path("/home/scratch.trt_llm_data/llm-models/")
+    if "LLM_MODELS_ROOT" in os.environ:
+        root = Path(os.environ.get("LLM_MODELS_ROOT"))
+
+    if not root.exists():
+        root = Path("/scratch.trt_llm_data/llm-models/")
+
+    assert root.exists(), (
+        "You shall set LLM_MODELS_ROOT env or be able to access /home/scratch.trt_llm_data to run this test"
+    )
+
+    return root if root.exists() else None
 
 
 def tests_path() -> Path:
