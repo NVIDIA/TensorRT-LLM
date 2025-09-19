ba2b86edfca9ee7fdc94258f16e1161cd3d68f6d
Lizhi Zhou <1432185+reasonsolo@users.noreply.github.com>
2025-09-18 23:52:59 -0700

    remove code for getting local ip/nic
    Signed-off-by: Lizhi Zhou <1432185+reasonsolo@users.noreply.github.com>

--- tests/integration/defs/test_e2e.py
+++ tests/integration/defs/test_e2e.py
@@ -1768,13 +1768,11 @@ def test_trtllm_multimodal_benchmark_serving(llm_root, llm_venv):
 
 
 @pytest.mark.skip_less_device(4)
-@pytest.mark.skip_less_device_memory(80000)
-@pytest.mark.parametrize("ctx_config", ["tp2pp1", "tp1pp2"],
-                         ids=["ctx_tp2pp1", "ctx_tp1pp2"])
-@pytest.mark.parametrize("gen_config", ["tp2pp1", "tp1pp2"],
-                         ids=["gen_tp2pp1", "gen_tp1pp2"])
-def test_openai_disaggregated_serving_multi_nodes(llm_root, llm_venv,
-                                                  ctx_config, gen_config):
+@pytest.mark.skip_less_device_memory(40000)
+@pytest.mark.parametrize("gen_config", ["gen_tp2pp1", "gen_tp1pp2"])
+@pytest.mark.parametrize("ctx_config", ["ctx_tp2pp1", "ctx_tp1pp2"])
+def test_openai_disagg_multi_nodes_completion(llm_root, llm_venv, ctx_config,
+                                              gen_config):
     test_root = unittest_path() / "llmapi" / "apps"
     llm_venv.run_cmd([
         "-m",
