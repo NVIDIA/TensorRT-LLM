62679a3b31b3489e26945ebcdfc975ff0827daad
John Calderon <jcalderon@nvidia.com>
2025-09-19 18:37:28 +0000

    add additional arguments for mm data
    Signed-off-by: John Calderon <jcalderon@nvidia.com>

--- tensorrt_llm/_torch/pyexecutor/_util.py
+++ tensorrt_llm/_torch/pyexecutor/_util.py
@@ -149,7 +149,8 @@ class KvCacheCreator:
             "Profiling with the default input dummy context request. This may not take into account the memory consumption of " \
             "ViT's encoder")
             return requests
-        text_prompt = input_processor.get_dummy_prompt(input_seq_len)
+        text_prompt = input_processor.get_dummy_prompt(input_seq_len,
+                                                       {'image': 1})
         max_beam_width = self._max_beam_width
         input_processor_with_hash = create_input_processor_with_hash(
             input_processor)
