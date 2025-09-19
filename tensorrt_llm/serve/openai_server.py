459cfcfd3915f67778fc11e50e14f2608c078194
Lizhi Zhou <1432185+reasonsolo@users.noreply.github.com>
2025-09-19 02:07:36 -0700

    implement disagg cluster auto-scaling
    Signed-off-by: Lizhi Zhou <1432185+reasonsolo@users.noreply.github.com>

--- tensorrt_llm/serve/openai_server.py
+++ tensorrt_llm/serve/openai_server.py
@@ -28,12 +28,15 @@ from tensorrt_llm.inputs.data import TokensPrompt
 from tensorrt_llm.inputs.utils import ConversationMessage, apply_chat_template
 from tensorrt_llm.llmapi import DisaggregatedParams as LlmDisaggregatedParams
 from tensorrt_llm.llmapi import MultimodalEncoder
-from tensorrt_llm.llmapi.disagg_utils import MetadataServerConfig, ServerRole
+from tensorrt_llm.llmapi.disagg_utils import (DisaggClusterConfig,
+                                              MetadataServerConfig, ServerRole)
 from tensorrt_llm.llmapi.llm import RequestOutput
 from tensorrt_llm.logger import logger
 from tensorrt_llm.metrics.collector import MetricsCollector
 from tensorrt_llm.serve.chat_utils import (check_multiple_response,
                                            parse_chat_messages_coroutines)
+from tensorrt_llm.serve.cluster_storage import create_cluster_storage_client
+from tensorrt_llm.serve.disagg_auto_scaling import DisaggClusterWorker
 from tensorrt_llm.serve.metadata_server import create_metadata_server
 from tensorrt_llm.serve.openai_protocol import (ChatCompletionRequest,
                                                 ChatCompletionResponse,
@@ -75,12 +78,17 @@ class OpenAIServer:
                  llm: Union[LLM, MultimodalEncoder],
                  model: str,
                  server_role: Optional[ServerRole],
-                 metadata_server_cfg: MetadataServerConfig):
+                 metadata_server_cfg: MetadataServerConfig,
+                 cluster_config: Optional[DisaggClusterConfig] = None):
         self.llm = llm
         self.tokenizer = llm.tokenizer
         self.metadata_server = create_metadata_server(metadata_server_cfg)
+        self.cluster_config = cluster_config
         self.server_role = server_role
-        self.binding_addr = None  # Will be set in __call__
+        # Will be set in __call__
+        self.binding_addr = None
+        self.host = None
+        self.port = None
         hf_tokenizer_path = llm._hf_model_dir or self.tokenizer.tokenizer.name_or_path
         trust_remote_code = llm.args.trust_remote_code
         try:
@@ -129,6 +137,10 @@ class OpenAIServer:
         else:
             self.use_harmony = (self.model_config.model_type == "gpt_oss")
 
+        # as disagg-worker
+        self.cluster_storage = None
+        self.cluster_worker = None
+
         @asynccontextmanager
         async def lifespan(app: FastAPI):
             if self.metadata_server is not None:
@@ -143,6 +155,12 @@ class OpenAIServer:
                 # Register with ETCD using the existing key format
                 self.metadata_server.put(f"trtllm/{self.llm.llm_id}", metadata)
                 logger.info(f"trtllm/{self.llm.llm_id} is registered")
+
+            if self.cluster_config:
+                logger.info(f"Cluster config: {self.cluster_config}")
+                self.cluster_storage = create_cluster_storage_client(self.cluster_config.cluster_uri, self.cluster_config.cluster_name)
+                self.cluster_worker= DisaggClusterWorker(self.server_role, self.host, self.port, self.cluster_config, self.cluster_storage)
+                await self.cluster_worker.register_worker()
 
             # terminate rank0 worker
             yield
@@ -945,6 +963,8 @@ class OpenAIServer:
     async def __call__(self, host, port):
         # Store the binding address for server registration
         self.binding_addr = f"http://{host}:{port}"
+        self.host = host
+        self.port = port
         config = uvicorn.Config(self.app,
                                 host=host,
                                 port=port,
