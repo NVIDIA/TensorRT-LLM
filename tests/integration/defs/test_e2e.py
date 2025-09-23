44a2e514caef9fda7b0809aa48440022172ae637
Enwei Zhu <21126786+syuoni@users.noreply.github.com>
2025-09-23 09:05:01 +0000

    fix
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>

--- tests/integration/defs/test_e2e.py
+++ tests/integration/defs/test_e2e.py
@@ -1677,7 +1677,7 @@ def test_openai_mmencoder_example(llm_root, llm_venv):
          str(test_root / "_test_openai_mmencoder.py")])
 
 
-def test_openai_chat_guided_decoding_example(llm_venv):
+def test_openai_chat_guided_decoding(llm_root, llm_venv):
     test_root = unittest_path() / "llmapi" / "apps"
     llm_venv.run_cmd([
         "-m", "pytest",
