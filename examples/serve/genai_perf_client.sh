0b4ec7f03a79fc2c961ef397ec946cdaeb923f94
Pengyun Lin <81065165+LinPoly@users.noreply.github.com>
2025-09-29 14:49:52 +0000

    Use local tokenizer
    Signed-off-by: Pengyun Lin <81065165+LinPoly@users.noreply.github.com>

--- examples/serve/genai_perf_client.sh
+++ examples/serve/genai_perf_client.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
 
+# Set to TinyLlama/TinyLlama-1.1B-Chat-v1.0 to download from Hugging Face.
+# Or set to the path of a local tokenizer directory.
+TOKENIZER_PATH_OR_NAME= \
+    "/home/scratch.trt_llm_data/llm-models/llama-models-v2/TinyLlama-1.1B-Chat-v1.0"
 genai-perf profile \
     -m TinyLlama-1.1B-Chat-v1.0 \
-    --tokenizer TinyLlama/TinyLlama-1.1B-Chat-v1.0 \
+    --tokenizer $TOKENIZER_PATH_OR_NAME \
     --endpoint-type chat \
     --random-seed 123 \
     --synthetic-input-tokens-mean 128 \
