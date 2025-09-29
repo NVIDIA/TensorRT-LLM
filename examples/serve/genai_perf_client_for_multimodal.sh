9ca018f4c2e3cb182d08d361615de0f826375851
Pengyun Lin <81065165+LinPoly@users.noreply.github.com>
2025-09-29 14:49:52 +0000

    Use local tokenizer
    Signed-off-by: Pengyun Lin <81065165+LinPoly@users.noreply.github.com>

--- examples/serve/genai_perf_client_for_multimodal.sh
+++ examples/serve/genai_perf_client_for_multimodal.sh
@@ -1,8 +1,12 @@
 #! /usr/bin/env bash
 
+# Set to TinyLlama/TinyLlama-1.1B-Chat-v1.0 to download from Hugging Face.
+# Or set to the path of a local tokenizer directory.
+TOKENIZER_PATH_OR_NAME= \
+    "/home/scratch.trt_llm_data/llm-models/Qwen2.5-VL-3B-Instruct"
 genai-perf profile \
     -m Qwen2.5-VL-3B-Instruct \
-    --tokenizer Qwen/Qwen2.5-VL-3B-Instruct \
+    --tokenizer $TOKENIZER_PATH_OR_NAME \
     --endpoint-type multimodal \
     --random-seed 123 \
     --image-width-mean 64 \
