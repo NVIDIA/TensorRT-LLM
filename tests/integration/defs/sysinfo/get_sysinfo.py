f917f8a0e45f9c78b7163811cd58d5169796a846
yufeiwu <230315618+yufeiwu-nv@users.noreply.github.com>
2025-09-29 05:45:50 +0000

    Upcase full_name and gpu_name
    Signed-off-by: yufeiwu <230315618+yufeiwu-nv@users.noreply.github.com>

--- tests/integration/defs/sysinfo/get_sysinfo.py
+++ tests/integration/defs/sysinfo/get_sysinfo.py
@@ -181,7 +181,7 @@ def construct_gpu_properties(mako_opts, device_index=0):
                 entity = str(pci_device_id).lower()
                 if entity in chip_name_mapping:
                     chip = str(chip_name_mapping[entity])
-                    full_name = "{} Bring-up Board".format(chip.upper())
+                    full_name = "{} BRING-UP BOARD".format(chip.upper())
                 else:
                     err_msg = r"Could not find a chip name associated with this device id - {}. Chip name won't be reported".format(
                         entity)
