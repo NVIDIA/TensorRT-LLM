1b7827c13a5c19bcc24997f5cb86d1783da4fa77
Pengyun Lin <81065165+LinPoly@users.noreply.github.com>
2025-09-30 12:13:06 +0000

    Fix comment and path
    Signed-off-by: Pengyun Lin <81065165+LinPoly@users.noreply.github.com>

--- examples/serve/genai_perf_client_for_multimodal.sh
+++ examples/serve/genai_perf_client_for_multimodal.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-# Set to TinyLlama/TinyLlama-1.1B-Chat-v1.0 to download from Hugging Face.
-# Or set to the path of a local tokenizer directory.
-TOKENIZER_PATH_OR_NAME="/home/scratch.trt_llm_data/llm-models/Qwen2.5-VL-3B-Instruct"
+# Set to Qwen/Qwen2.5-VL-3B-Instruct to download from Hugging Face.
+# Or set to the path of local tokenizer directory.
+TOKENIZER_PATH_OR_NAME="/scratch.trt_llm_data/llm-models/Qwen2.5-VL-3B-Instruct"
 genai-perf profile \
     -m Qwen2.5-VL-3B-Instruct \
     --tokenizer $TOKENIZER_PATH_OR_NAME \
