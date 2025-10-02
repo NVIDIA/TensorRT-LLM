07e4ac0112d87364e0b1767e804f1ee932b5697c
list <58580514+farazkh80@users.noreply.github.com>
2025-10-02 02:21:53 +0000

    remove comment
    Signed-off-by: list <58580514+farazkh80@users.noreply.github.com>

--- cpp/tests/unit_tests/kernels/mixtureOfExpertsTest.cu
+++ cpp/tests/unit_tests/kernels/mixtureOfExpertsTest.cu
@@ -223,7 +223,6 @@ protected:
         bool should_skip_no_device = mDeviceCount <= 0;
         bool should_skip_unsupported_fp8 = getSMVersion() < 89 && FP8;
         bool should_skip_unsupported_fp4 = (getSMVersion() < 100) && ANY_FP4;
-        // bool should_skip_unsupported_sm120 = (getSMVersion() >= 120) && !ANY_FPX;
         return should_skip_no_device || should_skip_unsupported_fp8 || should_skip_unsupported_fp4;
     }
 
