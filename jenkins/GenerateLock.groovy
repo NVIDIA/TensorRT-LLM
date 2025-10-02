f3bd1052cac3944eebade2efa1e54cbb2faef37f
Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>
2025-10-02 15:59:10 -0700

    some extra fix
    Signed-off-by: Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>

--- jenkins/GenerateLock.groovy
+++ jenkins/GenerateLock.groovy
@@ -47,7 +47,7 @@ def generate()
         sh "apt update"
         sh "apt install -y python3-dev git curl"
         sh "git config --global --add safe.directory ${env.WORKSPACE}"
-        sh "git config --global user.email \"tensorrt_llm@nvidia.com\""
+        sh "git config --global user.email \"90828364+tensorrt-cicd@users.noreply.github.com\""
         sh "git config --global user.name \"TensorRT LLM\""
         trtllm_utils.checkoutSource(LLM_REPO, params.llmBranch, env.WORKSPACE, false, false)
         sh "python3 --version"
@@ -66,6 +66,7 @@ def generate()
             withCredentials([usernamePassword(credentialsId: 'github-cred-trtllm-ci', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
                 def authedUrl = LLM_REPO.replaceFirst('https://', "https://${GIT_USER}:${GIT_PASS}@")
                 sh "git remote set-url origin ${authedUrl}"
+                // sh "git pull --rebase origin HEAD:${params.llmBranch}"
                 sh "git push origin HEAD:${params.llmBranch}"
             }
         }
