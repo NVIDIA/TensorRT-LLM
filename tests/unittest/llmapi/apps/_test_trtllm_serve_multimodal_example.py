117d9d7d9767d5861679c837e235213f178dfb5f
John Calderon <jcalderon@nvidia.com>
2025-10-04 23:36:47 +0000

    fix tests A10-PyTorch-1.test_e2e.test_openai_chat_multimodal_example and A10-PyTorch-1.test_e2e.test_trtllm_serve_multimodal_example
    Signed-off-by: John Calderon <jcalderon@nvidia.com>

--- tests/unittest/llmapi/apps/_test_trtllm_serve_multimodal_example.py
+++ tests/unittest/llmapi/apps/_test_trtllm_serve_multimodal_example.py
@@ -46,7 +46,10 @@ def temp_extra_llm_api_options_file(request):
 @pytest.fixture(scope="module")
 def server(model_name: str, temp_extra_llm_api_options_file: str):
     model_path = get_model_path(model_name)
-    args = ["--extra_llm_api_options", temp_extra_llm_api_options_file]
+    args = [
+        "--enable_chunked_prefill", "--extra_llm_api_options",
+        temp_extra_llm_api_options_file
+    ]
     with RemoteOpenAIServer(model_path, port=8000,
                             cli_args=args) as remote_server:
         yield remote_server
