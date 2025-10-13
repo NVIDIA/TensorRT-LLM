34038b0cae493a0606848b4b5bf5389bf7c32732
Amit Zuker <203509407+amitz-nv@users.noreply.github.com>
2025-10-13 08:32:30 +0000

    [https://nvbugs/5510879][fix] Fix pytorch & TRT-python flows fused LoRA adapter modules weight split with TP>1 (#8063)
    Signed-off-by: Amit Zuker <203509407+amitz-nv@users.noreply.github.com>

--- tests/unittest/llmapi/test_llm_multi_gpu_pytorch.py
+++ tests/unittest/llmapi/test_llm_multi_gpu_pytorch.py
@@ -5,7 +5,7 @@ from .test_llm import tinyllama_logits_processor_test_harness
 from tensorrt_llm import LLM
 from tensorrt_llm.llmapi import KvCacheConfig
 from tensorrt_llm.lora_helper import LoraConfig
-from .lora_test_utils import check_llama_7b_multi_lora_from_request_test_harness
+from .lora_test_utils import check_llama_7b_multi_lora_from_request_test_harness, check_phi3_lora_fused_modules_output_tp2_identical_to_tp1
 from .test_llm_pytorch import llama_7b_lora_from_dir_test_harness
 from .test_llm import _test_llm_capture_request_error
 # isort: on
@@ -57,3 +57,48 @@ def test_llama_7b_multi_lora_tp2():
         # Disable CUDA graph
         # TODO: remove this once we have a proper fix for CUDA graph in LoRA
         cuda_graph_config=None)
+
+
+@pytest.mark.gpu2
+def test_phi3_lora_fused_modules_output_on_tp2_identical_to_tp1() -> None:
+    check_phi3_lora_fused_modules_output_tp2_identical_to_tp1(
+        LLM,
+        # Disable CUDA graph
+        # TODO: remove this once we have a proper fix for CUDA graph in LoRA
+        cuda_graph_config=None)
+
+
+@pytest.mark.skip(reason="https://nvbugs/5560921")
+@skip_ray
+@pytest.mark.gpu2
+def test_llm_rpc_tp2():
+    with LLM(model=llama_model_path,
+             kv_cache_config=KvCacheConfig(free_gpu_memory_fraction=0.4),
+             orchestrator_type="rpc",
+             tensor_parallel_size=2) as llm:
+        assert isinstance(llm._executor, GenerationExecutorRpcProxy)
+
+        res = llm.generate("Tell me a joke",
+                           sampling_params=SamplingParams(max_tokens=10,
+                                                          end_id=-1))
+        print(f"get result: {res}")
+
+        assert len(res.outputs) == 1
+        assert len(res.outputs[0].token_ids) == 10
+
+
+@pytest.mark.skip(reason="https://nvbugs/5560921")
+@skip_ray
+@pytest.mark.gpu2
+@pytest.mark.asyncio
+async def test_llm_rpc_streaming_tp2():
+    with LLM(model=llama_model_path,
+             kv_cache_config=KvCacheConfig(free_gpu_memory_fraction=0.4),
+             orchestrator_type="rpc",
+             tensor_parallel_size=2) as llm:
+        assert isinstance(llm._executor, GenerationExecutorRpcProxy)
+
+        async for output in llm.generate_async("Tell me a joke",
+                                               sampling_params=SamplingParams(
+                                                   max_tokens=10, end_id=-1)):
+            print(f"get result: {output}")
