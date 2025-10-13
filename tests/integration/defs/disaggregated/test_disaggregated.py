507b84baecf47722381fe0c2179d06d234abcd6e
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2025-10-13 01:29:12 +0000

    increase server_start timeout to wait weight loading
    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>

--- tests/integration/defs/disaggregated/test_disaggregated.py
+++ tests/integration/defs/disaggregated/test_disaggregated.py
@@ -185,7 +185,7 @@ def run_disaggregated_test(example_dir,
         config_file
     ]
 
-    server_start_timeout = 900
+    server_start_timeout = 1200
     server_cmd = [
         'trtllm-serve', 'disaggregated', '--server_start_timeout',
         str(server_start_timeout), '-c', config_file
