b6121cd383c4f76d970183b6779d8580ae873930
wjueyao <wyao123@terpmail.umd.edu>
2025-10-15 16:15:50 +0800

    fix: install mooncake script
    Signed-off-by: wjueyao <wyao123@terpmail.umd.edu>

--- docker/common/install_mooncake.sh
+++ docker/common/install_mooncake.sh
@@ -3,8 +3,9 @@ set -ex
 
 MOONCAKE_VERSION="v0.3.5"
 MOONCAKE_REPO="https://github.com/kvcache-ai/Mooncake.git"
+MOONCAKE_INSTALL_PATH="/usr/local"
 
-git clone ${MOONCAKE_VERSION} ${MOONCAKE_REPO}
+git clone --depth 1 -b ${MOONCAKE_VERSION} ${MOONCAKE_REPO}
 cd Mooncake
 bash dependencies.sh
 mkdir build
@@ -12,3 +13,5 @@ cd build
 cmake .. -DUSE_CUDA=ON -DBUILD_SHARED_LIBS=ON
 make -j
 make install
+
+echo "export LD_LIBRARY_PATH=${MOONCAKE_INSTALL_PATH}/lib:\$LD_LIBRARY_PATH" >> "${ENV}"
