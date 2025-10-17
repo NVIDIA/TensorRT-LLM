a69789ed2048cb173ddd6aedd2f353593c501604
Yuxian Qiu <142763828+yuxianq@users.noreply.github.com>
2025-10-17 12:32:16 +0000

    upgrade fmha_v2.
    Signed-off-by: Yuxian Qiu <142763828+yuxianq@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/contextFusedMultiHeadAttention/cubin/fmha_v2_flash_attention_e4m3_fp32_64_32_S_qkv_192_sm89.cubin.cpp
+++ cpp/tensorrt_llm/kernels/contextFusedMultiHeadAttention/cubin/fmha_v2_flash_attention_e4m3_fp32_64_32_S_qkv_192_sm89.cubin.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dc99b32389f2d85ae2d4b139da6514bf257dc94524da879aaaf49107d1bb374
-size 723454
+oid sha256:e53abec3bc15e7a401ef2f33d4cc603a16de09310b84a21f0da29b97635cec36
+size 728190
