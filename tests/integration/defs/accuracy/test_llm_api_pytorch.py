c7dca90b0a2ef055be7e3e32ee923484cb8fb9af
yechank <161688079+yechank-nvidia@users.noreply.github.com>
2025-10-17 16:30:25 +0900

    add llava mmmu and remove all related tests
    Signed-off-by: yechank <161688079+yechank-nvidia@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api_pytorch.py
+++ tests/integration/defs/accuracy/test_llm_api_pytorch.py
@@ -3883,7 +3883,7 @@ class TestEXAONE4(LlmapiAccuracyTestHarness):
 class TestQwen2_VL_7B(LlmapiAccuracyTestHarness):
     MODEL_NAME = "Qwen/Qwen2-VL-7B-Instruct"
     MODEL_PATH = f"{llm_models_root()}/Qwen2-VL-7B-Instruct"
-
+    MAX_NUM_TOKENS = 16384
     # NOTE: MMMU adds <|endoftext|> to the stop token.
     sampling_params = SamplingParams(max_tokens=MMMU.MAX_OUTPUT_LEN,
                                      truncate_prompt_tokens=MMMU.MAX_INPUT_LEN,
@@ -3894,7 +3894,7 @@ class TestQwen2_VL_7B(LlmapiAccuracyTestHarness):
     @pytest.mark.skip(reason="https://nvbugs/5601909")
     def test_auto_dtype(self):
         with LLM(self.MODEL_PATH,
-                 max_num_tokens=16384,
+                 max_num_tokens=self.MAX_NUM_TOKENS,
                  kv_cache_config=self.kv_cache_config) as llm:
             task = MMMU(self.MODEL_NAME)
             task.evaluate(llm, sampling_params=self.sampling_params)
@@ -3903,6 +3903,7 @@ class TestQwen2_VL_7B(LlmapiAccuracyTestHarness):
 class TestQwen2_5_VL_7B(LlmapiAccuracyTestHarness):
     MODEL_NAME = "Qwen/Qwen2.5-VL-7B-Instruct"
     MODEL_PATH = f"{llm_models_root()}/Qwen2.5-VL-7B-Instruct"
+    MAX_NUM_TOKENS = 16384
 
     # NOTE: MMMU adds <|endoftext|> to the stop token.
     sampling_params = SamplingParams(max_tokens=MMMU.MAX_OUTPUT_LEN,
@@ -3913,7 +3914,7 @@ class TestQwen2_5_VL_7B(LlmapiAccuracyTestHarness):
 
     def test_auto_dtype(self):
         with LLM(self.MODEL_PATH,
-                 max_num_tokens=16384,
+                 max_num_tokens=self.MAX_NUM_TOKENS,
                  kv_cache_config=self.kv_cache_config) as llm:
             task = MMMU(self.MODEL_NAME)
             task.evaluate(llm, sampling_params=self.sampling_params)
@@ -4014,3 +4015,23 @@ class TestSeedOss_36B(LlmapiAccuracyTestHarness):
                           extra_evaluator_kwargs=dict(
                               apply_chat_template=True,
                               chat_template_kwargs=chat_template_kwargs))
+
+
+class TestLlava_V1_6_Mistral_7B(LlmapiAccuracyTestHarness):
+    MODEL_NAME = "llava-hf/llava-v1.6-mistral-7b"
+    MODEL_PATH = f"{llm_models_root()}/llava-v1.6-mistral-7b"
+    MAX_NUM_TOKENS = 16384
+
+    # NOTE: MMMU adds <|endoftext|> to the stop token.
+    sampling_params = SamplingParams(max_tokens=MMMU.MAX_OUTPUT_LEN,
+                                     truncate_prompt_tokens=MMMU.MAX_INPUT_LEN,
+                                     stop="<|endoftext|>")
+
+    kv_cache_config = KvCacheConfig(free_gpu_memory_fraction=0.6)
+
+    def test_auto_dtype(self):
+        with LLM(self.MODEL_PATH,
+                 max_num_tokens=self.MAX_NUM_TOKENS,
+                 kv_cache_config=self.kv_cache_config) as llm:
+            task = MMMU(self.MODEL_NAME)
+            task.evaluate(llm, sampling_params=self.sampling_params)
--- tests/integration/defs/test_e2e.py
+++ tests/integration/defs/test_e2e.py
@@ -2416,9 +2416,6 @@ def test_ptp_quickstart_advanced_mixed_precision(llm_root, llm_venv):
 @pytest.mark.parametrize("model_name,model_path", [
     ("NVILA-8B-FP16", "vila/NVILA-8B"),
     ("NVILA-15B-FP16", "NVILA-15B"),
-    ("llava-v1.6-mistral-7b", "llava-v1.6-mistral-7b-hf"),
-    ("qwen2-vl-7b-instruct", "Qwen2-VL-7B-Instruct"),
-    ("qwen2.5-vl-7b-instruct", "Qwen2.5-VL-7B-Instruct"),
     pytest.param("mistral-small-3.1-24b-instruct",
                  "Mistral-Small-3.1-24B-Instruct-2503",
                  marks=pytest.mark.skip_less_device_memory(80000)),
@@ -2507,38 +2504,6 @@ def test_ptp_quickstart_multimodal(llm_root, llm_venv, model_name, model_path,
                 ],
             ],
         },
-        "llava-v1.6-mistral-7b": {
-            "image": [
-                ["ocean", "sky", "large", "waves", "shore", "blue"],
-                ['mountain', 'flat', 'clouds', 'road', 'sky'],
-                ["highway", "vehicles", "traffic", "bus", "suburban"],
-            ],
-        },
-        "qwen2-vl-7b-instruct": {
-            "image": [
-                ["ocean", "waves", "atmosphere", "stormy", "clouds", "intense"],
-                ["trees", "winding", "road", "sunny", "sky", "atmosphere"],
-                ["traffic", "vehicles", "moderate", "lanes", "road", "cars"],
-            ],
-            "video": [
-                ["city", "night", "lights", "jacket", "wet"],
-                ["earth", "spinning", "black"],
-            ],
-        },
-        "qwen2.5-vl-7b-instruct": {
-            "image": [
-                ["dramatic", "moody", "ocean", "stormy", "sky", "waves"],
-                ["large", "dome", "yosemite", "landmark", "rock", "road"],
-                [
-                    "highway", "traffic", "vehicles", "lanes", "congestion",
-                    "road"
-                ],
-            ],
-            "video": [
-                ["woman", "neon", "night", "jacket", "wet"],
-                ["earth", "world", "night", "lights", "cities"],
-            ],
-        },
         "mistral-small-3.1-24b-instruct": {
             "image": [
                 ["dramatic", "seascape", "ocean", "turbulent", "waves", "dark"],
@@ -2574,10 +2539,6 @@ def test_ptp_quickstart_multimodal(llm_root, llm_venv, model_name, model_path,
         # TODO: remove this once kv cache reuse is supported for all VLM models
         "--disable_kv_cache_reuse",
     ]
-    # NOTE: Qwen2-VL and Qwen2-5-VL model need larger max_num_tokens for video.
-    if model_name in ["qwen2-vl-7b-instruct", "qwen2.5-vl-7b-instruct"
-                      ] and modality == "video":
-        cmd.append("--max_num_tokens=16384")
     if use_cuda_graph:
         cmd.append("--use_cuda_graph")
     # Gemma3 VLM needs a custom mask which is only supported by flashinfer backend currently.
@@ -2600,9 +2561,6 @@ def test_ptp_quickstart_multimodal(llm_root, llm_venv, model_name, model_path,
     output = llm_venv.run_cmd(cmd, caller=check_output)
 
     match_ratio = 4.0 / 5
-    if model_name == "qwen2-vl-7b-instruct" and modality == "image":
-        match_ratio = 4.0 / 6
-
     parsed_outputs = parse_output(output)
     for prompt_output, prompt_keywords in zip(
             parsed_outputs, expected_keywords[model_name][modality]):
@@ -2670,8 +2628,6 @@ def test_ptp_quickstart_multimodal(llm_root, llm_venv, model_name, model_path,
 @pytest.mark.parametrize(
     "model_name,model_path,match_ratio",
     [
-        ("llava-v1.6-mistral-7b", "llava-v1.6-mistral-7b-hf", 0.8),
-        ("qwen2.5-vl-7b-instruct", "Qwen2.5-VL-7B-Instruct", 0.8),
         ("phi4-multimodal-instruct", "multimodals/Phi-4-multimodal-instruct",
          0.8),
         pytest.param("phi4-multimodal-instruct-fp4",
@@ -2727,28 +2683,15 @@ def test_ptp_quickstart_multimodal_kv_cache_reuse(llm_root, llm_venv,
     }
 
     expected_keywords = {
-        "llava-v1.6-mistral-7b": {
-            "image": [
-                ["ocean", "sky", "large", "waves", "shore", "blue"],
-            ] * num_same_requests,
-        },
-        "qwen2.5-vl-7b-instruct": {
-            "image": [
-                ["dramatic", "moody", "ocean", "stormy", "sky", "waves"],
-            ] * num_same_requests,
-            "video": [
-                ["woman", "neon", "night", "jacket", "wet"],
-            ] * num_same_requests,
-        },
         "mistral-small-3.1-24b-instruct": {
             "image": [
                 [
-                    "cloud", "dramatic", "seascape", "ocean", "turbulent",
-                    "waves"
+                    "image", "depicts", "natural", "environment", "ocean",
+                    "water", "waves", "sky"
                 ],
             ] * num_same_requests,
         },
-        "phi4-multimodal-instruct": {
+        "phi4-multimodal-instruct-fp8": {
             "image": [
                 [
                     "image", "depicts", "natural", "environment", "ocean",
@@ -2787,11 +2730,6 @@ def test_ptp_quickstart_multimodal_kv_cache_reuse(llm_root, llm_venv,
         "--max_batch_size",  # single request at a time to test kv cache reuse
         "1",
     ]
-    # NOTE: Qwen2-VL and Qwen2-5-VL model need larger max_num_tokens for video.
-    if model_name in ["qwen2-vl-7b-instruct", "qwen2.5-vl-7b-instruct"
-                      ] and modality == "video":
-        cmd.append("--max_num_tokens=16384")
-
     if model_name.startswith("phi4-multimodal-instruct"):
         cmd.append("--max_seq_len=4096")
         cmd.append("--load_lora")
@@ -2820,8 +2758,6 @@ def test_ptp_quickstart_multimodal_kv_cache_reuse(llm_root, llm_venv,
 @pytest.mark.parametrize(
     "model_name,model_path,match_ratio",
     [
-        ("llava-v1.6-mistral-7b", "llava-v1.6-mistral-7b-hf", 0.8),
-        ("qwen2.5-vl-7b-instruct", "Qwen2.5-VL-7B-Instruct", 0.8),
         ("phi4-multimodal-instruct", "multimodals/Phi-4-multimodal-instruct",
          0.8),
         pytest.param("phi4-multimodal-instruct-fp4",
@@ -2850,8 +2786,7 @@ def test_ptp_quickstart_multimodal_chunked_prefill(llm_root, llm_venv,
         os.path.join(llm_models_root(), "multimodals", "test_data"))
     print(f"Accuracy test {model_name} {modality} mode with example inputs.")
     if modality == "video" and model_name in {
-            "llava-v1.6-mistral-7b", "mistral-small-3.1-24b-instruct",
-            "phi4-multimodal-instruct", "phi4-multimodal-instruct-fp4",
+            "mistral-small-3.1-24b-instruct", "phi4-multimodal-instruct", "phi4-multimodal-instruct-fp4",
             "phi4-multimodal-instruct-fp8"
     }:
         pytest.skip(f"Skipping video modality test for {model_name}")
@@ -2881,27 +2816,6 @@ def test_ptp_quickstart_multimodal_chunked_prefill(llm_root, llm_venv,
     }
 
     expected_keywords = {
-        "llava-v1.6-mistral-7b": {
-            "image": [
-                ["ocean", "sky", "large", "waves", "shore", "blue"],
-                ['mountain', 'flat', 'clouds', 'road', 'sky'],
-                ["highway", "vehicles", "traffic", "bus", "suburban"],
-            ],
-        },
-        "qwen2.5-vl-7b-instruct": {
-            "image": [
-                ["dramatic", "moody", "ocean", "stormy", "sky", "waves"],
-                ["large", "dome", "yosemite", "landmark", "rock", "road"],
-                [
-                    "highway", "traffic", "vehicles", "lanes", "congestion",
-                    "road"
-                ],
-            ],
-            "video": [
-                ["woman", "neon", "night", "jacket", "wet"],
-                ["earth", "world", "night", "lights", "cities"],
-            ],
-        },
         "mistral-small-3.1-24b-instruct": {
             "image": [
                 [
