cf15a7a90a705f26045f79663ce7fc874264cc29
Chenfei Zhang <chenfeiz@nvidia.com>
2025-10-20 19:28:58 -0700

    Add perf-sanity tests
    Signed-off-by: Chenfei Zhang <chenfeiz@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -2655,6 +2655,9 @@ def launchTestJobs(pipeline, testFilter)
         "DGX_B200-8_GPUs-PyTorch-1": ["b200-x8", "l0_dgx_b200", 1, 1, 8],
         "DGX_B200-4_GPUs-PyTorch-Post-Merge-1": ["b200-trtllm", "l0_dgx_b200", 1, 1, 4, 1, true],
         "DGX_B300-4_GPUs-PyTorch-Post-Merge-1": ["b300-x4", "l0_dgx_b300", 1, 1, 4],
+        // Perf sanity post merge test
+        "DGX_B200-4_GPUs-PyTorch-Perf-Sanity-Post-Merge-1": ["b200-x4", "perf_sanity_l0_dgx_b200", 1, 1, 4],
+        "DGX_B300-4_GPUs-PyTorch-Perf-Sanity-Post-Merge-1": ["b300-x4", "perf_sanity_l0_dgx_b300", 1, 1, 4],
     ]
     fullSet += x86SlurmTestConfigs.keySet()
 
@@ -2673,20 +2676,6 @@ def launchTestJobs(pipeline, testFilter)
     }]]}
 
     parallelJobs += parallelSlurmJobs
-
-    // Add Perf Sanity Test Slurm jobs
-    perfSanityTestConfigs = [
-        "DGX_B200-4_GPUs-PyTorch-Perf-Sanity-Post-Merge-1": ["b200-x4", "perf_sanity_l0_dgx_b200", 1, 1, 4],
-        "DGX_B300-4_GPUs-PyTorch-Perf-Sanity-Post-Merge-1": ["b300-x4", "perf_sanity_l0_dgx_b300", 1, 1, 4],
-    ]
-    fullSet += perfSanityTestConfigs.keySet()
-
-    parallelPerfSanityJobs = perfSanityTestConfigs.collectEntries{key, values -> [key, [createKubernetesPodConfig(LLM_DOCKER_IMAGE, "slurm", "amd64"), {
-        def config = VANILLA_CONFIG
-        runLLMTestlistOnSlurm(pipeline, values[0], values[1], config, key.contains("Perf"), key, values[2], values[3], values[4] ?: 1)
-    }]]}
-
-    parallelJobs += parallelPerfSanityJobs
 
     // Try to match what are being tested on x86 H100_PCIe.
     // The total machine time is scaled proportionally according to the number of each GPU.
