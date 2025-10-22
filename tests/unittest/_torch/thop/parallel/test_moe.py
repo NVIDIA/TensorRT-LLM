1375b9f0744f116df83a5eca01b1bbc1d152ff58
Simeng Liu <109828133+SimengLiu-nv@users.noreply.github.com>
2025-10-21 18:12:05 -0700

    [https://nvbugs/5515753][ci] Add NCCL_DEBUG=INFO flag to collect more info with CI failure.  (#8440)
    Signed-off-by: Simeng Liu <simengl@nvidia.com>

--- tests/unittest/_torch/thop/parallel/test_moe.py
+++ tests/unittest/_torch/thop/parallel/test_moe.py
@@ -1062,6 +1062,7 @@ class TestMoeFp4:
     )
     def test_autotune(self, num_tokens, hidden_size, intermediate_size,
                       routing_info):
+        pytest.skip("https://nvbugs/5575841")
 
         self.run_moe_fp4_test(num_tokens,
                               hidden_size,
@@ -1114,6 +1115,7 @@ class TestMoeFp4:
                              ids=["use_score_as_input", "use_topk_as_input"])
     def test_no_autotune(self, num_tokens, hidden_size, intermediate_size,
                          routing_info, use_topk_as_input):
+        pytest.skip("https://nvbugs/5575841")
 
         self.run_moe_fp4_test(num_tokens,
                               hidden_size,
@@ -1153,6 +1155,9 @@ class TestMoeFp4:
         padding = tile_tokens_dim
         if padding >= 256:
             pytest.skip("Routing kernel requires that padding be less than 256")
+
+        if intermediate_size == 384:
+            pytest.skip("https://nvbugs/5434352")
 
         assert top_k <= num_experts
         assert top_k <= 8
