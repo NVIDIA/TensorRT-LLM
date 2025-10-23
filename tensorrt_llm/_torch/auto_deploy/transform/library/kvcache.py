5b2f561d50c7b1e2127029851667012bf91cc7f9
Neta Zmora <96238833+nzmora-nvidia@users.noreply.github.com>
2025-10-23 04:05:34 -0700

    Add copyright notice
    Signed-off-by: Neta Zmora <96238833+nzmora-nvidia@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/transform/library/kvcache.py
+++ tensorrt_llm/_torch/auto_deploy/transform/library/kvcache.py
@@ -1,3 +1,19 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 """Graph transformation to automatically add kv cache into fused MHA op."""
 
 import operator
@@ -257,7 +273,7 @@ class ResizeKVCache(BaseTransform):
         current_num_pages = cm.info.num_pages
         self._log_info(
             f"Current cache size (MB): {current_cache_size // 1024**2}, "
-            f"Current num pages (MB): {current_num_pages}"
+            f"Current num pages: {current_num_pages}"
         )
         if current_kv_cache_size != current_cache_size:
             self._log_info(
