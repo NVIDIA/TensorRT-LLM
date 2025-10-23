e354174a6b2dc32fd4245ac07d13aa46d0540646
Aurelien Chartier <2567591+achartier@users.noreply.github.com>
2025-10-23 21:55:58 +0000

    Pass KvCacheRetentionConfig to torch LlmRequest
    Signed-off-by: Aurelien Chartier <2567591+achartier@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/llm_request.py
+++ tensorrt_llm/_torch/pyexecutor/llm_request.py
@@ -764,7 +764,8 @@ def executor_request_to_llm_request(
         cache_salt_id=executor_request.cache_salt_id,
         arrival_time=getattr(executor_request, "py_arrival_time", None),
         py_multimodal_data=getattr(executor_request, "py_multimodal_data",
-                                   None))
+                                   None),
+        kv_cache_retention_config=executor_request.kv_cache_retention_config)
     if child_req_ids:
         for child_id in child_req_ids:
             llm_request.create_child_request(child_id)
