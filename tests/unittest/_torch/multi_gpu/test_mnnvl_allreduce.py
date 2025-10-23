6eb589bc9021e1ff2ca4627e0d9932e4b157596f
Shiyu Li <shili@nvidia.com>
2025-10-22 22:59:34 -0700

    Waive large batch size for NCCL symmetric
    Signed-off-by: Shiyu Li <shili@nvidia.com>

--- tests/unittest/_torch/multi_gpu/test_mnnvl_allreduce.py
+++ tests/unittest/_torch/multi_gpu/test_mnnvl_allreduce.py
@@ -194,6 +194,9 @@ def row_linear_residual_norm_fusion_forward(
 def test_row_linear_residual_norm_fusion(seq_len, hidden_size, dtype, strategy,
                                          fusion):
 
+    if strategy == AllReduceStrategy.NCCL_SYMMETRIC and 2048 in seq_len:
+        pytest.skip("https://nvbugspro.nvidia.com/bug/5573856")
+
     torch.manual_seed(42)
     tensor_parallel_size = 2
 
