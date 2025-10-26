f1f373a4804a87fc979b3e80a120be91f0af8d75
Bo Deng <deemod@nvidia.com>
2025-10-26 16:38:54 +0000

    fix again
    Signed-off-by: Bo Deng <deemod@nvidia.com>

--- docker/common/install_mooncake.sh
+++ docker/common/install_mooncake.sh
@@ -3,14 +3,14 @@ set -ex
 
 MOONCAKE_VERSION="v0.3.6.post1"
 MOONCAKE_REPO="https://github.com/kvcache-ai/Mooncake.git"
-MOONCAKE_INSTALL_PATH="/usr/local"
+MOONCAKE_INSTALL_PATH="/usr/local/Mooncake"
 
 git clone --depth 1 -b ${MOONCAKE_VERSION} ${MOONCAKE_REPO}
 cd Mooncake
 bash dependencies.sh
 mkdir build
 cd build
-cmake .. -DUSE_CUDA=ON -DBUILD_SHARED_LIBS=ON
+cmake .. -DUSE_CUDA=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${MOONCAKE_INSTALL_PATH}
 make -j
 make install
 
