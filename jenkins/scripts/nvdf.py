8416796a5e5c94a1eb702b7ba2b7d6f00b4291e3
ZhanruiSunCh <184402041+ZhanruiSunCh@users.noreply.github.com>
2025-10-27 20:37:12 -0700

    Fix review
    Signed-off-by: ZhanruiSunCh <184402041+ZhanruiSunCh@users.noreply.github.com>

--- jenkins/scripts/nvdf.py
+++ jenkins/scripts/nvdf.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import hashlib
 import json
 import logging
@@ -133,6 +148,7 @@ class NVDF:
                 data=json_data,
                 headers=headers,
                 auth=HTTPProxyAuth(NVDF_USERNAME, NVDF_PASSWORD),
+                timeout=20,
             )
             if res.status_code in [200, 201, 202]:
                 if res.status_code != 200 and project == JOB_PROJECT_NAME:
