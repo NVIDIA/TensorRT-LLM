c32eb0fd3ee9c2c0411798a0242c87089b90fd59
zhengd-nv <200704041+zhengd-nv@users.noreply.github.com>
2025-10-30 09:53:45 +0000

    update install script
    Signed-off-by: zhengd-nv <200704041+zhengd-nv@users.noreply.github.com>

--- docker/common/install_mooncake.sh
+++ docker/common/install_mooncake.sh
@@ -5,13 +5,45 @@ MOONCAKE_VERSION="v0.3.6.post1"
 MOONCAKE_REPO="https://github.com/kvcache-ai/Mooncake.git"
 MOONCAKE_INSTALL_PATH="/usr/local/Mooncake"
 
+sudo apt-get update
+
+# https://kvcache-ai.github.io/Mooncake/getting_started/build.html
+# libboost-all-dev is removed because it will install a duplicated MPI library
+apt-get install -y --no-install-recommends \
+    build-essential \
+    cmake \
+    libibverbs-dev \
+    libgoogle-glog-dev \
+    libgtest-dev \
+    libjsoncpp-dev \
+    libnuma-dev \
+    libunwind-dev \
+    libpython3-dev \
+    libssl-dev \
+    libyaml-cpp-dev \
+    pybind11-dev \
+    libcurl4-openssl-dev \
+    libhiredis-dev \
+    pkg-config \
+    patchelf
+
+git clone --depth 1 https://github.com/alibaba/yalantinglibs.git
+cd yalantinglibs
+mkdir build && cd build
+cmake .. -DBUILD_EXAMPLES=OFF -DBUILD_BENCHMARK=OFF -DBUILD_UNIT_TESTS=OFF
+make -j
+make install
+cd ../..
+rm -rf yalantinglibs
+
 git clone --depth 1 -b ${MOONCAKE_VERSION} ${MOONCAKE_REPO}
 cd Mooncake
-bash dependencies.sh
-mkdir build
-cd build
+git submodule update --init --recursive --depth 1
+mkdir build && cd build
 cmake .. -DUSE_CUDA=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${MOONCAKE_INSTALL_PATH}
 make -j
 make install
+cd ../..
+rm -rf Mooncake
 
 echo "export LD_LIBRARY_PATH=${MOONCAKE_INSTALL_PATH}/lib:\$LD_LIBRARY_PATH" >> "${ENV}"
