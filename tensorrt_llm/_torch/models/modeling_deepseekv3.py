2631a2c8a45528fcf02d559df20a211faf883a6b
Chang Liu (Enterprise Products) <9713593+chang-l@users.noreply.github.com>
2025-10-29 23:17:16 -0700

    slight refactor
    Signed-off-by: Chang Liu (Enterprise Products) <9713593+chang-l@users.noreply.github.com>

--- tensorrt_llm/_torch/models/modeling_deepseekv3.py
+++ tensorrt_llm/_torch/models/modeling_deepseekv3.py
@@ -358,15 +358,11 @@ class DeepseekV3WeightLoader:
                     has_indexer = hasattr(
                         attn_module,
                         'indexer') and attn_module.indexer is not None
-                    if has_indexer:
-                        indexer_wk = weights[
-                            f"{'.'.join(names[:-1])}.indexer.wk.weight"][:]
-                        indexer_weights_proj = weights[
-                            f"{'.'.join(names[:-1])}.indexer.weights_proj.weight"][:]
-                        # validate all weights have matching dtype
-                        assert fused_a.dtype == indexer_wk.dtype == indexer_weights_proj.dtype, "all weights in kv_a_proj_with_mqa module must have matching dtype"
-                        fused_a = torch.cat(
-                            [fused_a, indexer_wk, indexer_weights_proj], dim=0)
+                    if has_indexer and attn_module.fuse_a_indexer_k_weight:
+                        from tensorrt_llm._torch.attention_backend.sparse.dsa import \
+                            Indexer
+                        fused_a = Indexer.load_weights_to_kv_a(
+                            fused_a, weights, names)
 
                     if f"{'.'.join(names[:-1])}.kv_a_proj_with_mqa.weight_scale_inv" in weights:
                         fused_a_scale = weights[
@@ -569,6 +565,14 @@ class DeepseekV32Attention(MLA):
     ):
         config = model_config.pretrained_config
         predicted_tokens_per_seq = model_config.spec_config.max_total_draft_tokens + 1 if model_config.spec_config is not None else 1
+
+        # DSV3.2 nvfp4 ckpt has kv_a_proj_with_mqa module in bfloat16
+        # TODO: check it more directly/robustly, e.g., indexer_weight_quant == fuseA_quant == indexer_quant
+        if model_config.get_quant_config().quant_algo == QuantAlgo.NVFP4:
+            self.fuse_a_indexer_k_weight = True
+        else:
+            self.fuse_a_indexer_k_weight = False
+
         super().__init__(hidden_size=config.hidden_size,
                          num_attention_heads=config.num_attention_heads,
                          num_key_value_heads=config.num_key_value_heads,
@@ -590,12 +594,18 @@ class DeepseekV32Attention(MLA):
                          config=model_config,
                          aux_stream=aux_stream)
 
+        if self.fuse_a_indexer_k_weight:
+            # avoid loading indexer.wk and indexer.weights_proj twice
+            self.indexer.wk = None
+            self.indexer.weights_proj = None
+
         # For DeepseekV32, the kv_a_proj_with_mqa includes:
         # q_a_proj + kv_a_proj_with_mqa + indexer.wk + indexer.weights_proj
         self.kv_a_proj_with_mqa = DeepseekV3Linear(
             config.hidden_size,
             self.kv_lora_rank + self.qk_rope_head_dim + self.q_lora_rank +
-            self.indexer.head_dim + self.indexer.n_heads,
+            (self.indexer.head_dim +
+             self.indexer.n_heads if self.fuse_a_indexer_k_weight else 0),
             bias=False,
             dtype=config.torch_dtype,
             quant_config=model_config.get_quant_config(),
