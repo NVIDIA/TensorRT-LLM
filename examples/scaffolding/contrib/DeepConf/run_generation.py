d3638d3f0997dafc456173b42c29cbe2e6d27728
Cao Dong <87467313+dcaox@users.noreply.github.com>
2025-11-03 16:05:50 +0800

    [None][feat] Add benchmark to DeepConf (#8776)
    Signed-off-by: Dong Cao <docao@nvidia.com>
    Signed-off-by: FredricZ-2007 <226039983+fredricz-20070104@users.noreply.github.com>

--- examples/scaffolding/contrib/DeepConf/run_generation.py
+++ examples/scaffolding/contrib/DeepConf/run_generation.py
@@ -1,8 +1,16 @@
 import argparse
+import json
 import time
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict
+
+import numpy as np
+from utils import equal_func, prepare_prompt
 
 from tensorrt_llm.scaffolding import (NativeGenerationController,
-                                      ScaffoldingLlm, TRTLLMWorker)
+                                      ScaffoldingLlm, TRTLLMWorker,
+                                      extract_answer_from_boxed)
 from tensorrt_llm.scaffolding.contrib.DeepConf import (
     DeepConfOfflineController, DeepConfOfflineMajorityVoteController,
     DeepConfOnlineController, DeepConfOnlineMajorityVoteController)
@@ -28,35 +36,91 @@ def parse_arguments():
                         required=True,
                         choices=list(_RUN_TYPE_TO_IMPL.keys()),
                         help="Type of the run. Available choices: %(choices)s")
-    parser.add_argument('--sample_num', type=int, default=20)
-    parser.add_argument('--conf_group_size', type=int, default=128)
+    parser.add_argument('--warmup_sample_num', type=int, default=16)
+    parser.add_argument('--sample_num', type=int, default=256)
+    parser.add_argument('--conf_group_size', type=int, default=2048)
     parser.add_argument('--conf_threshold', type=float, default=0.5)
     parser.add_argument('--vote_policy',
                         type=str,
                         default="top10_bottom_window_filtered")
-    parser.add_argument('--warmup_sample_num', type=int, default=5)
-    parser.add_argument('--confidence_percentile', type=int, default=90)
+    parser.add_argument('--confidence_percentile', type=int, default=10)
     parser.add_argument('--logprobs_topk', type=int, default=20)
-    parser.add_argument('--max_tokens', type=int, default=8192)
+    parser.add_argument('--max_tokens', type=int, default=64000)
     parser.add_argument('--temperature', type=float, default=0.6)
     parser.add_argument('--top_p', type=float, default=0.95)
+    parser.add_argument('--top_k', type=int, default=0)
+    parser.add_argument('--qid', type=int, default=-1)
+    parser.add_argument('--dataset', type=str, default="brumo_2025.jsonl")
+    parser.add_argument('--repeat_times', type=int, default=1)
+    parser.add_argument('--tensor_parallel_size', type=int, default=1)
     args = parser.parse_args()
     return args
 
 
-def run_scaffolding_llm(prompts, proposer_worker, controller):
+@dataclass
+class BenchResult:
+    right_answer_count: int = 0
+    total_answer_count: int = 0
+    accuracy: float = 0.0
+    generated_tokens: int = 0
+
+
+def run_scaffolding_llm(prompts,
+                        proposer_worker,
+                        controller,
+                        repeat_times=1,
+                        ground_truth=None,
+                        **kwargs):
     llm = ScaffoldingLlm(
         controller,
         {
             NativeGenerationController.WorkerTag.GENERATION: proposer_worker,
         },
     )
-    time_start = time.time()
-    results = llm.generate(prompts)
-    time_end = time.time()
-    print(f"time cost: {time_end - time_start} seconds")
-    for i, result in enumerate(results):
-        print(f"result {i}:\n{result.outputs[0].text}")
+
+    is_majority_vote = isinstance(
+        controller, DeepConfOnlineMajorityVoteController) or isinstance(
+            controller, DeepConfOfflineMajorityVoteController)
+    vote_policy_to_bench_result: Dict[str, BenchResult] = {}
+    times = []
+    for i in range(repeat_times):
+        print(f"=========== round {i} ===========")
+        start_time = time.time()
+        results = llm.generate(prompts)
+        times.append(time.time() - start_time)
+
+        for j, result in enumerate(results):
+            print(
+                f"result {j}: {extract_answer_from_boxed(result.outputs[0].text)}"
+            )
+
+            if is_majority_vote and ground_truth is not None:
+                vote_policy_to_voted_task = result.cur_output.vote_policy_to_voted_task
+                for vote_policy, voted_task in vote_policy_to_voted_task.items(
+                ):
+                    bench_result = vote_policy_to_bench_result.get(
+                        vote_policy, BenchResult())
+
+                    voted_answer = voted_task.customized_result_fields[
+                        'extracted_answer']
+                    if equal_func(voted_answer, ground_truth[j]):
+                        bench_result.right_answer_count += 1
+                    bench_result.total_answer_count += 1
+                    bench_result.generated_tokens += result.cur_output.output_token_num
+
+                    vote_policy_to_bench_result[vote_policy] = bench_result
+
+    print(f"e2e inference median time cost: {np.median(times):.2f} seconds")
+
+    if is_majority_vote:
+        for vote_policy, bench_result in vote_policy_to_bench_result.items():
+            bench_result.accuracy = bench_result.right_answer_count / bench_result.total_answer_count
+            print(
+                f"vote_policy: {vote_policy}, accuracy: {bench_result.accuracy}"
+            )
+
+        print(f"generated tokens: {bench_result.generated_tokens}")
+
     llm.shutdown(shutdown_workers=True)
 
 
@@ -83,7 +147,8 @@ def test_single_vote_controller(prompts,
         conf_group_size=conf_group_size,
         conf_threshold=conf_threshold,
     )
-    run_scaffolding_llm(prompts, proposer_worker, prototype_controller)
+    run_scaffolding_llm(prompts, proposer_worker, prototype_controller,
+                        **kwargs)
 
 
 def test_majority_vote_controller(prompts,
@@ -94,6 +159,7 @@ def test_majority_vote_controller(prompts,
                                   temperature,
                                   max_tokens,
                                   top_p,
+                                  top_k,
                                   sample_num,
                                   warmup_sample_num,
                                   vote_policy,
@@ -106,6 +172,7 @@ def test_majority_vote_controller(prompts,
             "max_tokens": max_tokens,
             "num_logprobs": logprobs_topk,
             "top_p": top_p,
+            "top_k": top_k,
         })
     DeepConfControllerKwargs = {
         "generation_controller": generation_controller,
@@ -125,7 +192,8 @@ def test_majority_vote_controller(prompts,
         vote_policy=vote_policy,
         warmup_sample_num=warmup_sample_num,
         confidence_percentile=confidence_percentile)
-    run_scaffolding_llm(prompts, proposer_worker, majority_vote_controller)
+    run_scaffolding_llm(prompts, proposer_worker, majority_vote_controller,
+                        **kwargs)
 
 
 def main():
@@ -138,24 +206,38 @@ def main():
         "warmup_sample_num": args.warmup_sample_num,
         "confidence_percentile": args.confidence_percentile,
         "logprobs_topk": args.logprobs_topk,
-        "max_tokens": args.max_tokens,
         "temperature": args.temperature,
         "top_p": args.top_p,
+        "top_k": args.top_k,
+        "repeat_times": args.repeat_times,
+        "max_tokens": args.max_tokens,
     }
-
-    prompts = [
-        "Natalia sold clips to 48 of her friends in April, and then she sold half as many clips in May. How many clips did Natalia sell altogether in April and May?\r\n\r\n",
-        "There exist real numbers $x$ and $y$, both greater than 1, such that $\\log_x\\left(y^x\\right)=\\log_y\\left(x^{4y}\\right)=10$. Find $xy$.",
-        "Find the largest possible real part of \\[(75+117i)z+\\frac{96+144i}{z}\\]where $z$ is a complex number with $|z|=4$.",
-    ]
 
     llm_worker = TRTLLMWorker.init_with_new_llm(
         args.model_dir,
         backend="pytorch",
-        max_batch_size=32,
-        max_num_tokens=kwargs.get("max_tokens"),
+        max_batch_size=2048,
+        max_num_tokens=args.max_tokens,
     )
     print(f"init llm worker done")
+
+    dataset_path = Path(__file__).parent / args.dataset
+    with open(dataset_path, 'r', encoding='utf-8') as file:
+        question_data = [json.loads(line.strip()) for line in file]
+
+    if args.qid != -1:
+        question_data = [question_data[args.qid]]
+    prompts = [
+        prepare_prompt(question_data['question'], llm_worker.tokenizer)
+        for question_data in question_data
+    ]
+    ground_truth = [
+        str(question_data.get('answer', '')).strip()
+        for question_data in question_data
+    ]
+    kwargs["ground_truth"] = ground_truth
+
+    print(f"has {len(prompts)} prompts")
 
     if args.run_type == "offline" or args.run_type == "online":
         test_single_vote_controller(prompts,
