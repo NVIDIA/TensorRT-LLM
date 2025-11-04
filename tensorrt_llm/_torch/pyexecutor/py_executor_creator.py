6e7cb4ae6317fcd896237e3244fe9a1aed89c2b7
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2025-11-04 06:13:59 +0000

    avoid cudaFree overlap with cuda graph
    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/py_executor_creator.py
+++ tensorrt_llm/_torch/pyexecutor/py_executor_creator.py
@@ -1,5 +1,6 @@
 import copy
 import enum
+import gc
 import importlib
 import os
 from concurrent.futures import ThreadPoolExecutor
@@ -600,6 +601,8 @@ def create_py_executor(
     with mem_monitor.observe_creation_stage(
             _ExecutorCreationStage.INIT_EXTRA_RESOURCES
             if estimating_kv_cache else _ExecutorCreationStage.EXTRA_RESOURCES):
+        # run gc.collect() to free memory of the previous py_executor, avoid cudaFree overlap with cuda graph capture
+        gc.collect()
         py_executor = create_py_executor_instance(
             dist=dist,
             resources=resources,
