f48c05bf9259021f12ec0e68db91b7c01d098a95
Ludwig Schneider <lschneider@nvidia.com>
2025-11-05 17:08:03 -0600

    reset files, I don't want to touch
    Signed-off-by: Ludwig Schneider <lschneider@nvidia.com>

--- setup.py
+++ setup.py
@@ -276,7 +276,7 @@ setup(
     package_data={
         'tensorrt_llm': package_data,
     },
-    license_files=["LICENSE", "ATTRIBUTIONS-CPP.md"],
+    license_files=get_license(),
     entry_points={
         'console_scripts': [
             'trtllm-build=tensorrt_llm.commands.build:main',
