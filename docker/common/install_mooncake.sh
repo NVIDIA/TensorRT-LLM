67d94eb2865dfa7d9176d705bf2e7f0204f6bb90
wjueyao <wyao123@terpmail.umd.edu>
2025-11-06 11:33:50 +0800

    refactor: compress mooncake source directory
    Signed-off-by: wjueyao <wyao123@terpmail.umd.edu>

--- docker/common/install_mooncake.sh
+++ docker/common/install_mooncake.sh
@@ -6,6 +6,11 @@ MOONCAKE_REPO="https://github.com/kvcache-ai/Mooncake.git"
 MOONCAKE_INSTALL_PATH="/usr/local"
 
 git clone --depth 1 -b ${MOONCAKE_VERSION} ${MOONCAKE_REPO}
+
+tar -czf mooncake-${MOONCAKE_VERSION}.tar.gz Mooncake
+mkdir -p /third-party-source
+mv mooncake-${MOONCAKE_VERSION}.tar.gz /third-party-source/
+
 cd Mooncake
 bash dependencies.sh
 mkdir build
@@ -13,5 +18,8 @@ cd build
 cmake .. -DUSE_CUDA=ON -DBUILD_SHARED_LIBS=ON
 make -j
 make install
+
+cd ../..
+rm -rf Mooncake
 
 echo "export LD_LIBRARY_PATH=${MOONCAKE_INSTALL_PATH}/lib:\$LD_LIBRARY_PATH" >> "${ENV}"
