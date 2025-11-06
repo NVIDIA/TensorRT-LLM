18a4b985f1ddd7bcf443580cf8353c6a9d123145
yuanjingx87 <197832395+yuanjingx87@users.noreply.github.com>
2025-11-05 19:06:29 -0800

    [None][infra] allow to choose repo when generate lock files (#8659)
    Signed-off-by: Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>

--- jenkins/GenerateLock.groovy
+++ jenkins/GenerateLock.groovy
@@ -38,18 +38,38 @@ def createKubernetesPodConfig()
     return podConfig
 }
 
+def getGitCredentialId (String repoUrlKey) {
+    if (repoUrlKey == "tensorrt_llm_internal") {
+        return 'svc_tensorrt_gitlab_api_token_no_username_as_string'
+    } else {
+        return 'github-token-trtllm-ci'
+    }
+}
+
 def generate()
 {
     sh "pwd && ls -alh"
 
     container("alpine") {
-        LLM_REPO = "https://github.com/NVIDIA/TensorRT-LLM.git"
+        def LLM_REPO = "https://github.com/NVIDIA/TensorRT-LLM.git"
+        if (params.repoUrlKey == "tensorrt_llm_internal") {
+            withCredentials([string(credentialsId: 'default-llm-repo', variable: 'DEFAULT_LLM_REPO')]) {
+                LLM_REPO = DEFAULT_LLM_REPO
+            }
+        }
+        if (params.repoUrlKey == "custom_repo") {
+            if (params.customRepoUrl == "") {
+                throw new Exception("Invalid custom repo url provided")
+            }
+            LLM_REPO = params.customRepoUrl
+        }
+        def CREDENTIAL_ID = getGitCredentialId(params.repoUrlKey)
         sh "apt update"
         sh "apt install -y python3-dev git curl git-lfs"
         sh "git config --global --add safe.directory ${env.WORKSPACE}"
         sh "git config --global user.email \"90828364+tensorrt-cicd@users.noreply.github.com\""
         sh "git config --global user.name \"TensorRT LLM\""
-        trtllm_utils.checkoutSource(LLM_REPO, params.llmBranch, env.WORKSPACE, false, true)
+        trtllm_utils.checkoutSource(LLM_REPO, params.branchName, env.WORKSPACE, false, true)
         sh "python3 --version"
         sh "curl -sSL https://install.python-poetry.org | POETRY_VERSION=1.8.5 python3 -"
         sh "cd ${env.WORKSPACE}"
@@ -58,18 +78,18 @@ def generate()
         def count = sh(script: "git status --porcelain security_scanning/ | wc -l", returnStdout: true).trim()
         echo "Changed/untracked file count: ${count}"
         if (count == "0") {
-            echo "No changes in Git"
+            echo "No update that needs to be checked in"
         } else {
             sh "git status"
             sh "git add \$(find . -type f \\( -name 'poetry.lock' -o -name 'pyproject.toml' \\))"
             sh "git commit -s -m \"[None][infra] Check in most recent lock file from nightly pipeline\""
-            withCredentials([usernamePassword(credentialsId: 'github-cred-trtllm-ci', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_PASS')]) {
-                def authedUrl = LLM_REPO.replaceFirst('https://', "https://${GIT_USER}:${GIT_PASS}@")
+            withCredentials([string(credentialsId: CREDENTIAL_ID, variable: 'API_TOKEN')]) {
+                def authedUrl = LLM_REPO.replaceFirst('https://', "https://svc_tensorrt:${API_TOKEN}@")
                 sh "git remote set-url origin ${authedUrl}"
-                sh "git fetch origin ${params.llmBranch}"
+                sh "git fetch origin ${params.branchName}"
                 sh "git status"
-                sh "git rebase origin/${params.llmBranch}"
-                sh "git push origin HEAD:${params.llmBranch}"
+                sh "git rebase origin/${params.branchName}"
+                sh "git push origin HEAD:${params.branchName}"
             }
         }
     }
@@ -81,7 +101,9 @@ pipeline {
         kubernetes createKubernetesPodConfig()
     }
     parameters {
-        string(name: 'llmBranch', defaultValue: 'main', description: 'the branch to generate the lock files')
+        string(name: 'branchName', defaultValue: 'main', description: 'the branch to generate the lock files')
+        choice(name: 'repoUrlKey', choices: ['tensorrt_llm_github','tensorrt_llm_internal', 'custom_repo'], description: "The repo url to process, choose \"custom_repo\" if you want to set your own repo")
+        string(name: 'customRepoUrl', defaultValue: '', description: 'Your custom repo to get processed, need to select \"custom_repo\" for repoUrlKey, otherwise it will be ignored')
     }
     options {
         skipDefaultCheckout()
