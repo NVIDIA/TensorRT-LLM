[#6507][fix] Fix precision issue due to KV layout mismatch for split/concat kernels
The split/concat kernels expect KV cache in
"[max_num_pages, 2, num_kv_heads, page_size, head_dim]" layout. However,
the actually used KV cache layout exposed by"KVCacheManager.get_buffers()" is
"[max_num_pages, 2, page_size, num_kv_heads, head_dim]".
This layout mismatch causes wrong indexing for split/concat kernels,
leading to incorrect transferred prefill KV cache. This patch is a quick fix
for flashinfer attn_backend.

Signed-off-by: ZhangGe6 <sjtu.zg123@gmail.com>

tests/unittest/_torch/attention/test_attention.py
@@ -438,8 +438,8 @@ def test_attention_backend(s: Scenario):
     flashinfer_kv_cache = torch.randn(num_layers,
                                       s.max_num_pages,
                                       2,
-                                      page_size,
                                       num_kv_heads,
+                                      page_size,
                                       head_dim,
                                       device="cuda").to(s.kvcache_dtype)
     ref_kv_cache = flashinfer_kv_cache.transpose(1, 2).contiguous().view(
@@ -588,8 +588,8 @@ def test_attention_backend_ifb(s: PagedScenario):
     flashinfer_kv_cache = torch.randn(num_layers,
                                       s.max_num_pages,
                                       2,
-                                      page_size,
                                       num_kv_heads,
+                                      page_size,
                                       head_dim,
                                       device="cuda").to(s.kvcache_dtype)
     ref_kv_cache = flashinfer_kv_cache.transpose(1, 2).contiguous().view(