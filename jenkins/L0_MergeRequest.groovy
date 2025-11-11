9d53653c8d91fc3f6857d7bcb4908de77df290d1
Zhanrui Sun <184402041+ZhanruiSunCh@users.noreply.github.com>
2025-11-11 16:27:10 +0800

    Update L0_MergeRequest.groovy
    Signed-off-by: Zhanrui Sun <184402041+ZhanruiSunCh@users.noreply.github.com>

--- jenkins/L0_MergeRequest.groovy
+++ jenkins/L0_MergeRequest.groovy
@@ -1,4 +1,4 @@
-@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@user/zhanruis/jira_9018']) _
+@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@main']) _
 
 import java.lang.InterruptedException
 import groovy.transform.Field
