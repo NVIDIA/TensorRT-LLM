a338a87bdb8e8ea12693c5f66c3f4bb47321c158
qqiao <qqiao@nvidia.com>
2025-11-12 02:02:01 +0000

    Update test data
    Signed-off-by: qqiao <qqiao@nvidia.com>

--- tests/integration/defs/test_e2e.py
+++ tests/integration/defs/test_e2e.py
@@ -1675,7 +1675,7 @@ def test_openai_lora(llm_root, llm_venv):
 
 
 def test_openai_chat_multimodal_example(llm_root, llm_venv):
-    time.sleep(310)
+    time.sleep(610)
     pytest.fail("Emma test")
     test_root = unittest_path() / "llmapi" / "apps"
     llm_venv.run_cmd(
