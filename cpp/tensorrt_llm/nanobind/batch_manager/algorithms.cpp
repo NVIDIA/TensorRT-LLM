ddafb23666b8f504ccd15dd904146aad2d431b4a
Robin Kobus <19427718+Funatiq@users.noreply.github.com>
2025-11-12 16:12:58 +0000

    Remove PauseRequests bindings from batch manager algorithms
    Signed-off-by: Robin Kobus <19427718+Funatiq@users.noreply.github.com>

--- cpp/tensorrt_llm/nanobind/batch_manager/algorithms.cpp
+++ cpp/tensorrt_llm/nanobind/batch_manager/algorithms.cpp
@@ -21,7 +21,6 @@
 #include "tensorrt_llm/batch_manager/kvCacheManager.h"
 #include "tensorrt_llm/batch_manager/llmRequest.h"
 #include "tensorrt_llm/batch_manager/microBatchScheduler.h"
-#include "tensorrt_llm/batch_manager/pauseRequests.h"
 #include "tensorrt_llm/batch_manager/peftCacheManager.h"
 #include "tensorrt_llm/common/tllmDataType.h"
 #include "tensorrt_llm/nanobind/common/customCasters.h"
@@ -102,12 +101,4 @@ void tensorrt_llm::nanobind::batch_manager::algorithms::initBindings(nb::module_
         .def("__call__", &MicroBatchScheduler::operator(), nb::arg("active_requests"), nb::arg("inflight_req_ids"),
             nb::arg("max_batch_size_runtime"), nb::arg("max_num_tokens_runtime"))
         .def("name", [](MicroBatchScheduler const&) { return MicroBatchScheduler::name; });
-
-    nb::class_<PauseRequests>(m, PauseRequests::name)
-        .def(nb::init<SizeType32>(), nb::arg("max_input_len"))
-        .def("__call__", &PauseRequests::operator(), nb::arg("requests_to_pause"), nb::arg("inflight_req_ids"),
-            nb::arg("req_ids_to_pause"), nb::arg("pause_flagged"), nb::arg("seq_slot_manager"),
-            nb::arg("kv_cache_manager") = std::nullopt, nb::arg("cross_kv_cache_manager") = std::nullopt,
-            nb::arg("peft_cache_manager") = std::nullopt)
-        .def("name", [](PauseRequests const&) { return PauseRequests::name; });
 }
