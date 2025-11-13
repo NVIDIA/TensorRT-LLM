5bb4188d713af0b8caa59e7d61cebbdbb46e4994
Enwei Zhu <21126786+syuoni@users.noreply.github.com>
2025-11-13 09:10:09 +0000

    move actAdapter
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/cutlass_kernels/include/moe_kernels.h
+++ cpp/tensorrt_llm/kernels/cutlass_kernels/include/moe_kernels.h
@@ -33,9 +33,6 @@
 #include <random>
 #include <utility>
 
-#ifdef __CUDACC__
-#include "cutlass/epilogue/thread/activation.h"
-#endif // __CUDACC__
 namespace tensorrt_llm::kernels
 {
 // Change to following declarations must sync with lora.h in public repo
@@ -146,60 +143,6 @@ struct ActivationParams
         return activation_type;
     }
 };
-
-// ============================== Activation Adaptors =================================
-#ifdef __CUDACC__
-
-template <template <class> class ActFn>
-struct IdentityAdaptor
-{
-    constexpr static bool IS_GLU = false;
-    float alpha = 1.0f;
-    float beta = 0.0f;
-    float limit = std::numeric_limits<float>::infinity();
-
-    template <class T>
-    __device__ T operator()(T const& x) const
-    {
-        ActFn<T> fn{};
-        return fn(x);
-    }
-};
-
-template <template <class> class ActFn>
-struct GLUAdaptor
-{
-    constexpr static bool IS_GLU = true;
-    float alpha = 1.0f;
-    float beta = 0.0f;
-    float limit = std::numeric_limits<float>::infinity();
-
-    template <class T>
-    __device__ T operator()(T const& gate, T const& linear) const
-    {
-        ActFn<T> fn{};
-        return fn(gate) * linear;
-    }
-};
-
-struct SwigluBiasAdaptor
-{
-    constexpr static bool IS_GLU = true;
-    float alpha = 1.0f;
-    float beta = 0.0f;
-    float limit = std::numeric_limits<float>::infinity();
-
-    template <class T>
-    __device__ T operator()(T const& gate, T const& linear) const
-    {
-        cutlass::epilogue::thread::Sigmoid<T> fn{};
-        T linear_clamped = cutlass::maximum<T>{}(cutlass::minimum<T>{}(linear, limit), -limit);
-        T gate_clamped = cutlass::minimum<T>{}(gate, limit);
-        return gate_clamped * fn(gate_clamped * alpha) * (linear_clamped + beta);
-    }
-};
-
-#endif // __CUDACC__
 
 /**
  * \brief Describes what parallelism mode the MoE is using
