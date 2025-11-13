e21bf18f2e1f092701f2b781cb7eb864484c4397
Rundong (David) Li <davidli@nvidia.com>
2025-11-13 02:12:29 -0800

    add cuda.tile RMA norm kernel and bindings
    Co-authored-by: Jinman Xie <jinmanx@nvidia.com>
    Co-authored-by: Alexey Bylinkin <abylinkin@nvidia.com>
    Co-authored-by: Qiqi Xiao <qiqix@nvidia.com>
    Co-authored-by: Biao Wang <biaow@nvidia.com>
    Co-authored-by: Thomas Schmid <thschmid@nvidia.com>
    Signed-off-by: Rundong (David) Li <davidli@nvidia.com>

---
+++ tensorrt_llm/_torch/cuda_tile_utils.py
@@ -0,0 +1,47 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import functools
+import platform
+
+from ..logger import logger
+
+IS_CUDA_TILE_AVAILABLE = False
+
+
+@functools.lru_cache()
+def next_power_of_2(n: int):
+    """Return the smallest power of 2 greater than or equal to n"""
+    n -= 1
+    n |= n >> 1
+    n |= n >> 2
+    n |= n >> 4
+    n |= n >> 8
+    n |= n >> 16
+    n |= n >> 32
+    n += 1
+    return n
+
+
+def ceil_div(a, b):
+    return (a + b - 1) // b
+
+
+if platform.system() != "Windows":
+    try:
+        import cuda.tile
+        IS_CUDA_TILE_AVAILABLE = True
+    except ImportError:
+        logger.warning("cuda.tile is not available, certain kernels will not be available")
