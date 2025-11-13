d51f8d135c9fdf9fbe8169dd583051dcb082e8d5
Dongfeng Yu <dongfengy@nvidia.com>
2025-11-13 00:04:36 +0000

    Add validM/N/K WAR
    Signed-off-by: Dongfeng Yu <dongfengy@nvidia.com>

--- cpp/tensorrt_llm/kernels/trtllmGenKernels/batchedGemm/KernelRunner.cpp
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/batchedGemm/KernelRunner.cpp
@@ -355,7 +355,7 @@ void TrtllmGenBatchedGemmRunner::run(int32_t m, int32_t n, int32_t k, int32_t va
     int32_t const* ctaIdxXyToBatchIdx, int32_t const* ctaIdxXyToMnLimit, int32_t const* numNonExitingCtas,
     void* workspace, CUstream stream, int device, int32_t configIndex)
 {
-    std::cout << "run 1" << std::endl;
+    std::cout << "run 1 fixed" << std::endl;
     std::cout << ptrBias << std::endl;
     auto bmm = BatchedGemmInterface();
 
@@ -405,6 +405,9 @@ void TrtllmGenBatchedGemmRunner::run(int32_t m, int32_t n, int32_t k, int32_t va
     gemmData.mProblemDimensions.mValidK = validK;
     gemmData.mProblemDimensions.mRank = 0;
     gemmData.mProblemDimensions.mWorldSize = 1;
+    gemmData.mProblemDimensions.mValidM = m;
+    gemmData.mProblemDimensions.mValidN = n;
+    gemmData.mProblemDimensions.mValidK = k;
 
     // Inputs
     gemmData.mInputBuffers.mPtrA = mOptions.transposeMmaOutput ? b : a;
@@ -557,6 +560,9 @@ std::vector<int64_t> TrtllmGenBatchedGemmRunner::getValidConfigIndices(int32_t m
     gemmData.mProblemDimensions.mRank = 0;
     gemmData.mProblemDimensions.mWorldSize = 1;
     gemmData.mProblemDimensions.mMaxNumCtasInTokenDim = maxNumCtasInBatchDim;
+    gemmData.mProblemDimensions.mValidM = m;
+    gemmData.mProblemDimensions.mValidN = n;
+    gemmData.mProblemDimensions.mValidK = k;
     auto cmpFunc = [&configs, &gemmData, &bmm, &multiProcessorCount](int64_t idx0, int64_t idx1)
     {
         auto const& optionsA = configs[idx0].mOptions;
@@ -681,6 +687,9 @@ bool TrtllmGenBatchedGemmRunner::isValidConfigIndex(int32_t configIndex, int32_t
     gemmData.mProblemDimensions.mRank = 0;
     gemmData.mProblemDimensions.mWorldSize = 1;
     gemmData.mProblemDimensions.mMaxNumCtasInTokenDim = maxNumCtasInBatchDim;
+    gemmData.mProblemDimensions.mValidM = m;
+    gemmData.mProblemDimensions.mValidN = n;
+    gemmData.mProblemDimensions.mValidK = k;
 
     auto const& config = configs[configIndex];
 
