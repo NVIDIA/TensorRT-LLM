3ba882f7adbdcc9c471b2579b0292b4ca4e114e0
qqiao <qqiao@nvidia.com>
2025-11-17 05:17:51 -0800

    Test for another rtxpro-6000d
    Signed-off-by: qqiao <qqiao@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1314,6 +1314,15 @@ def createKubernetesPodConfig(image, type, arch = "amd64", gpuCount = 1, perfMod
                     nvidia.com/driver_version: '${driverVersion}'
                     tensorrt/test_type: perf"""
         }
+        else if (type.contains("6000d"))
+        {
+            selectors = """
+                    kubernetes.io/arch: ${arch}
+                    kubernetes.io/os: linux
+                    nvidia.com/gpu_type: ${gpuType}
+                    kubernetes.io/hostname: 4u8g-gen-0237.ipp3a2.colossus
+                    nvidia.com/driver_version: '${driverVersion}'"""
+        }
         else
         {
             selectors = """
@@ -1400,8 +1409,6 @@ def createKubernetesPodConfig(image, type, arch = "amd64", gpuCount = 1, perfMod
                         requiredDuringSchedulingIgnoredDuringExecution:
                             nodeSelectorTerms:
                             - matchExpressions:
-                              - key: "tensorrt/taints"
-                                operator: DoesNotExist
                               - key: "tensorrt/affinity"
                                 operator: NotIn
                                 values:
