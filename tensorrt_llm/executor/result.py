8cc026e2bee7010f489cde7b300a8ad8366ff2a5
zhanghaotong <zhanghaotong.zht@antgroup.com>
2025-11-18 17:03:32 +0800

    [fix] Fix asyncQueue deque initialization
    Signed-off-by: zhanghaotong <zhanghaotong.zht@antgroup.com>

--- tensorrt_llm/executor/result.py
+++ tensorrt_llm/executor/result.py
@@ -854,7 +854,7 @@ class IterationResult:
     Runtime results for all available iterations.
     """
 
-    def __init__(self, maxsize=0):
+    def __init__(self, maxsize: int = 0):
         self._done = False
         self._timeout = 2
 
