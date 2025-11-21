d299b7a81f48cb49e472b69f25fc66bdcb1c5d1d
Wangshanshan <30051912+dominicshanshan@users.noreply.github.com>
2025-11-20 18:24:24 -0800

    Fix auto deploy demo logprobs return issue.
    Signed-off-by: Wangshanshan <30051912+dominicshanshan@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/shim/demollm.py
+++ tensorrt_llm/_torch/auto_deploy/shim/demollm.py
@@ -235,11 +235,11 @@ class DemoEngine(ADEngine):
         logits_shape = logits.shape
         logits = logits.view(-1, logits_shape[-1])  # sampling_batch expects 2D logits
         if isinstance(sampling_params.top_k, int) and sampling_params.top_k > 1:
-            idx_next, probs = top_k_sampling_batch(
+            idx_next, probs, _ = top_k_sampling_batch(
                 logits, top_k=sampling_params.top_k, temperature=1.0
             )
         else:
-            idx_next, probs = greedy_search_sampling_batch(logits)
+            idx_next, probs, _ = greedy_search_sampling_batch(logits)
         idx_next = idx_next.view(logits_shape[:-1])
         return idx_next, probs
 
