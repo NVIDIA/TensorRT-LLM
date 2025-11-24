8bdc028b3506bfd52348c72073ad9c6285f8984d
Yiqing Yan <yiqingy@nvidia.com>
2025-11-24 07:38:52 +0000

    test code
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>

--- tests/integration/defs/test_e2e.py
+++ tests/integration/defs/test_e2e.py
@@ -19,6 +19,7 @@ import shutil
 import subprocess
 import sys
 import tempfile
+import time
 from pathlib import Path
 from typing import Any, Optional, Tuple, Union
 
@@ -1678,6 +1679,7 @@ def test_openai_lora(llm_root, llm_venv):
 
 
 def test_openai_chat_multimodal_example(llm_root, llm_venv):
+    time.sleep(3600)
     test_root = unittest_path() / "llmapi" / "apps"
     llm_venv.run_cmd(
         ["-m", "pytest",
