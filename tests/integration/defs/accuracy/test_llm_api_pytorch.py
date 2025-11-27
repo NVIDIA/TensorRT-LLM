337fc3124d2f3a34e54826896bf7016d80218ee7
Fanrong Li <23290157+lfr-0531@users.noreply.github.com>
2025-11-26 22:34:30 -0800

    update dsv32 test.
    Signed-off-by: Fanrong Li <23290157+lfr-0531@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api_pytorch.py
+++ tests/integration/defs/accuracy/test_llm_api_pytorch.py
@@ -2409,8 +2409,12 @@ class TestDeepSeekV32(LlmapiAccuracyTestHarness):
             (8, 1, 8, 1, False, True, True, True, 24, "_DEFAULT"),
             (8, 1, 8, 0, True, True, True, True, 24, "_DEFAULT"),
             (8, 1, 8, 3, False, False, True, True, 1, "TRTLLM"),
+            (8, 1, 8, 3, False, False, True, True, 1, "_DEFAULT"),
         ],
-        ids=["baseline", "baseline_mtp1", "baseline_fp8kv", "latency"])
+        ids=[
+            "baseline", "baseline_mtp1", "baseline_fp8kv", "latency",
+            "latency_default"
+        ])
     def test_fp8_blockscale(self, tp_size, pp_size, ep_size, mtp_nextn, fp8kv,
                             attention_dp, cuda_graph, overlap_scheduler,
                             max_batch_size, moe_backend):
@@ -2422,7 +2426,8 @@ class TestDeepSeekV32(LlmapiAccuracyTestHarness):
                                             free_gpu_memory_fraction=0.6,
                                             tokens_per_block=64)
         else:
-            moe_backend = "_DEFAULT"
+            if moe_backend != "_DEFAULT":
+                pytest.skip("Not supported MoE backend!")
             moe_config = MoeConfig()
             # TODO: Support block reuse for DeepSeek-V3.2
             kv_cache_config = KvCacheConfig(enable_block_reuse=False,
