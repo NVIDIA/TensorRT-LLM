a21be43677a7f8e6c92b2b117e4daf83907fd435
Emma Qiao <qqiao@nvidia.com>
2025-11-27 15:42:38 +0800

    [TRTLLM-9279][infra] Use flexcache for gh200 nodes since they locate in Austin (#9405)
    Signed-off-by: qqiao <qqiao@nvidia.com>
    Signed-off-by: Emma Qiao <qqiao@nvidia.com>
    Co-authored-by: Yanchao Lu <yanchaol@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1426,8 +1426,8 @@ def createKubernetesPodConfig(image, type, arch = "amd64", gpuCount = 1, perfMod
                     server: 10.117.145.14
                     path: /vol/scratch1/scratch.michaeln_blossom
     """
-    if (type.contains("6000d")) {
-        // rtx-pro-6000d nodes are located in Austin DC, we use the FlexCache to speed up the data access.
+    if (type.contains("6000d") || type.contains("gh200")) {
+        // rtx-pro-6000d and gh200 nodes are located in Austin DC, we use the FlexCache to speed up the data access.
         llmModelVolume = """
                 - name: scratch-trt-llm-data
                   nfs:
