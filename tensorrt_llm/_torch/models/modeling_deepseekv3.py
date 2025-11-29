6543888ca91a7ca9871dc8924171854b60be8c8b
Enwei Zhu <21126786+syuoni@users.noreply.github.com>
2025-11-29 03:25:13 +0000

    gptoss
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>

--- tensorrt_llm/_torch/models/modeling_deepseekv3.py
+++ tensorrt_llm/_torch/models/modeling_deepseekv3.py
@@ -1555,7 +1555,6 @@ class DeepseekV3Model(DecoderModel):
             config.vocab_size,
             config.hidden_size,
             dtype=config.torch_dtype,
-            reduce_output=False,
         )
 
         self.layers = nn.ModuleList([
