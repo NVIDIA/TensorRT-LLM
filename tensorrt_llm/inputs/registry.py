26da09e5d134f838e804266b001d1ba831db88c0
Chang Liu (Enterprise Products) <9713593+chang-l@users.noreply.github.com>
2025-12-02 11:13:16 -0800

    Remove traceback dump for mm input processor
    Signed-off-by: Chang Liu (Enterprise Products) <9713593+chang-l@users.noreply.github.com>

--- tensorrt_llm/inputs/registry.py
+++ tensorrt_llm/inputs/registry.py
@@ -697,8 +697,6 @@ def create_input_processor_with_hash(
                     input_processor.multimodal_hashing_supported = True
                 return prompt_token_ids, extra_processed_inputs
             except Exception as e:
-                import traceback
-                traceback.print_exc()
                 logger.warning(f"Multimodal hashing failed: {e}.")
                 if try_multimodal_hashing:
                     # if trying for first time, fall back to basic input processor
@@ -708,8 +706,6 @@ def create_input_processor_with_hash(
                     try:
                         return input_processor(inputs, sampling_params)
                     except Exception as e2:
-                        import traceback
-                        traceback.print_exc()
                         logger.warning(f"Basic input processor failed: {e}.")
                         raise e2
                 else:
@@ -718,8 +714,6 @@ def create_input_processor_with_hash(
             try:
                 return input_processor(inputs, sampling_params)
             except Exception as e:
-                import traceback
-                traceback.print_exc()
                 logger.warning(f"Basic input processor failed: {e}.")
                 raise e
 
