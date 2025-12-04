5a75ae65a4d9786d398872ec55cbe794d02364e0
bhsueh <11360707+byshiue@users.noreply.github.com>
2025-12-03 17:59:46 -0800

    [Fix] fix bug of loading mistral config
    Signed-off-by: bhsueh <11360707+byshiue@users.noreply.github.com>

--- tensorrt_llm/_torch/models/modeling_mistral.py
+++ tensorrt_llm/_torch/models/modeling_mistral.py
@@ -539,6 +539,8 @@ class Mistral3VLM(PreTrainedModel):
         if name == "text_config":
             sub_model_config._frozen = False
             sub_model_config.skip_create_weights_in_init = True
+            if not hasattr(sub_model_config.pretrained_config, "architectures") or sub_model_config.pretrained_config.architectures is None:
+                sub_model_config.pretrained_config.architectures = model_config.pretrained_config.architectures
             sub_model_config._frozen = True
 
         # Make sure some fields that are not explicitly included in the sub config, but present
