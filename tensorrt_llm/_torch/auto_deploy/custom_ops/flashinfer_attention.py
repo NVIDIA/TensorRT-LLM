e6fc6b20690f6c58664eed280c4f9ccac2d6ea40
Chenghao Zhang <211069071+nvchenghaoz@users.noreply.github.com>
2025-12-05 09:15:06 -0800

    bring back k v scale
    Signed-off-by: Chenghao Zhang <211069071+nvchenghaoz@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/custom_ops/flashinfer_attention.py
+++ tensorrt_llm/_torch/auto_deploy/custom_ops/flashinfer_attention.py
@@ -299,6 +299,8 @@ def flashinfer_mha_with_cache(
         paged_kv_last_page_len,
         pp,
     )
+    # Assuming k_scale = v_scale = 1.0, we just have to cast k and v to fp8 before appending to kv cache
+    k_scale, v_scale = 1.0, 1.0
     y = wrapper.run(
         q, (k_cache, v_cache), k_scale=k_scale, v_scale=v_scale, enable_pdl=get_env_enable_pdl()
     )
