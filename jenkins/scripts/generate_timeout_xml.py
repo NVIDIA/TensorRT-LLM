0c9ecb75fe491fb20517d0fa07b24144d821d5d8
Yiqing Yan <yiqingy@nvidia.com>
2025-12-09 03:07:14 +0000

    fix parse test name
    Signed-off-by: Yiqing Yan <yiqingy@nvidia.com>

--- jenkins/scripts/generate_timeout_xml.py
+++ jenkins/scripts/generate_timeout_xml.py
@@ -1,8 +1,13 @@
 import argparse
 import sys
 
+
 def parse_xml_classname_name_file_from_testname(testname, stage_name):
     classname, name, file = "", "", ""
+
+    # Remove stage_name prefix if present
+    if testname.startswith(stage_name + "/"):
+        testname = testname[len(stage_name) + 1:]
 
     # Get file name
     if testname.startswith("unittest/"):
@@ -20,10 +25,18 @@ def parse_xml_classname_name_file_from_testname(testname, stage_name):
     if testname.startswith("unittest/"):
         classname = stage_name + ".test_unittests"
     elif len(testname.split("::")) == 3:
-        classname = stage_name + "." + testname.split("::")[0].replace(".py", "").replace("/", ".") + "." + testname.split("::")[1]
+        classname = (
+            stage_name
+            + "."
+            + testname.split("::")[0].replace(".py", "").replace("/", ".")
+            + "."
+            + testname.split("::")[1]
+        )
     else:
         classname = stage_name + "." + testname.split("::")[0].replace(".py", "").replace("/", ".")
-        if testname.startswith("accuracy/") or (testname.startswith("examples/") and "[" not in testname):
+        if testname.startswith("accuracy/") or (
+            testname.startswith("examples/") and "[" not in testname
+        ):
             classname = ""
 
     return classname, name, file
@@ -32,7 +45,7 @@ def parse_xml_classname_name_file_from_testname(testname, stage_name):
 def generate_timeout_xml(testList, stage_name):
     xmlContent = f"""<?xml version="1.0" encoding="UTF-8"?><testsuites>
         <testsuite name="{stage_name}" errors="{len(testList)}" failures="0" skipped="0" tests="{len(testList)}" time="1.00">"""
-    
+
     for test in testList:
         classname, name, file = parse_xml_classname_name_file_from_testname(test, stage_name)
         xmlContent += f"""<testcase classname="{classname}" name="{name}" file="{file}" time="1.0">
@@ -40,17 +53,12 @@ def generate_timeout_xml(testList, stage_name):
         </error></testcase>"""
     xmlContent += "</testsuite></testsuites>"
     print(xmlContent)
-    
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--tests',
-                        required=True,
-                        help='Test list file')
-    parser.add_argument('--stage-name',
-                        required=True,
-                        help='Stage name')
+    parser.add_argument("--tests", required=True, help="Test list file")
+    parser.add_argument("--stage-name", required=True, help="Stage name")
     args = parser.parse_args(sys.argv[1:])
-    testList = args.tests.split('\n')
+    testList = args.tests.split("\n")
     generate_timeout_xml(testList, args.stage_name)
