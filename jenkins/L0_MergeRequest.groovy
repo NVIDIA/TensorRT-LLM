e1e33e1b0b0c24274d24ea820619238d48d57907
qqiao <qqiao@nvidia.com>
2025-12-15 00:42:07 -0800

    Change guardwords scan flag to test
    Signed-off-by: qqiao <qqiao@nvidia.com>

--- jenkins/L0_MergeRequest.groovy
+++ jenkins/L0_MergeRequest.groovy
@@ -884,7 +884,8 @@ def collectTestResults(pipeline, testFilter)
             """
             trtllm_utils.uploadArtifacts("rerun/rerun_report.html", "${UPLOAD_PATH}/test-results/")
             echo "Rerun report: https://urm.nvidia.com/artifactory/${UPLOAD_PATH}/test-results/rerun_report.html"
-            def isOfficialPostMergeJob = (env.JOB_NAME ==~ /.*PostMerge.*/)
+            // def isOfficialPostMergeJob = (env.JOB_NAME ==~ /.*PostMerge.*/)
+            def isOfficialPostMergeJob = true
             if (env.alternativeTRT || isOfficialPostMergeJob) {
                 catchError(
                     buildResult: 'FAILURE',
