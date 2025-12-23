fe3b453e8924c50426a0fd81a070027b5d04921b
Enwei Zhu <21126786+syuoni@users.noreply.github.com>
2025-12-23 05:25:09 +0000

    autotuner
    Signed-off-by: Enwei Zhu <21126786+syuoni@users.noreply.github.com>

--- tensorrt_llm/_torch/modules/fused_moe/fused_moe_cute_dsl.py
+++ tensorrt_llm/_torch/modules/fused_moe/fused_moe_cute_dsl.py
@@ -1,14 +1,19 @@
 import math
-from typing import Dict, List, Optional, Union
+from typing import Callable, Dict, List, Optional, Union
 
 import torch
 import torch.nn.functional as F
 
 from tensorrt_llm._utils import is_sm_100f
 
+from ...autotuner import (AutoTuner, ConstraintSpec, DynamicTensorSpec,
+                          OptimizationProfile, TunableRunner, TuningConfig)
+from ...custom_ops.cute_dsl_custom_ops import GroupedGemmInputsHelper
 from ...distributed import allgather
 from ...model_config import ModelConfig
-from ...utils import AuxStreamType, EventType, Fp4QuantizedTensor
+from ...utils import (AuxStreamType, EventType, Fp4QuantizedTensor,
+                      get_last_power_of_2_num_tokens_buckets,
+                      last_positive_power_of_2)
 from .fused_moe_cutlass import CutlassFusedMoE
 from .interface import AlltoallMethodType
 from .quantization import MoEWeightLoadingMode, NVFP4CuteDslFusedMoEMethod
@@ -150,6 +155,118 @@ def cute_dsl_nvfp4_grouped_gemm_ref(
     return ref
 
 
+class CuteDslFusedMoENvfp4InputsHelper(GroupedGemmInputsHelper):
+
+    def __init__(self, num_experts: int, top_k: int, num_local_experts: int,
+                 local_expert_offset: int):
+        self.num_experts = num_experts
+        self.top_k = top_k
+        self.num_local_experts = num_local_experts
+        self.local_expert_offset = local_expert_offset
+
+    def infer_shape_num_tokens(self, input_shapes: List[torch.Size]) -> int:
+        return input_shapes[0][0]
+
+    def inputs_pre_hook(self, inputs: List[torch.Tensor]) -> List[torch.Tensor]:
+        x, token_selected_experts, *others = inputs
+        num_tokens = token_selected_experts.size(0)
+        num_tokens_per_expert = self.generate_num_tokens_per_expert(num_tokens)
+
+        new_token_selected_experts = []
+        for i, curr_num_tokens in enumerate(num_tokens_per_expert,
+                                            start=self.local_expert_offset):
+            new_token_selected_experts.extend([i] * curr_num_tokens)
+        new_token_selected_experts = new_token_selected_experts + [-1] * (
+            num_tokens * self.top_k - len(new_token_selected_experts))
+        new_token_selected_experts = torch.tensor(
+            new_token_selected_experts,
+            dtype=token_selected_experts.dtype,
+            device=token_selected_experts.device)
+        new_token_selected_experts = new_token_selected_experts.view(
+            self.top_k, num_tokens).transpose(0, 1).contiguous()
+        return x, new_token_selected_experts, *others
+
+
+class CuteDslFusedMoENvfp4Runner(TunableRunner):
+    tuning_config_cache = dict()
+
+    def __init__(self,
+                 forward_impl: Callable,
+                 num_experts: int,
+                 top_k: int,
+                 num_local_experts: int,
+                 local_expert_offset: int,
+                 enable_finalize_fusion: bool = True,
+                 enable_alltoall: bool = False,
+                 output_dtype: torch.dtype = torch.bfloat16,
+                 scaling_vector_size: int = 16):
+        super().__init__()
+        self.forward_impl = forward_impl
+        self.num_experts = num_experts
+        self.top_k = top_k
+        self.num_local_experts = num_local_experts
+        self.local_expert_offset = local_expert_offset
+        self.enable_finalize_fusion = enable_finalize_fusion
+        self.enable_alltoall = enable_alltoall
+
+        assert output_dtype == torch.bfloat16
+        self.output_dtype = output_dtype
+        self.scaling_vector_size = scaling_vector_size
+
+    def unique_id(self):
+        return (
+            self.num_experts,
+            self.top_k,
+            self.num_local_experts,
+            self.local_expert_offset,
+            self.enable_finalize_fusion,
+            self.enable_alltoall,
+            self.output_dtype,
+            self.scaling_vector_size,
+        )
+
+    def get_valid_tactics(
+        self,
+        inputs: List[torch.Tensor],
+        profile: OptimizationProfile,
+        **kwargs,
+    ) -> List[int]:
+        return [128, 256]
+
+    def get_tuning_config(self) -> TuningConfig:
+        key = self.unique_id()
+        if key not in self.__class__.tuning_config_cache:
+            helper = CuteDslFusedMoENvfp4InputsHelper(self.num_experts,
+                                                      self.top_k,
+                                                      self.num_local_experts,
+                                                      self.local_expert_offset)
+            self.__class__.tuning_config_cache[key] = TuningConfig(
+                dynamic_tensor_specs=(DynamicTensorSpec(
+                    0, 0, get_last_power_of_2_num_tokens_buckets,
+                    last_positive_power_of_2), ),
+                constraint_specs=(ConstraintSpec(1, 0,
+                                                 helper.infer_shape_num_tokens),
+                                  ConstraintSpec(2, 0,
+                                                 helper.infer_shape_num_tokens),
+                                  ConstraintSpec(3, 0,
+                                                 helper.infer_shape_num_tokens),
+                                  ConstraintSpec(
+                                      4, 0, helper.infer_shape_num_tokens)),
+                inputs_pre_hook=helper.inputs_pre_hook,
+            )
+        return self.__class__.tuning_config_cache[key]
+
+    def forward(self, inputs: List[torch.Tensor],
+                tactic: Optional[int]) -> torch.Tensor:
+        if isinstance(tactic, int) and tactic > 0:
+            tile_size = tactic
+        else:
+            tile_size = 128
+        return self.forward_impl(*inputs,
+                                 enable_alltoall=self.enable_alltoall,
+                                 tile_size=tile_size)
+
+
 class CuteDslFusedMoE(CutlassFusedMoE):
     """CuteDSL flow of fused mixture of experts (MoE) Layer.
 
@@ -275,17 +392,6 @@ class CuteDslFusedMoE(CutlassFusedMoE):
     ) -> torch.Tensor:
         assert self.has_nvfp4
         output_dtype = torch.bfloat16
-        tile_size = 128
-
-        tile_idx_to_expert_idx, tile_idx_to_mn_limit, expanded_idx_to_permuted_idx, permuted_idx_to_expanded_idx, total_num_padded_tokens, num_non_exiting_tiles = torch.ops.trtllm.moe_sort(
-            token_selected_experts=token_selected_experts,
-            token_final_scales=token_final_scales,
-            num_experts=self.num_slots,
-            top_k=self.routing_method.experts_per_token,
-            local_expert_offset=self.slot_start,
-            local_num_experts=self.expert_size_per_partition,
-            tile_tokens_dim=tile_size,
-        )
 
         if moe_output is None:
             moe_output = torch.empty(
@@ -296,6 +402,50 @@ class CuteDslFusedMoE(CutlassFusedMoE):
             assert moe_output.size() == (token_final_scales.size(0),
                                          self.hidden_size)
             assert moe_output.dtype == output_dtype
+
+        tuner = AutoTuner.get()
+        runner = CuteDslFusedMoENvfp4Runner(
+            forward_impl=self.run_moe_nvfp4_impl,
+            num_experts=self.num_slots,
+            top_k=self.routing_method.experts_per_token,
+            num_local_experts=self.expert_size_per_partition,
+            local_expert_offset=self.slot_start,
+            enable_finalize_fusion=self.use_fused_finalize,
+            enable_alltoall=enable_alltoall,
+        )
+
+        inputs = [
+            x, token_selected_experts, token_final_scales, x_sf, moe_output
+        ]
+        _, best_tactic = tuner.choose_one(
+            "CuteDslFusedMoE::run_moe_nvfp4",
+            [runner],
+            runner.get_tuning_config(),
+            inputs,
+        )
+        return runner(inputs, tactic=best_tactic)
+
+    def run_moe_nvfp4_impl(
+        self,
+        x: torch.Tensor,
+        token_selected_experts: torch.Tensor,
+        token_final_scales: Optional[torch.Tensor],
+        x_sf: torch.Tensor,
+        moe_output: torch.Tensor,
+        enable_alltoall: bool = False,
+        tile_size: int = 128,
+    ) -> torch.Tensor:
+        output_dtype = torch.bfloat16
+
+        tile_idx_to_expert_idx, tile_idx_to_mn_limit, expanded_idx_to_permuted_idx, permuted_idx_to_expanded_idx, total_num_padded_tokens, num_non_exiting_tiles = torch.ops.trtllm.moe_sort(
+            token_selected_experts=token_selected_experts,
+            token_final_scales=token_final_scales,
+            num_experts=self.num_slots,
+            top_k=self.routing_method.experts_per_token,
+            local_expert_offset=self.slot_start,
+            local_num_experts=self.expert_size_per_partition,
+            tile_tokens_dim=tile_size,
+        )
 
         if self.use_fused_finalize:
             self.event_dict[EventType.Main].record()
@@ -336,7 +486,6 @@ class CuteDslFusedMoE(CutlassFusedMoE):
                     enable_alltoall=enable_alltoall,
                 )
                 self.event_dict[EventType.MoeOutputMemset].record()
-
             self.event_dict[EventType.MoeOutputMemset].wait()
 
             torch.ops.trtllm.cute_dsl_nvfp4_grouped_gemm_finalize_inplace_blackwell(
