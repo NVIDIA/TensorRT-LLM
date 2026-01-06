a35db4c6e0e34f2f8813dbaeb54dedd0c9c288dd
Ludwig Schneider <lschneider@nvidia.com>
2026-01-06 09:45:25 -0800

    activate NCCL_SYMMETRIC auto-tuning
    Signed-off-by: Ludwig Schneider <lschneider@nvidia.com>

--- tensorrt_llm/_torch/custom_ops/torch_custom_ops.py
+++ tensorrt_llm/_torch/custom_ops/torch_custom_ops.py
@@ -1695,8 +1695,7 @@ class AllReduceRunner(TunableRunner):
         **kwargs,
     ) -> List[int]:
         valid_strategies = [
-            # TODO: NCCL_SYMMETRIC will cause hang during tuning process
-            # AllReduceStrategy.NCCL_SYMMETRIC.value,
+            AllReduceStrategy.NCCL_SYMMETRIC.value,
             AllReduceStrategy.NCCL.value,
         ]
         # Fallback in allreduceOp is set to NCCL_SYMMETRIC as default
@@ -1725,7 +1724,7 @@ class AllReduceRunner(TunableRunner):
         input, residual, norm_weight, scale, bias, workspace = inputs
         if tactic == -1:
             # TODO: Use NCCL instead of NCCL_SYMMETRIC to avoid hanging during tuning process
-            tactic = AllReduceStrategy.NCCL.value
+            tactic = AllReduceStrategy.NCCL_SYMMETRIC.value
 
         return torch.ops.trtllm.allreduce(
             input,
