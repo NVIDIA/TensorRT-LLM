b8473ebbee2de75b8f2054573e3fe60c3c783361
yuhangh <58161490+heyuhhh@users.noreply.github.com>
2026-01-06 07:05:35 +0000

    minor
    Signed-off-by: yuhangh <58161490+heyuhhh@users.noreply.github.com>

--- tests/unittest/_torch/attention/test_flashinfer_star_attn.py
+++ tests/unittest/_torch/attention/test_flashinfer_star_attn.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from dataclasses import dataclass
 from typing import List, Optional, Union
 
+import pytest
 import torch
 from parameterized import parameterized
 
