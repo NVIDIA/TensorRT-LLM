e392796644588e399c48d87771d7a82e111cb8bd
Taylor Yeonbok Lee <249374542+taylor-yb-lee@users.noreply.github.com>
2026-01-07 22:08:47 -0800

    Fix unittest failure
    Signed-off-by: Taylor Yeonbok Lee <249374542+taylor-yb-lee@users.noreply.github.com>

--- tensorrt_llm/llmapi/llm_args.py
+++ tensorrt_llm/llmapi/llm_args.py
@@ -1931,8 +1931,8 @@ class BaseLlmArgs(StrictBaseModel):
 
     # Below are all remaining arguments
 
-    model_kwargs: Dict[str, Any] = Field(
-        default_factory=dict,
+    model_kwargs: Optional[Dict[str, Any]] = Field(
+        default=None,
         description="Optional parameters overriding model config defaults. "
         "Precedence: (1) model_kwargs, (2) model config file, (3) model config class defaults. "
         "Unknown keys are ignored",
