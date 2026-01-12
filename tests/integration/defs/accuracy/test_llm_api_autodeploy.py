ae2e3e3effbd457d29c2d9ded1009f396f16a595
Chenghao Zhang <211069071+nvchenghaoz@users.noreply.github.com>
2026-01-12 12:55:12 -0800

    Update the FI version to 0.6.0
    Signed-off-by: Chenghao Zhang <211069071+nvchenghaoz@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api_autodeploy.py
+++ tests/integration/defs/accuracy/test_llm_api_autodeploy.py
@@ -193,27 +193,26 @@ class TestNemotronMOE(LlmapiAccuracyTestHarness):
         # TODO: multi-stream MOE seems to increase the memory usage
         kwargs["max_batch_size"] = 32
         kwargs["free_mem_ratio"] = 0.4
-        self.get_default_sampling_params()
         with AutoDeployLLM(model=self.MODEL_PATH_BF16,
                            tokenizer=self.MODEL_PATH_BF16,
                            **kwargs) as llm:
-            # TODO: Fix https://github.com/NVIDIA/TensorRT-LLM/issues/10580
-            # task = MMLU(self.MODEL_NAME)
-            # task.evaluate(llm, sampling_params=sampling_params)
+            sampling_params = self.get_default_sampling_params()
+            task = MMLU(self.MODEL_NAME)
+            task.evaluate(llm, sampling_params=sampling_params)
             task = GSM8K(self.MODEL_NAME)
             task.evaluate(llm)
 
     @pytest.mark.skip_less_device_memory(32000)
     def test_fp8(self):
         kwargs = self.get_default_kwargs()
+        kwargs["max_batch_size"] = 64
         with AutoDeployLLM(model=self.MODEL_PATH_FP8,
                            tokenizer=self.MODEL_PATH_FP8,
                            **kwargs) as llm:
             # Manually set quant_config for FP8 model to get the accuracy threshold
             llm.args.quant_config.quant_algo = QuantAlgo.FP8
             llm.args.quant_config.kv_cache_quant_algo = QuantAlgo.FP8
-            # TODO: Fix https://github.com/NVIDIA/TensorRT-LLM/issues/10580
-            sampling_params = self.get_default_sampling_params()            
+            sampling_params = self.get_default_sampling_params()
             task = MMLU(self.MODEL_NAME)
             task.evaluate(llm, sampling_params=sampling_params)
             task = GSM8K(self.MODEL_NAME)
