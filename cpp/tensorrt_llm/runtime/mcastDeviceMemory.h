290070c639a98cb83dfefc8fafeafe294e4f0a27
Yukun He <23156053+hyukn@users.noreply.github.com>
2026-01-13 14:52:43 +0000

    [https://nvbugs/5782112][fix] Fix hanging issue for MNNVL Allreduce under PP.
    Signed-off-by: Yukun He <23156053+hyukn@users.noreply.github.com>

--- cpp/tensorrt_llm/runtime/mcastDeviceMemory.h
+++ cpp/tensorrt_llm/runtime/mcastDeviceMemory.h
@@ -43,8 +43,8 @@ public:
     McastDeviceMemory(McastDeviceMemory const&) = delete;
     McastDeviceMemory& operator=(McastDeviceMemory const&) = delete;
 
-    McastDeviceMemory(
-        size_t bufSize, uint32_t groupSize, uint32_t groupRank, uint32_t splitColor, int deviceIdx, bool mnNvlink);
+    McastDeviceMemory(size_t bufSize, uint32_t groupSize, uint32_t groupRank, int deviceIdx, bool mnNvlink,
+        int64_t mpiCommFortranHandle);
 
     // We don't register the pointer in these two functions since we don't expect any python-level code would call
     // to obtain the raw pointers.
@@ -113,6 +113,7 @@ private:
     // For intra-node mcast
     tensorrt_llm::runtime::IpcNvlsHandle* mNvlsHandle;
 
+    void initMcastMemory();
     void allocMnMcastMem(size_t bufSize);
     void allocNvlsMcastMem(size_t bufSize);
 };
