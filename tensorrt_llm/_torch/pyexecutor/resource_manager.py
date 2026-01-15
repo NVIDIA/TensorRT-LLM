8de254f23f0e2b694daf28eb67f8ed89f0b991b3
yizhang-nv <187001205+yizhang-nv@users.noreply.github.com>
2026-01-14 18:16:33 -0800

    Fix CI
    Signed-off-by: yizhang-nv <187001205+yizhang-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/resource_manager.py
+++ tensorrt_llm/_torch/pyexecutor/resource_manager.py
@@ -658,7 +658,7 @@ class KVCacheManager(BaseResourceManager):
         accepted_draft_token_offsets, packed_accepted_draft_tokens_indices, rewind_draft_token_separate_adjustments = self.locate_accepted_draft_tokens(
             requests)
         past_key_value_lengths = attn_metadata.kv_lens_cuda[:len(requests)]
-        if attn_metadata.kv_cache_block_offsets is not None and attn_metadata.host_kv_cache_block_offsets is not None and attn_metadata.host_kv_cache_pool_pointers is not None and attn_metadata.host_kv_cache_pool_mapping is not None:
+        if attn_metadata.kv_cache_block_offsets is not None and attn_metadata.host_kv_cache_pool_pointers is not None and attn_metadata.host_kv_cache_pool_mapping is not None:
             use_paged_kv_cache = True
         else:
             use_paged_kv_cache = False
