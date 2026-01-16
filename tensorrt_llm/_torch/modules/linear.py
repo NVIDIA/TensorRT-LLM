Fix scaling factor in copy_weight function
Signed-off-by: Chenjie Luo <108829653+cjluo-nv@users.noreply.github.com>

tensorrt_llm/_torch/modules/linear.py
@@ -1093,7 +1093,7 @@ class NVFP4LinearMethod(LinearMethodBase):
                 assert len(v_scale) != 0
                 copy_weight(
                     module.kv_scales,
-                    torch.tensor([1.0, max(k_scale), max(v_scale) * 6.0], dtype=torch.float32))
+                    torch.tensor([1.0, max(k_scale), max(v_scale)], dtype=torch.float32))
                 module.inv_kv_scales.data = 1.0 / module.kv_scales
 
     def load_weights_fused_gate_up_linear(self, module: Linear,