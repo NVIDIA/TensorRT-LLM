ba53212e0934082b50157ece54a825ef510e9435
Chenjie Luo <108829653+cjluo-nv@users.noreply.github.com>
2026-01-15 21:40:52 -0800

    Align kv_scales with modelopt HF checkpoint
    Modelopt defines k_scale and v_scale as amax / 448 for both FP8 and NVFP4 KV cache quantization
    
    Signed-off-by: Chenjie Luo <108829653+cjluo-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/modules/linear.py
+++ tensorrt_llm/_torch/modules/linear.py
@@ -577,14 +577,9 @@ class FP8QDQLinearMethod(LinearMethodBase):
         if os.environ.get("TRTLLM_LOAD_KV_SCALES", "0") == "1":
             if len(k_scale) != 0:
                 assert len(v_scale) != 0
-                # The calibrated KV scales are amax / (6 * 448), but the requested KV scales are amax / 448,
-                # to avoid overflow when dequantizing NVFP4 in attention kernels.
                 copy_weight(
                     module.kv_scales,
-                    torch.tensor(
-                        [1.0, max(k_scale) * 6.0,
-                         max(v_scale) * 6.0],
-                        dtype=torch.float32))
+                    torch.tensor([1.0, max(k_scale), max(v_scale)], dtype=torch.float32))
                 module.inv_kv_scales.data = 1.0 / module.kv_scales
 
     def load_weights_fused_gate_up_linear(self, module: Linear,
@@ -1096,14 +1091,9 @@ class NVFP4LinearMethod(LinearMethodBase):
         if os.environ.get("TRTLLM_LOAD_KV_SCALES", "0") == "1":
             if len(k_scale) != 0:
                 assert len(v_scale) != 0
-                # The calibrated KV scales are amax / (6 * 448), but the requested KV scales are amax / 448,
-                # to avoid overflow when dequantizing NVFP4 in attention kernels using FP8 math.
                 copy_weight(
                     module.kv_scales,
-                    torch.tensor(
-                        [1.0, max(k_scale) * 6.0,
-                         max(v_scale) * 6.0],
-                        dtype=torch.float32))
+                    torch.tensor([1.0, max(k_scale), max(v_scale) * 6.0], dtype=torch.float32))
                 module.inv_kv_scales.data = 1.0 / module.kv_scales
 
     def load_weights_fused_gate_up_linear(self, module: Linear,
