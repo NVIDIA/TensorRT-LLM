2f39b799c6bdf29ff727dd2012c2a156374f8a7e
yechank <161688079+yechank-nvidia@users.noreply.github.com>
2026-01-20 16:22:24 +0900

    fix cuda graph + VSWA + MTP
    Signed-off-by: yechank <161688079+yechank-nvidia@users.noreply.github.com>

--- tensorrt_llm/_torch/model_config.py
+++ tensorrt_llm/_torch/model_config.py
@@ -560,11 +560,17 @@ class ModelConfig(Generic[TConfig]):
             attn_tp_size * attn_cp_size)
 
         hidden_size = self.pretrained_config.hidden_size // attn_tp_size
+        num_layers = self.pretrained_config.num_hidden_layers
+        num_attention_layers = self.get_num_attention_layers()
+        if (self.spec_config is not None
+                and self.spec_config.spec_dec_mode.is_mtp_one_model()):
+            num_layers += self.spec_config.num_nextn_predict_layers
+            num_attention_layers += self.spec_config.num_nextn_predict_layers
 
         model_config_cpp = ModelConfigCpp(
             vocab_size=self.pretrained_config.vocab_size,
-            num_layers=self.pretrained_config.num_hidden_layers,
-            num_attention_layers=self.get_num_attention_layers(),
+            num_layers=num_layers,
+            num_attention_layers=num_attention_layers,
             num_rnn_layers=0,
             num_heads=num_heads,
             hidden_size=hidden_size,
