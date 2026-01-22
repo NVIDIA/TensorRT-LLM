939ccef8dce766625df6540496856fddbd252391
qinzhenhuai4416 <76415635+qinzhenhuai4416@users.noreply.github.com>
2026-01-22 15:23:18 +0800

    [test] add sample for llm test
    [test] add sample for llm test
    
    Signed-off-by: qinzhenhuai4416 <76415635+qinzhenhuai4416@users.noreply.github.com>

--- tests/integration/defs/examples/run_llm_example.py
+++ tests/integration/defs/examples/run_llm_example.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
         "The president of the China is",
         "The future of chip is",
     ]
-    sampling_params = SamplingParams(temperature=0.7, top_p=0.95)
+    sampling_params = SamplingParams(temperature=1.0, top_p=0.95)
 
     model_path = Path(os.environ.get(
-        "LLM_MODELS_ROOT")) / "llama-models-v2/Qwen3-Next-80B-A3B-Instruct"
+        "LLM_MODELS_ROOT")) / "llama-models-v2/llama-v2-70b-chat-hf"
     print(f'model_path: {model_path}')
 
     with LLM(model=str(model_path)) as llm:
