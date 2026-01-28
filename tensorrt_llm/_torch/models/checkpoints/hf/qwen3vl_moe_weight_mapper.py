ce36c8a005ffd28945101fc32663592effb868d8
William Zhang <133824995+2ez4bz@users.noreply.github.com>
2026-01-27 16:50:50 -0800

    [https://nvbugs/5835925][fix] Add EPD disagg support for Qwen3 VL MoE (#10962)
    * Why?
    
    Trying to instantiate a `MultimodalEncoder` for a Qwen3 VL MoE model
    would fail during weight loading.
    
    * What?
    
    This commit fixes the bug, alongside:
    - explicit, intentional support for EPD for Qwen3 VL MoE.
    - extends EPD unit tests for Qwen3 VL MoE, albeit with dummy weights.
    - unit tests for the weight mapper fixes.
    
    
    Signed-off-by: William Zhang <133824995+2ez4bz@users.noreply.github.com>

--- tensorrt_llm/_torch/models/checkpoints/hf/qwen3vl_moe_weight_mapper.py
+++ tensorrt_llm/_torch/models/checkpoints/hf/qwen3vl_moe_weight_mapper.py
@@ -1,4 +1,8 @@
 from torch import nn
+from transformers.models.qwen3_vl_moe.configuration_qwen3_vl_moe import (
+    Qwen3VLMoeTextConfig,
+    Qwen3VLMoeVisionConfig,
+)
 
 from tensorrt_llm._torch.models.checkpoints.hf.qwen3_moe_weight_mapper import Qwen3MoeHfWeightMapper
 from tensorrt_llm._torch.models.modeling_utils import register_mapper
@@ -22,3 +26,20 @@ class Qwen3VLMoeHfWeightMapper(Qwen3MoeHfWeightMapper):
             module.load_weights(
                 weights=[updated_module_weights], allow_partial_loading=allow_partial_loading
             )
+
+    @property
+    def _num_kv_heads(self) -> int:
+        config = self._model.config
+        if isinstance(config, Qwen3VLMoeTextConfig):
+            num_kv_heads = getattr(config, "num_key_value_heads", None)
+            if num_kv_heads is None:
+                num_kv_heads = config.num_attention_heads
+        elif isinstance(config, Qwen3VLMoeVisionConfig):
+            num_kv_heads = config.num_heads
+        else:
+            raise TypeError(
+                "Expected `Qwen3VLMoeTextConfig` or `Qwen3VLMoeVisionConfig`, "
+                f"got {type(config).__name__}"
+            )
+
+        return num_kv_heads
