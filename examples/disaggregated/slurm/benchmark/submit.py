6b67d5dedd935bc5fd658a7b71be5842b14f4e21
Xianjie <5410381+qiaoxj07@users.noreply.github.com>
2026-02-02 19:09:32 -0800

    support auto reschedule jobs on OCI

--- examples/disaggregated/slurm/benchmark/submit.py
+++ examples/disaggregated/slurm/benchmark/submit.py
@@ -457,7 +457,7 @@ def submit_job(config, log_dir, dry_run):
 
     # Generate disagg server config
     server_config = convert_allocations_to_server_config(allocations)
-    with open(os.path.join(log_dir, "server_config.yaml"), "w") as f:
+    with open(os.path.join(log_dir, "server_config_base.yaml"), "w") as f:
         yaml.dump(server_config, f)
     disagg_server_hostname = server_config['hostname']
     disagg_server_port = server_config['port']
@@ -557,7 +557,7 @@ def submit_job(config, log_dir, dry_run):
     ]
     start_server_cmds.append(" ".join(cmd))
 
-    with open(os.path.join(log_dir, "start_server_cmds.sh"), "w") as f:
+    with open(os.path.join(log_dir, "start_server_cmds_base.sh"), "w") as f:
         f.write("\n".join(start_server_cmds) + "\n")
 
     # Generate client commands (use script_dir for benchmark scripts)
@@ -625,6 +625,12 @@ def submit_job(config, log_dir, dry_run):
             ]
             client_cmds.append(" ".join(accuracy_prefix + accuracy_cmd))
 
+    # post_process_cmd = [
+    #     f"bash /lustre/fsw/portfolios/coreai/users/xqiao/bench-disagg-r1/scripts/post_process.sh {log_dir} {gen_tp_size} {mtp_size} {log_dir}",
+    #     f"&> {log_dir}/8_post_process.log",
+    # ]
+    # client_cmds.append(" ".join(client_slurm_prefix + post_process_cmd))
+
     # record ${SLURM_JOB_NODELIST} to ${log_dir}/8_done_job_id.txt
     done_cmd = [
         "echo", "${SLURM_JOB_NODELIST}", ">",
@@ -632,7 +638,7 @@ def submit_job(config, log_dir, dry_run):
     ]
     client_cmds.append(" ".join(done_cmd))
 
-    with open(os.path.join(log_dir, "client_cmds.sh"), "w") as f:
+    with open(os.path.join(log_dir, "client_cmds_base.sh"), "w") as f:
         f.write("\n".join(client_cmds) + "\n")
 
     # Resolve slurm script_file path
