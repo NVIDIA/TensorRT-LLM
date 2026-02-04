29c534d854770ef5df978c33804effc279006a66
Yifei Zhang <219273404+yifeizhang-c@users.noreply.github.com>
2026-02-04 06:04:11 +0000

    fix copyright
    Signed-off-by: Yifei Zhang <219273404+yifeizhang-c@users.noreply.github.com>

--- tensorrt_llm/_torch/cute_dsl_kernels/blackwell/blockwise_gemm/blockwise_gemm.py
+++ tensorrt_llm/_torch/cute_dsl_kernels/blackwell/blockwise_gemm/blockwise_gemm.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
