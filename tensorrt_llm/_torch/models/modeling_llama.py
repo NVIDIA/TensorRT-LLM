1c0ca7279fa4fe28aeef71cb7b3869294c474112
Ludwig Schneider <lschneider@nvidia.com>
2026-02-09 11:46:00 -0600

    more adjustments
    Signed-off-by: Ludwig Schneider <lschneider@nvidia.com>

--- tensorrt_llm/_torch/models/modeling_llama.py
+++ tensorrt_llm/_torch/models/modeling_llama.py
@@ -785,6 +785,8 @@ class LlamaDecoderLayer(DecoderLayer):
             attention_mask=self.attention_mask,
             all_reduce_params=AllReduceParams(
                 enable_allreduce=not self.disable_attn_allreduce),
+            allow_window_output=(self.disable_attn_allreduce
+                                 and self.PRE_MLP_FUSION),
             **kwargs,
         )
         # Fully Connected
@@ -838,6 +840,8 @@ class LlamaDecoderLayer(DecoderLayer):
             hidden_states,
             final_all_reduce_params=AllReduceParams(
                 enable_allreduce=not self.disable_mlp_allreduce),
+            allow_window_output=(self.disable_mlp_allreduce
+                                 and self.POST_MLP_FUSION),
             **kwargs,
         )
 
