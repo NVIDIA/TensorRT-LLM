1c366dae1ad4927c6baf71a5221c5717dbcd7a2e
leslie-fang25 <leslief@nvidia.com>
2026-02-08 21:01:30 -0800

    update the check conditions
    Signed-off-by: leslie-fang25 <leslief@nvidia.com>

--- tensorrt_llm/_torch/modules/fused_moe/fused_moe_trtllm_gen.py
+++ tensorrt_llm/_torch/modules/fused_moe/fused_moe_trtllm_gen.py
@@ -531,9 +531,7 @@ class TRTLLMGenFusedMoE(MoE):
 
         routing_bias = routing_bias if router_logits is not None else None
 
-        if (token_selected_experts is not None
-                and token_final_scales is not None
-                and token_selected_experts.dim() == 2):
+        if token_selected_experts is not None:
             # for cases like deepep low latency where fake top_k=1 might be used
             top_k = token_selected_experts.shape[1]
 
