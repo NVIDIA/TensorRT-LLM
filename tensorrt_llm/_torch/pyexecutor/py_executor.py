a08fd7d51fddf6564310cd8580991706545d3319
scutizhang <scutizhang@tencent.com>
2026-02-10 17:53:36 +0800

    fix empty line

--- tensorrt_llm/_torch/pyexecutor/py_executor.py
+++ tensorrt_llm/_torch/pyexecutor/py_executor.py
@@ -3370,7 +3370,6 @@ class PyExecutor:
                 else:
                     if not request.is_disagg_context_transmission_state:
                         requests_to_terminate.append(request)
-
             else:
                 new_active_requests.append(request)
 
