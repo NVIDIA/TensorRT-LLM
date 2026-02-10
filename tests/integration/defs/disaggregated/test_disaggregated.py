7e1028218b7ca603f0ac3c700b1ab23ff72cc1fa
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2026-02-10 06:17:30 +0000

    fix cacheTransceiver
    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>

--- tests/integration/defs/disaggregated/test_disaggregated.py
+++ tests/integration/defs/disaggregated/test_disaggregated.py
@@ -131,6 +131,10 @@ def get_test_config(test_desc, example_dir, test_root):
         (4,
          f"{test_configs_root}/disagg_config_ctxtp2_gentp2_deepseek_v3_lite_nixl.yaml"
          ),
+        "deepseek_v3_lite_fp8_transceiver_runtime_python":
+        (4,
+         f"{test_configs_root}/disagg_config_ctxtp2_gentp2_deepseek_v3_lite_transceiver_runtime_python.yaml"
+         ),
         "deepseek_v3_lite_fp8_tp1":
         (2,
          f"{test_configs_root}/disagg_config_ctxtp1_gentp1_deepseek_v3_lite.yaml"
@@ -436,11 +440,11 @@ def run_disaggregated_test(example_dir,
     cleanup_output_files()
     run_env = env.copy()
 
-    # on some CI nodes , we set UCX_TLS to "^ib" to avoid the issue that IB equipped but not available.
+    # on some CI nodes , we set UCX_TLS to "^ib,gdr_copy" to avoid the issue that IB equipped but not available, and gdr_copy pin buffer failed.
     # we set UCX_MM_ERROR_HANDLING to "y" to avoid the issue that NIXL cannot use IB or TCP for notify on some CI nodes,
     # setting it to "y" will enable NIXL to use system memory for notify.
 
-    run_env["UCX_TLS"] = "^ib"
+    run_env["UCX_TLS"] = "^ib,gdr_copy"
     run_env["UCX_MM_ERROR_HANDLING"] = "y"
     num_ranks, config_file = get_test_config(test_desc, example_dir,
                                              os.path.dirname(__file__))
@@ -1260,7 +1264,7 @@ def test_disaggregated_deepseek_v3_lite_fp8_ucx(disaggregated_test_root,
             os.symlink(src, dst, target_is_directory=True)
     env = llm_venv._new_env.copy()
     env["TRTLLM_USE_UCX_KVCACHE"] = "1"
-    env["UCX_TLS"] = "^ib"
+    env["UCX_TLS"] = "^ib,gdr_copy"
     run_disaggregated_test(disaggregated_example_root,
                            "deepseek_v3_lite_fp8_ucx",
                            env=env,
@@ -1287,7 +1291,7 @@ def test_disaggregated_deepseek_v3_lite_fp8_nixl(disaggregated_test_root,
             os.symlink(src, dst, target_is_directory=True)
     env = llm_venv._new_env.copy()
     env["TRTLLM_USE_NIXL_KVCACHE"] = "1"
-    env["UCX_TLS"] = "^ib"
+    env["UCX_TLS"] = "^ib,gdr_copy"
     env["UCX_MM_ERROR_HANDLING"] = "y"
     run_disaggregated_test(disaggregated_example_root,
                            "deepseek_v3_lite_fp8_nixl",
@@ -1313,7 +1317,7 @@ def test_disaggregated_deepseek_v3_lite_fp8_ucx_tp1_single_gpu(
             os.symlink(src, dst, target_is_directory=True)
     env = llm_venv._new_env.copy()
     env["TRTLLM_USE_UCX_KVCACHE"] = "1"
-    env["UCX_TLS"] = "^ib"
+    env["UCX_TLS"] = "^ib,gdr_copy"
 
     run_disaggregated_test(disaggregated_example_root,
                            "deepseek_v3_lite_fp8_tp1",
@@ -1595,7 +1599,7 @@ def run_disaggregated_benchmark(example_dir,
                                 skip_warmup=False):
     """Run disaggregated test with given configuration."""
     run_env = env.copy()
-    run_env["UCX_TLS"] = "^ib"
+    run_env["UCX_TLS"] = "^ib,gdr_copy"
     run_env["UCX_MM_ERROR_HANDLING"] = "y"
     workers_cmd = [
         'mpirun', '--allow-run-as-root', '--oversubscribe', '-n',
@@ -1776,7 +1780,7 @@ def run_disaggregated_aiperf(config_file,
     """
     cleanup_output_files()
     run_env = env.copy()
-    run_env["UCX_TLS"] = "^ib"
+    run_env["UCX_TLS"] = "^ib,gdr_copy"
     run_env["UCX_MM_ERROR_HANDLING"] = "y"
 
     workers_cmd = [
@@ -2302,7 +2306,7 @@ def run_disaggregated_cancel_test(example_dir,
     """Run disaggregated test with request cancellation stress test."""
     cleanup_output_files()
     run_env = env.copy()
-    run_env["UCX_TLS"] = "^ib"
+    run_env["UCX_TLS"] = "^ib,gdr_copy"
 
     num_ranks, config_file = get_test_config(test_desc, example_dir,
                                              os.path.dirname(__file__))
