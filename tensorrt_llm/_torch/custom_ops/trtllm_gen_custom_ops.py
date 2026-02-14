f2f35604a7205d28ee5104832fa3f0188df5c49d
michaelfeil <63565275+michaelfeil@users.noreply.github.com>
2026-02-13 20:42:32 -0800

    register custom ops

--- tensorrt_llm/_torch/custom_ops/trtllm_gen_custom_ops.py
+++ tensorrt_llm/_torch/custom_ops/trtllm_gen_custom_ops.py
@@ -89,6 +89,13 @@ def prepare_dummy_topk_and_hook(
             lambda: torch.randn(
                 num_experts, dtype=torch.bfloat16, device=hidden_states.device)
         })
+    if routing_method_type == RoutingMethodType.MiniMaxM2:
+        routing_cls_kwargs.update({
+            'callable_e_score_correction_bias':
+            lambda: torch.randn(
+                num_experts, dtype=torch.bfloat16, device=hidden_states.device)
+            'num_experts': num_experts,
+        })
     routing_method = ROUTING_METHOD_TYPE_TO_CLASS[routing_method_type](
         top_k=top_k, **routing_cls_kwargs)
 
