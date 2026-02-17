bbc11111fb8d219a9eb4f2433c4a188d3080dd36
djns99 <40156487+djns99@users.noreply.github.com>
2026-02-17 14:58:18 +1300

    Update tests issues
    Signed-off-by: djns99 <40156487+djns99@users.noreply.github.com>

--- cpp/kernels/fmha_v2/fmha_test.py
+++ cpp/kernels/fmha_v2/fmha_test.py
@@ -1,3 +1,17 @@
+# SPDX-FileCopyrightText: Copyright (c) 2020-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import subprocess
 
 import pytest
@@ -285,14 +299,12 @@ def test_trtllm_sliding_window_attention(sliding_window_size, mask_type):
     if mask_type == "-bidirectional-sliding-window-mask":
         sliding_window_size *= 2
 
-    subprocess.run(
-        f"bin/fmha.exe -d 128 -b 4 -h 5 -bf16 -s 8192 -min-s 4096 -bf16 \
+    subprocess.run(f"bin/fmha.exe -d 128 -b 4 -h 5 -s 8192 -min-s 4096 -bf16 \
         -sliding-window-size {sliding_window_size} {mask_type}",
-        shell=True,
-        check=True)
+                   shell=True,
+                   check=True)
 
-    subprocess.run(
-        f"bin/fmha.exe -d 64 -b 4 -h 5 -bf16 -s 8192 -min-s 4096 -bf16 \
+    subprocess.run(f"bin/fmha.exe -d 64 -b 4 -h 5 -s 8192 -min-s 4096 -bf16 \
         -sliding-window-size {sliding_window_size} {mask_type}",
-        shell=True,
-        check=True)
+                   shell=True,
+                   check=True)
