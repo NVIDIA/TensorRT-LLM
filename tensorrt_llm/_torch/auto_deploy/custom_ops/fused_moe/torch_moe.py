c429f929753b879f11b475b35df7df695936bc09
Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>
2026-02-18 19:25:21 -0800

    clean rebase + allToall MoE
    Signed-off-by: Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/custom_ops/fused_moe/torch_moe.py
+++ tensorrt_llm/_torch/auto_deploy/custom_ops/fused_moe/torch_moe.py
@@ -788,6 +788,9 @@ def torch_quant_finegrained_fp8_moe(
     w3_weight_scale_inv: List[torch.Tensor],
     is_gated_mlp: bool = True,
     act_fn: int = int(ActivationType.Silu),
+    mapping_config: str = "",
+    max_num_tokens: int = 0,
+    apply_routing_on_input: bool = False,
 ) -> torch.Tensor:
     """
     FineGrainedFP8 MoE op using block-wise FP8 quantized linear operations.
@@ -874,7 +877,15 @@ def torch_quant_finegrained_fp8_moe(
 
         mlps = [make_finegrained_fp8_mlp(i) for i in range(len(w1_weight))]
 
-    return _template_moe(x, selected_experts, routing_weights, mlps)
+    return _template_moe(
+        x,
+        selected_experts,
+        routing_weights,
+        mlps,
+        apply_routing_on_input,
+        mapping_config,
+        max_num_tokens,
+    )
 
 
 @torch_quant_finegrained_fp8_moe.register_fake
@@ -890,5 +901,8 @@ def torch_quant_finegrained_fp8_moe_fake(
     w3_weight_scale_inv: List[torch.Tensor],
     is_gated_mlp: bool = True,
     act_fn: int = int(ActivationType.Silu),
+    mapping_config: str = "",
+    max_num_tokens: int = 0,
+    apply_routing_on_input: bool = False,
 ) -> torch.Tensor:
     return torch.empty_like(x)
