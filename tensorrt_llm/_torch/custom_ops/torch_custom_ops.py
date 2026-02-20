7d7a3dfe501aa70244b3226eeafdb198900d256b
Ludwig Schneider <lschneider@nvidia.com>
2026-02-20 07:56:44 -0600

    unrelated bug fix
    Signed-off-by: Ludwig Schneider <lschneider@nvidia.com>

--- tensorrt_llm/_torch/custom_ops/torch_custom_ops.py
+++ tensorrt_llm/_torch/custom_ops/torch_custom_ops.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2022-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import threading
 from dataclasses import replace
 from enum import IntEnum
@@ -1282,7 +1297,7 @@ def _(
         output_dtype: torch.dtype,
         output_buffer_kind: int = int(OutputBufferKind.DEFAULT),
 ) -> torch.Tensor:
-    return act_fp8.new_empty((act_fp8.size(0), weight.size(0)),
+    return act_fp4.new_empty((act_fp4.size(0), weight.size(0)),
                              dtype=output_dtype)
 
 
