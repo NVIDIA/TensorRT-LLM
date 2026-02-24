381b70670d9e71b82157310e165bced7f6939b13
djns99 <40156487+djns99@users.noreply.github.com>
2026-02-24 15:18:54 +1300

    Fix oversized executable in the fmha_v2 tests
    Signed-off-by: djns99 <40156487+djns99@users.noreply.github.com>

--- tests/integration/defs/test_fmha.py
+++ tests/integration/defs/test_fmha.py
@@ -3,6 +3,8 @@ from functools import partial
 from pathlib import Path
 from subprocess import run
 
+from tests.unittest.utils.util import getSMVersion
+
 
 def test_fmha():
     build_run = partial(run, shell=True, check=True)
@@ -13,6 +15,15 @@ def test_fmha():
 
     try:
         os.chdir(fmha_v2_dir)
+
+        test_arch = getSMVersion()
+        # SM70 is deprecated in TRTLLM, so we don't need to test it
+        all_archs = [80, 86, 89, 90, 100, 120]
+
+        # Select the family we belong to (e.g. 103 -> 100)
+        test_arch = max(filter(lambda x: x < test_arch, all_archs))
+
+        build_only_on_archs = filter(lambda x: x != test_arch, all_archs)
 
         env = os.environ.copy()
         env.update({
@@ -26,10 +37,19 @@ def test_fmha():
             "1",  # Do not run tests with skip-softmax feature.
         })
 
-        build_run(
-            "rm -rf generated temp obj .pytest_cache __pycache__ bin cubin")
-        build_run("python3 setup.py", env=env)
-        build_run("make -j 16", env=env)
+        # The test executable is too large if we build all the architectures, so we must build architectures individually
+        def build_arch(arch):
+            env["FMHA_FILTER_ARCH"] = str(arch)
+            build_run(
+                "rm -rf generated temp obj .pytest_cache __pycache__ bin cubin")
+            build_run("python3 setup.py", env=env)
+            build_run("make -j 16", env=env)
+
+        # As part of the test we should compile all the architectures, even the ones we dont have executors for
+        for arch in build_only_on_archs:
+            build_arch(arch)
+
+        build_arch(test_arch)
         build_run("pytest fmha_test.py", env=env)
 
     finally:
