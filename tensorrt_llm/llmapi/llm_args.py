4786834382aac85a4fc6e54d36c8ab6c7fe4336f
Izzy Putterman <iputterman@nvidia.com>
2026-03-05 10:44:01 -0800

    [None][feat] External Drafter One Model (#11758)
    Signed-off-by: Izzy Putterman <iputterman@nvidia.com>

--- tensorrt_llm/llmapi/llm_args.py
+++ tensorrt_llm/llmapi/llm_args.py
@@ -758,7 +758,7 @@ class DecodingBaseConfig(StrictBaseModel):
         """
         return True
 
-    @functools.cached_property
+    @property
     def spec_dec_mode(self):
         # spec_dec_mode has more functionality than the raw decoding_mode string.
         # Use an alias for the import here to avoid name collisions with the one for the
@@ -776,6 +776,9 @@ class DecodingBaseConfig(StrictBaseModel):
     def tokens_per_gen_step(self) -> int:
         """Total tokens per gen request in one spec dec iteration (including golden token)."""
         return 1 + self.max_total_draft_tokens
+
+    def num_capture_layers(self) -> int:
+        return 0
 
 
 class KvCacheConnectorConfig(StrictBaseModel):
@@ -1186,6 +1189,7 @@ class SADecodingConfig(DecodingBaseConfig):
 
 class DraftTargetDecodingConfig(DecodingBaseConfig):
     decoding_type: Literal["Draft_Target"] = "Draft_Target"
+    _draft_target_one_model: bool = PrivateAttr(True)
 
     @model_validator(mode="after")
     def validate_draft_target_config(self):
@@ -1199,6 +1203,14 @@ class DraftTargetDecodingConfig(DecodingBaseConfig):
 
     def supports_backend(self, backend: str) -> bool:
         return backend == "pytorch" or backend == "_autodeploy"
+
+    @functools.cached_property
+    def spec_dec_mode(self):
+        from tensorrt_llm._torch.speculative.interface import \
+            SpeculativeDecodingMode as TorchSpeculativeDecodingMode
+        if self._draft_target_one_model:
+            return TorchSpeculativeDecodingMode.DRAFT_TARGET_ONE_MODEL
+        return TorchSpeculativeDecodingMode.DRAFT_TARGET
 
 
 class MTPDecodingConfig(DecodingBaseConfig):
@@ -3276,6 +3288,11 @@ class TorchLlmArgs(BaseLlmArgs):
                 self.disable_overlap_scheduler = True
                 self.cuda_graph_config = None
                 self.speculative_config.max_draft_len = 1
+            elif isinstance(self.speculative_config, DraftTargetDecodingConfig):
+                assert self.speculative_config.max_draft_len > 0
+                assert self.speculative_config.speculative_model is not None, "Draft model must be specified."
+                if self.backend == "_autodeploy":
+                    self.speculative_config._draft_target_one_model = False
 
         else:
             self.decoding_config = None
