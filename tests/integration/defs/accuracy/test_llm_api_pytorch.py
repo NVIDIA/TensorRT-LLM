aa621811e68da7ff8d0bd8bf789769c2007dcba1
Pengbo Wang <221450789+pengbowang-nv@users.noreply.github.com>
2026-03-06 15:11:01 +0800

    [https://nvbugs/5624818][fix] Add unittest for GPT-OSS non-paged_context_fmha (#11415)
    Signed-off-by: Pengbo Wang <221450789+pengbowang-nv@users.noreply.github.com>

--- tests/integration/defs/accuracy/test_llm_api_pytorch.py
+++ tests/integration/defs/accuracy/test_llm_api_pytorch.py
@@ -4619,11 +4619,13 @@ class TestGPTOSS(LlmapiAccuracyTestHarness):
             (4, 1, 4, True, True, True),
         ],
         ids=["tp4", "ep4", "dp4"])
-    @pytest.mark.parametrize("v2_kv_cache", [True, False],
-                             ids=["v2_kv_cache", "v1_kv_cache"])
-    def test_w4_4gpus(self, v2_kv_cache, kv_cache_dtype, moe_backend, tp_size,
-                      pp_size, ep_size, attention_dp, cuda_graph,
-                      overlap_scheduler, mocker):
+    @pytest.mark.parametrize(
+        "v2_kv_cache,kv_cache_reuse", [(True, True), (False, True),
+                                       (True, False)],
+        ids=["v2_kv_cache", "v1_kv_cache", "v2_kv_cache_no_reuse"])
+    def test_w4_4gpus(self, v2_kv_cache, kv_cache_reuse, kv_cache_dtype,
+                      moe_backend, tp_size, pp_size, ep_size, attention_dp,
+                      cuda_graph, overlap_scheduler, mocker):
 
         MAX_OUTPUT_LEN = 128179
         MAX_INPUT_LEN = 32768
@@ -4642,6 +4644,7 @@ class TestGPTOSS(LlmapiAccuracyTestHarness):
 
         kv_cache_config = KvCacheConfig(free_gpu_memory_fraction=0.7,
                                         dtype=kv_cache_dtype,
+                                        enable_block_reuse=kv_cache_reuse,
                                         use_kv_cache_manager_v2=v2_kv_cache)
 
         max_seq_len = MAX_INPUT_LEN + MAX_OUTPUT_LEN
