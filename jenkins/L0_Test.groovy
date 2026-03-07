e633ad8d704c886646515b8b67e1825b1c3cdef2
Yanchao Lu <yanchaol@nvidia.com>
2026-03-07 23:53:24 +0800

    Update L0_Test.groovy
    Signed-off-by: Yanchao Lu <yanchaol@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -2999,7 +2999,8 @@ def runLLMBuild(pipeline, cpu_arch, reinstall_dependencies=false, wheel_path="",
 
     if (reinstall_dependencies == true) {
         // Test installation in the new environment
-        def pip_keep = "-e 'pip'"
+        // Reserve CUDA 13.0 torch and torchvision packages
+        def pip_keep = "-e pip torch torchvision"
         def remove_trt = "rm -rf /usr/local/tensorrt"
         if (env.alternativeTRT) {
             pip_keep += " -e tensorrt"
@@ -3009,7 +3010,7 @@ def runLLMBuild(pipeline, cpu_arch, reinstall_dependencies=false, wheel_path="",
         sh "#!/bin/bash \n" + "yum remove -y libcudnn* libnccl* libcublas* && ${remove_trt}"
     }
     // Test preview installation
-    trtllm_utils.llmExecStepWithRetry(pipeline, script: "#!/bin/bash \n" + "cd tensorrt_llm/ && pip3 install pytest==9.0.2 build/tensorrt_llm-*.whl")
+    trtllm_utils.llmExecStepWithRetry(pipeline, script: "#!/bin/bash \n" + "cd tensorrt_llm/ && pip3 install pytest build/tensorrt_llm-*.whl")
     if (env.alternativeTRT) {
         sh "bash -c 'pip3 show tensorrt || true'"
     }
@@ -3048,7 +3049,8 @@ def runPackageSanityCheck(pipeline, wheel_path, reinstall_dependencies=false, cp
     }
     if (reinstall_dependencies) {
         // Test installation in the new environment
-        def pip_keep = "-e 'pip'"
+        // Reserve CUDA 13.0 torch and torchvision packages
+        def pip_keep = "-e pip torch torchvision"
         def remove_trt = "rm -rf /usr/local/tensorrt"
         if (env.alternativeTRT) {
             pip_keep += " -e tensorrt"
@@ -3058,7 +3060,7 @@ def runPackageSanityCheck(pipeline, wheel_path, reinstall_dependencies=false, cp
         sh "bash -c 'yum remove -y libcudnn* libnccl* libcublas* && ${remove_trt}'"
     }
     // Test preview installation
-    trtllm_utils.llmExecStepWithRetry(pipeline, script: "bash -c 'pip3 install pytest==9.0.2 tensorrt_llm-*.whl'")
+    trtllm_utils.llmExecStepWithRetry(pipeline, script: "bash -c 'pip3 install pytest tensorrt_llm-*.whl'")
     if (env.alternativeTRT) {
         sh "bash -c 'pip3 show tensorrt || true'"
     }
@@ -3642,11 +3644,7 @@ def launchTestJobs(pipeline, testFilter)
                             echo "###### Extra PyTorch CUDA 13.0 install Start ######"
                             // Use internal mirror instead of https://download.pytorch.org/whl/cu130 for better network stability.
                             // PyTorch CUDA 13.0 package and torchvision package can be installed as expected.
-                            if (k8s_arch == "amd64") {
-                                trtllm_utils.llmExecStepWithRetry(pipeline, script: "pip3 install torch==2.10.0+cu130 torchvision==0.25.0+cu130 --extra-index-url https://urm.nvidia.com/artifactory/api/pypi/pytorch-cu128-remote/simple")
-                            } else {
-                                trtllm_utils.llmExecStepWithRetry(pipeline, script: "pip3 install torch==2.10.0+cu130 torchvision==0.25.0 --extra-index-url https://urm.nvidia.com/artifactory/api/pypi/pytorch-cu128-remote/simple")
-                            }
+                            trtllm_utils.llmExecStepWithRetry(pipeline, script: "pip3 install torch==2.10.0+cu130 torchvision==0.25.0+cu130 --index-url https://urm.nvidia.com/artifactory/api/pypi/pytorch-cu128-remote/simple")
                         }
 
                         def libEnv = []
