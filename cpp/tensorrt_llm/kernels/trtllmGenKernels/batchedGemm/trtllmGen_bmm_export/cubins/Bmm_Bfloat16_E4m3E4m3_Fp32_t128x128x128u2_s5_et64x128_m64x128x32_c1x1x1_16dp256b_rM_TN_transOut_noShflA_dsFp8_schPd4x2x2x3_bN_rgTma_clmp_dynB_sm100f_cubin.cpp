d766364c95c0dff12ffcd39107b45d953d8c2988
Lain <fusiyuan2000@gmail.com>
2026-03-09 10:06:49 -0700

    [None][feat] Add silu to trtllm-gen MoE (#11663)
    Signed-off-by: Siyuan Fu <siyuanf@nvidia.com>

---
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/batchedGemm/trtllmGen_bmm_export/cubins/Bmm_Bfloat16_E4m3E4m3_Fp32_t128x128x128u2_s5_et64x128_m64x128x32_c1x1x1_16dp256b_rM_TN_transOut_noShflA_dsFp8_schPd4x2x2x3_bN_rgTma_clmp_dynB_sm100f_cubin.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:20d7bf9e45059e2a40ef01e7aaa9715a78fb3cb4eedcffeb66798425e7481e47
+size 693138
---
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/batchedGemm/trtllmGen_bmm_export/cubins/Bmm_E2m1_E2m1E2m1_Fp32_bA16_bB16_bC16_t128x16x512_s5_et128x16_m256x16x64_c2x1x1_16dp256b_rM_TN_transOut_schPd2x1x2x3_biasM_silu_bN_tma_tmaSf_rgTma_clmp_dynB_sm100f_cubin.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4ef3d9079e5bd84f9fc5ed0dbc8bb87508bc9821621d84a5082b133fd72b7aaa
+size 620252
