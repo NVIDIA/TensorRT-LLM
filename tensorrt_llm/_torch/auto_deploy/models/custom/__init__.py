86abf1a0ccf3299b742199cf38ca88d00e3e555c
Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>
2026-03-11 10:57:58 -0700

    ckpt
    Signed-off-by: Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
+++ tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
@@ -4,7 +4,9 @@ from .modeling_kimi_k2 import KimiK2ForCausalLM, KimiK25ForConditionalGeneration
 from .modeling_nemotron_flash import NemotronFlashForCausalLM, NemotronFlashPreTrainedTokenizerFast
 from .modeling_nemotron_h import NemotronHForCausalLM
 from .modeling_phi4 import Phi4ForCausalLM
+from .modeling_phi4_visionr import Phi4VisionRForConditionalGeneration
 from .modeling_phi4flash import Phi4FlashForCausalLM
+from .modeling_phi4mm import Phi4MMForCausalLM
 from .modeling_qwen3_5_moe import Qwen3_5MoeForCausalLM, Qwen3_5MoeForConditionalGeneration
 
 __all__ = (
@@ -16,7 +18,9 @@ __all__ = (
     "NemotronFlashPreTrainedTokenizerFast",
     "NemotronHForCausalLM",
     "Phi4ForCausalLM",
+    "Phi4MMForCausalLM",
     "Phi4FlashForCausalLM",
+    "Phi4VisionRForConditionalGeneration",
     "Qwen3_5MoeForCausalLM",
     "Qwen3_5MoeForConditionalGeneration",
 )
