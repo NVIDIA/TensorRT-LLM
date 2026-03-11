4e1a9b67b69884891faf14437baf0778c21a59e7
Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>
2026-03-11 10:57:58 -0700

    ckpt
    Signed-off-by: Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
+++ tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
@@ -16,7 +16,9 @@ from .modeling_nemotron_h import NemotronHForCausalLM
 from .modeling_qwen3 import Qwen3ForCausalLM
 from .modeling_qwen3_5 import Qwen3_5ForCausalLM, Qwen3_5ForConditionalGeneration
 from .modeling_phi4 import Phi4ForCausalLM
+from .modeling_phi4_visionr import Phi4VisionRForConditionalGeneration
 from .modeling_phi4flash import Phi4FlashForCausalLM
+from .modeling_phi4mm import Phi4MMForCausalLM
 from .modeling_qwen3_5_moe import Qwen3_5MoeForCausalLM, Qwen3_5MoeForConditionalGeneration
 from .modeling_skywork_r1v2 import SkyworkR1V2ForConditionalGeneration
 from .modeling_starcoder2 import Starcoder2ForCausalLM
@@ -44,8 +46,10 @@ __all__ = (
     "Qwen3_5ForCausalLM",
     "Qwen3_5ForConditionalGeneration",
     "Phi4ForCausalLM",
+    "Phi4MMForCausalLM",
     "Phi4FlashForCausalLM",
     "Qwen3ForCausalLM",
+    "Phi4VisionRForConditionalGeneration",
     "Qwen3_5MoeForCausalLM",
     "Qwen3_5MoeForConditionalGeneration",
     "SkyworkR1V2ForConditionalGeneration",
