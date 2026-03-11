21549387cbe9ad2cb4dcdc2e24408f3fffa55f2a
gramnarayan <105831528+govind-ramnarayan@users.noreply.github.com>
2026-03-10 17:51:51 -0700

    Add AD custom models for GraniteHybridMoE variants(#188)
    Working models: ibm-granite/granite-4.0-micro, ibm-granite/granite-4.0-tiny-preview, ibm-granite/granite-4.0-h-small 
    
    Signed-off-by: Govind Ramnarayan <105831528+govind-ramnarayan@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
+++ tensorrt_llm/_torch/auto_deploy/models/custom/__init__.py
@@ -1,5 +1,6 @@
 from .modeling_deepseek import DeepSeekV3ForCausalLM
 from .modeling_glm4_moe_lite import Glm4MoeLiteForCausalLM
+from .modeling_granite_moe_hybrid import GraniteMoeHybridForCausalLM
 from .modeling_kimi_k2 import KimiK2ForCausalLM, KimiK25ForConditionalGeneration
 from .modeling_nemotron_flash import NemotronFlashForCausalLM, NemotronFlashPreTrainedTokenizerFast
 from .modeling_nemotron_h import NemotronHForCausalLM
@@ -8,6 +9,7 @@ from .modeling_qwen3_5_moe import Qwen3_5MoeForCausalLM, Qwen3_5MoeForConditiona
 __all__ = (
     "DeepSeekV3ForCausalLM",
     "Glm4MoeLiteForCausalLM",
+    "GraniteMoeHybridForCausalLM",
     "KimiK2ForCausalLM",
     "KimiK25ForConditionalGeneration",
     "NemotronFlashForCausalLM",
