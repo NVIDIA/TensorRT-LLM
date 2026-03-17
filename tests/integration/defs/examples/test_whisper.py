d351dc738bada3ac9991c4a0e1d75c1fdcd666bc
Aurelien Chartier <2567591+achartier@users.noreply.github.com>
2026-03-17 15:43:19 -0700

    fix check_call
    Signed-off-by: Aurelien Chartier <2567591+achartier@users.noreply.github.com>

--- tests/integration/defs/examples/test_whisper.py
+++ tests/integration/defs/examples/test_whisper.py
@@ -190,7 +190,7 @@ def test_whisper_beam_search_generation_logits(llm_venv, engine_dir,
             build_cmd.append("--bert_attention_plugin=float16")
             build_cmd.append("--gpt_attention_plugin=float16")
 
-        check_call(" ".join(build_cmd), env=llm_venv._new_env)
+        check_call(build_cmd, env=llm_venv._new_env)
 
     print("Run generation logits beam search validation...")
     validation_script = os.path.join(os.path.dirname(__file__),
