63366dcda0a433392e9672875da7f2ba899d29ae
Patrice Castonguay <55748270+pcastonguay@users.noreply.github.com>
2026-03-25 06:00:31 -0700

    Addressing review comments
    Signed-off-by: Patrice Castonguay <55748270+pcastonguay@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/scheduler/waiting_queue.py
+++ tensorrt_llm/_torch/pyexecutor/scheduler/waiting_queue.py
@@ -1,3 +1,18 @@
+# Copyright (c) 2026, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import heapq
 import itertools
 from abc import ABC, abstractmethod
@@ -39,7 +54,8 @@ class WaitingQueue(ABC):
         """Re-insert a request that could not be scheduled.
 
         Implementations must ensure the request is served before any request
-        that was added *after* it originally arrived (i.e. it does not lose
+        (of equal priority in the case of a priority queue) that was added
+        *after* it originally arrived (i.e. it does not lose
         its place to later-arriving requests of equal priority).  For FCFS
         queues this means inserting at the front; for priority queues this
         means using a tiebreaker that sorts ahead of normally-added entries.
