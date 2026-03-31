d917e03c42ba5558ae1190c69e496fd1a919e562
nv-guomingz <137257613+nv-guomingz@users.noreply.github.com>
2026-03-31 03:08:11 +0000

    [None][feat] Add the Qwen3.5 multimodal support.
    Signed-off-by: nv-guomingz <137257613+nv-guomingz@users.noreply.github.com>

--- tensorrt_llm/_torch/configs/__init__.py
+++ tensorrt_llm/_torch/configs/__init__.py
@@ -24,4 +24,7 @@ def _register_custom_configs_with_transformers() -> None:
 _register_custom_configs_with_transformers()
 del _register_custom_configs_with_transformers
 
-__all__ = ["DeepseekV3Config"]
+__all__ = [
+    "DeepseekV3Config",
+    "Qwen3_5MoeConfig",
+]
