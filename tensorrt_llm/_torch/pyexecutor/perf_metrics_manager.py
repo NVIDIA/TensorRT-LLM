cb237c04e3d980940f9940e0fa802c7ec95f42c9
jthomson04 <jwillthomson19@gmail.com>
2026-03-31 13:17:48 -0700

    fix
    Signed-off-by: jthomson04 <jwillthomson19@gmail.com>

--- tensorrt_llm/_torch/pyexecutor/perf_metrics_manager.py
+++ tensorrt_llm/_torch/pyexecutor/perf_metrics_manager.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Performance metrics manager for PyExecutor.
 
 Encapsulates GPU/CPU timing instrumentation: event creation, recording,
@@ -163,6 +178,10 @@ class PerfMetricsManager:
 
             # Compute once per batch, reuse for all requests
             if batch_gpu_forward_time is None:
+                if not perf.gpu_forward_end_event.query():
+                    perf.gpu_forward_end_event.synchronize()
+                if perf.gpu_sample_end_event and not perf.gpu_sample_end_event.query():
+                    perf.gpu_sample_end_event.synchronize()
                 batch_gpu_forward_time = perf.gpu_forward_start_event.elapsed_time(
                     perf.gpu_forward_end_event
                 )
