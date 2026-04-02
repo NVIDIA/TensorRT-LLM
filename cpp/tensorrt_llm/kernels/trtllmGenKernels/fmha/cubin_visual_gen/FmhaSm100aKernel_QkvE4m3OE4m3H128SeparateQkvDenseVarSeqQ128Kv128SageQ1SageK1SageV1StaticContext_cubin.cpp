1b66e964be12a537ac571d9c26bae2b9bab66e33
RuQing Xu <roger.hsu.sol@outlook.com>
2026-04-02 11:21:06 +0900

    [TRTLLM-11119][feat] Blackwell SageAttention, Integrate into AttentionOp API (#11718)
    Signed-off-by: Ruqing Xu <ruqingx@nvidia.com>

---
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin_visual_gen/FmhaSm100aKernel_QkvE4m3OE4m3H128SeparateQkvDenseVarSeqQ128Kv128SageQ1SageK1SageV1StaticContext_cubin.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9adee4d795ee9c82e67716e2c03f2d70ea5a0114f0400239674e37fb99e981a1
+size 781321
---
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/cubin_visual_gen/FmhaSm103aKernel_QkvE4m3OE4m3H128SeparateQkvDenseVarSeqQ128Kv128SageQ1SageK1SageV1PersistentContext_cubin.cpp
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ece76825e7fde0b6af8170c9b69cad0c23051bbf49f561d7549bd444e8432c8e
+size 879205
