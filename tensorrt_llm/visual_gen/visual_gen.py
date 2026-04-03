007688da5e7d2fb33f545417c87e91f559e0aadd
Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>
2026-04-03 03:40:11 +0000

    pre-commit
    Signed-off-by: Yibin Li <109242046+yibinl-nvidia@users.noreply.github.com>

--- tensorrt_llm/visual_gen/visual_gen.py
+++ tensorrt_llm/visual_gen/visual_gen.py
@@ -13,8 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import asyncio
-import os
 import atexit
+import os
 import queue
 import socket
 import threading
