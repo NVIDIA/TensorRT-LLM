a214aff0a7110d48c10c4362e6882bd5b3e7a5d8
Yihan Wang <yihwang@nvidia.com>
2026-04-08 23:51:32 -0700

    WA nvidia-cutlass-dsl updating issue
    Signed-off-by: Yihan Wang <yihwang@nvidia.com>

--- tensorrt_llm/_torch/cute_dsl_utils.py
+++ tensorrt_llm/_torch/cute_dsl_utils.py
@@ -1,4 +1,6 @@
+import os
 import platform
+import sys
 
 from ..logger import logger
 
@@ -7,8 +9,26 @@ IS_CUTLASS_DSL_AVAILABLE = False
 if platform.system() != "Windows":
     try:
         import cutlass  # noqa
+    except ImportError:
+        # nvidia-cutlass-dsl-libs-base installs cutlass under
+        # nvidia_cutlass_dsl/python_packages/ and registers a .pth file to
+        # make it importable.  The .pth file can be lost when pip upgrades
+        # nvidia-cutlass-dsl (pip ordering bug).  Fall back to adding the
+        # path manually.
+        try:
+            import nvidia_cutlass_dsl
+            _pp = os.path.join(nvidia_cutlass_dsl.__path__[0],
+                               "python_packages")
+            if os.path.isdir(_pp) and _pp not in sys.path:
+                sys.path.insert(0, _pp)
+            import cutlass  # noqa
+        except (ImportError, AttributeError):
+            pass
+
+    try:
+        import cutlass  # noqa
         import cutlass.cute as cute  # noqa
-        logger.info(f"cutlass dsl is available")
+        logger.info("cutlass dsl is available")
         IS_CUTLASS_DSL_AVAILABLE = True
     except ImportError:
         pass
