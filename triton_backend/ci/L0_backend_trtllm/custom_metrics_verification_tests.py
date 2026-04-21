e2c2daf997be7ed9f305ca8f8c43e44c075229ea
Misha Chornyi <mchornyi@nvidia.com>
2026-04-21 19:27:06 +0000

    chore: update copyright year to 2024-2026 in modified test files
    Relates-to: TRI-978

--- triton_backend/ci/L0_backend_trtllm/custom_metrics_verification_tests.py
+++ triton_backend/ci/L0_backend_trtllm/custom_metrics_verification_tests.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2024-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
