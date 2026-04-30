7d5fd9ec23750ff1eb722eeea38588ab88d24352
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2026-04-30 01:16:36 -0700

    [None][feat] KV cache manager v2 + python transceiver bug fix (#12928)
    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
    Signed-off-by: Fanrong Li <lfr-0531@users.noreply.github.com>
    (cherry picked from commit 2c5fe787be1ef5cd762615b87977748ebc3fee74)
    Signed-off-by: Yi Zhang <187001205+yizhang-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/pyexecutor/_util.py
+++ tensorrt_llm/_torch/pyexecutor/_util.py
@@ -1251,6 +1251,7 @@ def _create_kv_cache_manager(
             is_estimating_kv_cache=estimating_kv_cache,
             execution_stream=execution_stream,
             layer_mask=layer_mask,
+            max_num_tokens=max_num_tokens,
             is_disagg=is_disagg,
         )
     elif is_nemotron_hybrid(config):
