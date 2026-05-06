3642c394193eb72907b510594221f65685dfae4d
Yao Yao <lowsfer@users.noreply.github.com>
2026-05-06 05:10:39 +0000

    Clear mLevels in StorageManager::destroy() for double-call safety
    If destroy() is called explicitly before the destructor, the second
    call now returns early since mLevels is empty.
    
    Signed-off-by: Yao Yao <lowsfer@users.noreply.github.com>

--- cpp/tensorrt_llm/batch_manager/kv_cache_manager_v2/storageManager.cpp
+++ cpp/tensorrt_llm/batch_manager/kv_cache_manager_v2/storageManager.cpp
@@ -198,6 +198,7 @@ void StorageManager::destroy()
     for (auto& lvl : mLevels)
         if (lvl.storage)
             lvl.storage->destroy();
+    mLevels.clear();
 }
 
 // ---------------------------------------------------------------------------
