22768306a388be1a711043d6a5af33b32c53dc89
Yuhang He <58161490+heyuhhh@users.noreply.github.com>
2026-05-12 02:57:59 +0000

    fix: route token sparse fmha to cubins
    Signed-off-by: Yuhang He <58161490+heyuhhh@users.noreply.github.com>
    (cherry picked from commit f1ebebbb67f207fa0bd9871a5ad9612b88be86fc)
    Signed-off-by: Fanrong Li <lfr-0531@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/fmhaKernels.h
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/fmha/fmhaKernels.h
@@ -231,7 +231,7 @@ public:
             return false;
         }
         return options.mFmhaKernelType == FmhaKernelType::SwapsMmaAbForGeneration
-            && options.mDtypeKv != tg::Dtype::E2m1;
+            && !isTokenSparse(options.mSparseType) && options.mDtypeKv != tg::Dtype::E2m1;
     }
 
     std::pair<bool, std::string> checkIfKernelExist(RunnerParams const& params) const
