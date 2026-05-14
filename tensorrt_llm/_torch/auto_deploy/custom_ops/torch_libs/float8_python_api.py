e039b54c19f107839a5676ef55e3fde1b664f7fb
Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>
2026-05-14 10:32:47 -0700

    update year
    Signed-off-by: Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/custom_ops/torch_libs/float8_python_api.py
+++ tensorrt_llm/_torch/auto_deploy/custom_ops/torch_libs/float8_python_api.py
@@ -4,7 +4,7 @@
 # This source code is licensed under the BSD 3-Clause license found in the
 # LICENSE file in the root directory of this source tree.
 #
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- tensorrt_llm/_torch/auto_deploy/models/custom/mla_rope_utils.py
+++ tensorrt_llm/_torch/auto_deploy/models/custom/mla_rope_utils.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
 """Shared MLA RoPE utilities for auto_deploy custom models.
