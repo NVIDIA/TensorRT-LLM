8bf53dd187f5969a91dc27e76463b336dd6ac498
Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>
2026-05-19 13:12:11 -0700

    fla fixes
    Signed-off-by: Balamurugan Marimuthu <246387390+bmarimuthu-nv@users.noreply.github.com>

--- tensorrt_llm/_torch/auto_deploy/custom_ops/fla/delta_rule/chunk.py
+++ tensorrt_llm/_torch/auto_deploy/custom_ops/fla/delta_rule/chunk.py
@@ -1,6 +1,9 @@
-# Copyright (c) 2023-2025 Songlin Yang
-# Licensed under the MIT License.
-# Original source: https://github.com/fla-org/flash-linear-attention
+# Copyright (c) 2023-2026, Songlin Yang, Yu Zhang, Zhiyuan Li
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+# For a list of all contributors, visit:
+#   https://github.com/fla-org/flash-linear-attention/graphs/contributors
 #
 # SPDX-FileCopyrightText: Copyright (c) 2024-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
