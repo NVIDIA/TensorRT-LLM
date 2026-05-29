c7683f2f0ac975e8398cd552b62069a6cf569450
Dom Brown <3886319+DomBrown@users.noreply.github.com>
2026-05-29 13:52:26 +0100

    [None][feat] add Poolside Laguna tool parser (#14638)
    Signed-off-by: Dom Brown <3886319+DomBrown@users.noreply.github.com>

--- tensorrt_llm/serve/tool_parser/tool_parser_factory.py
+++ tensorrt_llm/serve/tool_parser/tool_parser_factory.py
@@ -11,6 +11,7 @@ from .glm4_parser import Glm4ToolParser
 from .glm47_parser import Glm47ToolParser
 from .kimi_k2_tool_parser import KimiK2ToolParser
 from .minimax_m2_parser import MiniMaxM2ToolParser
+from .poolside_v1_parser import PoolsideV1ToolParser
 from .qwen3_coder_parser import Qwen3CoderToolParser
 from .qwen3_tool_parser import Qwen3ToolParser
 
@@ -31,6 +32,7 @@ MODEL_TYPE_TO_TOOL_PARSER: dict[str, str] = {
     "glm_moe_dsa": "glm47",
     "gemma4": "gemma4",
     "gemma4_text": "gemma4",
+    "laguna": "poolside_v1",
 }
 
 
@@ -59,6 +61,7 @@ class ToolParserFactory:
         "glm4": Glm4ToolParser,
         "glm47": Glm47ToolParser,
         "minimax_m2": MiniMaxM2ToolParser,
+        "poolside_v1": PoolsideV1ToolParser,
     }
 
     @staticmethod
