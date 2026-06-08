1a8eac274b0467def85018a17637b9afb57554cc
EmmaQiaoCh <qqiao@nvidia.com>
2026-06-08 08:49:52 +0300

    Change back mpi for perf multi-node
    Signed-off-by: EmmaQiaoCh <qqiao@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1253,7 +1253,7 @@ def runLLMTestlistWithSbatch(pipeline, platform, testList, config=VANILLA_CONFIG
                     """
                 } else {
                     if(nodeCount > 1) {
-                        srunArgs.add("--mpi=pmix")
+                        srunArgs.add("--mpi=pmi2")
                     }
 
                     def scriptContent = """
