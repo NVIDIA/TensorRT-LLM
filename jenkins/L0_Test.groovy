94f7893b0dc01b1af811756b31c35485e70e7057
EmmaQiaoCh <qqiao@nvidia.com>
2026-06-11 15:34:25 +0300

    Apply dev fix for pmi, waive some failed cases
    Signed-off-by: EmmaQiaoCh <qqiao@nvidia.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1527,7 +1527,7 @@ def runLLMTestlistWithSbatch(pipeline, platform, testList, config=VANILLA_CONFIG
                     """
                 } else {
                     if(nodeCount > 1) {
-                        srunArgs.add("--mpi=pmi2")
+                        srunArgs.add("--mpi=pmix")
                     }
 
                     def scriptContent = """
