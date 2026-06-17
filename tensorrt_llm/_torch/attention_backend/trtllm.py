04083799d5f9f5b29792969f8f3cafcdb717bbe8
Yuxian Qiu <142763828+yuxianq@users.noreply.github.com>
2026-06-17 03:16:11 +0000

    [TRTLLM-12807][fix] Clean up FMHA rebase metadata
    Signed-off-by: Yuxian Qiu <142763828+yuxianq@users.noreply.github.com>

--- tensorrt_llm/_torch/attention_backend/trtllm.py
+++ tensorrt_llm/_torch/attention_backend/trtllm.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import functools
 import math
 import os
@@ -105,7 +120,6 @@ class TrtllmAttentionMetadata(AttentionMetadata):
     spec_decoding_bl_tree_mask_offset: Optional[torch.Tensor] = None
     spec_decoding_bl_tree_mask: Optional[torch.Tensor] = None
     spec_bl_tree_first_sparse_mask_offset_kv: Optional[torch.Tensor] = None
-    max_total_draft_tokens: Optional[int] = None
 
     # TRTLLM-Gen FMHA JIT warmup controls.
     trtllm_gen_jit_warmup: bool = False
