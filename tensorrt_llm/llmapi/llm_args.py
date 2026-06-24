5bef6adfa612f513e024d2953ac361f17b6f74d8
Yuan Tong <13075180+tongyuantongyu@users.noreply.github.com>
2026-06-24 15:20:08 +0800

    Extract CPU only unittests - trunk
    Signed-off-by: Yuan Tong <13075180+tongyuantongyu@users.noreply.github.com>

--- tensorrt_llm/llmapi/llm_args.py
+++ tensorrt_llm/llmapi/llm_args.py
@@ -4679,16 +4679,6 @@ class BaseLlmArgs(StrictBaseModel):
             raise ValueError("Configuration file root must be a mapping.")
         return cls(**config_dict)
 
-    @field_validator("dtype")
-    @classmethod
-    def validate_dtype(cls, v, info):
-        if torch.cuda.get_device_properties(0).major < 8:
-            if v == 'auto':
-                v = 'float16'
-            if v == 'bfloat16':
-                raise RuntimeError("Pre SM 80 GPUs do not support bfloat16")
-        return v
-
     @field_validator("gpus_per_node", mode='before')
     @classmethod
     def validate_gpus_per_node(cls, v, info):
