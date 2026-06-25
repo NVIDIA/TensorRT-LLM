e53a5b077c01d41f5849fe4a280b449f5ca0f88a
tensorrt-cicd <tensorrt-cicd@users.noreply.github.com>
2026-06-25 03:56:14 -0700

    [nvbugs/6322073][chore] Remove stale waiver after fix
    Signed-off-by: tensorrt-cicd <tensorrt-cicd@users.noreply.github.com>
    Signed-off-by: trtllm-agent <296075020+trtllm-agent@users.noreply.github.com>

--- tensorrt_llm/_torch/custom_ops/torch_custom_ops.py
+++ tensorrt_llm/_torch/custom_ops/torch_custom_ops.py
@@ -116,7 +116,6 @@ def init_nccl_pp_workaround() -> bool:
     return True
 
 
-
 # Used to WAR an issue in torch.bmm that it would break the graph when the out is not contiguous.
 @torch.library.custom_op("trtllm::bmm_out", mutates_args=("out", ))
 def bmm_out(a: torch.Tensor, b: torch.Tensor, out: torch.Tensor) -> None:
