fff441f7eee1a6fdf4c5a0a563ff23656276016c
tensorrt-cicd <90828364+tensorrt-cicd@users.noreply.github.com>
2026-06-26 20:56:56 -0700

    [nvbugs/6336801][fix] Drop stale beam_width alias and unused multi_item_part_lens exclusion
    Signed-off-by: tensorrt-cicd <90828364+tensorrt-cicd@users.noreply.github.com>

--- tests/unittest/_torch/attention_backend/test_attention_op_sync.py
+++ tests/unittest/_torch/attention_backend/test_attention_op_sync.py
@@ -61,7 +61,6 @@ _SOURCE_CLASSES = {
 }
 
 _THOP_KWARG_SOURCE_ALIASES: dict[str, tuple[str, tuple[str, ...]]] = {
-    "beam_width": ("metadata", ("effective_beam_width",)),
     "context_lengths": ("metadata", ("prompt_lens_cuda_runtime",)),
     "head_size": ("attn", ("head_dim",)),
     "host_context_lengths": ("metadata", ("prompt_lens_cpu_runtime",)),
