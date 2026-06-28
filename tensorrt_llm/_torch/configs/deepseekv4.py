854521049f827da62a54fd28861ab0b61ed0f004
Fanrong Li <lfr-0531@users.noreply.github.com>
2026-06-28 11:29:42 +0000

    Fix DSv4 sparse config review issues
    Signed-off-by: Fanrong Li <lfr-0531@users.noreply.github.com>

--- tensorrt_llm/_torch/configs/deepseekv4.py
+++ tensorrt_llm/_torch/configs/deepseekv4.py
@@ -1,3 +1,4 @@
+# SPDX-FileCopyrightText: Copyright (c) 2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 from transformers.configuration_utils import PretrainedConfig
