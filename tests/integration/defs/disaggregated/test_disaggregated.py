5bf26d2b5419376a2652cffcce7b1a5ef2605ee1
yiqingy <yiqingy@tensorrt-llm-infra-debug-vm-01.nvidia.com>
2026-07-01 22:56:36 -0700

    Revert "Add test code"
    This reverts commit fec27b43b36984a71655128789cd1dce67014a03.
    
    Signed-off-by: yiqingy <yiqingy@tensorrt-llm-infra-debug-vm-01.nvidia.com>

--- tests/integration/defs/disaggregated/test_disaggregated.py
+++ tests/integration/defs/disaggregated/test_disaggregated.py
@@ -1065,7 +1065,6 @@ def test_disaggregated_mixed(disaggregated_test_root, llm_venv,
                          indirect=True)
 def test_disaggregated_overlap(disaggregated_test_root, llm_venv,
                                disaggregated_example_root, llama_model_root):
-    time.sleep(600)
     setup_model_symlink(llm_venv, llama_model_root,
                         "TinyLlama/TinyLlama-1.1B-Chat-v1.0")
 
