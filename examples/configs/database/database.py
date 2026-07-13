aacf9b67e62333d4e562968085330ced0344df19
Maxim Gluhovskoi <mgluhovskoi@nvidia.com>
2026-07-13 13:33:47 -0700

    [TRTLLM-13694][chore] Remove unnecessary future import
    Signed-off-by: Maxim Gluhovskoi <mgluhovskoi@nvidia.com>

--- examples/configs/database/database.py
+++ examples/configs/database/database.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import annotations
-
 import logging
 import re
 from collections import defaultdict
