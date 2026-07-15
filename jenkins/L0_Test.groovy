fbb28b02df031b744916eb6245227c5dff3390cf
Abby Wei <18545893+mzweilz@users.noreply.github.com>
2026-07-15 13:06:59 +0800

    Remove test commit
    Signed-off-by: Abby Wei <18545893+mzweilz@users.noreply.github.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -761,10 +761,6 @@ def runLLMTestlistWithAgent(pipeline, platform, testList, config=VANILLA_CONFIG,
 
         stage('Check If Node Is Online') {
             CloudManager.withSlurmSshCredentials(pipeline, partition.clusterName, cluster) { remote ->
-                // TODO: THIS IS FOR FUNCTIONAL TESTING, THIS SHOULD BE REMOVED BEFORE MERGE.
-                def slurmLogPathTmp = "/home/svc_tensorrt/slurm-logs/slurm-${slurmJobID}-${nodeName}.out"
-                echoRemoteLogTail(pipeline, remote, slurmLogPathTmp)
-
                 // Check the SLURM job once; if it is no longer active, raise a typed
                 // InfraFailure(SLURM) so the retry layer routes it via instanceof (scope=SLURM).
                 def checkSlurmJobActive = {
