228bc57299bab8910d2f03fb0ba9527e21947a9e
Chien-Chun Hung <2679986+chienchunhung@users.noreply.github.com>
2026-07-23 11:02:38 -0700

    [NVBUG-6448152][test] apply byte-identical async consensus factor
    Signed-off-by: Chien-Chun Hung <2679986+chienchunhung@users.noreply.github.com>

--- cpp/tests/unit_tests/multi_gpu/mpiUtilsTest.cpp
+++ cpp/tests/unit_tests/multi_gpu/mpiUtilsTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2024, NVIDIA CORPORATION.  All rights reserved.
+ * Copyright (c) 2022-2026, NVIDIA CORPORATION.  All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,6 +25,8 @@
 #endif // ENABLE_MULTI_DEVICE
 
 #include <algorithm>
+#include <cstdint>
+#include <thread>
 
 namespace mpi = tensorrt_llm::mpi;
 namespace tr = tensorrt_llm::runtime;
@@ -46,6 +48,29 @@ TEST(MPIUtils, WorldRankAndSize)
     auto const size = comm.getSize();
     EXPECT_LE(rank, size);
 }
+
+#if ENABLE_MULTI_DEVICE
+TEST(MPIUtils, AsyncSendCanBePolled)
+{
+    auto& comm = mpi::MpiComm::world();
+    auto const rank = comm.getRank();
+    auto const size = comm.getSize();
+    auto const destination = (rank + 1) % size;
+    auto const source = (rank + size - 1) % size;
+    std::uint64_t const sentValue = static_cast<std::uint64_t>(rank);
+    std::uint64_t receivedValue = 0;
+
+    auto request
+        = comm.sendAsync(&sentValue, 1, mpi::MpiType::kUINT64, destination, mpi::MpiTag::kContextTransferEvent);
+    static_cast<void>(comm.recv(&receivedValue, 1, mpi::MpiType::kUINT64, source, mpi::MpiTag::kContextTransferEvent));
+    while (!request->isCompleted())
+    {
+        std::this_thread::yield();
+    }
+
+    EXPECT_EQ(receivedValue, static_cast<std::uint64_t>(source));
+}
+#endif // ENABLE_MULTI_DEVICE
 
 template <typename T>
 void testBroadcast()
