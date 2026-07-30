05efb7db98865df529f86873da9762fec998fe5b
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2026-07-30 16:39:07 +0800

    [None][infra] Select UCX env for perf sanity by cluster name (#16725)
    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>

--- jenkins/L0_Test.groovy
+++ jenkins/L0_Test.groovy
@@ -1859,7 +1859,8 @@ def runLLMTestlistWithSbatch(pipeline, platform, testList, config=VANILLA_CONFIG
                         --script-prefix ${scriptLaunchPrefixPathLocal} \\
                         --srun-args ${scriptLaunchSrunArgsPathLocal} \\
                         --split-group ${splitId} \\
-                        --stage-name ${stageName}
+                        --stage-name ${stageName} \\
+                        --cluster-name ${partition.clusterName}
                     """
                 } else {
                     if(nodeCount > 1) {
