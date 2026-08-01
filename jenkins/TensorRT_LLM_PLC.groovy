5936f3c4e90056150d66999c46d57be58ded065e
Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>
2026-08-01 10:48:09 -0700

    Add back lock file generation
    Signed-off-by: Yuanjing Xue <197832395+yuanjingx87@users.noreply.github.com>

--- jenkins/TensorRT_LLM_PLC.groovy
+++ jenkins/TensorRT_LLM_PLC.groovy
@@ -438,7 +438,7 @@ pipeline {
                     }
                     steps {
                         script {
-                            //generateLockFiles(env.LLM_REPO, env.REF)
+                            generateLockFiles(env.LLM_REPO, env.REF)
                             pulseScanSourceCode(env.LLM_REPO, env.REF)
                         }
                     }
