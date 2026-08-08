6be611eda81325943e7a3ad9736ef1a93b53a234
Erin Ho <14718778+hchings@users.noreply.github.com>
2026-08-07 17:32:29 -0700

    add auth for rl endpoints
    Signed-off-by: Erin Ho <14718778+hchings@users.noreply.github.com>

--- tensorrt_llm/serve/openai_protocol.py
+++ tensorrt_llm/serve/openai_protocol.py
@@ -1424,7 +1424,7 @@ class ResponsesStreamResponse(OpenAIBaseModel):
 
 
 class MemoryUpdateRequest(OpenAIBaseModel):
-    tags: List[str] = Field(default=["model", "kv_cache"])
+    tags: List[str] = Field(min_length=1)
 
 
 class UpdateWeightsRequest(OpenAIBaseModel):
