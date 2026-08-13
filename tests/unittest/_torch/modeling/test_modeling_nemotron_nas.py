aced3a1a5a11e42d699a54d9a7e15aa1fb725043
Bowen Fu <5812640+BowenFu@users.noreply.github.com>
2026-08-13 23:36:45 +0000

    [TRTLLM-6934] Re-enable Nemotron NAS HF parity test
    Signed-off-by: Bowen Fu <5812640+BowenFu@users.noreply.github.com>

--- tests/unittest/_torch/modeling/test_modeling_nemotron_nas.py
+++ tests/unittest/_torch/modeling/test_modeling_nemotron_nas.py
@@ -1,3 +1,18 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025-2026 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import unittest
 from copy import deepcopy
 from dataclasses import dataclass
@@ -360,7 +375,6 @@ class TestNemotronNAS(unittest.TestCase):
     ], lambda testcase_func, param_num, param:
                           f"{testcase_func.__name__}[{param.args[0]}]")
     @torch.no_grad()
-    @unittest.skip("https://nvbugspro.nvidia.com/bug/5439817")
     def test_nemotron_nas_allclose_to_hf(self, scenario: Scenario) -> None:
         """
         Compare output to HF
