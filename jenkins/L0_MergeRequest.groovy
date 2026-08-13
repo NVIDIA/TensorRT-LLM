2437c33a064b992dff9bc7dc40ea9aae14fcea6e
Weimin Wang <301118019+weiminwang-nv@users.noreply.github.com>
2026-08-13 10:17:16 +0800

    [no merge] test
    Signed-off-by: Weimin Wang <301118019+weiminwang-nv@users.noreply.github.com>

--- jenkins/L0_MergeRequest.groovy
+++ jenkins/L0_MergeRequest.groovy
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@main']) _
+@Library(['bloom-jenkins-shared-lib@main', 'trtllm-jenkins-shared-lib@be321bd']) _
 
 import java.lang.InterruptedException
 import groovy.transform.Field
