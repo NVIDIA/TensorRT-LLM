b0c0433865cf1831b72a548e7bd9886271056c92
Chuang Zhu <111838961+chuangz0@users.noreply.github.com>
2026-08-13 02:18:20 +0000

    [https://nvbugs/6541356][fix] Align the transceiver precheck with the serving KV cache setup
    Signed-off-by: Chuang Zhu <111838961+chuangz0@users.noreply.github.com>

--- tests/unittest/scripts/test_cluster_env.py
+++ tests/unittest/scripts/test_cluster_env.py
@@ -83,12 +83,14 @@ def test_gpu_type_from_supported_gpus(
         ),
         (
             "oci-aga-cs-001",
-            "export UCX_TLS=^tcp,rc_gda,gga UCX_IB_MLX5_DEVX=n "
-            "UCX_NET_DEVICES="
-            "rdma_vf_rail0:1,rdma_vf_rail1:1,rdma_vf_rail2:1,rdma_vf_rail3:1 "
-            "UCX_IB_TRAFFIC_CLASS=96 TRTLLM_NIXL_NUM_THREADS=1",
+            "export UCX_TLS=cuda_ipc,cuda_copy,sm,self,tcp UCX_TCP_AF_PRIO=inet",
         ),
-        ("aws-cmh", "export UCX_TLS=cuda_ipc,cuda_copy,sm,self,tcp"),
+        (
+            "aws-cmh",
+            "export UCX_TLS=cuda_ipc,cuda_copy,sm,self,tcp "
+            "UCX_NET_DEVICES=eth0,mlx5_0:1,mlx5_1:1,mlx5_2:1,mlx5_3:1,"
+            "mlx5_4:1,mlx5_5:1,mlx5_6:1,mlx5_7:1",
+        ),
         ("aws-dfw-prod", "export UCX_TLS=^gdr_copy"),
     ),
 )
