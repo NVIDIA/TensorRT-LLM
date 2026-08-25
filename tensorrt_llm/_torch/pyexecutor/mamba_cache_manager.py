491f11a691995e63d0c48625c26811e6bf826fb3
Mike Iovine <miovine@nvidia.com>
2026-08-25 11:00:52 -0400

    [TRTLLM-14388][refactor] Remove 2 model spec dec drafting loops (#17562)
    Signed-off-by: Mike Iovine <miovine@nvidia.com>
    Signed-off-by: allisonlim-nv <allim@nvidia.com>
    Signed-off-by: Allison Lim <allim@nvidia.com>
    Co-authored-by: allisonlim-nv <allim@nvidia.com>

--- tensorrt_llm/_torch/pyexecutor/mamba_cache_manager.py
+++ tensorrt_llm/_torch/pyexecutor/mamba_cache_manager.py
@@ -2501,11 +2501,6 @@ class CppMambaHybridCacheManager(KVCacheManager, MambaHybridCacheManager):
         use_mrope: bool = False,
         max_beam_width: int = 1,
         encoder_output_lens: Optional[List[int]] = None,
-        # For capturable drafting loops. During normal inference, the draft model always
-        # has enough KV cache space to fit all of our draft tokens. During warmup, however,
-        # we need to make the KV cache manager aware that multiple autoregressive steps will
-        # occur.
-        num_extra_decoding_steps: int = 0,
         draft_kv_cache_manager: Optional[KVCacheManager] = None,
     ) -> List[LlmRequest]:
         requests = super().add_dummy_requests(
@@ -2518,7 +2513,6 @@ class CppMambaHybridCacheManager(KVCacheManager, MambaHybridCacheManager):
             use_mrope=use_mrope,
             max_beam_width=max_beam_width,
             encoder_output_lens=encoder_output_lens,
-            num_extra_decoding_steps=num_extra_decoding_steps,
             draft_kv_cache_manager=draft_kv_cache_manager,
         )
         if requests:
@@ -3564,7 +3558,6 @@ class MambaHybridCacheManagerV2(KVCacheManagerV2, MambaHybridCacheManager):
         use_mrope: bool = False,
         max_beam_width: int = 1,
         encoder_output_lens: Optional[List[int]] = None,
-        num_extra_decoding_steps: int = 0,
         draft_kv_cache_manager: Optional[BaseResourceManager] = None,
     ) -> List[LlmRequest]:
         requests = super().add_dummy_requests(
@@ -3577,7 +3570,6 @@ class MambaHybridCacheManagerV2(KVCacheManagerV2, MambaHybridCacheManager):
             use_mrope=use_mrope,
             max_beam_width=max_beam_width,
             encoder_output_lens=encoder_output_lens,
-            num_extra_decoding_steps=num_extra_decoding_steps,
             draft_kv_cache_manager=draft_kv_cache_manager,
         )
         if requests and prepare_resource:
