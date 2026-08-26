30993b6e91c8e7ca1315c4869b2ed3d27ef68949
Connor Carpenter <connorc@nvidia.com>
2026-08-26 16:20:13 -0700

    fix(ci): detect Linux distribution without distro
    Signed-off-by: Connor Carpenter <connorc@nvidia.com>

--- tests/integration/defs/sysinfo/get_sysinfo.py
+++ tests/integration/defs/sysinfo/get_sysinfo.py
@@ -110,11 +110,15 @@ def is_power():
 
 def get_linux_distribution():
     try:
-        import distro
-        return (distro.id(), distro.version(), distro.codename())
-    except:
+        release = platform.freedesktop_os_release()
+        return (
+            release.get("ID", "na"),
+            release.get("VERSION_ID", "na"),
+            release.get("VERSION_CODENAME", "na"),
+        )
+    except OSError:
         logger.warning(
-            "Unable to use distro module, defaulting operating system to ('na', 'na', 'na')"
+            "Unable to read the operating system release, defaulting to ('na', 'na', 'na')"
         )
         return ("na", "na", "na")
 
