152d44ef7a895e2739fb387fecef46b703150552
Wanli Jiang <35160485+Wanli-Jiang@users.noreply.github.com>
2026-08-27 11:16:22 -0700

    [None][perf] remove redundant MoE routing initialization
    Signed-off-by: Wanli Jiang <35160485+Wanli-Jiang@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/trtllmGenKernels/blockScaleMoe/routing/RoutingCustom.cu
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/blockScaleMoe/routing/RoutingCustom.cu
@@ -1740,7 +1740,8 @@ void run(Data const& data, void* stream)
 
         if (useCoop)
         {
-            launchInitExpertCounts(data, numThreadsHist, stream);
+            // routingIndicesHistogramScoresKernel already cleared both expert-count
+            // regions before publishing TopK scores to this cooperative kernel.
             launchCoopKernel(lastKernelData, numBlocksCoop, numThreadsHist, stream);
         }
         else
