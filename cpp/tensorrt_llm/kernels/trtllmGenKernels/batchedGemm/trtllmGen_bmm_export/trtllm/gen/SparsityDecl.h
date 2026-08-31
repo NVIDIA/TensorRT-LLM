fd1295541883b62d7997c0980e5a97f63dbad53f
Faraz Khoubsirat <58580514+farazkh80@users.noreply.github.com>
2026-08-31 19:34:17 +0000

    [WIP][chore] clang-format v6 export headers
    Signed-off-by: Faraz Khoubsirat <58580514+farazkh80@users.noreply.github.com>

--- cpp/tensorrt_llm/kernels/trtllmGenKernels/batchedGemm/trtllmGen_bmm_export/trtllm/gen/SparsityDecl.h
+++ cpp/tensorrt_llm/kernels/trtllmGenKernels/batchedGemm/trtllmGen_bmm_export/trtllm/gen/SparsityDecl.h
@@ -26,106 +26,102 @@
 //
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-namespace batchedGemm {
+namespace batchedGemm
+{
 
-namespace trtllm {
-namespace gen {
+namespace trtllm
+{
+namespace gen
+{
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // This enumeration defines structured sparsity modes. Please refer to the PTX ISA for more details.
-enum class Sparsity {
-  // No sparsity.
-  Dense,
+enum class Sparsity
+{
+    // No sparsity.
+    Dense,
 
-  // For each chunk of 2 elements, 1 is non-zero. Only non-zero elements are stored.
-  // A 4-bit index is used to indicate the position of the non-zero element.
-  // The index may only take the value 0b1110 or 0b0100, other values are undefined behavior.
-  //
-  // 0b1110:                               0b0100:
-  // |------ a ------|------ 0 ------|     |------ 0 ------|------ a ------|
-  // |  11   |  10   |  01   |  00   |     |  11   |  10   |  01   |  00   |
-  Any_1_2,
+    // For each chunk of 2 elements, 1 is non-zero. Only non-zero elements are stored.
+    // A 4-bit index is used to indicate the position of the non-zero element.
+    // The index may only take the value 0b1110 or 0b0100, other values are undefined behavior.
+    //
+    // 0b1110:                               0b0100:
+    // |------ a ------|------ 0 ------|     |------ 0 ------|------ a ------|
+    // |  11   |  10   |  01   |  00   |     |  11   |  10   |  01   |  00   |
+    Any_1_2,
 
-  // For each chunk of 4 elements, 2 are non-zero. Only non-zero elements are stored.
-  // A 4-bit index is used to indicate the position of the non-zero elements.
-  // Meaningful values are: 0b0100, 0b1000, 0b1100, 0b1001, 0b1101, 0b1110.
-  // Most other values are undefined behavior.
-  //
-  // E.g. 0b1100 corresponds to:
-  // |-- b --|-- 0 --|-- 0 --|-- a --|
-  // |  11   |  10   |  01   |  00   |
-  Any_2_4,
+    // For each chunk of 4 elements, 2 are non-zero. Only non-zero elements are stored.
+    // A 4-bit index is used to indicate the position of the non-zero elements.
+    // Meaningful values are: 0b0100, 0b1000, 0b1100, 0b1001, 0b1101, 0b1110.
+    // Most other values are undefined behavior.
+    //
+    // E.g. 0b1100 corresponds to:
+    // |-- b --|-- 0 --|-- 0 --|-- a --|
+    // |  11   |  10   |  01   |  00   |
+    Any_2_4,
 
-  // For each chunk of 8 elements, 4 are non-zero. Only non-zero elements are stored.
-  // Further, the zero and non-zero elements are grouped in pairs.
-  // A 4-bit index is used to indicate the position of the non-zero elements.
-  // Meaningful values are: 0b0100, 0b1000, 0b1100, 0b1001, 0b1101, 0b1110.
-  // Most other values are undefined behavior.
-  //
-  // E.g. 0b1100 corresponds to:
-  // | d | c | 0 | 0 | 0 | 0 | b | a |
-  // |  11   |  10   |  01   |  00   |
-  Pairwise_4_8,
+    // For each chunk of 8 elements, 4 are non-zero. Only non-zero elements are stored.
+    // Further, the zero and non-zero elements are grouped in pairs.
+    // A 4-bit index is used to indicate the position of the non-zero elements.
+    // Meaningful values are: 0b0100, 0b1000, 0b1100, 0b1001, 0b1101, 0b1110.
+    // Most other values are undefined behavior.
+    //
+    // E.g. 0b1100 corresponds to:
+    // | d | c | 0 | 0 | 0 | 0 | b | a |
+    // |  11   |  10   |  01   |  00   |
+    Pairwise_4_8,
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-inline bool isSparse(Sparsity sparsity) {
-  return sparsity != Sparsity::Dense;
+inline bool isSparse(Sparsity sparsity)
+{
+    return sparsity != Sparsity::Dense;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-inline std::string sparsityToString(Sparsity sparsity) {
-  switch (sparsity) {
-  case Sparsity::Dense:
-    return "dense";
-  case Sparsity::Any_1_2:
-    return "1:2";
-  case Sparsity::Any_2_4:
-    return "2:4";
-  case Sparsity::Pairwise_4_8:
-    return "4:8";
-  default:
-    assert(false);
-    return "Unsupported sparsity";
-  }
+inline std::string sparsityToString(Sparsity sparsity)
+{
+    switch (sparsity)
+    {
+    case Sparsity::Dense: return "dense";
+    case Sparsity::Any_1_2: return "1:2";
+    case Sparsity::Any_2_4: return "2:4";
+    case Sparsity::Pairwise_4_8: return "4:8";
+    default: assert(false); return "Unsupported sparsity";
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Size of a sparsity chunk, for sparse modes.
-inline int32_t getSparsityChunkSize(Sparsity sparsity) {
-  switch (sparsity) {
-  case Sparsity::Any_1_2:
-    return 2;
-  case Sparsity::Any_2_4:
-    return 4;
-  case Sparsity::Pairwise_4_8:
-    return 8;
-  case Sparsity::Dense:
-  default:
-    assert(false);
-    return 0;
-  }
+inline int32_t getSparsityChunkSize(Sparsity sparsity)
+{
+    switch (sparsity)
+    {
+    case Sparsity::Any_1_2: return 2;
+    case Sparsity::Any_2_4: return 4;
+    case Sparsity::Pairwise_4_8: return 8;
+    case Sparsity::Dense:
+    default: assert(false); return 0;
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Number of bytes needed to store the sparsity information.
-inline size_t getNumBytesSparsityInfo(Sparsity sparsity, size_t numElts) {
-  switch (sparsity) {
-  case Sparsity::Dense:
-    return 0;
-  case Sparsity::Any_1_2:
-  case Sparsity::Any_2_4:
-  case Sparsity::Pairwise_4_8:
-    return numElts / getSparsityChunkSize(sparsity) * 4 /*bits*/ / 8;
-  default:
-    assert(false);
-    return 0;
-  }
+inline size_t getNumBytesSparsityInfo(Sparsity sparsity, size_t numElts)
+{
+    switch (sparsity)
+    {
+    case Sparsity::Dense: return 0;
+    case Sparsity::Any_1_2:
+    case Sparsity::Any_2_4:
+    case Sparsity::Pairwise_4_8: return numElts / getSparsityChunkSize(sparsity) * 4 /*bits*/ / 8;
+    default: assert(false); return 0;
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
