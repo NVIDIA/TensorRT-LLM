8355ecf7ee4aa6b92763f24d612c590278be2c7a
zhaoyangwang-nvidia <zhaoyangw@nvidia.com>
2026-09-01 18:07:56 +0800

    [TRTLLM-15405][refactor] Remove the C++ decoder stack behind TRTLLMSampler (#18233)
    Signed-off-by: ZhaoyangWang <zhaoyangw@nvidia.com>

--- cpp/tensorrt_llm/nanobind/batch_manager/algorithms.cpp
+++ cpp/tensorrt_llm/nanobind/batch_manager/algorithms.cpp
@@ -20,16 +20,13 @@
 #include "tensorrt_llm/batch_manager/allocateKvCache.h"
 #include "tensorrt_llm/batch_manager/assignReqSeqSlots.h"
 #include "tensorrt_llm/batch_manager/capacityScheduler.h"
-#include "tensorrt_llm/batch_manager/createNewDecoderRequests.h"
 #include "tensorrt_llm/batch_manager/kvCacheManager.h"
 #include "tensorrt_llm/batch_manager/llmRequest.h"
-#include "tensorrt_llm/batch_manager/medusaBuffers.h"
 #include "tensorrt_llm/batch_manager/microBatchScheduler.h"
 #include "tensorrt_llm/batch_manager/pauseRequests.h"
 #include "tensorrt_llm/batch_manager/peftCacheManager.h"
 #include "tensorrt_llm/common/tllmDataType.h"
 #include "tensorrt_llm/nanobind/common/customCasters.h"
-#include "tensorrt_llm/runtime/decoderState.h"
 #include "tensorrt_llm/runtime/torch.h"
 #include "tensorrt_llm/runtime/torchView.h"
 
@@ -128,29 +125,4 @@ void tensorrt_llm::nanobind::batch_manager::algorithms::initBindings(nb::module_
             nb::arg("generation_requests"), nb::arg("model_config"), nb::arg("cross_kv_cache_manager") = std::nullopt,
             nb::call_guard<nb::gil_scoped_release>())
         .def("name", [](AllocateKvCache const&) { return AllocateKvCache::name; });
-
-    nb::class_<CreateNewDecoderRequests>(m, CreateNewDecoderRequests::name)
-        .def(nb::init<bool, bool, bool>(), nb::arg("speculative_decoding_fast_logits"),
-            nb::arg("is_leader_in_orch_mode"), nb::arg("is_normalize_log_probs"))
-        .def(
-            "__call__",
-            [](CreateNewDecoderRequests& self, tr::ModelConfig const& modelConfig, tr::WorldConfig const& worldConfig,
-                executor::DecodingConfig const& decodingConfig, RequestVector const& contextRequests,
-                tensorrt_llm::DataType logitsType, DecoderInputBuffers& inputBuffers,
-                runtime::decoder::DecoderState& decoderState, tensorrt_llm::runtime::CudaStream const& runtimeStream,
-                tensorrt_llm::runtime::CudaStream const& decoderStream, SizeType32 maxSequenceLength,
-                SizeType32 beamWidth)
-            {
-                OptionalRef<MedusaBuffers const> medusaBuffers = std::nullopt;
-                auto [batchSlots, samplingConfigs, lookaheadPrompt, lookaheadAlgoConfigs]
-                    = self(modelConfig, worldConfig, decodingConfig, contextRequests, logitsType, inputBuffers,
-                        decoderState, runtimeStream, decoderStream, maxSequenceLength, beamWidth, medusaBuffers);
-
-                return std::tuple{runtime::Torch::tensor(batchSlots), std::move(samplingConfigs),
-                    std::move(lookaheadPrompt), std::move(lookaheadAlgoConfigs)};
-            },
-            nb::arg("model_config"), nb::arg("world_config"), nb::arg("decoding_config"), nb::arg("context_requests"),
-            nb::arg("logits_type"), nb::arg("decoder_input_buffers"), nb::arg("decoder_state"),
-            nb::arg("runtime_stream"), nb::arg("decoder_stream"), nb::arg("max_sequence_length"), nb::arg("beam_width"))
-        .def("name", [](CreateNewDecoderRequests const&) { return CreateNewDecoderRequests::name; });
 }
