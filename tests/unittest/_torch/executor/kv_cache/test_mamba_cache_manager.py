2ebe4e65193c4232e6a326ee7dbf14770f6c4e74
Yi Zhang <187001205+yizhang-nv@users.noreply.github.com>
2026-09-15 07:04:41 +0800

    [https://nvbugs/6732123][fix] Correct V2 KV cache quota estimation (#18988)
    Signed-off-by: Yi Zhang <187001205+yizhang-nv@users.noreply.github.com>

--- tests/unittest/_torch/executor/kv_cache/test_mamba_cache_manager.py
+++ tests/unittest/_torch/executor/kv_cache/test_mamba_cache_manager.py
@@ -1797,6 +1797,7 @@ def test_v2_hybrid_warns_when_avg_seq_len_is_missing(monkeypatch):
 
 def test_v2_hybrid_rejects_quota_below_live_state_floor():
     mgr = object.__new__(MambaHybridCacheManagerV2)
+    mgr._generation_kv_capacity_headroom = 1
     mgr._has_cp_helix = False
     mgr.max_batch_size = 2
     mgr.mapping = Mapping(world_size=1, rank=0, tp_size=1, pp_size=1)
@@ -1827,6 +1828,7 @@ def test_v2_hybrid_rejects_quota_below_live_state_floor():
 
 def test_v2_hybrid_pure_mamba_rank_does_not_reserve_attention_page():
     mgr = object.__new__(MambaHybridCacheManagerV2)
+    mgr._generation_kv_capacity_headroom = 1
     mgr._has_cp_helix = False
     mgr.max_batch_size = 2
     mgr.mapping = Mapping(world_size=1, rank=0, tp_size=1, pp_size=1)
@@ -2054,6 +2056,7 @@ def test_expect_snapshot_points_binding_round_trip():
 def test_v2_hybrid_pool_ratio_controls_allocated_memory():
     def allocated_memory(pool_ratio):
         mgr = object.__new__(MambaHybridCacheManagerV2)
+        mgr._generation_kv_capacity_headroom = 1
         mgr._has_cp_helix = False
         mgr.kv_cache_type = CacheTypeCpp.SELF
         mgr.head_dim_per_layer = [64, 64]
