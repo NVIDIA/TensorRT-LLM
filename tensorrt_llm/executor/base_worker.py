5982425ca809eae1863092984b5657a482098449
Junpan Wu <77491544+shikicloud@users.noreply.github.com>
2026-09-21 16:49:31 +0800

    [None][feat] Router Replay (R3): return per-token MoE routing to training engine in post train. (#18397)
    Signed-off-by: Shiki Wu <shikiw@nvidia.com>

--- tensorrt_llm/executor/base_worker.py
+++ tensorrt_llm/executor/base_worker.py
@@ -520,6 +520,8 @@ class BaseWorker(GenerationExecutor):
             executor_request.py_logprobs_mode = request.sampling_params.logprobs_mode
             executor_request.py_logprobs_simple_format = (
                 request.sampling_params.logprobs_simple_format)
+            executor_request.py_return_routed_experts = (
+                request.sampling_params.return_routed_experts)
 
             # here we add executor_request.py_disaggregated_params= request.disaggregated_params for python cache transceiver
             if self._is_pytorch_backend and request.disaggregated_params is not None:
